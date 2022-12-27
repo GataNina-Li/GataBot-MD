403a015705b76de6a867360f76e29b1dbac024be
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-12-27 13:56:36 -0300

    Update grupo-setdesc.js

--- plugins/grupo-setdesc.js
+++ plugins/grupo-setdesc.js
@@ -1,23 +1,10 @@
-/*let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, args }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
 await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
 conn.sendButton(m.chat, wm, lenguajeGB.smsDest(), pp, [[lenguajeGB.smsConMenu(), `/menu`]], fkontak, m)
 }
 handler.command = /^setdesk|setdesc|newdesc|descripción|descripcion$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-export default handler
-
-*/
-let handler = async (m, { conn, args }) => {
-await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
-m.reply('*✅ La descripción del grupo se modifico correctamente*')
-}
-handler.help = ['Setdesc <text>']
-handler.tags = ['group']
-handler.command = /^setdesk|setdesc$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
