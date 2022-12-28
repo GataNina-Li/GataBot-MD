8da1b33c29534b1410e34f52f9389f22b9d19670
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-28 19:27:51 -0400

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
   if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw '*YA ES ADMIN DEL GRUPO MI CREADOR/A*'
+  if (isAdmin) throw '*YA ES ADMIN DEL GRUPO MI Propietario(a) *'
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
 handler.command = /^admin.|atad|autoadmin$/i
