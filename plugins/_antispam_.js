bded2d9aab6773c6cd47633a5ccc459e4000c6bf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-12-28 05:16:27 -0500

    Update _antispam_.js

--- plugins/_antispam_.js
+++ plugins/_antispam_.js
@@ -8,7 +8,7 @@ return
 if (!user.antispam)
 return !0
 if (+new Date() > user.antispam) {
-let tiempo = 320000 * 5
+let tiempo = 6000 * 1
 setTimeout(() => {
 //this.updateBlockStatus(m.chat, 'unblock') No me gustó jjjjj
 user.banned = false
