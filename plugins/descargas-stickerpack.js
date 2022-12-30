d39c4225b64e621ebf266b782ac50466326216de
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-12-30 14:52:24 -0300

    Add files via upload

--- plugins/descargas-stickerpack.js
+++ plugins/descargas-stickerpack.js
@@ -10,10 +10,10 @@ let json = await res.json()
 for (let data of (json.result || json)) {
 const stikers = await sticker(false, data, global.packname, global.author)
 conn.sendFile(m.chat, stikers, null, { asSticker: true }, m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': true }}, { quoted: m })
-//await delay(1500)
+await delay(5000)
 }} catch {   
 await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*')  
 }}
 handler.command = /^stickerpack$/i
 export default handler
-//const delay = time => new Promise(res => setTimeout(res, time))
+const delay = time => new Promise(res => setTimeout(res, time))
