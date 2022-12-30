43c1cb9b12b027d22ffecdbe16b16320b935b360
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-12-30 12:15:59 -0300

    Update descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -1,34 +1,19 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] INGRESE EL NOMBRE DE ALGUNA CANCION A BUSCAR*`
 try {
-let res = await fetch(global.API('zeks', '/api/spotify', { q: text }, 'apikey'))
-if (!res.ok) throw await res.text()
+let res = await fetch(`https://api.lolhuman.xyz/api/spotifysearch?apikey=${lolkeysapi}&query=${text}`)
 let json = await res.json()
-if(!json.data[0]) throw json
-let { title, artists, album, thumb, url, preview_mp3 } = json.data[0]
-let spotifyi = `❒═════❬ 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 ❭═════╾❒
-┬
-├‣✨ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
-┴
-┬
-├‣🗣️ *𝙰𝚁𝚃𝙸𝚂𝚃𝙰:* ${artists}
-┴
-┬
-├‣🎆 *𝙰𝙻𝙱𝚄𝙼:* ${album}
-┴
-┬
-├‣🌐 *𝚄𝚁𝙻*: ${url}
-┴
-┬
-├‣💚 *𝚄𝚁𝙻 𝙳𝙸𝚁𝙴𝙲𝚃𝙾:* ${preview_mp3}\n┴\n\n*_- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 𝚖𝚞𝚜𝚒𝚌𝚊 𝚍𝚎 𝚙𝚛𝚎𝚟𝚒𝚜𝚞𝚊𝚕𝚒𝚣𝚊𝚌𝚒𝚘𝚗_*\n\n${wm}`
-
-conn.sendFile(m.chat, thumb, '', spotifyi, m)
-conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyi, m)
-} catch (e) {
+let { link } = json.result[0]
+let res2 = await fetch(`https://api.lolhuman.xyz/api/spotify?apikey=${lolkeysapi}&url=${link}`)
+let json2 = await res2.json()
+let { thumbnail, title, artists } = json2.result
+let spotifyi = `❒═════❬ 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 ❭═════╾❒\n┬\n├‣✨ *TITULO:* ${title}\n┴\n┬\n├‣🗣️ *ARTISTA:* ${artists}\n┴\n┬\n├‣🌐 *URL*: ${link}\n┴\n┬\n├‣💚 *URL DE DESCARGA:* ${json2.result.link}\n┴`
+conn.sendFile(m.chat, thumbnail, 'error.jpg', spotifyi, m)
+let aa = await conn.sendMessage(m.chat, { audio: { url: json2.result.link }, fileName: `error.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  
+if (!aa) return conn.sendFile(m.chat, json2.result.link, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' }) 
+} catch {
 throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙻𝙾𝙶𝚁𝙾 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙿𝙰𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙴𝚂𝚃𝙰 𝙲𝙰𝙸𝙳𝙰, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*'
 }}
 handler.command = /^(spotify|music)$/i
-handler.help = ['spotify']
-handler.tags = ['general']
 export default handler
