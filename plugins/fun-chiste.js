0050ef6b2204258db9cf48b600c36234bc4c9407
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-02 11:06:08 -0300

    Add files via upload

--- plugins/fun-chiste.js
+++ plugins/fun-chiste.js
@@ -2,7 +2,7 @@
 
 let handler = async (m, { conn, text}) => {
 
-m.reply(`╭┄〔 *${wm}* 〕┄⊱\n┊\nდ *😹 "${pickRandom(global.chiste)}" 😹*\n┊\n*╰━━━⊰ 𓃠 ${vs} ⊱━━━━დ*`)
+m.reply(`╭┄〔 *${wm}* 〕┄⊱\n┊\n *😹 ${pickRandom(global.chiste)} 😹*\n┊\n*╰━━━⊰ 𓃠 ${vs} ⊱━━━━დ*`)
 }
 handler.tags = ['humor']
 handler.command = ['chiste']
