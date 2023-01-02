6073d5db51979ee167481bf24295ae2ec51e3a8d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-02 08:02:17 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -125,8 +125,8 @@ global.gatadiosig = ig
 global.md = 'https://github.com/GataNina-Li/GataBot-MD'
 global.gatabot = md
 
-global.nna = 'https://chat.whatsapp.com/HBiHQBwqXhZIvQ3i79Fjr4' //UPDATE GATABOT
-global.nna2 = 'HBiHQBwqXhZIvQ3i79Fjr4'
+global.nna = 'https://chat.whatsapp.com/KS53R4vQuSE6unEifyFUho' //UPDATE GATABOT
+global.nna2 = 'KS53R4vQuSE6unEifyFUho'
 
 global.nn = 'https://chat.whatsapp.com/EAvTFU2ZUIM6qDnfwoJvCt' //Grupo 1
 
