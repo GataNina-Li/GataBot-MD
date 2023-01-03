b3ec2fd562d7f9c52e55d65288253b8eff942968
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-03 10:09:08 -0300

    Add files via upload

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -2,7 +2,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
 if (!m.message) return !0
-if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('estado') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0
+if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') | m.text.includes('estado') || m.text.includes('bots') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
