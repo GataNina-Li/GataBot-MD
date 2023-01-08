ccf457d7857718e5a4dcfed5f15b3047a5e2831f
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-07 23:54:23 -0400

    Create info-mc-server.js

---
+++ plugins/info-mc-server.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn }) => {
+m.reply(global.mc)
+}
+handler.command = /^(MC-SERVER|MC-SERVER|mc-server)$/i
+
+export default handler
+
+global.mc =
+
+`┏━━━━━━━━━━━━━┓
+┃ *<MINECRAFT SERVER/>*
+║≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋
+┣ • *IP: eu1.soluruse.com*
+┣ • *PUERTO: 10000*
+┣ • *BEDROCK Y JAVA*
+┗━━━━━━━━━━━━━┛`
