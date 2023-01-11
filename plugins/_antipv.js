e0c31dfbb906cc3c7a469d0de717f604d013c93d
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-11 19:35:26 -0300

    Add files via upload

--- plugins/_antipv.js
+++ plugins/_antipv.js
@@ -2,7 +2,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
 let user = global.db.data.users[m.sender]
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
-if (!m.message) return !0
+if (opts['gconly'] && !m.chat.endsWith('g.us'))
 if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') | m.text.includes('estado') || m.text.includes('bots') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[this.user.jid] || {}
