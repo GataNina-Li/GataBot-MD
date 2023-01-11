ae2d9791ba4cb9216c49b334e1b962f70a04ad84
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-11 18:40:49 -0500

    Update info-grupos-lista.js

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, isBotAdmin }) => {
 let txt = ''
 let group = m.chat
 for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('https://chat.whatsapp.com/') + conn.groupInviteCode(jid) && chat.isChats)) 
-txt += `🐈 ${await conn.getName(jid)}\n
-✦ ${await conn.getName(jid)}\n 
-${jid.split`@`[0].length >= 15 ? `*Creador no encontrado*` : `*Wa.me/${jid.split`@`[0]}*`}\n
+txt += `🐈 ${await conn.getName(jid)}
+✦ ${await conn.getName(jid)}
+${jid.split`@`[0].length >= 15 ? `*Creador no encontrado*` : `*Wa.me/${jid.split`@`[0]}*`}
 ${chat?.metadata?.read_only ? '❌ *SIN ESTAR AQUÍ | NO*' : '✅ *SIGO AQUÍ | YES*'}\n\n`
 
 m.reply(`*${gt} ESTÁ EN ESTOS GRUPOS*
