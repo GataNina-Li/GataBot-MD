78ba06df31844ee7fc3a961d8e5a35918bfd40f0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-11 18:30:36 -0500

    Update info-grupos-lista.js

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -2,12 +2,12 @@ import PhoneNumber from 'awesome-phonenumber'
 let handler = async (m, { conn, isBotAdmin }) => { 
 let txt = ''
 let group = m.chat
-for (let [jid, chat, creators] of Object.entries(conn.chats).filter(([jid, chat, creators]) => jid.endsWith('https://chat.whatsapp.com/') + conn.groupInviteCode(jid) && jid.endsWith('https://chat.whatsapp.com/') + conn.groupInviteCode(jid) && chat.isChats)) 
+for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@s.whatsapp.net') + conn.groupInviteCode(jid) && chat.isChats)) 
 txt += `\n
 🐈 ${await conn.getName(jid)}
 ✦ ${await conn.getName(jid)}\n
 *CREADOR(A):* ` + `${PhoneNumber ? `${PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')}` : '*Creador no encontrado*'}` == undefined ? '' : '*Creador no encontrado*' + '\n' +
-`${jid.split`@`[0].length  > 17 ? `*Creador no encontrado*` : `*Wa.me/${jid.split`@`[0]}*`}\n` +
+`${jid.split`@`[0].length >= 15 ? `*Creador no encontrado*` : `*Wa.me/${jid.split`@`[0]}*`}\n` +
 `${chat?.metadata?.read_only ? '❌ *SIN ESTAR AQUÍ | NO*' : '✅ *SIGO AQUÍ | YES*'}\n\n`
 
 m.reply(`*${gt} ESTÁ EN ESTOS GRUPOS*
