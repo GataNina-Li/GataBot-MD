ae01d08fb7b97319d62811c4511d9b69c2fd39a9
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-11 20:32:04 -0300

    Delete owner-gups.js

--- plugins/owner-gups.js
+++
@@ -1,5 +0,0 @@
-let handler = async(m, { conn, text }) => {
- var url= await conn.groupInviteCode(`${text}`) 
-m.reply("https://chat.whatsapp.com/" + url)}
-handler.command = ['prueba1']
-export default handler
