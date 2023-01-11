cfa1832805fa55fc4ad5139d2421ec09f25fbf2c
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-11 18:39:47 -0300

    Add files via upload

---
+++ plugins/a-prueba.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn }) => { 
+let txt = ''
+let group = m.chat
+conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
+for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n🐈 ${await conn.getName(jid)}\n✦ ${jid} \n${chat?.metadata?.read_only ? '❌ *SIN ESTAR AQUÍ | NO*' : '✅ *SIGO AQUÍ | YES*'}\n\n`
+m.reply(`*${gt} ESTÁ EN ESTOS GRUPOS*
+*IS IN THESE GROUPS:*`.trim())
+
+conn.sendHydrated(m.chat, txt, wm, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '.menu'],
+['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '/cuentasgb']
+], m,)
+}
+handler.help = ['groups', 'grouplist']
+handler.tags = ['info']
+handler.command = /^(prueba)$/i
+handler.exp = 30
+export default handler
