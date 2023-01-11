15aeb7a3e42aa7b7c8109834f3c0d8bae93692b1
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-11 19:08:25 -0300

    Add files via upload

--- plugins/a-prueba.js
+++ plugins/a-prueba.js
@@ -1,18 +1,21 @@
-let handler = async (m, { conn, isBotAdmin }) => { 
+let handler = async (m, { conn }) => { 
 let txt = ''
-for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('https://chat.whatsapp.com/') && chat.isChats)) txt += `\n🐈 ${await conn.getName(jid)}\n✦` + `${isBotAdmin ? `${jid}` : 'No permitido'}` + `\n${chat?.metadata?.read_only ? '❌ *SIN ESTAR AQUÍ | NO*' : '✅ *SIGO AQUÍ | YES*'}\n\n`
+let group = m.chat
+for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('https://chat.whatsapp.com/') &&  + await conn.groupInviteCode(group)) txt += `\n🐈 ${await await conn.groupInviteCode(group)}\n✦ ${jid} \n${chat?.metadata?.read_only ? '❌ *SIN ESTAR AQUÍ | NO*' : '✅ *SIGO AQUÍ | YES*'}\n\n`
 m.reply(`*${gt} ESTÁ EN ESTOS GRUPOS*
-*IS IN THESE GROUPS:*
-
-txt`.trim())
-
-//conn.sendHydrated(m.chat, txt, wm, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-//['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '.menu'],
-//['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '/cuentasgb']
-//], m,)
+*IS IN THESE GROUPS:*`.trim())
+/*
+conn.sendHydrated(m.chat, txt, wm, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '.menu'],
+['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '/cuentasgb']
+], m,)
+*/
+}
+var url= await conn.groupInviteCode('@g.us')
+m.reply("https://chat.whatsapp.com/" + url)
 }
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
-handler.command = /^(prueba9)$/i
+handler.command = /^(prueba)$/i
 handler.exp = 30
 export default handler
