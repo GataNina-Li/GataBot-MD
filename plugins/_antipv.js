55bcfff95b88d1764ef38f7f962982b1f3f88902
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-11 23:28:28 -0500

    Update _antipv.js

--- plugins/_antipv.js
+++ plugins/_antipv.js
@@ -1,4 +1,4 @@
-/*export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
 if (!m.message) return !0
@@ -13,4 +13,4 @@ user.banned = true
 if (!m.isGroup) return !1
 user.banned = false  
 return !1
-}*/
+}
