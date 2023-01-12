924b98c159160b89488b13d5c04ba77a1a83c1e2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-11 19:20:31 -0500

    Update info-grupos-lista.js

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -1,5 +1,5 @@
 import PhoneNumber from 'awesome-phonenumber'
-let handler = async (m, { conn, isBotAdmin, usedPrefix, groupMetadata  }) => { 
+let handler = async (m, { conn, isBotAdmin, usedPrefix, groupMetadata, participants }) => { 
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
 
