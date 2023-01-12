9e50e445d65f0e2aff6971375641f1b47cf98b74
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-12 11:10:17 -0300

    Update _antipv.js

--- plugins/_antipv.js
+++ plugins/_antipv.js
@@ -8,5 +8,11 @@ let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antipv && !isOwner && !isROwner) {
 await m.reply(`*[❗] 𝙃𝙊𝙇𝘼 @${m.sender.split`@`[0]}*, 𝙀𝙎𝙏𝘼 𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊 𝙃𝘼𝘽𝙇𝘼 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊 𝘿𝙀𝙇 𝘽𝙊𝙏 𝙎𝙀𝙍𝘼𝙎 𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝘿𝙊\n𝙃𝘼𝘽𝙇𝘼 𝘾𝙊𝙉 𝙈𝙄 𝘾𝙍𝙀𝘼𝘿𝙊𝙍𝘼 𝙎𝙄 𝙋𝙊𝙍 𝙐𝙉 𝙀𝙍𝙍𝙊𝙍 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙎𝙏𝙀 𝘼𝙇 𝘽𝙊𝙏 𝙔 𝙌𝙐𝙄𝙀𝙍𝙀 𝙌𝙐𝙀 𝙏𝙀 𝘿𝙀𝙎𝘽𝙇𝙊𝙌𝙐𝙀𝙀.\n 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼 𝙋𝘼𝙍𝘼 𝙐𝙎𝙐𝘼𝙍𝙄𝙊: https://instagram.com/gata_dios\n\n*Unete al Grupo para usar GataBot ${nn}*`, false, { mentions: [m.sender] })
 user.banned = true
+if (!m.message) return !0
+return !0
+}
+if (m.isGroup) return !1
+chat.isBanned = false
+await m.reply(`desbaneado`, m) 
 return !1
-}}
+}
