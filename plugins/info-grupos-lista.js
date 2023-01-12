2c9017142a3958d9f3461859703dddea40a3ae6f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-11 19:43:46 -0500

    Update info-grupos-lista.js

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -6,6 +6,7 @@ const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "stat
 let txt = ''
 let group = m.chat
 for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('https://chat.whatsapp.com/') + conn.groupInviteCode(jid) && chat.isChats)) 
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
 const groupsIn = chats.filter(([jid]) => jid.endsWith('@g.us'))
 
 let Link = conn.groupInviteCode(jid) 
