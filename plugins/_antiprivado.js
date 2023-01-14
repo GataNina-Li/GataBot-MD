3ba4d90dbd416928b0311b6d513c63deae740930
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-14 03:31:51 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -14,7 +14,7 @@ let bot = global.db.data.settings[this.user.jid] || {}
 let user = global.db.data.users[m.sender]
 
 if (user.registered === true) return !0 
-if (!m.isGroup && bot.antiPrivate && !isOwner && !isROwner) {
+if (m.isGroup && bot.antiPrivate && !isOwner && !isROwner) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 let text1 = `✅ *BIENVENIDO(A) @${m.sender.split`@`[0]} : CUENTA OFICIAL ${gt}*
