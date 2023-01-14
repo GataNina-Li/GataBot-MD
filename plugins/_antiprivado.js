5e5123966bef0cf73fd7164fc5eab37a3fc7307b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-14 03:25:39 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,6 +1,6 @@
 //export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, usedPrefix }) {
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
 //if ( owner[0][0] == 593993684821 ) {
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
