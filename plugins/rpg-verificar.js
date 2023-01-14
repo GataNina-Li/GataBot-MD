8ee0f4c973f4e2f3ffee0d333de3a2f6e855c6bd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-14 03:06:11 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -11,22 +11,16 @@ user.age = age
 user.regTime = + new Date
 user.registered = true
 let sn = createHash('md5').update(m.sender).digest('hex')
-global.db.data.users[m.sender].money += 1000
-global.db.data.users[m.sender].limit += 15
-global.db.data.users[m.sender].exp += 1500
-global.db.data.users[m.sender].joincount += 5
+//global.db.data.users[m.sender].money += 1000
+//global.db.data.users[m.sender].limit += 15
+//global.db.data.users[m.sender].exp += 1500
+//global.db.data.users[m.sender].joincount += 5
 let caption = `╭━━━[ *𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 | 𝙑𝙀𝙍𝙄𝙁𝙔* ]━━━━⬣
 ┃ *NOMBRE* 
 ┃ ${name} ${user.registered === true ? 'ͧͧͧͦꙶͣͤ✓ᚲᴳᴮ' : ''}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃ *EDAD* 
 ┃ *${age} años*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *BONO* 
-┃ *$1500 XP*
-┃ *$1000 GATACOINS*
-┃ *$15 DIAMANTES*
-┃ *$5 TOKENS*
 ╰━━━━━━━━━━━━━━━━━━⬣`
 //let author = global.author
 await conn.sendButton(m.chat, caption, `𝙏𝙐 𝙉𝙐𝙈𝙀𝙍𝙊 𝘿𝙀 𝙎𝙀𝙍𝙄𝙀 𝙏𝙀 𝙎𝙀𝙍𝙑𝙄𝙍𝘼 𝙀𝙉 𝘾𝘼𝙎𝙊 𝙌𝙐𝙀 𝙌𝙐𝙄𝙀𝙍𝘼 𝘽𝙊𝙍𝙍𝘼𝙍 𝙎𝙐 𝙍𝙀𝙂𝙄𝙎𝙏𝙍𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n${usedPrefix}unreg numero de serie\n${wm}`, [['𝘼𝙝𝙤𝙧𝙖 𝙚𝙨𝙩𝙤𝙮 𝙑𝙚𝙧𝙞𝙛𝙞𝙘𝙖𝙙𝙤(𝙖)!! ✅', '/profile']], m)
