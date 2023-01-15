2c17a423e2ed637cf74645bfabf206d88f7be39e
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-15 08:15:27 -0300

    Delete herramientas-destruirbot.js

--- plugins/herramientas-destruirbot.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { conn }) => {
-m.reply(global.destruir)
-}
-handler.command = /^(destruirbot|autodestruirbot)$/i
-export default handler
-
-global.destruir =  `mi creadora me trata mal`
