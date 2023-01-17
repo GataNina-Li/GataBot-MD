2ec644c4fed7f5f2d5fd9603f72d3a5da335d9f9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-17 17:48:32 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/comandos+18-cosplay2.js
+++ plugins/comandos+18-cosplay2.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, command, usedPrefix }) => {
-let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCont18Porn(), sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(img16)).buffer() }}}
+let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCont18Porn2(), sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(img16)).buffer() }}}
 let user = global.db.data.users[m.sender]
 let yh = global.cosplay
 let url = yh[Math.floor(Math.random() * yh.length)]
