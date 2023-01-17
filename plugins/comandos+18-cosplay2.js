d2d8ca63bca9671577290b9c71a8e82b0307f70d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-17 16:58:58 -0500

    Update comandos+18-cosplay2.js

--- plugins/comandos+18-cosplay2.js
+++ plugins/comandos+18-cosplay2.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 let user = global.db.data.users[m.sender]
 let yh = global.cosplay
 let url = yh[Math.floor(Math.random() * yh.length)]
-await conn.sendButton(m.chat, lenguajeGB.smsCont18(), wm + `| *_${lenguajeGB['smsBotonM7']()}_* » ${user.premiumTime > 0 ? '✅' : '❌'}`, url, [[lenguajeGB.smsSig(), `${usedPrefix + command}`]], m)
+await conn.sendButton(m.chat, lenguajeGB.smsCont18Porn(), wm + `| *_${lenguajeGB['smsBotonM7']()}_* » ${user.premiumTime > 0 ? '✅' : '❌'}`, url, [[lenguajeGB.smsSig(), `${usedPrefix + command}`]], m)
 }
 handler.command = /^(cosplay2)$/i
 export default handler
