58310a8817543e79802e292304c4db0a2f831a12
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-19 16:22:54 -0300

    Update propietario(a)-broadcast_totales.js

--- plugins/propietario(a)-broadcast_totales.js
+++ plugins/propietario(a)-broadcast_totales.js
@@ -5,7 +5,7 @@ Creado por https://github.com/DIEGO-OFC | https://github.com/GataNina-Li
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
 let groups = Object.keys(await conn.groupFetchAllParticipating())
-m.reply(`_mengirim pesan siaran ke ${groups.length}`)
+m.reply(`✅ *El mensaje fue enviado a ${groups.length}Chats Totales*\n*Es posible que no se haya enviado a todos los Chats Totales. Disculpe.*\n\n✅ *The message was sent to ${groups.length} Totals Chats*\n*May not have been sent to all Totals Chats. Excuse me.*`)
 for (let id of groups) {
 let bg = img13
         
