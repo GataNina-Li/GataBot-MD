aa4187bd035fb7db0e138e9807a279e0a02f2769
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-19 17:24:54 -0300

    Update propietario(a)-bcgc.js

--- plugins/propietario(a)-bcgc.js
+++ plugins/propietario(a)-bcgc.js
@@ -1,26 +1,26 @@
 // by 
-import fetch from 'node-fetch'
+import { randomBytes } from 'crypto'
+
 let handler = async (m, { conn, text }) => {
-let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
-m.reply(`✅ *El mensaje fue enviado a ${groups.length}Chats Totales*\n*Es posible que no se haya enviado a todos los Chats Totales. Disculpe.*\n\n✅ *The message was sent to ${groups.length} Totals Chats*\n*May not have been sent to all Totals Chats. Excuse me.*`)
-for (let id of groups) { 
-let bg = img13
-        
-conn.sendButton(id, `*╭━━[ 𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 | 𝙉𝙊𝙏𝙄𝘾𝙀 ]━━━⬣*\n*┃*\n*┃💌* ${text}\n*┃*\n*╰━━━━━━━━━━━━━━━━━━⬣*`, '✅ *𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇*\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🎁 𝙄𝙣𝙛𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡', '.cuentasgb'],['🐈 𝙈𝙚𝙣𝙪', '.menu']], false, { 
-contextInfo: { externalAdReply: {
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
-body: 'Super Bot WhatsApp', 
-sourceUrl: `https://www.instagram.com/gata_dios`, 
-thumbnail: fs.readFileSync('./media/menus/Menu3.jpg') }}})
+  let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
+  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? `${htki} *BROADCAST* ${htka}\n` + teks : `${htki} *BROADCAST* ${htka}\n` + teks + '\n' + readMore + '\n\n' + botdate), true).catch(_ => _)
+  m.reply('Selesai Broadcast All Chat :)')
 }
-m.reply('*✅*')
-}
-handler.help = ['bcloc'].map(v => v + ' <teks>')
+handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(prueba2)$/i
+handler.command = /^(broadcast|bc)$/i
+
 handler.owner = true
 
 export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
 
 
 /*
