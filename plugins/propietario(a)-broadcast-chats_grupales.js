bd98c3f3aebd2fc27f1d1ff8c34844826f94eaf9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-19 14:59:37 -0500

    Create propietario(a)-broadcast-chats_grupales.js

---
+++ plugins/propietario(a)-broadcast-chats_grupales.js
@@ -0,0 +1,16 @@
+/*
+Creado por https://github.com/DIEGO-OFC | https://github.com/GataNina-Li
+
+*/
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text }) => {
+let groups = Object.keys(await conn.groupFetchAllParticipating())
+m.reply(`✅ *El mensaje fue enviado a ${groups.length}Chats Totales*\n\n✅ *The message was sent to ${groups.length} Totals Chats*`)
+for (let id of groups) {      
+await conn.sendButtonLoc(id, img13, text, wm, "Menu", ".menu", m)
+}
+m.reply('*✅*')
+}
+handler.command = /^(pruebaloc)$/i
+handler.owner = true
+export default handler
