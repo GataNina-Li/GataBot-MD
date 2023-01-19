f3e3c329f90a85580b602eeb9ceb555dd9138ded
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-19 12:45:55 -0500

    Update propietario(a)-broadcast_totales.js

--- plugins/propietario(a)-broadcast_totales.js
+++ plugins/propietario(a)-broadcast_totales.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
     let groups = Object.keys(await conn.groupFetchAllParticipating())
     m.reply(`_mengirim pesan siaran ke ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`)
     for (let id of groups) {
-        let bg = 'https://telegra.ph/file/42ca1bf46a366d929e7ce.jpg'
+        let bg = img13
         await conn.delay(1500)
         await conn.sendButtonLoc(id, await (await fetch(bg)).buffer(), text, wm, 'broadcast', 'broadcast')
     }
