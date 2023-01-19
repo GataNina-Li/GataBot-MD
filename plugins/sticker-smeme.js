4d0a962f8d5594eeb28d64961bc6da1ccd62c860
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-19 11:17:57 -0300

    Add files via upload

---
+++ plugins/sticker-smeme.js
@@ -0,0 +1,21 @@
+import uploadImage from '../lib/uploadImage.js'
+import { sticker } from '../lib/sticker.js'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let [atas, bawah] = text.split`|`
+    let q = m.quoted ? m.quoted : m
+    let mime = m.quoted.mimetype || ''
+if (!/webp/.test(mime)) throw 'Responde a una imagen\nejemplo: .smeme bot|uwu`
+    m.reply(global.wait)
+    let img = await q.download()
+    let url = await uploadImage(img)
+    let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
+    stiker = await addExif(false, meme, global.packname, global.author)
+    if (stiker) await conn.sendFile(m.chat, stiker, '', author, m, '', { asSticker: 1 })
+}
+handler.help = ['smeme (teks|teks)']
+handler.tags = ['sticker']
+handler.command = /^(smeme)$/i
+
+handler.limit = true
+
+export default handler
