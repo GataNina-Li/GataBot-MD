e311f276637b2ef71ff493a45067453257e3dcc8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-19 15:43:48 -0500

    Update propietario(a)-broadcast-chats_grupales.js

--- plugins/propietario(a)-broadcast-chats_grupales.js
+++ plugins/propietario(a)-broadcast-chats_grupales.js
@@ -2,15 +2,5 @@
 Creado por https://github.com/DIEGO-OFC | https://github.com/GataNina-Li
 
 */
-import fetch from 'node-fetch'
-let handler = async (m, { conn, text }) => {
-let groups = Object.keys(await conn.groupFetchAllParticipating())
-m.reply(`✅ *El mensaje fue enviado a ${groups.length}Chats Totales*\n\n✅ *The message was sent to ${groups.length} Totals Chats*`)
-for (let id of groups) {      
-await conn.sendButtonLoc(id, img13, text, wm, "Menu", ".menu", m)
-}
-m.reply('*✅*')
-}
-handler.command = /^(broadcast|bc)(group|grup|gc)$/i
-handler.owner = true
-export default handler
+//await conn.sendButtonLoc(id, img13, text, wm, "Menu", ".menu", m)
+const _0x21650e=_0x2b17;(function(_0x20fda5,_0x901a41){const _0x1a71e6=_0x2b17,_0x1aa984=_0x20fda5();while(!![]){try{const _0x5f3ccc=-parseInt(_0x1a71e6(0x1e1))/0x1*(parseInt(_0x1a71e6(0x1e8))/0x2)+-parseInt(_0x1a71e6(0x1f0))/0x3+-parseInt(_0x1a71e6(0x1ec))/0x4+-parseInt(_0x1a71e6(0x1fa))/0x5+parseInt(_0x1a71e6(0x1f7))/0x6*(parseInt(_0x1a71e6(0x1ef))/0x7)+parseInt(_0x1a71e6(0x1f6))/0x8+parseInt(_0x1a71e6(0x1ee))/0x9*(parseInt(_0x1a71e6(0x1e4))/0xa);if(_0x5f3ccc===_0x901a41)break;else _0x1aa984['push'](_0x1aa984['shift']());}catch(_0x455bf8){_0x1aa984['push'](_0x1aa984['shift']());}}}(_0x56d2,0xdc55e));function _0x2b17(_0x2f08be,_0xa40893){const _0x56d2f3=_0x56d2();return _0x2b17=function(_0x2b1754,_0x38a816){_0x2b1754=_0x2b1754-0x1e1;let _0x573787=_0x56d2f3[_0x2b1754];return _0x573787;},_0x2b17(_0x2f08be,_0xa40893);}function _0x56d2(){const _0x2d1efc=['subject','reply','8393765KJynyN','🌟\x20INFO\x20OFICIAL','5qaLRbo','\x20Totals\x20Chats*','smsConMenu','25063770xeeMtc','Chats\x20Totales*\x0a\x0a✅\x20*The\x20message\x20was\x20sent\x20to\x20','✅\x20*El\x20mensaje\x20fue\x20enviado\x20a\x20','command','476434TFmeyp','keys','getRandom','.cuentasgb','7103824JPULtH','owner','9GZoHHK','672iwQMby','366555jOpVeY','parseMention','.menu','*✅*','sendButton','dpdf','12760576yRKxND','98064HpqzwE'];_0x56d2=function(){return _0x2d1efc;};return _0x56d2();}import _0x17113f from'node-fetch';let handler=async(_0xa673c3,{conn:_0x4d0b12,text:_0x34460a,groupMetadata:_0x4f3d90,participants:_0x5ee508})=>{const _0x40d2a1=_0x2b17;let _0x4fa35f=Object[_0x40d2a1(0x1e9)](await _0x4d0b12['groupFetchAllParticipating']());_0xa673c3[_0x40d2a1(0x1f9)](_0x40d2a1(0x1e6)+_0x4fa35f['length']+_0x40d2a1(0x1e5)+_0x4fa35f['length']+_0x40d2a1(0x1e2));for(let _0x28fd4d of _0x4fa35f){await _0x4d0b12[_0x40d2a1(0x1f4)](_0x28fd4d,wm,_0x34460a,img13,[[_0x40d2a1(0x1fb),_0x40d2a1(0x1eb)],[lenguajeGB[_0x40d2a1(0x1e3)](),_0x40d2a1(0x1f2)]],null,{'mentions':this[_0x40d2a1(0x1f1)](_0x34460a),'document':{'url':'https://wa.me/18059196237'},'mimetype':global[_0x40d2a1(0x1f5)],'fileName':'✅\x20𝗖𝗢𝗠𝗨𝗡𝗜𝗖𝗔𝗗𝗢\x20𝗢𝗙𝗜𝗖𝗜𝗔𝗟\x20✅','fileLength':0x38d7ea4c67fff,'pageCount':0x3e7,'contextInfo':{'externalAdReply':{'showAdAttribution':!![],'mediaType':0x2,'mediaUrl':welgata[_0x40d2a1(0x1ea)](),'title':''+_0x4f3d90[_0x40d2a1(0x1f8)],'body':welgata['getRandom'](),'sourceUrl':md,'thumbnail':img13}}});}_0xa673c3[_0x40d2a1(0x1f9)](_0x40d2a1(0x1f3));};handler[_0x21650e(0x1e7)]=/^(broadcast|bc)(group|grup|gc)$/i,handler[_0x21650e(0x1ed)]=!![];export default handler;
