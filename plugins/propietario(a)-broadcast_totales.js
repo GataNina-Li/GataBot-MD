bc147b1f0b0ab2cb0f80845d53b711e040d8ffad
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-19 16:06:06 -0300

    Update propietario(a)-broadcast_totales.js

--- plugins/propietario(a)-broadcast_totales.js
+++ plugins/propietario(a)-broadcast_totales.js
@@ -8,7 +8,7 @@ let groups = Object.keys(await conn.groupFetchAllParticipating())
 m.reply(`_mengirim pesan siaran ke ${groups.length}`)
 for (let id of groups) {
 let bg = img13
-        
+await delay(5 * 5000)
 await conn.sendButtonLoc(id, bg, text, wm, "Okey", "Ok", m)
 }
 m.reply('*✅*')
@@ -19,6 +19,7 @@ handler.command = /^(pruebaloc)$/i
 handler.owner = true
 
 export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 
 
