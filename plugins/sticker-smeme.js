3f19305294d8ad2cdfedee86c4178a33f5833e67
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-19 11:35:20 -0300

    Add files via upload

--- plugins/sticker-smeme.js
+++ plugins/sticker-smeme.js
@@ -3,7 +3,6 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [atas, bawah] = text.split`|`
     let q = m.quoted ? m.quoted : m
-    let mime = m.quoted.mimetype || ''
 if (!m.quoted) throw '*[❗𝙄𝙣𝙛𝙤❗] 𝙍𝙚𝙨𝙥𝙤𝙣𝙙𝙚 𝙖𝙡 𝙨𝙩𝙞𝙘𝙠𝙚𝙧 𝙦𝙪𝙚 𝙙𝙚𝙨𝙚𝙖 𝙖𝙜𝙧𝙚𝙜𝙖 𝙪𝙣 𝙥𝙖𝙦𝙪𝙚𝙩𝙚 𝙮 𝙪𝙣 𝙣𝙤𝙢𝙗𝙧𝙚*'
 let stiker = false
 try {
