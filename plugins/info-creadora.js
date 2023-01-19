5f59a9de3c93ef9ea8cce6359c99126d254e7b26
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-19 20:20:58 -0300

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -1,3 +1,88 @@
+let { MessageType } = (await import('@adiwajshing/baileys')).default
+
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+  let type = (args[0] || '').toLowerCase()
+  let _type = (args[0] || '').toLowerCase()
+
+//------- NOMOR
+  let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
+  let teksnomor = `${htki} *OWNER* ${htka}
+✦ @${nomorown.split`@`[0]} ✦
+------- ${wm} -------
+
+📮 *Nomre:*
+• h`
+
+//------------ BIO
+let ppown = 'https://telegra.ph/file/02a2903c1e25228285740.jpg'
+let teksbio = `${htki} *BIO* ${htka}
+❑ *Nama* :Bara
+
+*Instagram:* ${ig}
+*Github:* ${gt}
+•·––––––––––––––––––––––––––·•
+`
+  let teks = 'hola selecciones aqui'
+const sections = [
+   {
+	title: `${htjava} OWNER –––––––––·•`,
+	rows: [
+	    {title: "📱 • Numero", rowId: ".owner"},
+	{title: "🌎 • Script", rowId: ".sc"},
+	]
+    },{
+	title: `${htjava} apoyos –––––––·•`,
+	rows: [
+	    {title: "💹 • Donar", rowId: ".donar"},
+	{title: "🔖 • Grupos", rowId: ".grupos"}
+	]
+  },
+]
+
+const listMessage = {
+  text: teks,
+  footer: null,
+  title: `${htki} *OWNER* ${htka}`,
+  buttonText: "Click !",
+  sections
+}
+
+  try {
+    if (/(pruebaa)/i.test(command)) {
+      const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
+        switch (type) {
+          case 'nomor':
+          conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
+            break
+            
+          default:
+            return await conn.sendMessage(m.chat, listMessage, m, { contextInfo: { mentionedJid: [m.sender] }})
+        }
+    } else if (/enchant|enchan/i.test(command)) {
+      const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
+      switch (_type) {
+        case 't':
+          break
+        case '':
+          break
+
+        default:
+          return conn.sendButton( m.chat, caption, wm, null, [`⋮☰ Menu`, `.menu`], m)
+      }
+    }
+  } catch (err) {
+    m.reply("Error\n\n\n" + err.stack)
+  }
+}
+
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+handler.command = /^(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
+export default handler 
+
+export default handler
+
+/*
 import fs from 'fs'
 let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
@@ -63,4 +148,4 @@ handler.help = ['owner', 'creator']
 handler.tags = ['info']
 handler.command = /^(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
 export default handler 
-
+*/
