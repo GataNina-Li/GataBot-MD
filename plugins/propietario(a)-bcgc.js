598647af92b3ed4e5e6f5d5887fe277d6b69b48a
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-19 16:57:42 -0300

    Update propietario(a)-bcgc.js

--- plugins/propietario(a)-bcgc.js
+++ plugins/propietario(a)-bcgc.js
@@ -1,3 +1,29 @@
+// by 
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text }) => {
+let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
+m.reply(`✅ *El mensaje fue enviado a ${groups.length}Chats Totales*\n*Es posible que no se haya enviado a todos los Chats Totales. Disculpe.*\n\n✅ *The message was sent to ${groups.length} Totals Chats*\n*May not have been sent to all Totals Chats. Excuse me.*`)
+for (let id of groups) { 
+let bg = img13
+        
+conn.sendButton(id, `*╭━━[ 𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 | 𝙉𝙊𝙏𝙄𝘾𝙀 ]━━━⬣*\n*┃*\n*┃💌* ${text}\n*┃*\n*╰━━━━━━━━━━━━━━━━━━⬣*`, '✅ *𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇*\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🎁 𝙄𝙣𝙛𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡', '.cuentasgb'],['🐈 𝙈𝙚𝙣𝙪', '.menu']], false, { 
+contextInfo: { externalAdReply: {
+title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+body: 'Super Bot WhatsApp', 
+sourceUrl: `https://www.instagram.com/gata_dios`, 
+thumbnail: fs.readFileSync('./media/menus/Menu3.jpg') }}})
+}
+m.reply('*✅*')
+}
+handler.help = ['bcloc'].map(v => v + ' <teks>')
+handler.tags = ['owner']
+handler.command = /^(prueba2)$/i
+handler.owner = true
+
+export default handler
+
+
+/*
 import fs from 'fs'
 let handler = async (m, { conn, text } ) => {  
 let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
@@ -21,3 +47,5 @@ handler.exp = 500
 //handler.owner = true
 export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+*/
