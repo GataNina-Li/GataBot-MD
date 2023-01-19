58a29bc816a06c6ec49fb8471d51afc799813ea3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-19 03:18:36 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url'
 import path, { join } from 'path'
 import { unwatchFile, watchFile } from 'fs'
 import chalk from 'chalk'   
+import fetch from 'node-fetch'
 
 /**
  * @type {import('@adiwajshing/baileys')}  
