5f6b4f2ce6a8cf37d45abb6891b365eff8253341
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-19 13:33:22 -0500

    Update propietario(a)-broadcast_totales.js

--- plugins/propietario(a)-broadcast_totales.js
+++ plugins/propietario(a)-broadcast_totales.js
@@ -1,4 +1,4 @@
-import fetch 'node-fetch'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
     let groups = Object.keys(await conn.groupFetchAllParticipating())
     m.reply(`_mengirim pesan siaran ke ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`)
