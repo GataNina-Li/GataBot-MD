d40124f1cd31f8310aab7c0e8488f3aca41b102e
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-20 12:27:22 -0300

    Add files via upload

---
+++ plugins/a-prueba.js
@@ -0,0 +1,23 @@
+
+import fetch from 'node-fetch'
+let handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} mabar`
+    
+    let json = await fetch(`https://anabotofc.herokuapp.com/api/carigrup?apikey=AnaBot&query=${text}`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*`
+        for (let x of jsons.result) {
+        caption += `
+*Nama* : ${x.title}
+*Link :* ${x.link}
+*Desc :* ${x.desc}
+`}
+        return m.reply(caption)
+        
+}
+handler.help = ['prue <pencarian>']
+handler.tags = ['tools']
+
+handler.command = /^prue)/i
+
+export default handler
