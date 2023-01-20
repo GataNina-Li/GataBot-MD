e9222e770e0cfd7bbce115c4a24f33ba57533498
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-20 13:45:53 -0300

    Update a-prueba.js

--- plugins/a-prueba.js
+++ plugins/a-prueba.js
@@ -1,29 +1,16 @@
 
-import didyoumean from 'didyoumean'
-import similarity from 'similarity'
-//import { plugins } from '../lib/plugins.js'
-
-export async function before(m, { conn, match, usedPrefix, command }) {
+export async function all(m) {
 	
-	     if ((usedPrefix = (match[0] || '')[0])) {
-		let noPrefix = m.text.replace(usedPrefix, '')
-		let args = noPrefix.trim().split` `.slice(1)
-		let text = args.join` `
-		let help = Object.values(plugins).filter(v => v.help && !v.disabled).map(v => v.help).flat(1)
-	       if (help.includes(noPrefix)) return
-		let mean = didyoumean(noPrefix, help)
-		let sim = similarity(noPrefix, mean)
-		let som = sim * 100
-		let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-		let name = await conn.getName(who)
-		let caption = `
-🐱 Hola @${who.split("@")[0]}
-
-Tal vez quisiste decir : 
-
- 🟢 *${usedPrefix + mean}*
- 🟢 *Similitud:* _${parseInt(som)}%_`
-	    if (mean) this.sendButton(m.chat, caption, fgig, null, [['✅ SI', `${usedPrefix + mean}`], ['❎ NO', '.ok']], m, { mentions: [who]})
-	    }
+    // cuando alguien envía un enlace de un grupo al dm del bot
+    if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Abre este enlace')) && !m.isBaileys && !m.isGroup) {
+     this.sendButton(m.chat, `*Invitar bot a un grupo* 
+        
+  Hola @${m.sender.split('@')[0]} 
+  puedes alquilar el bot para que se una a un grupo 
+  más info click en el botón
+`.trim(), fgig, null, [['Alquilar', '/buyprem']] , m, { mentions: [m.sender] })
+    m.react('💎')
+  } 
+  
+   return !0
 }
-export const disabled = true
