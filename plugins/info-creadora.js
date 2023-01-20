3aaeac77baadb4854815c04d340b187521281f60
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-20 12:57:22 -0300

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -1,26 +1,5 @@
 import fs from 'fs'
 let handler = async (m, { conn, usedPrefix }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-let name = await conn.getName(who)
-
-  const sendMessage = await conn.sendContact(m.chat, [
-    [`${wm}`, `${await conn.getName(wm+'@s.whatsapp.net')}`, `💌 Developer Bot `, `ɴᴏᴛ ғᴀᴍᴏᴜs ᴊᴜsᴛ ᴀʟᴏɴᴇ ʙᴏʏ`, `yanxiao021@gmail.com`, `🇮🇩 Indonesia`, `📍 htt`, `👤 gata`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `📵 no`, `ɴᴏᴛʜɪɴɢ`, `arg`, `📍 https://github`, `gata`]
-  ], ig)
-  await m.reply(`ʜᴇʟʟᴏ @${m.sender.split(`@`)[0]}`)
-  } 
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
-export default handler 
-
-/*
-
-import fs from 'fs'
-let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
 let text = `
@@ -69,6 +48,10 @@ let buttonMessage= {
 await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+}
+await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 //await conn.sendContact(m.chat, `${owner[0][0]}`, m)//, 
 //await conn.sendContact(m.chat, conn.getName(owner[0][0]+'@s.whatsapp.net', m)
   
@@ -84,5 +67,4 @@ handler.help = ['owner', 'creator']
 handler.tags = ['info']
 handler.command = /^(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
 export default handler 
-*/
 
