ad0d4f135e7e0a68d025ccf945024e4784a3821f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-21 02:49:45 -0500

    Update comandos+18-adult2.js

--- plugins/comandos+18-adult2.js
+++ plugins/comandos+18-adult2.js
@@ -7,8 +7,8 @@ let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCon
 let user = global.db.data.users[m.sender]
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}` 
 
-if (command == global.temaX[0][1]) {
-let res = await fetch(APIs.nekobot + "image?type=" + global.temaX[0][0]) 
+if (command == temaX[0][1]) {
+let res = await fetch(APIs.nekobot + "image?type=" + temaX[0][0]) 
 let json = await res.json()
 let link = json.message
 if (link.slice(-3) == 'gif') {
@@ -18,6 +18,6 @@ await conn.sendFile(m.chat, stickerr, 'sticker.webp', '', m, true, { contextInfo
 await conn.sendButton(m.chat, lenguajeGB.smsCont18PornP(), ` *_${lenguajeGB['smsBotonM7']()}_* » ${user.premiumTime > 0 ? '✅' : '❌'}\n` + wm, link, [[lenguajeGB.smsSigPrem(), `${usedPrefix + command}`]], m, frep)}
 }}  
 
-handler.command = ['hentai']
+handler.command = [temaX[0][1]]
 export default  handler
 
