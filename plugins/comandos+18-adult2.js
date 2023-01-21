08d2320ac4fccdc35fbd5082fa92abd7249166dd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-21 03:38:28 -0500

    `No modificar nada`

--- plugins/comandos+18-adult2.js
+++ plugins/comandos+18-adult2.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
-const temaX = [['hentai','hentai3'], ['ass', 'nsfwass2'], ['pgif', 'porngif'], ['thigh', 'porngirl']]  
+const temaX = [['hentai','hentai3'], ['ass', 'nsfwass2'], ['pgif', 'pornsticker'], ['thigh', 'porngirl']]  
 
 let handler = async (m, {command, conn, usedPrefix}) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
