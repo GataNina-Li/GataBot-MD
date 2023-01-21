61c078c0a5dbff0ebff1bbe3b10f504979232db7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-20 23:50:00 -0500

    Update comandos+18-adult2.js

--- plugins/comandos+18-adult2.js
+++ plugins/comandos+18-adult2.js
@@ -6,8 +6,8 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContA
 if (command == 'prueba5') {
 //let res = (await axios.get("https://nekobot.xyz/api/image?type=hentai")).data  
 let res = await conn.getFile("https://nekobot.xyz/api/image?type=hentai") 
-let haha = await res[Math.floor(res.length * Math.random())]  
-conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `/${command}`]], m)}
+//let haha = await res[Math.floor(res.length * Math.random())]  
+conn.sendButton(m.chat, `_${command}_`.trim(), author, res.getRandom(), [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `/${command}`]], m)}
   
 }  
 handler.command = ['prueba5']
