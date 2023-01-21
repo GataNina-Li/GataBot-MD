83c8f4d8b7c9295c980a7d63cc1a633ffcd3b5d4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-21 02:33:34 -0500

    Update comandos+18-adult2.js

--- plugins/comandos+18-adult2.js
+++ plugins/comandos+18-adult2.js
@@ -1,29 +1,16 @@
 import fetch from 'node-fetch'
 let handler = async (m, {command, conn, usedPrefix}) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
-
-let btn = [{
-                                urlButton: {
-                                    displayText: 'A',
-                                    url: md
-                                }
-                            }, {
-                                quickReplyButton: {
-                                    displayText: 'B',
-                                    id: '.s'
-                                }
-                            }]
   
 if (command == 'prueba5') {
 let res = await fetch(APIs.nekobot + "image?type=" + "hentai") 
 let json = await res.json()
 let link = json.message
-
 if (link.slice(-3) == 'gif') {
-//await m.reply('Error ' + json.message) 
-return conn.sendButtonGif(m.chat, wm, link, { url: link }, btn, img13)
+let stickerr = await sticker(false, link, global.packname, global.author)
+await conn.sendFile(m.chat, stickerr, 'sticker.webp', '', m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `h`, mediaType: 2, sourceUrl: redesMenu.getRandom(), thumbnail: imagen1}}}, { quoted: m })
 }else{
-await conn.sendButton(m.chat, `${json.message}`.trim(), author, link, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `/${command}`]], m)}
+await conn.sendButton(m.chat, `${json.message}`.trim(), wm, link, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `/${command}`]], m)}
 }}  
 handler.command = ['prueba5']
 export default  handler
