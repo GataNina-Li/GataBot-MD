50e0ffe95701c64e2607b4cfce0b09f18edb85cc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-20 23:16:21 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,4 +1,4 @@
-/*export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
 if (!m.message) return !0
@@ -15,4 +15,4 @@ await m.reply(`*@${m.sender.split`@`[0]} NO PUEDE USAR ESTE BOT EN CHAT PRIVADO,
 }
 user.banned = true
 return !1
-}*/
+}
