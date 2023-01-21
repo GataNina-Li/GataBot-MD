1d4dd8b00fbd2346417034aee50e92571a464b4b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-20 22:34:11 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,4 +1,4 @@
-/*let handler = m => m
+let handler = m => m
 handler.all = async function (m) {
 let setting = global.db.data.settings[this.user.jid]
 	
@@ -21,4 +21,4 @@ function clockString(ms) {
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('') 
-} */
+} 
