d335270a98f098e697b960d0bc4629991ab6a3e5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-21 01:34:20 -0500

    Update comandos+18-adult2.js

--- plugins/comandos+18-adult2.js
+++ plugins/comandos+18-adult2.js
@@ -1,6 +1,10 @@
 import fetch from 'node-fetch'
-let handler = async (m, {command, conn}) => {
+let handler = async (m, {command, conn, usedPrefix}) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
+
+let btn = [{ urlButton: { displayText: 'Chat Owner', url: 'https://wa.me/' + nomorown }},
+{ quickReplyButton: { displayText: 'Boton 1', id: usedPrefix + 'menu' }}, 
+{ quickReplyButton: { displayText: 'Boton 2', id: usedPrefix + 'allmenu' }}]
   
 if (command == 'prueba5') {
 let res = await fetch(APIs.nekobot + "image?type=" + "hentai") 
@@ -8,7 +12,8 @@ let json = await res.json()
 let link = json.message
 
 if (link.slice(-3) == 'gif') {
-await m.reply('Error ' + json.message) 
+//await m.reply('Error ' + json.message) 
+await conn.sendButtonGif(m.chat, `${json.message}`.trim(), wm, { url: link }, btn, m)
 }else{
 await conn.sendButton(m.chat, `${json.message}`.trim(), author, link, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `/${command}`]], m)}
 }}  
