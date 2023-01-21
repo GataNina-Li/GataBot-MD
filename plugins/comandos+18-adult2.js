9cb2cb162392e6d9d5300c164923384b3bdff9f8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-20 23:10:15 -0500

    Create comandos+18-adult2.js

---
+++ plugins/comandos+18-adult2.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+import fetch from 'node-fetch'
+let handler = async (m, {command, conn}) => {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
+  
+if (command == 'prueba5') {
+let res = (await axios.get("https://nekobot.xyz/api/image?type=hentai")).data  
+//let haha = await res[Math.floor(res.length * Math.random())]  
+conn.sendButton(m.chat, `_${command}_`.trim(), author, res, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `/${command}`]], m)}
+  
+}  
+handler.command = ['prueba5']
+export default  handler
