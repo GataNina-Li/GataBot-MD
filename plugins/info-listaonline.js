0b9b635e79ba0003ce032735fc96898e6218c587
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-22 12:56:34 -0300

    Update info-listaonline.js

--- plugins/info-listaonline.js
+++ plugins/info-listaonline.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
   let id = args && /\d+\-\d+@g.us/.test(args[0]) ? args[0] : m.chat
-  let online = [...Object.keys(conn.chats.get(id).presences), conn.user.jid]
+  let online = [...Object.keys(conn.reply(id).presences), conn.user.jid]
   conn.reply(m.chat, 'En línea:\n' + online.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, m, {
     contextInfo: { mentionedJid: online }
   })
