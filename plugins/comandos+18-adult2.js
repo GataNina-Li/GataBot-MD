06fe4f8542c6f072e45a5c325576c738c50efa39
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-22 05:47:29 -0500

    Update comandos+18-adult2.js

--- plugins/comandos+18-adult2.js
+++ plugins/comandos+18-adult2.js
@@ -352,8 +352,8 @@ await conn.sendButton(m.chat, lenguajeGB.smsCont18PornP(), `*_${lenguajeGB['smsB
 if (command == temaX[33][0]) {
 let res = await fetch("https://lewd.tritan.dev/api/v1/yaoi") 
 //let json = JSON.parse(JSON.stringify(res.url))
-let myObj = JSON.parse(res.url)
-await m.reply(myObj)}
+let myObj = JSON.parse(res)
+await m.reply(myObj.url)}
 /*if (link.slice(-3) == 'gif') {
 let stickerr = await sticker(false, link, global.packname, global.author)
 await conn.sendFile(m.chat, stickerr, 'sticker.webp', '', m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: lenguajeGB.smsCont18PornP2(), body: wm, mediaType: 2, sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(img16)).buffer()}}}, { quoted: m })
