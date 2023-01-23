a961a3e4cbe52e09fdcafd33253f2306e7aa2539
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-23 10:46:47 -0500

    Update comandos+18-pack.js

--- plugins/comandos+18-pack.js
+++ plugins/comandos+18-pack.js
@@ -3,18 +3,6 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsAvisoAG']()}𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 +18 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙐𝙎𝙀 #𝙤𝙣 𝙢𝙤𝙙𝙤𝙝𝙤𝙧𝙣𝙮 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n+18 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 𝘼𝙍𝙀 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 #𝙤𝙣 𝙢𝙤𝙙𝙤𝙝𝙤𝙧𝙣𝙮 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀*`
 await delay(5000)
 switch (command) {
-case "pack": 
-let url = await pack[Math.floor(Math.random() * pack.length)]
-conn.sendButton(m.chat, `_🥵 Pack 🥵_`, author, url, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `${usedPrefix + command}`]], m)
-break
-case "pack2":  
-let url2 = await packgirl[Math.floor(Math.random() * packgirl.length)]
-conn.sendButton(m.chat, `_🥵 Pack 2 🥵_`, author, url2, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `${usedPrefix + command}`]], m) 
-break
-case "pack3":
-let url3 = await packmen[Math.floor(Math.random() * packmen.length)]
-conn.sendButton(m.chat, `_🥵 Pack 3 🥵_`, author, url3, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `${usedPrefix + command}`]], m) 
-break
 case "videoxxx": case "vídeoxxx":    
 let url4 = await videosxxxc[Math.floor(Math.random() * videosxxxc.length)] 
 await conn.sendButton(m.chat, `*𝘿𝙄𝙎𝙁𝙍𝙐𝙏𝘼 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊 🥵*`, author, url4, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `${usedPrefix + command}`]], m)  
@@ -24,104 +12,13 @@ let url5 = await videosxxxc2[Math.floor(Math.random() * videosxxxc2.length)]
 await conn.sendButton(m.chat, `*𝘿𝙄𝙎𝙁𝙍𝙐𝙏𝘼 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊 🥵*`, author, url5, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🥵', `${usedPrefix + command}`]], m)    
 break    
 }}
-handler.command = /^(pack|pack2|pack3|videoxxx|vídeoxxx|videoxxxlesbi|videolesbixxx|pornolesbivid|pornolesbianavid|pornolesbiv|pornolesbianav|pornolesv)$/i
+handler.command = /^(videoxxx|vídeoxxx|videoxxxlesbi|videolesbixxx|pornolesbivid|pornolesbianavid|pornolesbiv|pornolesbianav|pornolesv)$/i
 export default handler
 handler.level = 2
 handler.money = 90
 const delay = time => new Promise(res => setTimeout(res, time))
 
-global.pack = [
-  "https://i.imgur.com/XbW7FO2.jpg",
-  "https://i.imgur.com/ciuzM98.jpg",
-  "https://i.imgur.com/uHsrrrx.jpg",
-  "https://i.imgur.com/PVi8YDi.jpg",
-  "https://i.imgur.com/FLC3ZXE.jpg",
-  "https://i.imgur.com/54m52tX.jpg",
-  "https://i.imgur.com/OsxYPgQ.jpg",
-  "https://i.imgur.com/vpw6Xnr.jpg",
-  "https://i.imgur.com/aAyPrZx.jpg",
-  "https://i.imgur.com/Gh3ORCd.jpg",
-  "https://i.imgur.com/rjGhygM.jpg",
-  "https://i.imgur.com/zdHVFEI.jpg",
-  "https://i.imgur.com/kAplnSG.jpg",
-  "https://i.imgur.com/15UiO8o.jpg",
-  "https://i.imgur.com/qjjyr6G.jpg",
-  "https://i.imgur.com/bQZRhBU.jpg",
-  "https://i.imgur.com/vpw6Xnr.jpg",
-  "https://i.imgur.com/aAyPrZx.jpg",
-  "https://i.imgur.com/Gh3ORCd.jpg",
-  "https://i.imgur.com/rjGhygM.jpg",
-  "https://i.imgur.com/0MhmmF4.jpg",
-  "https://i.imgur.com/2MX4wvq.jpg",
-  "https://i.imgur.com/HYL5ggu.jpg",
-  "https://i.imgur.com/7ZjOD2a.jpg",
-  "https://i.imgur.com/zbEUy3m.jpg",
-  "https://i.imgur.com/tZ6vlg6.jpg",
-  "https://i.imgur.com/jdPns8O.jpg",
-  "https://i.imgur.com/VyjBQHT.jpg",
-  "https://i.imgur.com/ozAGqBD.jpg",
-  "https://i.imgur.com/DsSj9S1.jpg",
-  "https://i.imgur.com/KYHpjNc.jpg",
-]
-global.packgirl = [
-  "https://i.imgur.com/mwLJaxU.jpg",
-  "https://i.imgur.com/9ptmlPl.jpg",
-  "https://i.imgur.com/38tVliz.jpg",
-  "https://i.imgur.com/2NqCKE3.jpg",
-  "https://i.imgur.com/pveviMG.jpg",
-  "https://i.imgur.com/d71dnkv.jpg",
-  "https://i.imgur.com/cr7Ypj1.jpg",
-  "https://i.imgur.com/jAxzCj4.jpg",
-  "https://i.imgur.com/xokuFLf.jpg",
-  "https://i.imgur.com/Hi4zLaf.jpg",
-  "https://i.imgur.com/OlaQtwW.jpg",
-  "https://i.imgur.com/Dm4GLuF.jpg",
-  "https://i.imgur.com/k6Y2E9b.jpg",
-  "https://i.imgur.com/1rk7jdu.jpg",
-  "https://i.imgur.com/TFmEVPc.jpg",
-  "https://i.imgur.com/0XefLlJ.jpg",
-  "https://i.imgur.com/bwa9LYZ.jpg",
-  "https://i.imgur.com/WgrpTmg.jpg",
-  "https://i.imgur.com/Z5f5YAw.jpg",
-  "https://i.imgur.com/xEuBtPO.jpg",
-  "https://i.imgur.com/NA0fHxn.jpg",
-  "https://i.imgur.com/InueCKQ.jpg",
-  "https://i.imgur.com/3syOcHe.jpg",
-  "https://i.imgur.com/N1dgels.jpg",
-  "https://i.imgur.com/IxKAJaV.jpg",
-  "https://i.imgur.com/8VrxL1d.jpg",
-  "https://i.imgur.com/8B4Y0bG.jpg",
-  "https://i.imgur.com/wgkGOjF.jpg",
-  "https://i.imgur.com/765Wi6q.jpg",
-  "https://i.imgur.com/5joeWnm.jpg",
-  "https://i.imgur.com/71fjmmM.jpg",
-  "https://i.imgur.com/cAuKeyZ.jpg",
-  "https://i.imgur.com/SDZ2Hs5.jpg",
-  "https://i.imgur.com/skkEyqI.jpg",
-  "https://i.imgur.com/6dXFsBW.jpg",
-  "https://i.imgur.com/6CeG9ZX.jpg"
-]
-global.packmen = [
-  "https://i.imgur.com/TK0qLAu.jpg",
-  "https://i.imgur.com/q8lKT40.jpg",
-  "https://i.imgur.com/OwWdL9u.jpg",
-  "https://i.imgur.com/Er7WiQo.jpg",
-  "https://i.imgur.com/u4y0q4P.jpg",
-  "https://i.imgur.com/y8y4PPr.jpg",
-  "https://i.imgur.com/qgfLlRY.jpg",
-  "https://i.imgur.com/irgyUTD.jpg",
-  "https://i.imgur.com/uXrqfBl.jpg",
-  "https://i.imgur.com/lgXjetf.jpg",
-  "https://i.imgur.com/81QLh8s.jpg",
-  "https://i.imgur.com/R3AlYe1.jpg",
-  "https://i.imgur.com/a2Myr3F.jpg",
-  "https://i.imgur.com/Wp9cgGw.jpg",
-  "https://i.imgur.com/ggKUnxt.jpg",
-  "https://i.imgur.com/eCJNWBl.jpg",
-  "https://i.imgur.com/6lcrBQB.jpg",
-  "https://i.imgur.com/eSSbXJ1.jpg",
-  "https://i.imgur.com/tNyvzyO.jpg"
-]
+
 global.videosxxxc = [
 "https://l.top4top.io/m_2235dduf01.mp4",
 "https://a.top4top.io/m_2235268m61.mp4",
