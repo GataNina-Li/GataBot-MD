9605682777ab71a6048f51870f13c0150db902f6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-23 17:43:40 -0500

    Update comandos+18-menu.js

--- plugins/comandos+18-menu.js
+++ plugins/comandos+18-menu.js
@@ -207,9 +207,9 @@ await conn.sendButton(m.chat, menuA, menuB, pp, [
 if (command == 'listhorny') {
 let sections = Object.keys(temaX).map((v, index, temaX2) => ({ title: `${lenguajeGB['smsTex4']()} : ${wm}`,
 rows: [{ 
-title: `${1 + index <= 33 ? '🥵' : user.premiumTime > 0 ? '🥵' : '⚠️'} ${temaX[index][1].toUpperCase()} ${1 + index <= 33 ? '🥵' : user.premiumTime > 0 ? '🥵' : '⚠️'} || ${lenguajeGB['smsBotonM7']()} ➜ ${user.premiumTime > 0 ? '✅' : '❌'} ${`${usedPrefix}` + 1 + index <= 33 ? lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1] : user.premiumTime > 0 ? lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1] : 'pase premium'}`, 
+title: `${1 + index <= 33 ? '🥵' : user.premiumTime > 0 ? '🥵' : '⚠️'} ${temaX[index][1].toUpperCase()} ${1 + index <= 33 ? '🥵' : user.premiumTime > 0 ? '🥵' : '⚠️'} || ${lenguajeGB['smsBotonM7']()} ➜ ${user.premiumTime > 0 ? '✅' : '❌'} ${usedPrefix}${1 + index <= 33 ? lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1] : user.premiumTime > 0 ? lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1] : 'pase premium'}`, 
 description: `${1 + index}. ${lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1]} ➜ ${1 + index <= 33 ? user.money < 90 ? 'No tiene suficiente GataCoins\nPresione aquí para comprar' : 'Contenido Gratis disponible' : 'Contenido no disponible\nCompre premium'}`, 
-rowId: `${usedPrefix}` + 1 + index <= 33 ? lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1] : user.premiumTime > 0 ? lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1] : 'pase premium' }], })) //temaX[index][0] : temaX[index][1]  }], }))
+rowId: `${usedPrefix}${1 + index <= 33 ? lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1] : user.premiumTime > 0 ? lenguajeGB.lenguaje() == 'es' ? temaX[index][0] : temaX[index][1] : 'pase premium'}` }], })) //temaX[index][0] : temaX[index][1]  }], }))
 
 
 let name = await conn.getName(m.sender)
