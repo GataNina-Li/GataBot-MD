c765b6a46a526ffb5ff957154c7468897a01d53d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-22 19:37:47 -0500

    Update comandos+18-adult.js

--- plugins/comandos+18-adult.js
+++ plugins/comandos+18-adult.js
@@ -1,6 +1,5 @@
 import axios from "axios"
 import fetch from 'node-fetch'
-const temaX = [['hentai','hentai3'], ['ass', 'nsfwass2'], ['pgif', 'pornsticker']]
                
 let handler = async (m, {usedPrefix, command, conn}) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
