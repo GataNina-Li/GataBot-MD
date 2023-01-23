944304e304c80316a38d984051bb7f721db50c34
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-23 03:13:16 -0500

    Update comandos+18-adult.js

--- plugins/comandos+18-adult.js
+++ plugins/comandos+18-adult.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import fetch from 'node-fetch'
+import { sticker } from '../lib/sticker.js'
                
 let handler = async (m, {usedPrefix, command, conn}) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
