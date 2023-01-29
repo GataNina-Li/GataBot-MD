d8f566b688e80b7535365a0295b1dce654951cdf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-01-29 18:04:03 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -84,7 +84,7 @@ const listMessage = {
 }
 
   try {
-    if (/(creator|owner)/i.test(command)) {
+    if (/(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)/i.test(command)) {
       const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
         switch (type) {
           case 'nomor':
