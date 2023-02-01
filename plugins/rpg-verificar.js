347c2625f2c44ffc65818ff62fd61f8bc7903995
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-02-01 12:58:24 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -77,7 +77,7 @@ user.age = text.slice(1).trim()
 if (command == 'edad2' || command == 'age2') {
 if (text.slice(1).trim() > usedPrefix + usedPrefix + '50'.slice(1).trim()) throw 'Que viejo (。-`ω´-)'
 if (text.slice(1).trim() < usedPrefix + usedPrefix + '10'.slice(1).trim()) throw '🚼  Basado, los bebes no saber escribir.✍️😳'
-if (verificar.test(text) == false || !text) return conn.sendButton(m.chat, 'REGISTRE SU EDAD COMO ESTE EJEMPLO\n' + usedPrefix 'edad2 ' + usedPrefix + '35', wm, null, [[`USAR CON WA`, usedPrefix + `nombre2 ` + usedPrefix + nombreWA]], m)
+if (verificar.test(text) == false || !text) return conn.sendButton(m.chat, 'REGISTRE SU EDAD COMO ESTE EJEMPLO\n' + usedPrefix + 'edad2 ' + usedPrefix + '35', wm, null, [[`USAR CON WA`, usedPrefix + `nombre2 ` + usedPrefix + nombreWA]], m)
 if (verificar.test(text) == true) return conn.sendButton(m.chat, 'GENIAL SE HA REGISTRADO COMO: ' + user.age + ' años', wm, null, [[`REGISTRAR MI GÉNERO `, usedPrefix + `genero`]], m)
 user.age = text.slice(1).trim()
 }
