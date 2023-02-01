492aaa0da6ffde03b4a1166f47f82460a0af9914
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-02-01 11:50:30 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- handler.js
+++ handler.js
@@ -56,6 +56,7 @@ export async function handler(chatUpdate) {
 		                    	 
 		    if (!('name' in user)) user.name = m.name
                     if (!isNumber(user.age)) user.age = -1
+		    if (!isNumber(user.genero)) user.genero = -1
                     if (!isNumber(user.anggur)) user.anggur = 0
                     if (!isNumber(user.apel)) user.apel = 0
                     if (!isNumber(user.bibitanggur)) user.bibitanggur = 0
