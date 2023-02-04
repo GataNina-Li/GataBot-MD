391400a4de086e715559988e2a39ea53f707ca6d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-02-03 22:42:01 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -9,7 +9,9 @@ global.db.data.users[m.sender].money -= 400
 global.db.data.users[m.sender].limit -= 7
 global.db.data.users[m.sender].exp -= 250
 global.db.data.users[m.sender].joincount -= 3
-  
+
+user.registroC = false
+user.registroR = false
 global.db.data.users[m.sender].name = 0
 global.db.data.users[m.sender].age = 0
 global.db.data.users[m.sender].genero = 0
