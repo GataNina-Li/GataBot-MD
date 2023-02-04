5ec7e6567e0e28d6ecd1f18e54e71e18adcd4240
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-02-04 04:31:28 -0500

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,6 +1,6 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
-let sn = createHash('md5').update(m.sender).digest('hex')
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
 await m.reply(`⬇️ *ESE ES SU NUMERO DE SERIE* ⬇️`.trim())
 await m.reply(`${sn}`.trim())
 }
