247493790802a29d8a69ab207515e5c488d72b71
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-05 17:37:50 -0400

    Update propietario(a)-broadcast-chats_privados.js

--- plugins/propietario(a)-broadcast-chats_privados.js
+++ plugins/propietario(a)-broadcast-chats_privados.js
@@ -1,6 +1,6 @@
 //NO USAR A MENOS QUE ESTE SOLUCIONADO ESTE CÓDIGO 
 //creado por https://github.com/DIEGO-OFC
-/*import { randomBytes } from 'crypto'
+import { randomBytes } from 'crypto'
 let handler = async (m, { conn, text }) => {
   let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
@@ -21,6 +21,6 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
-const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)*/
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
 
   
