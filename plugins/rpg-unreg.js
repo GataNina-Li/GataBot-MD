e7d9149add6f7d9b5957abfa99787f9a2c071697
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-02-07 18:54:57 -0500

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -10,8 +10,8 @@ global.db.data.users[m.sender].limit -= 7
 global.db.data.users[m.sender].exp -= 250
 global.db.data.users[m.sender].joincount -= 3
 
-user.registroC = false
-user.registroR = false
+global.db.data.users[m.sender]['registroC'] = false
+global.db.data.users[m.sender]['registroR'] = false
 global.db.data.users[m.sender].name = 0
 global.db.data.users[m.sender].age = 0
 global.db.data.users[m.sender].genero = 0
