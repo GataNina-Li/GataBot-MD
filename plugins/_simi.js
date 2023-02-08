0a58870356963312552a4ef1807731e22c84d73f
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-08 09:00:31 -0300

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,6 +6,7 @@ let chat = global.db.data.chats[m.chat]
 if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
 if (!m.text) return
+await conn.sendPresenceUpdate('composing', m.chat)
 let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
 let data = await ressimi.json();
 if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return 0
