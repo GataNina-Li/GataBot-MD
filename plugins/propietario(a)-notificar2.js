bec4cd1783fecc50b8f9ff3e1df6d91e69a02829
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-02-08 17:34:28 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/propietario(a)-notificar2.js
+++ plugins/propietario(a)-notificar2.js
@@ -5,6 +5,9 @@ const grupoOficial3 = nnnt
 const grupoOficial4 = nnntt
 const grupoOficial5 = nnnttt
 const grupoOficial6 = nnnttt2
+const grupoOficial7 = nnnttt3
+const grupoOficial8 = nnnttt4
+
 
 let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => {
 let users = m.sender.split`@`[0]
@@ -16,6 +19,8 @@ let [_3, code3] = grupoOficial3.match(linkRegex) || []
 let [_4, code4] = grupoOficial4.match(linkRegex) || []
 let [_5, code5] = grupoOficial5.match(linkRegex) || []
 let [_6, code6] = grupoOficial6.match(linkRegex) || []
+let [_7, code7] = grupoOficial7.match(linkRegex) || []
+let [_8, code8] = grupoOficial8.match(linkRegex) || []
 
 if ( users == 593993684821 || users == 593968585383 ) try {
 if (!text) return m.reply(`*Falta Texto*`) 
@@ -26,13 +31,17 @@ let res3 = await conn.groupAcceptInvite(code3)
 let res4 = await conn.groupAcceptInvite(code4)
 let res5 = await conn.groupAcceptInvite(code5)
 let res6 = await conn.groupAcceptInvite(code6)
+let res7 = await conn.groupAcceptInvite(code7)
+let res8 = await conn.groupAcceptInvite(code8)
 
 await conn.sendMessage(res1, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res1}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await conn.sendMessage(res2, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res2}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await conn.sendMessage(res3, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res3}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await conn.sendMessage(res4, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res4}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await conn.sendMessage(res5, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res5}`)).participants.map(v => v.id) }, { quoted: fkontak })
-await conn.sendMessage(res6, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res5}`)).participants.map(v => v.id) }, { quoted: fkontak })
+await conn.sendMessage(res6, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res6}`)).participants.map(v => v.id) }, { quoted: fkontak })
+await conn.sendMessage(res7, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res7}`)).participants.map(v => v.id) }, { quoted: fkontak })
+await conn.sendMessage(res8, { text: text + '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_', mentions: (await conn.groupMetadata(`${res8}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await m.reply(`✅ *MENSAJE ENVIADO CON ÉXITO EN LOS GRUPOS* `)
 
 } catch (e) {
