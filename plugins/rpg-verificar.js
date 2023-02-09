7173732cab43b78e373e4668869cadfa15c41bb6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-02-09 00:47:54 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -270,7 +270,7 @@ let _cinco = _cuatro + ', ' + user.pas5
 
 
 //if (user.pasatiempo === 0 || null || -1 || undefined && user.pas1 != user.pasatiempo) {
-if (user.pasatiempo == 0) {
+if (user.pas1 === 0) {
 user.pasatiempo = user.pas1
 conn.sendButton(m.chat, '*GENIAL!! SE HA AGREGADO UN PASATIEMPO:*\n*PASATIEMPO(S):* ' + user.pasatiempo , 'Puede agregar hasta 5 pasatiempos consecutivos, o puede avanzar con el registro teniendo mínimo un pasatiempo\n\n' + wm, null, [[`❇️ AGREGAR OTRO PASATIEMPO`, usedPrefix + 'pasatiempo'], [`🐈 FINALIZAR REGISTRO`, usedPrefix + 'finalizar']], m)
 
