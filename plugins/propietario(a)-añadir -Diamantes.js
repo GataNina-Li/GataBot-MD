b5521bd13ad1c3db0ee55ad2577c45fa2c9c7d66
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-13 11:19:48 -0300

    Update propietario(a)-añadir -Diamantes.js

--- plugins/propietario(a)-añadir -Diamantes.js
+++ plugins/propietario(a)-añadir -Diamantes.js
@@ -2,7 +2,7 @@ import MessageType from '@adiwajshing/baileys'
 
 let pajak = 0
 let handler = async (m, { conn, text, usedPrefix, command, groupMetadata }) => {
-let _user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
 
 let who
 if (m.isGroup) who = m.mentionedJid[0]
