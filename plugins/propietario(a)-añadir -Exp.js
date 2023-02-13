db4427f5fb1c899e9deef50931338d49eab50f19
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-12 22:31:02 -0400

    Update propietario(a)-añadir -Exp.js

--- plugins/propietario(a)-añadir -Exp.js
+++ plugins/propietario(a)-añadir -Exp.js
@@ -5,28 +5,23 @@ let handler = async (m, { conn, text, usedPrefix, command, groupMetadata }) => {
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-
-  if (!text)
-    return m.reply(
-      `${ag}𝘿𝙀𝘽𝙀 𝘿𝙀 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙍 𝘼𝙇 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 *@tag*\n\n𝙔𝙊𝙐 𝙈𝙐𝙎𝙏 𝙏𝘼𝙂 𝙏𝙃𝙀 𝙐𝙎𝙀𝙍 *@tag*`
-    );
-
-  let _user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-  if (!_user in global.db.data.users)
-    return m.reply(`El usuɑrio no estά registrɑdo en lɑ bɑse de dɑtos!`);
-  if (m.quoted?.sender) m.mentionedJid.push(m.quoted.sender);
-  if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
-  if (global.db.data.users[_user] == undefined)
-    return m.reply(`El usuɑrio no estά registrɑdo en lɑ bɑse de dɑtos!`);
-   let uuser = global.db.data.users[_user];
-  let txt = text.replace('@' + who.split`@`[0], '').trim()
+if (global.db.data.users[ga] == undefined)
+if (m.quoted?.sender) m.mentionedJid.push(m.quoted.sender);
+if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
+if (ga.startsWith(conn.user.jid.split`@`[0]))
+if (!who) throw `${ag}𝘿𝙀𝘽𝙀 𝘿𝙀 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙍 𝘼𝙇 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 *@tag*\n\n𝙔𝙊𝙐 𝙈𝙐𝙎𝙏 𝙏𝘼𝙂 𝙏𝙃𝙀 𝙐𝙎𝙀𝙍 *@tag*`
+let txt = text.replace('@' + who.split`@`[0], '').trim()
 if (!txt) throw `${ag}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙇𝘼 𝘾𝘼𝙉𝙏𝙄𝘿𝘼𝘿 𝘿𝙀 𝙀𝙓𝙋\n\n𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙉𝙐𝙈𝘽𝙀𝙍 𝙊𝙁 𝙀𝙓𝙋`
-
-  let gata = `╭━[ 𝙀𝙓𝙋𝙀𝙍𝙄𝙀𝙉𝘾𝙄𝘼 | 𝙀𝙓𝙋 ⚡]━⬣\n┃\n┃ღ *PARA | FOR:*\n┃ღ @${_user.split("@s.whatsapp.net")[0]}\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ღ *SE LE AÑADIÓ | NOW YOU HAVE*\n┃ღ *${xp} EXP* ⚡\n┃\n╰━━━━━━━━━━━━━━⬣`
+if (isNaN(txt)) throw `${mg}𝙎𝙄𝙉 𝙎𝙄𝙈𝘽𝙊𝙇𝙊𝙎, 𝙎𝙊𝙇𝙊 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙉𝙐𝙈𝙀𝙍𝙊𝙎\n\n𝙉𝙊 𝙎𝙔𝙈𝘽𝙊𝙇𝙎, 𝙅𝙐𝙎𝙏 𝙀𝙉𝙏𝙀𝙍 𝙉𝙐𝙈𝘽𝙀𝙍𝙎`
 let xp = parseInt(txt)
 let exp = xp
 let pjk = Math.ceil(xp * pajak)
 exp += pjk 
+  let _user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+      let uuser = global.db.data.users[_user];
+  
+  let gata = `╭━[ 𝙀𝙓𝙋𝙀𝙍𝙄𝙀𝙉𝘾𝙄𝘼 | 𝙀𝙓𝙋 ⚡]━⬣\n┃\n┃ღ *PARA | FOR:*\n┃ღ @${_user.split("@s.whatsapp.net")[0]}\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ღ *SE LE AÑADIÓ | NOW YOU HAVE*\n┃ღ *${xp} EXP* ⚡\n┃\n╰━━━━━━━━━━━━━━⬣`
+
 
  conn.sendMessage(
     m.chat,
@@ -78,7 +73,7 @@ else who = m.chat
   if (m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
    
-if (user.startsWith(conn.user.jid.split`@`[0]))
+
 if (!who) throw `${ag}𝘿𝙀𝘽𝙀 𝘿𝙀 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙍 𝘼𝙇 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 *@tag*\n\n𝙔𝙊𝙐 𝙈𝙐𝙎𝙏 𝙏𝘼𝙂 𝙏𝙃𝙀 𝙐𝙎𝙀𝙍 *@tag*`
 let txt = text.replace('@' + who.split`@`[0], '').trim()
 if (!txt) throw `${ag}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙇𝘼 𝘾𝘼𝙉𝙏𝙄𝘿𝘼𝘿 𝘿𝙀 𝙀𝙓𝙋\n\n𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙉𝙐𝙈𝘽𝙀𝙍 𝙊𝙁 𝙀𝙓𝙋`
