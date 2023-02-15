6ad9b9187d8eccd53cdc3420e72fab21b7bb28e7
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-15 08:09:30 -0300

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,18 +1,16 @@
-/*export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+//este antiprivado  todavía está el desarrollo.
+
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
-if (!m.message) return !0
-if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') 
-    ||  m.text.includes('bots') || m.text.includes('serbot') || m.text.includes('jadibot') 
-    || m.text.includes('creadora') || m.text.includes('ping') || m.text.includes('bottemporal') || m.text.includes('gruposgb') 
-    || m.text.includes('instalarbot') || m.text.includes('términos') || m.text.includes('donar')) return !0
+if (!m.message) return !0 
+if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('bots') || m.text.includes('creadora') || m.text.includes('ping') || m.text.includes('bottemporal') || m.text.includes('gruposgb') 
+    || m.text.includes('instalarbot') || m.text.includes('términos') || m.text.includes('deletebot') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0
 let chat = global.db.data.chats[m.chat]
-let bot = global.db.data.settings[this.user.jid] || {}
 let user = global.db.data.users[m.sender]
-if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`*@${m.sender.split`@`[0]} NO PUEDE USAR ESTE BOT EN CHAT PRIVADO, SERÁ BANEADO(A)*`, false, { mentions: [m.sender] })
-//await this.updateBlockStatus(m.chat, 'block')
-}
+let bot = global.db.data.settings[this.user.jid] || {}
+if (bot.antipv && !isOwner && !isROwner) {
+await m.reply(`Hola @${m.sender.split`@`[0]} NO PUEDE USAR ESTE BOT EN CHAT PRIVADO, SERÁ BANEADO(A)\nGrupo para usar al bot ${nn}`, false, { mentions: [m.sender] })
 user.banned = true
 return !1
-}*/ //ESTA DAÑADO ESPEREN O SOLUCIONEN JJJJJ
+}}
