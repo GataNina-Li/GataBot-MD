f3b7de5657f4d2650e230df8b653b88fd16aa882
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-15 22:23:58 -0300

    Add files via upload

--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -1,15 +1,18 @@
 import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-    if (global.conn.user.jid == conn.user.jid) {
-    await m.reply('```𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩 . . .```')
+    if (conn.user.jid == conn.user.jid) {
+    await m.reply('🚀🚀')
+         await m.reply('🚀🚀🚀🚀')
+         await m.reply('🚀🚀🚀🚀🚀🚀')
+         await m.reply('𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩') 
     process.send('reset')
-  } else throw '_eeeeeiiittsssss..._'
+  } else throw 'eh'
 }
 
 handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = /^(res(tart)?)$/i
+handler.command = ['restart','reiniciar'] 
 
 handler.rowner = true
 
