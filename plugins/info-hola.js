03a3c9da4d819227ce76674ae789762ec8ec45c6
Azamiwithlogic <119384234+AzamiWithLogic@users.noreply.github.com>
2023-02-19 00:39:32 -0600

    Create info-hola.js

---
+++ plugins/info-hola.js
@@ -0,0 +1,22 @@
+import fs from "fs"
+let handler = m => m
+
+handler.all = async function (m) {
+let vn = './media/bot.mp3'
+const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
+message: { 
+orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Super Bot WhatsApp', orderTitle: 'Bang', thumbnail: fs.readFileSync('./media/menus/Menu3.jpg'), sellerJid: '0@s.whatsapp.net'    
+}}}
+const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
+message: { 
+"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
+}}}  
+
+if (/^hola$/i.test(m.text)) {
+    
+conn.sendButton(m.chat, `Hola señor/a`, wm, [['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `#menu`]], 'conversation', { sendEphemeral: true, quoted: estilo })
+conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })   
+}
+return !0
+}
+export default handler
