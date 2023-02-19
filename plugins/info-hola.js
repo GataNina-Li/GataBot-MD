3dbf6ccbc1e93c8ff04255392a9071e5dca16457
Azamiwithlogic <119384234+AzamiWithLogic@users.noreply.github.com>
2023-02-19 01:01:33 -0600

    Update info-hola.js

--- plugins/info-hola.js
+++ plugins/info-hola.js
@@ -3,10 +3,11 @@ let handler = m => m
 
 handler.all = async function (m) {
 global.fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }   
+}}}
 
 if (/^hola$/i.test(m.text)) {
     
-conn.sendButton(m.chat, `𝙃𝙊𝙇𝘼 ${taguser}`, wm, [['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `#menu`]], 'conversation', { sendEphemeral: true, quoted: estilo })
+conn.sendButton(m.chat, `𝙃𝙊𝙇𝘼 ${taguser}`, { sendEphemeral: true, quoted: estilo })
 conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })   
 }
 return !0
