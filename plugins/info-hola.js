96e93fd2ed4c41a396490490099b49d997659ea4
Azamiwithlogic <119384234+AzamiWithLogic@users.noreply.github.com>
2023-02-19 11:38:26 -0600

    Update info-hola.js

--- plugins/info-hola.js
+++ plugins/info-hola.js
@@ -5,7 +5,7 @@ handler.all = async function (m) {
 let vn = './media/bot.mp3'
 const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
 message: { 
-orderMessage: { itemCount : 999999, status: 1, surface : 1, message: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', orderTitle: 'Bang', thumbnail: fs.readFileSync('./media/menus/Menu3.jpg'), sellerJid: '0@s.whatsapp.net'    
+orderMessage: { itemCount : 999999, status: 1, surface : 1, message: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐈💖', orderTitle: 'Bang', thumbnail: fs.readFileSync('./media/menus/Menu3.jpg'), sellerJid: '0@s.whatsapp.net'    
 }}}
 
 if (/^hola$/i.test(m.text)) {
