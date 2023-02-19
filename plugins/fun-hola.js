efd735f499bae4765fd224ddad16c36c7afa0b8c
Azamiwithlogic <119384234+AzamiWithLogic@users.noreply.github.com>
2023-02-19 11:55:09 -0600

    Update fun-hola.js

--- plugins/fun-hola.js
+++ plugins/fun-hola.js
@@ -5,12 +5,12 @@ handler.all = async function (m) {
 let vn = './media/bot.mp3'
 const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
 message: { 
-orderMessage: { itemCount : 999999, status: 1, surface : 1, message: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', orderTitle: 'Bang', thumbnail: fs.readFileSync('./media/menus/Menu3.jpg'), sellerJid: '0@s.whatsapp.net'    
+orderMessage: { itemCount : 999999, status: 1, surface : 1, message: '𝗦𝗨𝗣𝗘𝗥 𝗚𝗔𝗧𝗔 𝗕𝗢𝗧', orderTitle: 'Bang', thumbnail: fs.readFileSync('./media/menus/Menu3.jpg'), sellerJid: '0@s.whatsapp.net'    
 }}}
 
 if (/^hola$/i.test(m.text)) {
     
-conn.sendButton(m.chat, `Hola ${taguser}`, wm, [['𝗠 𝗘 𝗡 𝗨 ☘️', `.menu`]], 'conversation', { sendEphemeral: true, quoted: estilo })
+conn.sendButton(m.chat, `Hola Master 😎`, wm, [['𝗠 𝗘 𝗡 𝗨 ☘️', `#menu`]], 'conversation', { sendEphemeral: true, quoted: estilo })
 conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })   
 }
 return !0
