077224adb87804266f46e336059025a9cf11fc5e
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-21 08:16:57 -0300

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -10,7 +10,7 @@ let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`Hola @${m.sender.split`@`[0]} NO PUEDE USAR ESTE BOT EN CHAT PRIVADO\nUnirte al Grupo oficial del bot para poder usar el bot ${nn}`, false, { mentions: [m.sender] })
+await m.reply(`Hola @${m.sender.split`@`[0]} NO PUEDE USAR ESTE BOT EN CHAT PRIVADO\nUnirte al Grupo oficial del bot para poder usar el bot\n ${nn}`, false, { mentions: [m.sender] })
 handler.group = true
 return !1
 }}
