9d41c53328930a6564b02b8f9b71ad2038ad150b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-02-22 23:46:57 -0500

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -83,7 +83,7 @@ text: `╭━⊰ *${ucapan()}* ⊱━⬣
 ┃ *︾ U S U A R I O ︾*
 ┃
 ┃»» *${user.genero === 0 ? '👤' : user.genero == 'Ocultado 🕶️' ? `🕶️` : user.genero == 'Mujer 🚺' ? `🚺` : user.genero == 'Hombre 🚹' ? `🚹` : '👤'} ${user.registered === true ? user.name : name}*
-┃»» *Mi estado* ➺ ${user.miestado === undefined ? '_#miestado || Estado no asignado_' : '_' + user.miestado + '_'}
+┃»» *Mi estado* ➺ ${typeof user.miestado !== 'string' ? '_#miestado || Estado no asignado_' : '_' + user.miestado + '_'}
 ┃»» *Registrado ➺ ${user.registered === true ? '✅' : '❌'}* *»» Premium ➺ ${user.premiumTime > 0 ? '✅' : '❌'}*
 ┃
 ┃ *︾ M E N U ︾*
