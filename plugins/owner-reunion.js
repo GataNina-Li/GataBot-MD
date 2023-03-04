78dca836009c010b3ae22a0beda8bb0b600a1ea7
Azamiwithlogic <119384234+AzamiWithLogic@users.noreply.github.com>
2023-03-04 17:55:04 -0600

    Update owner-reunion.js

--- plugins/owner-reunion.js
+++ plugins/owner-reunion.js
@@ -2,9 +2,9 @@ let handler = async(m, { conn, command }) => {
 
 let text = `*☘️ El Owner @${m.sender.split`@`[0]} ha empezado una reunión y por eso se te ha mandado este mensaje, dirigirse al grupo de Staff lo más pronto posible ya que puede tratarse de un asunto importante.*
 
-`*ɢᴀᴛᴀʙᴏᴛ sᴛᴀғғ 🐈*`
+`ɢᴀᴛᴀʙᴏᴛ sᴛᴀғғ 🐈`
 m.reply('*📍 Enviando mensaje de reunión a todos los owners.*')
-for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
+for (let [text] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
                         conn.sendPayment(data.jid, '999999999', text, m)
