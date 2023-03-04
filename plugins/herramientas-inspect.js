980af85a8b82d156a99b8ec78279f186f04f744a
Azamiwithlogic <119384234+AzamiWithLogic@users.noreply.github.com>
2023-03-03 19:55:56 -0600

    Update herramientas-inspect.js

--- plugins/herramientas-inspect.js
+++ plugins/herramientas-inspect.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, text }) => {
 `
 	await conn.reply(m.chat, groupinfo, m)
 	const botones = [
-{index: 1, urlButton: {displayText: `Copiar Descripción`, url: `https://www.whatsapp.com/otp/copy/${data.desc}`}},
+{index: 1, urlButton: {displayText: `Copiar Descripción 📍`, url: `https://www.whatsapp.com/otp/copy/${data.desc}`}},
 ]
 await conn.sendMessage(m.chat, { text: `*╭──────────────╮*\n│☘️ • ¿Desea copiar la descripción?\n*╰──────────────╯*`, templateButtons: botones, footer: author })
 }
