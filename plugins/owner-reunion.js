f8ca668959116c824bde1c1b31044fbceb7ba095
@Azami <119384234+AzamiGood@users.noreply.github.com>
2023-03-05 12:17:35 -0600

    Update owner-reunion.js

--- plugins/owner-reunion.js
+++ plugins/owner-reunion.js
@@ -1,13 +1,16 @@
 let handler = async(m, { conn, command }) => {
-let text = `*☘️ El Owner @${m.sender.split`@`[0]} ha empezado una reunión y por eso se te ha mandado este mensaje, dirigirse al grupo de Staff lo más pronto posible ya que puede tratarse de un asunto importante.*
-m.reply('*_⚠️ Enviando mensaje de reunión a todos los owners._*')
+
+let text = `*_☘️ El Owner @${m.sender.split`@`[0]} ha empezado una reunión y por eso se te ha mandado este mensaje, dirigirse al grupo de Staff lo más pronto posible ya que puede tratarse de un asunto importante._*
+sᴛᴀғғ ɢᴀᴛᴀʙᴏᴛ`
+m.reply('*_💫 Enviando mensaje de reunión a todos los owners._*')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
-                        conn.sendPayment(data.jid, '999999999', text, m)                       
-                          // conn.reply(data.jid, text, m, { mentions: [m.sender] }
-                             )
-  }
+                        conn.sendPayment(data.jid, '999999999', text, m)
+                        
+                           // conn.reply(data.jid, text, m, { mentions: [m.sender] })
+                    }
+
 }
 handler.tags = ['owner']
 handler.command = handler.help =['reunionstaff']
