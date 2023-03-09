f2f30e5ae7a7f303fd3dabc4ee8713b19d214c97
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-08 18:50:42 -0600

    Update owner-reunion.js

--- plugins/owner-reunion.js
+++ plugins/owner-reunion.js
@@ -1,17 +1,15 @@
-/*Código en proceso de desarrollo*/
-
 let handler = async(m, { conn, command }) => {
 
 let text = `*_📚 El Owner @${m.sender.split`@`[0]} ha empezado una reunión y por eso se te ha mandado este mensaje, dirigirse al grupo de Staff lo más pronto posible ya que puede tratarse de un asunto importante._*
 
-_sᴛᴀғғ🔒_`
-m.reply('*_📑 Enviando mensaje de reunión a todos los owners._*')
+*sᴛᴀғғ 🐱*`
+m.reply('*_🗓️ Enviando mensaje de reunión a todos los owners._*')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
-                        conn.sendPayment(data.jid, text)
+                        conn.sendPayment(data.jid, '999999999', text, m)
                         
-                           // conn.reply(data.jid, text, { mentions: [m.sender] })
+                           // conn.reply(data.jid, text, m, { mentions: [m.sender] })
                     }
 
 }
