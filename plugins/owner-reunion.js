b586b6cc33cbce81e1d3262560bbf23b948a00fd
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-08 18:33:14 -0600

    Update owner-reunion.js

--- plugins/owner-reunion.js
+++ plugins/owner-reunion.js
@@ -1,4 +1,6 @@
-/*let handler = async(m, { conn, command }) => {
+/*Código en proceso de desarrollo*/
+
+let handler = async(m, { conn, command }) => {
 
 let text = `*_☘️ El Owner @${m.sender.split`@`[0]} ha empezado una reunión y por eso se te ha mandado este mensaje, dirigirse al grupo de Staff lo más pronto posible ya que puede tratarse de un asunto importante._*
 sᴛᴀғғ ɢᴀᴛᴀʙᴏᴛ`
@@ -15,4 +17,4 @@ handler.tags = ['owner']
 handler.command = handler.help =['reunionstaff']
 handler.rowner = true
 
-export default handler*/
+export default handler
