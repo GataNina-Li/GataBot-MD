e9a94b77559f2f8d6331ed40778abe8a116948c5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-16 01:56:49 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; 
 import './config.js';
 import { createRequire } from "module"; 
 import path, { join } from 'path'
