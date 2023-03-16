a8d4289e135cb3fe199f105cd08a7610a168be47
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-16 03:31:18 -0500

    Update gata.sh

--- gata.sh
+++ gata.sh
@@ -29,6 +29,9 @@ echo -e "\e[36m
 ╚██████╔╝██║░░░██║░░░
 ░╚═════╝░╚═╝░░░╚═╝░░░\n\e[0m"
 #echo -e "\033[1;36m"  
+if command -v git >/dev/null 2>&1; then
+echo -e "\033[01;33mGit ya fue instalado.\033[0m"
+else
 if pkg install git -y 2>&1 >/dev/null | grep -E -i -q '(command not found|unable to locate package|E: Could not get lock|debconf: delaying package configuration|Package not found|Failed to fetch|404 Not Found|Hash sum mismatch|503 Service Unavailable|504 Gateway Timeout|408 Request Timeout|Connection timed out|Temporary failure resolving)'; then
 error=$(pkg install git -y 2>&1 >/dev/null)
 echo -e "\033[0;31mError: $error\033[0m" 
@@ -37,6 +40,7 @@ echo -e "\033[01;33mpkg install git -y\npkg install nodejs -y\npkg install ffmpe
 exit 1
 else
 echo -e "\033[01;32m\033[01mGit se ha instalado correctamente.\nGit has been installed successfully.\n\033[0m" 
+fi
 fi
 
 #if pg istal gi -y; then
@@ -165,33 +169,41 @@ v1="${b}\033[32m"
 b="\033[0m"
 
 menu_ayuda() {
-    echo -e "${v1}[${b}++${v1}]${v} MENÚ DE CONFIGURACIÓN"
-    echo -e "${v1}[${b}01${v1}]${v} Visistar Grupo de Actualizaciones"
-    echo -e "${v1}[${b}02${v1}]${v} Elegir idioma español"
-    echo -e "${v1}[${b}03${v1}]${v} Elegir idioma inglés"
-    printf "${v1}[${b}++${v1}]${v} Elige una opción: "
-    read opcion
-    case $opcion in
-        01|1)
-            am start -a android.intent.action.VIEW https://chat.whatsapp.com/DVhu9S9Zr7cBFFl26N61U3
-            echo -e "${b}[${v1}++${b}]${v} Accediendo al Grupo Oficial\n"
-            ;;
-        02|2)
-            echo -e "${b}[${v1}++${b}]${v} Seleccionado idioma español\n"
-            
-            ;;
-        03|3)
-            echo -e "${b}[${v1}++${b}]${v} Seleccionado idioma inglés\n"
-            
-            ;;
-        *)
-            echo -e "${v1}Opción inválida."
-            echo ""
-            menu_ayuda
-            ;;
-    esac
+    menu_imprimido=false
+    while true; do
+        if ! $menu_imprimido; then
+            echo -e "${v1}[${b}++${v1}]${v} MENÚ DE CONFIGURACIÓN"
+            echo -e "${v1}[${b}01${v1}]${v} Visistar Grupo de Actualizaciones"
+            echo -e "${v1}[${b}02${v1}]${v} Elegir idioma español"
+            echo -e "${v1}[${b}03${v1}]${v} Elegir idioma inglés"
+            menu_imprimido=true
+        fi
+        printf "${v1}[${b}++${v1}]${v} Elige una opción: "
+        read opcion
+        case $opcion in
+            01|1)
+                am start -a android.intent.action.VIEW https://chat.whatsapp.com/DVhu9S9Zr7cBFFl26N61U3
+                echo -e "${b}[${v1}++${b}]${v} Accediendo al Grupo Oficial\n"
+                break
+                ;;
+            02|2)
+                echo -e "${b}[${v1}++${b}]${v} Seleccionado idioma español\n"
+                break
+                ;;
+            03|3)
+                echo -e "${b}[${v1}++${b}]${v} Seleccionado idioma inglés\n"
+                break
+                ;;
+            *)
+                echo -e "${v1}Opción inválida."
+                echo ""
+                ;;
+        esac
+    done
 }
+
 menu_ayuda
+
 
 #clear
 echo -e "\e[36m
