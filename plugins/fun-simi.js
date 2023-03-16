25460ae5d28dc1ce95bbea3704496d79a16c0c35
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-03-16 15:35:55 -0300

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,19 +1,39 @@
-import fetch from 'node-fetch'
-let handler = async (m, { text, usedPrefix, command }) => {
+import translate from '@vitalets/google-translate-api'
+import fetch from "node-fetch"
+let handler = async (m, { text, command, args, usedPrefix }) => {
 if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Hola Gata Bot*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝙏𝘼𝙇𝙆 𝙏𝙊 𝙈𝙀\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Hello Gata Bot*`
-await conn.sendPresenceUpdate('composing', m.chat)
-let res = await fetch (`https://api.simsimi.net/v2/?text=${text}&lc=es`)    //(`https://simsimi.info/api/?text=${text}&lc=es`)    
-let json = await res.json()
-let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
-//m.reply(`${tes}`) 
-conn.sendHydrated(m.chat, `${tes}`, `𝘼𝙄 | ${wm}`, null, null, null, null, null, [
+  try {
+  await conn.sendPresenceUpdate('composing', m.chat)
+  let api = await fetch("https://api.simsimi.net/v2/?text=" + text + "&lc=es")
+  let resSimi = await api.json()
+  //m.reply(resSimi.success)      
+  conn.sendHydrated(m.chat, `${resSimi}`, `𝘼𝙄 | ${wm}`, null, null, null, null, null, [
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m)
+  } catch {
+  try {
+  if (text.includes('Hola')) text = text.replace('Hola', 'Hello')
+  if (text.includes('hola')) text = text.replace('hola', 'Hello')
+  if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO')    
+  let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + text)
+  let resu = await reis.json()  
+  let nama = m.pushName || '1'
+  let api = await fetch("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=" + nama + "&msg=" + resu[0][0][0])
+  let res = await api.json()
+  let reis2 = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=" + res.cnt)
+  let resu2 = await reis2.json()
+  m.reply(resu2[0][0][0])      
+  } catch {  
+  let reisss = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=id&dt=t&q=" + text)
+  let resuuu = await reisss.json()      
+  let res222 = await fetch(`https://violetics.pw/api/utility/simsimi?apikey=beta&text=${resuuu[0][0][0]}`)  
+  let json222 = await res222.json()
+  let resulttt = json222.result
+  let lolll = await translate(`${resulttt}`, { to: 'es', autoCorrect: true })
+  m.reply(lolll.text)      
+  }}
 }
 handler.help = ['simsimi']
 handler.tags = ['General']
-handler.command = ['bot', 'simi', 'simsimi', 'alexa', 'bixby', 'cortana', 'siri', 'okgoogle']
+handler.command = ['bot', 'simi', 'simsimi'] 
 export default handler
-
-
-
