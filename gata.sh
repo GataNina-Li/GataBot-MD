cadcd00cecaa5c7429b871d78d66034ec545d4e3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-17 00:30:45 -0500

    Update gata.sh

--- gata.sh
+++ gata.sh
@@ -15,7 +15,7 @@ echo -e "\e[35m
 echo -e "\033[01;93mPreparando instalación...\nPreparing installation...\n\033[0m"
 if wget -q https://github.com/GataNina-Li/GataBot-MD/raw/master/musicSH/install_automatic2.mp3; then
 echo -e "\033[01;32mDescarga exitosa. Reproduciendo sonido de intro...\nDownload successful. Playing intro sound...\n\033[0m"
-mpv --no-terminal install_automatic1.mp3 &
+mpv --no-terminal install_automatic2.mp3 &
 else
 echo -e "\033[01;91mSonido de intro no disponible.\nIntro sound not available.\n\033[0m"
 fi
