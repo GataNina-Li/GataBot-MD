0599380496d45a487b9697e17200fb427e0c8993
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-28 14:54:00 -0500

    Update propietario(a)-backup.js

--- plugins/propietario(a)-backup.js
+++ plugins/propietario(a)-backup.js
@@ -1,34 +1,22 @@
-/*BY https://github.com/DIEGO-OFC*/
-
 import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
-	let fdoc = {
-  key : {
-  remoteJid: 'status@broadcast',
-  participant : '0@s.whatsapp.net'
-  },
-  message: {
-  documentMessage: {
-  title: '𝙳 𝙰 𝚃 𝙰 𝙱 𝙰 𝚂 𝙴', 
-  jpegThumbnail: fs.readFileSync('./media/menus/Menu1.jpg'),
-                            }
-                          }
-                        }
-	let d = new Date
-            let date = d.toLocaleDateString('id', {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric'
-            })
-conn.reply(m.chat, '*enviando backup.*', m)
-conn.reply('593968585383' + '@s.whatsapp.net', `*🗓️ Database:* ${date}`, null)
-          conn.sendFile('593968585383' + '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json', quoted: fdoc})
- }
- 
- handler.help = ['backup']
-handler.tags = ['owner']
-handler.command = /^(backup)$/i
-handler.rowner = true
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+await m.reply(`_*🗂️ Enviando base de datos. . .*_`)
+try {
+let d = new Date
+let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
+let database = await fs.readFileSync(`./database.json`)
+let creds = await fs.readFileSync(`./GataBotSession/creds.json`)
+await conn.reply(m.sender, `*🗓️ Database:* ${date}`, fkontak)
+await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: m })
+await conn.sendMessage(m.sender, {document: creds, mimetype: 'application/json', fileName: `creds.json`}, { quoted: m })
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}}
+
+handler.command = /^(backup|respaldo|copia)$/i
+handler.owner = true
 
 export default handler
