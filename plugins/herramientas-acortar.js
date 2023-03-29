6ecc89c3f80489dbae1f2e3a7cbf13cd8917851e
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-03-28 19:12:06 -0600

    Update herramientas-acortar.js

--- plugins/herramientas-acortar.js
+++ plugins/herramientas-acortar.js
@@ -1,15 +1,50 @@
-
 import fetch from 'node-fetch'
-let handler = async(m, { conn, args, text }) => {
-if (!text) throw `{mg} 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝙋𝘼𝙍𝘼 𝘼𝘾𝙊𝙍𝙏𝘼𝙍\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙇𝙄𝙉𝙆 𝙏𝙊 𝙎𝙃𝙊𝙍𝙏𝙀𝙉*`
-let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()  
-if (!shortUrl1) throw `*[❗] 𝙀𝙍𝙍𝙊𝙍, 𝘾𝙊𝙈𝙋𝙍𝙐𝙀𝘽𝙀 𝙌𝙐𝙀 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊 𝙄𝙉𝙂𝙍𝙀𝙎𝘼𝘿𝙊 𝙎𝙀𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙀 𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘿𝙀 𝙉𝙐𝙀𝙑𝙊*`
-let done = `*✅ 𝙎𝙀 𝙍𝙀𝘼𝙇𝙄𝙕𝙊 𝘾𝙊𝙉 𝙀𝙓𝙄𝙏𝙊\n𝙄𝙏 𝙒𝘼𝙎 𝙎𝙐𝘾𝘾𝙀𝙎𝙎𝙁𝙐𝙇!*\n\n*n𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝘼𝙉𝙏𝙀𝙎 | 𝘽𝙀𝙁𝙊𝙍𝙀 𝙇𝙄𝙉𝙆:*\n${text}\n*n𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝘼𝙃𝙊𝙍𝘼 | 𝙇𝙄𝙉𝙆 𝙉𝙊𝙒:*\n${shortUrl1}`.trim()   
-m.reply(done)}
-handler.help = ['tinyurl','acortar'].map(v => v + ' <link>')
-handler.tags = ['tools']
-handler.command = /^(tinyurl|short|acortar|corto)$/i
-handler.limit = 1
-handler.register = true
-handler.fail = null
+
+let handler = async (m, { conn, usedPrefix, args }) => {
+	let title = `— *S H O R T E D  U R L* —`
+    let caption = 'Por favor seleccione el tipo de URL'
+const sections = [
+   {
+	title: "TYPE URL",
+	rows: [
+	    {title: "TinyUrl", rowId: ".short " + args[0] + " tinyurl"},
+	    {title: "LinkPoi", rowId: ".short " + args[0] + " linkpoi"},
+	    {title: "Bitly", rowId: ".short " + args[0] + " bitly"},
+	    {title: "OuO", rowId: ".short " + args[0] + " ouo"},
+	]
+    },
+]
+
+const listMessage = {
+  text: caption,
+  footer: null,
+  title: title,
+  buttonText: "Enlaces en cortocircuito",
+  sections
+}
+
+if (!args[0]) return m.reply('¿Dónde está el enlace??')
+if (!args[0].startsWith('https://')) throw 'Ingrese URL con prefijo *https://*'
+if (!args[1]) return conn.sendMessage(m.chat, listMessage, { quoted: m })
+
+let tesk = '🚀 *ʟɪɴᴋ:* '
+let pros = '_*ᴄ ᴏ ɴ ᴠ ɪ ʀ ᴛ ɪ ᴇ ɴ ᴅ ᴏ . . .*_'
+//TINY
+if (args[1] == "tinyurl") {
+	let tiny = await (await fetch(`https://api.lolhuman.xyz/api/shortlink?apikey=${global.lolkey}url=${args[0]}`)).json()
+m.reply(pros).then(_ => conn.reply(m.chat, `${tesk}${tiny.result}`,m))
+}
+//--------------
+
+//LINKPOI
+if (args[1] == "linkpoi") {
+	let poi = await(await fetch(`https://linkpoi.ga/api.php?url=${args[0]}`)).json()
+	m.reply(pros).then(_=> conn.reply(m.chat, `${tesk}${poi.shorturl.replace('\/','/')}`,m))
+	}
+}
+handler.help = ['short <url> <type>']
+handler.tags = ['internet']
+handler.command = /^(short(url)?)$/i
+
 export default handler
+
