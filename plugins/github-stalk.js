414a48c87fd5fa0094f32a3b0d47f0f9c21de534
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-03-30 22:00:19 -0600

    Update github-stalk.js

--- plugins/github-stalk.js
+++ plugins/github-stalk.js
@@ -7,7 +7,7 @@ var handler = async(m, { conn, text }) => {
   let request = await githubstalk(text) 
     let { username, following, followers, type, bio, company, blog, location, email, public_repo, public_gists, profile_pic } = request
     let thumb = await (profile_pic)
-    let hasil = `*── 「 𝙂𝙄𝙏𝙃𝙐𝘽 𝙎𝙏𝘼𝙇𝙆 」 ──*
+    let hasil = `*⬤── 「 𝙂𝙄𝙏𝙃𝙐𝘽 𝙎𝙏𝘼𝙇𝙆 」 ──⬤*`
 ➸ *Username*: ${username}
 ➸ *Bio*: ${bio}
 ➸ *Perusahaan*: ${company}
