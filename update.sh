6b3d97c743f0696d3034dafb39bbb5422842b7e8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-04 03:37:47 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- update.sh
+++ update.sh
@@ -4,13 +4,13 @@
 BOT_DIR="GataBot-MD"
 BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR"
 DB_FILE="database.json"
-INSTALL_DP="yarn install && npm install && cd"
+INSTALL_DP="yarn install && npm install && cd $HOME"
 
 if [[ $(pwd) == *$BOT_DIR ]]; then
 if [ -e "$DB_FILE" ]; then
-mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
+mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && "$INSTALL_DP"
 else
-cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
+cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && "$INSTALL_DP"
 fi
 
 if [ -e "$HOME/$DB_FILE" ]; then
@@ -23,7 +23,7 @@ if [ -d "$HOME/$BOT_DIR" ]; then
 cd "$BOT_DIR"
 
 if [ -e "$BOT_DIR/$DB_FILE" ]; then
-mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
+mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && "$INSTALL_DP"
 
 if [ -e "$HOME/$DB_FILE" ]; then
 mv "$DB_FILE" "/$BOT_DIR" && cd "$BOT_DIR" && npm start
