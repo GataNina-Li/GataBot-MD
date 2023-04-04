f4b381c9635e52984ba64afb4333a9921b58a182
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-04 04:06:47 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- update.sh
+++ update.sh
@@ -6,34 +6,34 @@ BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR"
 DB_FILE="database.json"
 INSTALL_DP="yarn install --ignore-scripts && npm install && cd $HOME"
 
+
 if [[ $(pwd) == *$BOT_DIR ]]; then
 if [ -e "$DB_FILE" ]; then
-mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && "$INSTALL_DP"
+mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
 else
-cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && "$INSTALL_DP"
+cd "$HOME" && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
 fi
 
 if [ -e "$HOME/$DB_FILE" ]; then
-mv "$DB_FILE" "/$BOT_DIR" && cd "$BOT_DIR" && npm start
+mv "$HOME/$DB_FILE" "$BOT_DIR" && cd "$BOT_DIR" && npm start
 else
-cd "$BOT_DIR" && npm start
+cd "$HOME/$BOT_DIR" && npm start
 fi
+
 else
 if [ -d "$HOME/$BOT_DIR" ]; then
-cd "$BOT_DIR"
-
-if [ -e "$BOT_DIR/$DB_FILE" ]; then
-mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && "$INSTALL_DP"
-
+if [ -e "$HOME/$BOT_DIR/$DB_FILE" ]; then
+mv "$HOME/$BOT_DIR/$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
 if [ -e "$HOME/$DB_FILE" ]; then
-mv "$DB_FILE" "/$BOT_DIR" && cd "$BOT_DIR" && npm start
+mv "$HOME/$DB_FILE" "$BOT_DIR" && cd "$BOT_DIR" && npm start
 else
-cd "$BOT_DIR" && npm start
-fi
-else
-cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn install && npm install && npm start
+cd "$HOME/$BOT_DIR" && npm start
 fi
 else
-rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn install && npm install && npm start
+cd "$HOME" && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn install && npm install && npm start
+fi
+else
+rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn install && npm install && npm start
 fi
 fi
+
