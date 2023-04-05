02ccff3a7dfe65a2372b46b31cdc8009df7e7b56
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-05 17:26:35 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,16 +12,16 @@ RESET='\033[0m'
 if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then
 if [ -e "$DB_FILE" ]; then
 echo -e "${BOLD}${GREEN}Moviendo \"$DB_FILE\" a \"$HOME\" y clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
-mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
+mv "$DB_FILE" "$HOME" && cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && $INSTALL_DP
 else
 echo -e "${BOLD}${GREEN}No se encontró \"$DB_FILE\" en \"$BOT_DIR\" clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
-cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
+cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && $INSTALL_DP
 fi
 
 if [[ $(basename "$PWD") == "$HOME" ]]; then
 if [ -e "$HOME/$DB_FILE" ]; then
 echo -e "${BOLD}${GREEN}Rescatando archivo \"$DB_FILE\" y moviendo a \"$BOT_DIR\".${RESET}"
-mv "$HOME/$DB_FILE" "$BOT_DIR/" && cd "$BOT_DIR" && 
+mv "$HOME/$DB_FILE" "$HOME/$BOT_DIR/" && cd "$HOME/$BOT_DIR" && 
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
 npm start
 else
