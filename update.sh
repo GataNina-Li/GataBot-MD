f21a97a2c978a04c9dba0a0b1fc610bef6e88e67
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-05 17:02:47 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 BOT_DIR="GataBot-MD"
 BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR"
 DB_FILE="database.json"
-INSTALL_DP="yarn install --force --ignore-scripts && npm install && cd"
+INSTALL_DP="yarn --ignore-scripts && npm install && cd"
 
 GREEN='\033[32m'
 BOLD='\033[1m'
@@ -10,49 +10,63 @@ RESET='\033[0m'
 
 if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then
 if [ -e "$DB_FILE" ]; then
-echo -e "${BOLD}${GREEN}Moviendo database.json a $HOME y clonando el repositorio $BOT_REPO en $BOT_DIR...${RESET}"
+echo -e "${BOLD}${GREEN}Moviendo \"$DB_FILE\" a \"$HOME\" y clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
 mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
 else
-echo -e "${BOLD}${GREEN}Clonando el repositorio $BOT_REPO en $BOT_DIR...${RESET}"
+echo -e "${BOLD}${GREEN}No se encontró \"$DB_FILE\" en \"$BOT_DIR\" clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
 cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
 fi
 
 if [[ $(basename "$PWD") == "$HOME" ]]; then
 if [ -e "$HOME/$DB_FILE" ]; then
-echo -e "${BOLD}${GREEN}Rescatando archivo $DB_FILE...${RESET}"
-mv "$HOME/$DB_FILE" "$BOT_DIR" && cd "$BOT_DIR" && npm start
-else
+echo -e "${BOLD}${GREEN}Rescatando archivo \"$DB_FILE\" y moviendo a \"$BOT_DIR\".${RESET}"
+mv "$HOME/$DB_FILE" "$BOT_DIR/" && cd "$BOT_DIR" && 
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-cd "$HOME/$BOT_DIR" && npm start
+npm start
+else
+echo -e "${BOLD}${GREEN}No existe \"$DB_FILE\" en \"$HOME\"${RESET}"
+cd "$HOME/$BOT_DIR" && 
+echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
+npm start
 fi
 else
+echo -e "${BOLD}${GREEN}Dirigiéndome a \"$BOT_DIR\"...${RESET}"
+cd && cd "$HOME/$BOT_DIR" &&
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-cd && cd "$HOME/$BOT_DIR" && npm start
+npm start
 fi
 
 else
-echo -e "${BOLD}${GREEN}Ubicación actual predeterminada.${RESET}"
+echo -e "${BOLD}${GREEN}Ubicación actual: \"$HOME\"${RESET}"
 cd "$HOME"
 if [ -e "$HOME/$BOT_DIR" ]; then
-echo -e "${BOLD}${GREEN}Dirigiéndome a $BOT_DIR${RESET}"
+echo -e "${BOLD}${GREEN}Dirigiéndome a \"$BOT_DIR\".${RESET}"
 cd "$HOME/$BOT_DIR"
 if [ -e "$HOME/$BOT_DIR/$DB_FILE" ]; then
-echo -e "${BOLD}${GREEN}Moviendo $DB_FILE a $HOME y clonando el repositorio $BOT_REPO en $HOME...${RESET}"
+echo -e "${BOLD}${GREEN}Moviendo \"$DB_FILE\" a \"$HOME\" y clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
 mv "$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && $INSTALL_DP
 if [ -e "$HOME/$DB_FILE" ]; then
-echo -e "${BOLD}${GREEN}Rescatando archivo $DB_FILE...${RESET}"
-mv "$HOME/$DB_FILE" "$BOT_DIR" && cd "$BOT_DIR" && npm start
-else
+echo -e "${BOLD}${GREEN}Rescatando archivo \"$DB_FILE\" y moviendo a \"$BOT_DIR\".${RESET}"
+mv "$HOME/$DB_FILE" "$BOT_DIR/" && cd "$BOT_DIR" &&
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-cd "$BOT_DIR" && npm start
+npm start
+else
+echo -e "${BOLD}${GREEN}Dirigiéndome a \"$BOT_DIR\"...${RESET}"
+cd "$BOT_DIR" && 
+echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
+npm start
 fi
 else
-echo -e "${BOLD}${GREEN}No se encontro $DB_FILE. Clonando el repositorio $BOT_REPO en $HOME...${RESET}"
-cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn install --force --ignore-scripts && npm install && npm start
+echo -e "${BOLD}${GREEN}No se existe \"$DB_FILE\" clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
+cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install &&
+echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
+npm start
 fi
 else
-echo -e "${BOLD}${GREEN}No se encontro $BOT_DIR. Clonando el repositorio $BOT_REPO en $HOME...${RESET}"
-git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn install --force --ignore-scripts && npm install && npm start
+echo -e "${BOLD}${GREEN}No se existe \"$BOT_DIR\" clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
+git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install &&
+echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
+npm start
 fi
 fi
 
