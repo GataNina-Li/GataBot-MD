af7dba84779db42590e994cdb4aad5f6231b1ba8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-05 21:48:24 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,45 +13,28 @@ if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then
 if [ -e "$DB_FILE" ]; then 
 echo -e "${BOLD}${GREEN}Moviendo \"$DB_FILE\" a \"$HOME\" y clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
 mv "$DB_FILE" "$HOME" && cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install && cd
-#if [[ $(basename "$PWD") == "$HOME" ]]; then
 if [ -e "$HOME/$DB_FILE" ]; then
 echo -e "${BOLD}${GREEN}Rescatando archivo \"$DB_FILE\" y moviendo a \"$BOT_DIR\".${RESET}"
-mv "$DB_FILE" "$BOT_DIR/" && cd "$BOT_DIR" && 
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-npm start
+mv "$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && npm start
 else
 echo -e "${BOLD}${GREEN}No existe \"$DB_FILE\" en \"$HOME\"${RESET}"
-cd "$BOT_DIR" && 
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-npm start
+cd "$BOT_DIR" && npm start
 fi
-#else
-#echo -e "${BOLD}${GREEN}Dirigiéndome a \"$BOT_DIR\"...${RESET}"
-#cd && cd "$BOT_DIR" &&
-#echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-#npm start
-#fi
+
 else
 echo -e "${BOLD}${GREEN}No se encontró \"$DB_FILE\" en \"$BOT_DIR\" clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
 cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install && cd
-#if [[ $(basename "$PWD") == "$HOME" ]]; then
 if [ -e "$HOME/$DB_FILE" ]; then
 echo -e "${BOLD}${GREEN}Rescatando archivo \"$DB_FILE\" y moviendo a \"$BOT_DIR\".${RESET}"
-mv "$DB_FILE" "$BOT_DIR/" && cd "$BOT_DIR" && 
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-npm start
+mv "$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && npm start
 else
 echo -e "${BOLD}${GREEN}No existe \"$DB_FILE\" en \"$HOME\"${RESET}"
-cd "$BOT_DIR" && 
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-npm start
+cd "$BOT_DIR" && npm start
 fi
-#else
-#echo -e "${BOLD}${GREEN}Dirigiéndome a \"$BOT_DIR\"...${RESET}"
-#cd "$BOT_DIR" &&
-#echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-#npm start
-#fi
 fi
 
 else
