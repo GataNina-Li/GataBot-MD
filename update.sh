82e228d4461ee04fa9009af6f17efb628cbbf604
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-05 19:34:16 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,10 +14,10 @@ RESET='\033[0m'
 if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then 
 if [ -e "$DB_FILE" ]; then
 echo -e "${BOLD}${GREEN}Moviendo \"$DB_FILE\" a \"$HOME\" y clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
-mv "$DB_FILE" "$HOME" && cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && rm -rf $HOME/$BOT_DIR/node_modules && $INSTALL_DP
+mv "$DB_FILE" "$HOME" && cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && rm -rf $HOME/$BOT_DIR/node_modules && yarn cache clean && $INSTALL_DP
 else
 echo -e "${BOLD}${GREEN}No se encontró \"$DB_FILE\" en \"$BOT_DIR\" clonando el repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
-cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && rm -rf $HOME/$BOT_DIR/node_modules && $INSTALL_DP
+cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && rm -rf $HOME/$BOT_DIR/node_modules && yarn cache clean && $INSTALL_DP
 fi
 
 if [[ $(basename "$PWD") == "$HOME" ]]; then
