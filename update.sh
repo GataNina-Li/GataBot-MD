81b521d1ea9359c9ff165c76d92da69e087ab863
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-05 22:16:02 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,11 +12,11 @@ RESET='\033[0m'
 if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then 
 if [ -e "$DB_FILE" ]; then 
 echo -e "${BOLD}${GREEN}Moviendo \"$DB_FILE\" a \"$HOME\" y clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
-mv "$DB_FILE" "$HOME" && cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install && cd
+mv "$HOME/$BOT_DIR/$DB_FILE" "$HOME" && cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install && cd
 if [ -e "$HOME/$DB_FILE" ]; then
 echo -e "${BOLD}${GREEN}Rescatando archivo \"$DB_FILE\" y moviendo a \"$BOT_DIR\".${RESET}"
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-mv "$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && npm start
+mv "$HOME/$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && npm start
 else
 echo -e "${BOLD}${GREEN}No existe \"$DB_FILE\" en \"$HOME\"${RESET}"
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
@@ -29,7 +29,7 @@ cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" &&
 if [ -e "$HOME/$DB_FILE" ]; then
 echo -e "${BOLD}${GREEN}Rescatando archivo \"$DB_FILE\" y moviendo a \"$BOT_DIR\".${RESET}"
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
-mv "$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && npm start
+mv "$HOME/$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && npm start
 else
 echo -e "${BOLD}${GREEN}No existe \"$DB_FILE\" en \"$HOME\"${RESET}"
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
