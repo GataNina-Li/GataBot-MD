0035400a763898a1a91e821938f84aa389f49376
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-05 22:26:31 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 # Código desarrollado por @gata_dios  
-
+  
 #!/data/data/com.termux/files/usr/bin/bash  
 BOT_DIR="GataBot-MD"  
 BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR" 
@@ -8,7 +8,7 @@ DB_FILE="database.json"
 GREEN='\033[32m' 
 BOLD='\033[1m' 
 RESET='\033[0m' 
- 
+  
 if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then 
 if [ -e "$DB_FILE" ]; then 
 echo -e "${BOLD}${GREEN}Moviendo \"$DB_FILE\" a \"$HOME\" y clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
@@ -58,16 +58,16 @@ echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
 npm start
 fi
 else
-echo -e "${BOLD}${GREEN}No se existe \"$DB_FILE\" clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
+echo -e "${BOLD}${GREEN}No existe \"$DB_FILE\" clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
 cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install &&
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
 npm start
 fi
 else
-echo -e "${BOLD}${GREEN}No se existe \"$BOT_DIR\" clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
+echo -e "${BOLD}${GREEN}No existe \"$BOT_DIR\" clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
 git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install && cd
 if [ -e "$HOME/$DB_FILE" ]; then
-echo -e "${BOLD}${GREEN}He encontrado un archivo \"$DB_FILE\" en \"$HOME\"  lo moveré a \"$BOT_DIR\".${RESET}"
+echo -e "${BOLD}${GREEN}He encontrado un archivo \"$DB_FILE\" en \"$HOME\" lo moveré a \"$BOT_DIR\".${RESET}"
 mv "$HOME/$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && 
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
 npm start
