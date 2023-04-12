40de33cdf5e32068a56ad317143deca82cd35eb4
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-04-12 15:50:44 -0300

    Add files via upload

---
+++ plugins/descargas-ringtone.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
+if (!text) throw `*[❗] 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊 𝙌𝙐𝙀 𝘿𝙀𝙎𝙀𝙀 𝘽𝙐𝙎𝘾𝘼𝙍, 𝙀𝙅𝙀𝙈𝙋𝙇𝙊: ${usedPrefix + command} Hola*`
+let anu = await ringtone(text)
+let result = anu[Math.floor(Math.random() * anu.length)]
+conn.sendMessage(m.chat, { audio: { url: result.audio }, fileName: result.title+'.mp3', mimetype: 'audio/mpeg' }, { quoted: m })}
+handler.command  = ['ringtone']
+export default handler
+async function ringtone(title) {
+return new Promise((resolve, reject) => {
+axios.get('https://meloboom.com/es/search/'+title).then((get) => {
+let $ = cheerio.load(get.data)
+let hasil = []
+$('#__next > main > section > div.jsx-2244708474.container > div > div > div > div:nth-child(4) > div > div > div > ul > li').each(function (a, b) {
+hasil.push({ title: $(b).find('h4').text(), source: 'https://meloboom.com/'+$(b).find('a').attr('href'), audio: $(b).find('audio').attr('src') })
+})
+resolve(hasil)})})}
+
