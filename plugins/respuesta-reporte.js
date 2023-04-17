5284e5f862bf351780512b648286cfb6537e5471
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-17 01:59:43 -0500

    Update respuesta-reporte.js

--- plugins/respuesta-reporte.js
+++ plugins/respuesta-reporte.js
@@ -25,7 +25,20 @@ async function handler(m, { conn, command, text }) {
             conn.sendPresenceUpdate('composing', m.chat);
         }
     }
-    await conn.reply('972529277026@s.whatsapp.net', txt.trim(), m)
+    //await conn.reply('972529277026@s.whatsapp.net', txt.trim(), m)
+    const quote = {
+  key: {
+    remoteJid: "972529277026@s.whatsapp.net"
+  },
+  message: {
+    conversation: "Mensaje personalizado"
+  },
+  messageTimestamp: new Date() / 1000
+};
+await conn.sendMessage("573136855110@s.whatsapp.net", { 
+  text: txt.trim(), 
+  quote: quote 
+})
     global.db.data.users[m.sender].reporte = 0
        // await conn.sendMessage(`${numero}@s.whatsapp.net`, { text: txt.trim(), mentions: conn.parseMention(txt) }, { userJid: conn.user.jid, quoted: m, ephemeralExpiration: 1 * 100, disappearingMessagesInChat: true });
     
