dcf025a12ee0ec544013868148fb61288582934f
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-04-27 18:07:23 -0300

    Add files via upload

---
+++ plugins/herramientas-topdf.js
@@ -0,0 +1,12 @@
+import uploadImage from '../lib/uploadImage.js'
+let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw '𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙀 / 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉'    
+let img = await q.download?.()
+let url = await uploadImage(img)    
+let docname = text ? text : m.pushName || 'documento'
+conn.sendFile(m.chat, `http://api.lolhuman.xyz/api/convert/imgtopdf?apikey=${lolkeysapi}&img=${url}`, docname + '.pdf', '', m, false, { asDocument: true })
+}
+handler.command = /^topdf$/i
+export default handler
