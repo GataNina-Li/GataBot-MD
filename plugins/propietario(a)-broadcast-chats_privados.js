252a9e65870922b10c2371153d2546a27a49a8ab
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-13 20:40:15 -0300

    Add files via upload

--- plugins/propietario(a)-broadcast-chats_privados.js
+++ plugins/propietario(a)-broadcast-chats_privados.js
@@ -1,14 +1,30 @@
-//NO USAR A MENOS QUE ESTE SOLUCIONADO ESTE CÓDIGO 
-//creado por https://github.com/DIEGO-OFC
-/*import { randomBytes } from 'crypto'
-let handler = async (m, { conn, text }) => {
-  let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
-  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
-  let teks = text ? text : cc.text
-  await delay(5 * 5000)
-  conn.reply(m.chat, `*[❕] ᴍᴇɴsᴀᴊᴇ ᴇɴᴠɪᴀᴅᴏ ᴀ ${chats.length} ᴄʜᴀᴛs ᴘʀɪᴠᴀᴅᴏs*`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 *𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇 ✅* | ' + wm + ' 」\n' + randomID(32)), true).catch(_ => _)}
-
+import { randomBytes } from 'crypto'
+let handler = async (m, { conn, command, usedPrefix, text }) => {
+  if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))        
+let cc3 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+let teks3 = text ? text : cc3.text  
+let chats = Object.keys(global.db.data.users).filter(user => user.endsWith('@s.whatsapp.net')) 
+await conn.reply(m.chat, lenguajeGB.smsBCMensaje2(), m)
+let start = new Date().getTime()
+let totalPri = 0
+for (let user of chats) {
+await new Promise(resolve => setTimeout(resolve, 2000)) // 2 segundos
+await conn.reply(user, `${lenguajeGB.smsBCbot7()}\n\n` + teks3, null)
+totalPri++
+if (totalPri >= 3000) { 
+break
+}}   
+let end = new Date().getTime() 
+let time = Math.floor((end - start) / 1000)
+if (time >= 60) {
+let minutes = Math.floor(time / 60)
+let seconds = time % 60
+time = `${minutes} minutos y ${seconds} segundos`
+} else {
+time = `${time} segundos`
+}
+await m.reply(lenguajeGB.smsBCMensaje3(totalPri, time))
+}
 handler.help = ['broadcastchats', 'bcchats'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(broadcastchats?|bcc(hats?)?)$/i
@@ -21,6 +37,6 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
-const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)*/
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
 
   
