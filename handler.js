e99204d0034e51535b05c3f51739000f00b191a9
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-14 20:09:08 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ import path, { join } from 'path'
 import { unwatchFile, watchFile } from 'fs'
 import chalk from 'chalk'   
 import fetch from 'node-fetch' 
-
+ 
 /**
  * @type {import('@adiwajshing/baileys')}  
  */
