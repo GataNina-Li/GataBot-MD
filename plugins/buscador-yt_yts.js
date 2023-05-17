014986daffc4ca3d28f945d2bb0f945462f9a695
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-16 22:41:20 -0300

    Add files via upload

--- plugins/buscador-yt_yts.js
+++ plugins/buscador-yt_yts.js
@@ -2,6 +2,31 @@ import yts from "yt-search"
 let handler = async (m, { text, conn, args, command, usedPrefix }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
+//await conn.reply(m.chat, global.wait, m)
+  let results = await yts(text)
+  let tes = results.all
+  let teks = results.all.map(v => {
+    switch (v.type) {
+      case 'video': return `❤️꙰༻ *TÍTULO:* ${v.title}
+⁖🩵꙰༻ *ENLACE:* ${v.url}
+⁖💜꙰༻ *DURACIÓN:* ${v.timestamp}
+⁖💚꙰༻ *SUBIDO:* ${v.ago}
+⁖🧡꙰༻ *VISTAS:* ${v.views}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
+}
+handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
+handler.tags = ['tools']
+handler.command = /^playlist|ytbuscar|yts(earch)?$/i
+handler.exp = 70
+handler.limit = 1
+handler.level = 4
+export default handler
+
+
+/*import yts from "yt-search"
+let handler = async (m, { text, conn, args, command, usedPrefix }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
 try {
 let search = await yts(args.join(" "))
 let listAudio = []
@@ -41,5 +66,4 @@ handler.exp = 70
 handler.limit = 1
 handler.level = 4
 export default handler
-
-
+*/
