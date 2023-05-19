493f9bb1a619ef43e0ec3f60ad7e66dc2d2ad8aa
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-19 14:40:03 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -1,18 +1,21 @@
 import PhoneNumber from 'awesome-phonenumber'
-let handler = async (m, { conn, isBotAdmin, usedPrefix, groupMetadata, participants }) => { 
+let handler = async (m, { conn, __dirname, isRowner, isOwner, isBotAdmin, usedPrefix, groupMetadata, groups, _package, participants }) => { 
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
 
-let txt = ''
+let txt
 const chats = Object.entries(conn.chats).filter(([jid, data]) => jid && data.isChats)
-const groupsIn = chats.filter(([jid]) => jid.endsWith('@g.us'))
-for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('https://chat.whatsapp.com/') + conn.groupInviteCode(jid) && chat.isChats)) 
+groups = Object.values(await conn.groupFetchAllParticipating())
+txt = `${packname} ${lenguajeGB.smsLisA()}
+${lenguajeGB.smsLisB()} ${groups.length}\n`
 
-txt += `*✦ Grupo:* ${conn.getName(jid) == jid.split`@`[0] ? '*Grupo no encontrado*' : await conn.getName(jid)}
-*✦ Creador(a):* ${jid.split`@`[0].length >= 15 ? `*Creador no encontrado*` : `*_Wa.me/${jid.split`@`[0]}_*`}
-*✦ Mí estadía:* ${chat?.metadata?.read_only ? '❌ *SIN ESTAR AQUÍ | NO*' : '✅ *SIGO AQUÍ | YES*'}\n\n`
-  
-conn.sendButton(m.chat, `*${gt} ESTÁ EN ESTOS GRUPOS*\n*IS IN THESE GROUPS:*\n\n*✦ Total de Grupos:* *_${groupsIn.length}_*\n\n`, txt, null, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], fkontak, m)
+for (let i = 0; i < groups.length; i++) {
+txt += ` 
+${lenguajeGB.smsLisC()} ${groups[i].subject}
+${lenguajeGB.smsLisD()} ${groups[i].id}
+${isOwner ? `${lenguajeGB.smsLisE()} ${groups[i].participants.length}` : ''}\n\n`
+}
+m.reply(txt.trim())
 }
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
