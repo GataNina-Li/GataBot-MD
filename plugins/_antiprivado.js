1215c65e3588852de6c8f91b3322358e0190c178
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-06-07 14:35:16 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -9,13 +9,7 @@ let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
-user.warn += 1
-if (!(user.warn >= 4)) await m.reply(`Hola *@${m.sender.split`@`[0]}*, NO PUEDE USAR ESTE BOT EN CHAT PRIVADO\n\nUnirte al Grupo oficial del bot para poder usar el bot\n${nn}`, false, { mentions: [m.sender] })
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat')}
-//await m.reply(`*${lenguajeGB['smsCreA']()}* *@${m.sender.split`@`[0]}*, ${lenguajeGB['smsprivado']()}\n${nn}`,mentions: [m.sender] }, { quoted: fkontak })
-if (user.warn >= 4) {
-user.warn = 0 
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat') 
-}
+await m.reply(`Hola *@${m.sender.split`@`[0]}*, NO PUEDE USAR ESTE BOT EN CHAT PRIVADO\n\nUnirte al Grupo oficial del bot para poder usar el bot\n${nn}`, false, { mentions: [m.sender] })
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat')
 return !1
-}
+}}
