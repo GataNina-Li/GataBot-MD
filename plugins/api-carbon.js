898a0f8b16a114ccc150a30affa6cd60805e04e0
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-06-29 22:15:50 -0600

    Create api-carbon.js

---
+++ plugins/api-carbon.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Masukkan Text\nContoh : .carbon require("node-fetch")'
+  m.reply('C A R G A N D O')
+  let res = `https://api.lolhuman.xyz/api/carbon?apikey=BrunoSobrino&code=${text}&language=python`
+  conn.sendFile(m.chat, res, 'carbon.jpg', `© CuriosityBot-MD`, m, false)
+}
+handler.help = ['carbon'].map(v => v + ' <text>')
+handler.tags = ['maker']
+handler.command = /^(carbon)$/i
+
+module.exports = handler
