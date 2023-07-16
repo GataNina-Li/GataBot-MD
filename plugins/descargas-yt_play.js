9dd85e9680190b5606359f3b6d9df83a0cd50c16
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-16 18:55:57 -0500

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -56,17 +56,18 @@ if (command == 'play') {
 let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytplay?apikey=${lolkeysapi}&url=${title}`)    
 let lolh = await lolhuman.json()
 let n = lolh.result.title || 'error'
+await conn.sendMessage(m.chat, { audio: { url: lolh["result"]["audio"]["link"] }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
    
-  conn.sendMessage(m.chat, { audio: { url: lolh["result"]["audio"]["link"] }, mimetype: 'audio/mpeg', contextInfo: {
-  externalAdReply: {
-  title: title,
-  body: "",
-  thumbnailUrl: tmb,
-  sourceUrl: web,
-  mediaType: 1,
-  showAdAttribution: true,
-  renderLargerThumbnail: true
-  }}} , { quoted: m })
+ // conn.sendMessage(m.chat, { audio: { url: lolh["result"]["audio"]["link"] }, mimetype: 'audio/mpeg', contextInfo: {
+//  externalAdReply: {
+//  title: title,
+//  body: "",
+//  thumbnailUrl: tmb,
+//  sourceUrl: web,
+//  mediaType: 1,
+//  showAdAttribution: true,
+//  renderLargerThumbnail: true
+//  }}} , { quoted: m })
 }
 if (command == 'play2') {
 var captionvid = `*𓆩 𓃠 𓆪 ✧═══ ${vs} ═══✧ 𓆩 𓃠 𓆪*
