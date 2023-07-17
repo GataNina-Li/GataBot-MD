dd738ffc72a5c3e7923b12ed0e8dd04cc46f06d0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-17 00:08:15 -0500

    Update descargas-yta.js

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -6,7 +6,8 @@ if (!args[0]) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙄𝙉�
 await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}𝙎𝙀 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙉𝘿𝙊 𝙎𝙐 𝙑𝙄𝘿𝙀𝙊, 𝙀𝙎𝙋𝙀𝙍𝙀 𝙐𝙉 𝙈𝙊𝙈𝙀𝙉𝙏𝙊 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍\n\n𝙔𝙊𝙐𝙍 𝙑𝙄𝘿𝙀𝙊 𝙄𝙎 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿𝙄𝙉𝙂, 𝙒𝘼𝙄𝙏 𝘼 𝙈𝙊𝙈𝙀𝙉𝙏 𝙋𝙇𝙀𝘼𝙎𝙀`, fkontak, m)
 //try{
 var vid = (await youtubeSearch(text)).video[0]
-var { title, thumbnail } = vid
+var { title, thumbnail, videoId } = vid
+var url = 'https://www.youtube.com/watch?v=' + videoId
 let web = `https://yt.btch.bz/downloadAudio?URL=${url}&videoName=video`
 var tmb = thumbnail
   
