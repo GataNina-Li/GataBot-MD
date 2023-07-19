d0af9d4f6eaa808b4095ec940b546f280a58149d
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-07-18 23:47:08 -0600

    Update propietario(a)-reunion.js

--- plugins/propietario(a)-reunion.js
+++ plugins/propietario(a)-reunion.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, command }) => {
 let text = `*❕ EL OWNER @${m.sender.split`@`[0]}  HA SOLICITADO SU PRESENCIA EN EL STAFF, FAVOR DE DIRIGIRSE LO ANTES POSIBLE AL STAFF*
 
 *𝙎𝙩𝙖𝙛𝙛 𝙂𝙖𝙩𝙖𝘽𝙤𝙧-𝙈𝘿 🐱*`
-m.reply('*⚠️ SOLICITANDO LA PRESENCIA DE LOS OWNERS EN EL STAFF*')
+m.reply('*⚠️ ENVIANDO MENSAJE A TODOS LOS OWNERS*')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
