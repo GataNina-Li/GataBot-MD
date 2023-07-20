d51f01afae53c643b49a9961ca3315567c7de935
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-07-19 22:54:43 -0600

    Update herramientas-say.js

--- plugins/herramientas-say.js
+++ plugins/herramientas-say.js
@@ -2,7 +2,7 @@ let handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `*❕ Ingresa el texto, ejemplo:*\n${usedPrefix + command} Hola`
   let textfilter = text.replace(listkatakotor, "(*kata kotor*)")
   conn.reply(m.chat, textfilter, null)
-};
+}
 handler.help = ["say <teks>"]
 handler.tags = ["tools"];
 handler.command = /^(say)$/i
