9af9a9f987f05cb76e615e736f11d68b70e9dc1d
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-07-19 23:20:57 -0600

    Update chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -2,6 +2,7 @@
  
 let handler = m => m
 handler.all = async function (m) {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let chat = global.db.data.chats[m.chat]
 let name = conn.getName(m.sender)
 
