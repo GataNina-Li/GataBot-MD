a765b77fc1d09b4e1b852a06cd58a18c4722113a
cuentasecundariaa <139152004+cuentasecundariaa@users.noreply.github.com>
2023-07-22 16:02:55 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -1,6 +1,7 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙐𝙎𝙀 𝘿𝙀 𝙇𝘼 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 𝙈𝘼𝙉𝙀𝙍𝘼\n*${usedPrefix + command} Gata*\n\n𝙐𝙎𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿 𝙇𝙄𝙆𝙀 𝙏𝙃𝙄𝙎\n*${usedPrefix + command} Cat*`
+if (text.includes('gore', 'cp', 'Porno', 'Gore')) return m.reply('*🙄 No busques cosa enferma*')
 const res = await googleImage(text)
 let image = res.getRandom()
 let link = image
