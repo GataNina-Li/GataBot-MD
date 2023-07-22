ba0a463134cb4b6253a60b368a316528f6ef695b
cuentasecundariaa <139152004+cuentasecundariaa@users.noreply.github.com>
2023-07-22 17:36:31 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/propietario(a) -actualizacion.js
+++ plugins/propietario(a) -actualizacion.js
@@ -12,10 +12,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
       if (sha !== previousCommitSHA || message !== previousUpdatedAt) {
         previousCommitSHA = sha
         previousUpdatedAt = message
-    conn.sendMessage(m.chat, { text: `${lenguajeGB['smsAvisoIIG']()} ¡¡𝙀𝙇 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊 𝙃𝘼 𝙎𝙄𝘿𝙊 𝘼𝘾𝙏𝙐𝙇𝙄𝙕𝘼𝘿𝙊!!*\n*𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊:* ${html_url}\n*- 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝘿𝙀 𝘾𝙊𝙈𝙈𝙄𝙏:* ${message}` }, { quoted: m })
+    conn.sendMessage(m.chat, { text: `${lenguajeGB['smsAvisoIIG']()} 𝙀𝙇 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊 𝙃𝘼 𝙎𝙄𝘿𝙊 𝘼𝘾𝙏𝙐𝙇𝙄𝙕𝘼𝘿𝙊!!\n𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊: ${html_url}\n*• 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝘿𝙀 𝘾𝙊𝙈𝙈𝙄𝙏:* ${message}` }, { quoted: m })
    } 
   } catch (error) {
-    await m.reply('${fg} 𝙀𝙍𝙍𝙊𝙍 𝘼𝙇 𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 𝙀𝙇 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊:*', error.message)
+    await m.reply(`${fg} 𝙀𝙍𝙍𝙊𝙍 𝘼𝙇 𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 𝙀𝙇 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊:'`, error.message)
    }
   }
  setInterval(checkRepoUpdates, 60000)
--- plugins/propietario(a)-autoadmin.js
+++ plugins/propietario(a)-autoadmin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
   if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw '*YA ES ADMIN DEL GRUPO MI Propietario(a) *'
+  if (isAdmin) throw `*𝙔𝘼 𝙀𝙎 𝘼𝘿𝙈𝙄𝙉 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝙈𝙄 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊(𝘼)*`
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
 handler.command = /^admin.|atad|autoadmin$/i
