ca2ea2825c26c5b93933a7dc1374e3d5a4b525ce
cuentasecundariaa <139152004+cuentasecundariaa@users.noreply.github.com>
2023-07-24 07:53:47 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -1,7 +1,7 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙐𝙎𝙀 𝘿𝙀 𝙇𝘼 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 𝙈𝘼𝙉𝙀𝙍𝘼\n*${usedPrefix + command} Gata*\n\n𝙐𝙎𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿 𝙇𝙄𝙆𝙀 𝙏𝙃𝙄𝙎\n*${usedPrefix + command} Cat*`
-if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) || m.text.includes('xxx')) return m.reply('🙄')
+if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34') || m.text.includes('xxx')) return m.reply('🙄')
 const res = await googleImage(text)
 let image = res.getRandom()
 let link = image
