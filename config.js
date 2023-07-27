0252cda2eab2bf0f20a7bd36d306286d3b9f228c
barby2204 <127579314+barby2204@users.noreply.github.com>
2023-07-27 08:59:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import moment from 'moment-timezone'
 import { en, es, id, ar, pt, fr, hi } from './lib/idiomas/total-idiomas.js' 
 
 
-//⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰  
+//⊱ ━━━━━'541137042743'.━━━━ ⊰  
 
 global.owner = [
   ['593993684821', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈', true],  
