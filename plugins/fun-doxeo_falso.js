38d656e07eb882ab7fc5c7b13d916b69b34b39c8
cuentasecundariaa <139152004+cuentasecundariaa@users.noreply.github.com>
2023-08-01 17:13:09 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/fun-doxeo_falso.js
+++ plugins/fun-doxeo_falso.js
@@ -10,10 +10,13 @@ let boost2 = `*${pickRandom(['21','22','23','24','25','26','27','28','29','30','
 let boost3 = `*${pickRandom(['41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60'])}%*`
 let boost4 = `*${pickRandom(['61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80'])}%*`
 let boost5 = `*${pickRandom(['81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100'])}%*`
-await m.reply(start)
-await m.reply(boost)
-await m.reply(boost3)
-await m.reply(boost5)
+const { key } = await conn.sendMessage(m.chat, {text: start}, {quoted: m});
+await delay(1000 * 1);
+await conn.sendMessage(m.chat, {text: boost, edit: key});
+await delay(1000 * 1);
+await conn.sendMessage(m.chat, {text: boost3, edit: key});
+await delay(1000 * 1);
+await conn.sendMessage(m.chat, {text: boost5, edit: key});
   
   
 //DATOS FALSOS | FALSE DATA
@@ -68,7 +71,8 @@ Sof02s32inf14.1e100.net
 *Tcp:* 192.168.629-->92.28.211.167:8615
 *EXTERNAL MAC:* 6U:77:89:ER:O4
 *MODEM JUMPS:* 64`
-conn.reply(m.chat, doxeo, m)
+await conn.sendMessage(m.chat, {text: doxeo, edit: key});
+//conn.reply(m.chat, doxeo, m)
 
 user.prue = new Date * 1  
 }
@@ -77,6 +81,7 @@ handler.tags = ['fun']
 handler.command = /^Doxxeo|doxxeo|doxxear|Doxxear|doxeo|doxear|doxxeame|doxeame/i
 handler.fail = null
 export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]}
--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -2,18 +2,18 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🚀🚀')
-         await m.reply('🚀🚀🚀🚀')
-         await m.reply('🚀🚀🚀🚀🚀🚀')
-         await m.reply('𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩') 
+    const { key } = await conn.sendMessage(m.chat, {text: `🚀🚀`}, {quoted: m});
+await delay(1000 * 1);
+await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key});
+await delay(1000 * 1);
+await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key});
+await conn.sendMessage(m.chat, {text: `𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩`, edit: key});
     process.send('reset')
   } else throw 'eh'
 }
-
 handler.help = ['restart']
 handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
-
 handler.rowner = true
-
 export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
