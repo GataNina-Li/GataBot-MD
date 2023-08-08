537689c78817beca11a4316ff8540531244af69a
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-08 05:32:06 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -1,4 +1,48 @@
-import fetch from 'node-fetch'
+import {find_lyrics} from '@brandond/findthelyrics';
+import {getTracks} from '@green-code/music-track-data';
+import {googleImage} from '@bochilteam/scraper';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+const teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : '';
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+if (!teks) throw `${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉𝘼 𝘾𝘼𝙉𝘾𝙄𝙊𝙉 𝙋𝘼𝙍𝘼 𝙊𝘽𝙏𝙀𝙉𝙀𝙍 𝙇𝘼 𝙇𝙀𝙏𝙍𝘼\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Runaway*\n\n𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙎𝙊𝙉𝙂 𝙏𝙊 𝙂𝙀𝙏 𝙏𝙃𝙀 𝙇𝙔𝙍𝙄𝘾𝙎\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Billie Eilish bored*`
+try {
+const result = await getTracks(teks);
+const lyrics = await find_lyrics(`${result[0].artist} ${result[0].title}`);
+const res = await fetch(global.API('https://some-random-api.com', '/lyrics', {title: result[0].artist + result[0].title}));
+const json = await res.json();
+let img;
+try {
+img = result.album.artwork;
+} catch {
+try {
+img = json.thumbnail.genius;
+} catch {
+const bochil = await googleImage(`${result[0].artist} ${result[0].title}`);
+img = await bochil.getRandom();
+}}
+await conn.sendMessage(m.chat, {image: {url: img}, caption: `𝙏𝙄𝙏𝙐𝙇𝙊 | 𝙏𝙄𝙏𝙇𝙀 
+💚 *${result[0].title || ''}*
+
+𝘼𝙐𝙏𝙊𝙍(𝘼) | 𝘼𝙐𝙏𝙃𝙊𝙍
+💜 *${result[0].artist || ''}*
+
+${lyrics || ''}`}, {quoted: fkontak});
+await conn.sendMessage(m.chat, {audio: {url: result[0].preview}, fileName: `${result[0].artist} ${result[0].title}.mp3`, mimetype: 'audio/mp4'}, {quoted: m});
+} catch (e) {
+await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${usedPrefix + command}\n\n${wm}`, fkontak, m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+handler.limit = 0
+}}
+handler.help = ['lirik','letra'].map(v => v + ' <Apa>')
+handler.tags = ['internet']
+handler.command = /^(lirik|lyrics|lyric|letra)$/i
+handler.limit = 1
+handler.level = 3
+handler.exp = 55
+export default handler
+
+/*import fetch from 'node-fetch'
 import { lyrics, lyricsv2, googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
@@ -36,4 +80,5 @@ handler.command = /^(lirik|lyrics|lyric|letra)$/i
 handler.limit = 1
 handler.level = 3
 handler.exp = 55
-export default handler 
+export default handler */
+
