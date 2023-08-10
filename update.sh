10488177473f5d3dee2e28fcc763189c0633b0f1
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-09 20:07:09 -0600

    © 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
-# Código desarrollado por @gata_dios  
+# Código desarrollado por @gata_dios
   
-#!/data/data/com.termux/files/usr/bin/bash  
+#!/data/data/com.termux/files/usr/bin/bash 
 BOT_DIR="GataBot-MD"  
-BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR" 
+BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR"
 DB_FILE="database.json"
  
-GREEN='\033[32m' 
-BOLD='\033[1m' 
-RESET='\033[0m' 
+GREEN='\033[32m'
+BOLD='\033[1m'
+RESET='\033[0m'
   
-if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then 
+if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then
 if [ -e "$DB_FILE" ]; then 
 echo -e "${BOLD}${GREEN}Moviendo \"$DB_FILE\" a \"$HOME\" y clonando repositorio \"$BOT_REPO\" en \"$HOME\"...${RESET}"
 echo -e "${BOLD}${GREEN}Moving \"$DB_FILE\" to \"$HOME\" and cloning repository \"$BOT_REPO\" into \"$HOME\"...\n${RESET}"
@@ -89,7 +89,7 @@ git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install &
 if [ -e "$HOME/$DB_FILE" ]; then
 echo -e "${BOLD}${GREEN}He encontrado un archivo \"$DB_FILE\" en \"$HOME\", lo moveré a \"$BOT_DIR\".${RESET}"
 echo -e "${BOLD}${GREEN}I have found a file \"$DB_FILE\" in \"$HOME\", moving it to \"$BOT_DIR\".\n${RESET}"
-mv "$HOME/$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && 
+mv "$HOME/$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" &&
 echo -e "${BOLD}${GREEN}Iniciando $BOT_DIR...${RESET}"
 echo -e "${BOLD}${GREEN}Starting  $BOT_DIR...\n${RESET}"
 npm start
