f6c5c7732b9d06859f41a077d8e971df5a20780d
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-11 10:49:36 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/_auto-eliminadosession.js
@@ -0,0 +1,32 @@
+/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
+//🟢 También mejorado por: https://github.com/elrebelde21 🤣
+//🟢 ELIMINAR AUTOMÁTICAMENTE LAS SESSIONES NO IMPORTARTE (ESTOS ESTA PRUEBA) 
+import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
+import path from 'path';
+
+const handler = async (m, { conn, usedPrefix }) => {
+if (global.conn.user.jid !== conn.user.jid) {
+}
+await delay(60000) // 600000 = 10 minutos
+const sessionPath = './GataBotSession/';
+try {
+if (!existsSync(sessionPath)) {
+}
+const files = await fs.readdir(sessionPath);
+let filesDeleted = 0;
+for (const file of files) {
+if (file !== 'creds.json') {
+await fs.unlink(path.join(sessionPath, file));
+filesDeleted++;
+}}
+if (filesDeleted === 0) {
+console.log("NO HAY NADA POR ELIMINAR")
+} else {
+console.log(`🟢 SE ELIMINADO ${filesDeleted} ARCHIVOS DE SESSION CON ÉXITO`)
+}
+} catch (err) {
+console.error('Error al leer la carpeta o los archivos de sesión:', err);
+}};
+handler.command = /^()$/i;
+export default handler;
+const delay = time => new Promise(res => setTimeout(res, time))
