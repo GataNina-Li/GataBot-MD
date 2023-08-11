669de91a6baeabd3e395595b92f04626594ba021
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-11 15:48:36 -0400

    buen index

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import { join, dirname } from 'path'
+import { join, dirname } from "path";
 import { createRequire } from "module";
-import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
-import chalk from "chalk"
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) 
-const { name, author } = require(join(__dirname, './package.json')) 
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
+import { fileURLToPath } from "url";
+import { setupMaster, fork } from "cluster";
+import { watchFile, unwatchFile } from "fs";
+import cfonts from "cfonts";
+import chalk from "chalk";
+import { createInterface } from "readline";
+import yargs from "yargs";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname);
+const { name, author } = require(join(__dirname, "./package.json"));
+const { say } = cfonts;
+const rl = createInterface(process.stdin, process.stdout);
 
 say('Gata\nBot\nMD', {
 font: 'chrome',
@@ -20,48 +20,48 @@ gradient: ['red', 'magenta']})
 say(`Por Gata Dios | DIEGO-OFC`, {
 font: 'console',
 align: 'center',
-gradient: ['red', 'magenta']})
+gradient: ['red', 'magenta']});
 
-var isRunning = false
+var isRunning = false;
 /**
 * Start a js file
 * @param {String} file `path/to/file`
 */
 function start(file) {
 if (isRunning) return
-isRunning = true
+isRunning = true;
 let args = [join(__dirname, file), ...process.argv.slice(2)]
   
 setupMaster({
 exec: args[0],
-args: args.slice(1), })
-let p = fork()
+args: args.slice(1), });
+let p = fork();
 p.on('message', data => {
 switch (data) {
 case 'reset':
-p.process.kill()
+p.process.kill();
 isRunning = false
-start.apply(this, arguments)
+start.apply(this, arguments);
 break
 case 'uptime':
-p.send(process.uptime())
-break }})
+p.send(process.uptime());
+break }});
 p.on('exit', (_, code) => {
-isRunning = false
-console.error('⚠️ Error Inesperado ⚠️', code)
+isRunning = false;
+console.error('⚠️ Error Inesperado ⚠️', code);
   
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
+p.process.kill();
+isRunning = false;
+start.apply(this, arguments);
   
 if (process.env.pm_id) {
-process.exit(1)
+process.exit(1);
 } else {
-process.exit()
-}
-})
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test'])
+process.exit();
+};
+});
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+if (!opts['test']);
 if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim())})}
+p.emit('message', line.trim())})};
 start('main.js')
