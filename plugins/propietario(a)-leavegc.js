154300e1ecc58d5890914e9f7a3bb6b2ceed31fc
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-18 19:40:17 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/propietario(a)-leavegc.js
+++ plugins/propietario(a)-leavegc.js
@@ -1,7 +1,15 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
+let chat = global.db.data.chats[m.chat]
+chat.welcome = false
 await conn.reply(id, `*${wm} 𝘼𝘽𝘼𝙉𝘿𝙊𝙉𝘼 𝙀𝙇 𝙂𝙍𝙐𝙋𝙊, 𝙁𝙐𝙀 𝙂𝙀𝙉𝙄𝘼𝙇 𝙀𝙎𝙏𝘼𝙍 𝘼𝙌𝙐𝙄 👋*`) 
-await conn.groupLeave(id)}
+await conn.groupLeave(id)
+try {  
+chat.welcome = true
+} catch (e) {
+await m.reply(`${fg}`) 
+return console.log(e)
+}}
 handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
 handler.group = true
 handler.rowner = true
