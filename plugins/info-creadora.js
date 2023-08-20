e908ca3d04a1fe743c07ba44f604886287a07fd4
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-20 14:17:05 -0600

    Gata: Modificación de vídeo a imagen

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -1,6 +1,6 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let pp = gataVidMenu.getRandom()	
+let pp = gataImg.getRandom()	
 const cat = `𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
 *Wa.me/573136855110*
 
