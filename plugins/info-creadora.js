f17b6f77d29adf8126f849ba101bc6a235fd9b9a
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-20 14:00:37 -0600

    Gata

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -1,5 +1,6 @@
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+	
 	conn.reply(m.chat, `𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
 *Wa.me/573136855110*
 
