09f8062c8facbc4e17164c7e17a26825f9620d63
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-22 11:46:38 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,9 +1,8 @@
 import fetch from 'node-fetch'
-const scp1 = require('./lib/scraper') 
 let handler = async (m, { conn, command, args }) => {
 let full = /f$/i.test(command)
-if (!text) return conn.reply(m.chat, '*Porfavor ingresa un url de la página a la que se le tomará captura 🔎*', m)
-let krt = await scp1.ssweb(text)
+if (!args[0]) return conn.reply(m.chat, '*Porfavor ingresa un url de la página a la que se le tomará captura 🔎*', m)
+let krt = await scp1.ssweb(args)
 //let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
 //let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
 conn.sendFile(m.chat, krt.result, 'error.png', m)
@@ -13,3 +12,39 @@ handler.tags = ['internet']
 handler.command = /^ss(web)?f?$/i
 handler.money = 40
 export default handler
+
+const ssweb = (url, device = 'desktop') => {
+     return new Promise((resolve, reject) => {
+          const base = 'https://www.screenshotmachine.com'
+          const param = {
+            url: url,
+            device: device,
+            cacheLimit: 0
+          }
+          axios({url: base + '/capture.php',
+               method: 'POST',
+               data: new URLSearchParams(Object.entries(param)),
+               headers: {
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+               }
+          }).then((data) => {
+               const cookies = data.headers['set-cookie']
+               if (data.data.status == 'success') {
+                    axios.get(base + '/' + data.data.link, {
+                         headers: {
+                              'cookie': cookies.join('')
+                         },
+                         responseType: 'arraybuffer'
+                    }).then(({ data }) => {
+                        result = {
+                            status: 200,
+                            result: data
+                        }
+                         resolve(result)
+                    })
+               } else {
+                    reject({ status: 404, statuses: `Link Error`, message: data.data })
+               }
+          }).catch(reject)
+     })
+          }
