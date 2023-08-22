58b00dfae71d00496c948766ea674f13f88a7b11
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-22 11:31:30 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch'
+const scp1 = require('./lib/scraper') 
 let handler = async (m, { conn, command, args }) => {
 let full = /f$/i.test(command)
 if (!args[0]) return conn.reply(m.chat, '*Porfavor ingresa un url de la página a la que se le tomará captura 🔎*', m)
-let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
-conn.sendFile(m.chat, ss, 'error.png', url, m)
+let krt = await scp1.ssweb(q)
+//let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
+//let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
+conn.sendFile(m.chat, krt.result, 'error.png', m)
 }
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['internet']
