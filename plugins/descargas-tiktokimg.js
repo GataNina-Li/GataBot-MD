4918562374266dc8df5445de59aa08e19ebc4db9
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-08-24 01:08:48 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨
    Se añade función de descargar tiktok de imágenes

--- plugins/descargas-tiktokimg.js
+++ plugins/descargas-tiktokimg.js
@@ -12,7 +12,7 @@ anu = anu.result
 if (anu.length == 0) throw Error('Error : no data')
 let c = 0
 for (let x of anu) {
-if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `✅ Enviando 1 de ${anu.length} diapositivas de imagen. ✅\n_(El resto se enviará vía chat privado..)_` }, { quoted : m })
+if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `✅ Se ha enviado 1 de ${anu.length} imágenes. ✅\nEl resto podrá ser visible en el chat privado del bot 😸_\n(Para evitar el spam)` }, { quoted : m })
 else await conn.sendMessage(m.sender, { image: { url: x } }, { quoted : m })
 c += 1
 }
