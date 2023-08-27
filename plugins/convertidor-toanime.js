a0710fe896eb468c7d8b024e92f302d65b329a7f
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-08-27 16:20:28 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨
    Corrección de texto en el mensaje

--- plugins/convertidor-toanime.js
+++ plugins/convertidor-toanime.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
 const q = m.quoted ? m.quoted : m;
 const mime = (q.msg || q).mimetype || q.mediaType || '';
 if (!/image/g.test(mime)) throw `${lenguajeGB.smsAvisoMG()}𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝙊 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉`
-  m.reply(`${lenguajeGB.smsAvisoIIG()}𝘼𝙂𝙐𝘼𝙍𝘿𝙀 𝙀𝙎𝙏𝙊𝙔 𝘾𝙊𝙉𝙑𝙀𝙍𝙏𝙄𝘿𝙊 𝙄𝙈𝘼𝙂𝙀𝙉 𝘼 𝘿𝙄𝙎𝙀𝙉̃𝙊 𝘼𝙉𝙄𝙈𝙀, 𝙎𝙀𝘼 𝙋𝘼𝘾𝙄𝙀𝙉𝙏𝙀 𝙀𝙉 𝙇𝙊 𝙌𝙐𝙀 𝙀𝙉𝙑𝙄𝙊 𝙀𝙇 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊`);
+  m.reply(`${lenguajeGB.smsAvisoIIG()}𝘼𝙂𝙐𝘼𝙍𝘿𝙀 𝙀𝙎𝙏𝙊𝙔 𝘾𝙊𝙉𝙑𝙄𝙍𝙏𝙄𝙀𝙉𝘿𝙊 𝙇𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝘼 𝘿𝙄𝙎𝙀𝙉̃𝙊 𝘼𝙉𝙄𝙈𝙀, 𝙎𝙀𝘼 𝙋𝘼𝘾𝙄𝙀𝙉𝙏𝙀 𝙀𝙉 𝙇𝙊 𝙌𝙐𝙀 𝙀𝙉𝙑𝙄𝙊 𝙀𝙇 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊`);
 const data = await q.download?.();
 const image = await uploadImage(data);
 try {
@@ -18,7 +18,7 @@ try {
 const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
 await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
 } catch (e) {
-throw `${lenguajeGB.smsAvisoFG()}𝙀𝙍𝙍𝙊𝙍, 𝙑𝙀𝙍𝙄𝙁𝙄𝙌𝙐𝙀 𝙌𝙐𝙀 𝙀𝙉 𝙇𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙎𝙀𝘼 𝙀𝙇 𝙍𝙊𝙎𝙏𝙍𝙊 𝘿𝙀 𝙐𝙉𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼`
+throw `${lenguajeGB.smsAvisoFG()}𝙀𝙍𝙍𝙊𝙍, 𝙑𝙀𝙍𝙄𝙁𝙄𝙌𝙐𝙀 𝙌𝙐𝙀 𝙇𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙎𝙀𝘼 𝙀𝙇 𝙍𝙊𝙎𝙏𝙍𝙊 𝘿𝙀 𝙐𝙉𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼`
 }}}}
 handler.help = ['toanime'];
 handler.tags = ['tools'];
