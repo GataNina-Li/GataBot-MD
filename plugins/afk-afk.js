0ad8efb0d444e89f527a91690a94adbe59186684
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-04 08:31:08 -0500

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,10 +1,20 @@
-let handler = async (m, { text, usedPrefix, command }) => {
+let handler = async (m, { text, usedPrefix, command, args }) => {
 let user = global.db.data.users[m.sender]
-if (!text) return m.reply(`${lenguajeGB['smsAfkQ1'](usedPrefix, command)}`)
+  
+if (args.length >= 1) {
+text = args.slice(0).join(" ")
+} else if (m.quoted && m.quoted.text) {
+text = m.quoted.text
+} else return m.reply(`${lenguajeGB['smsAfkQ1'](usedPrefix, command)}`)
+  
 if (text.length < 10) return m.reply(`${lenguajeGB['smsAfkQ2']()}`)
 user.afk = + new Date
 user.afkReason = text
-m.reply(`${lenguajeGB['smsAfkM1A']()} *${conn.getName(m.sender)}* ${lenguajeGB['smsAfkM1B']()}${text ? ': ' + text : ''}`)}
+m.reply(`${lenguajeGB['smsAfkM1A']()}*@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1B']()}${text ? ': ' + text : ''}`)}
+
+await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}✴️ *A F K* ✴️
+*▔▔▔▔▔▔▔▔▔*
+*${lenguajeGB['smsAfkM1A']()} @${m.sender.split("@")[0]} ${lenguajeGB['smsAfkM1B']()}${text ? ':*\n ' + text : ''}`, m, { mentions: [m.sender] })
 
 handler.command = /^afk$/i
 export default handler
