11baeb7c6ea9453fda2ae01947d32a9ff906e798
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-04 08:02:49 -0500

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,12 +1,10 @@
-let handler = async (m, { text, usedPrefix }) => {
+let handler = async (m, { text, usedPrefix, command }) => {
 let user = global.db.data.users[m.sender]
-  if (!text) return m.reply(`${lenguajeGB['smsAvisoMG']()}𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝘾𝙊𝙇𝙊𝙌𝙐𝙀 𝙎𝙐 𝙈𝙊𝙏𝙄𝙑𝙊 𝙋𝘼𝙍𝘼 𝙀𝙎𝙏𝘼𝙍 𝘼𝙁𝙆\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊:\n*${usedPrefix}afk Voy a comer*`)
-    if (text.length < 10) return m.reply(`${lenguajeGB['smsAvisoMG']()}𝙀𝙇 𝙈𝙊𝙏𝙄𝙑𝙊 𝙀𝙎 𝙈𝙐𝙔 𝘾𝙊𝙍𝙏𝙊, 𝙈𝙄𝙉𝙄𝙈𝙊 10 𝘾𝘼𝙍𝘼́𝘾𝙏𝙀𝙍𝙀𝙎`)
+if (!text) return m.reply(`${lenguajeGB['smsAfkQ1'](usedPrefix, command)}`)
+if (text.length < 10) return m.reply(`${lenguajeGB['smsAfkQ2']()}`)
 user.afk = + new Date
 user.afkReason = text
-m.reply(`${lenguajeGB['smsAfkM1A']()} *${conn.getName(m.sender)}* ${lenguajeGB['smsAfkM1B']()}${text ? ': ' + text : ''}
-`)}
-handler.help = ['afk [alasan]']
-handler.tags = ['main']
+m.reply(`${lenguajeGB['smsAfkM1A']()} *${conn.getName(m.sender)}* ${lenguajeGB['smsAfkM1B']()}${text ? ': ' + text : ''}`)}
+
 handler.command = /^afk$/i
 export default handler
