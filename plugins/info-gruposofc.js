ce4e55f4ec23673445a634f242db169915121f78
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-09-06 22:46:13 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -37,7 +37,9 @@ let str = `💕 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝙊𝙎
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 🐈 *${nnnttt3}*\n
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-🐈 *https://chat.whatsapp.com/J8HOf2T3QYeKu3UN9UBkT6*\n
+🐈 *${nnnttt4}*\n
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ 
+🐈 *${nnnttt5}*\n
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 *Por favor, no ingresar con números de Bots, y mantener el respeto.*\n
 *Please, do not enter with Bot numbers, and maintain respect.*`
