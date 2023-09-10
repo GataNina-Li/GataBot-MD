caa2ba81ded91b41e59cf1380e6fa884483147c1
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-09-10 17:40:56 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨
    Hora dar la advetencia 5 veces despues yo no envia mas mensaje 💫

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -9,7 +9,13 @@ let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`Hola *@${m.sender.split`@`[0]}*, NO PUEDE USAR ESTE BOT EN CHAT PRIVADO\n\nUnirte al Grupo oficial del bot para poder usar el bot\n${nn}`, false, { mentions: [m.sender] })
+user.warn += 1;
+if (!(user.warn >= 5)) await m.reply(`Hola *@${m.sender.split`@`[0]}*, NO PUEDE USAR ESTE BOT EN CHAT PRIVADO\n\nUnirte al Grupo oficial del bot para poder usar el bot\n${nn}`, false, {mentions: [m.sender]})}
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat')
+if (user.warn >= 5) {
+ user.warn = 0;
+//await m.reply(`Te repite 5 veces no privado hora sera bloqueado bye`, false, {mentions: [m.sender]})
+//await this.updateBlockStatus(m.sender, 'block')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat')
 return !1
 }}
