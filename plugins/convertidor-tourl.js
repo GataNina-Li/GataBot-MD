919bc5a9e107c25808751e864192e75b095091fc
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-09-12 19:55:47 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨
    Arreglado 😼

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -2,43 +2,27 @@
 # Créditos a https://github.com/Undefined17
 •• @Azami19 ••
 */
-
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
-
 let handler = async (m) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => gataImg.getRandom())
 let name = await conn.getName(who)
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝙑𝙄𝘿𝙀𝙊\n𝙍𝙀𝙎𝙋𝙊𝙉𝘿 𝙏𝙊 𝘼𝙉 𝙄𝙈𝘼𝙂𝙀 𝙊𝙍 𝙑𝙄𝘿𝙀𝙊'
-  let media = await q.download()
-  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
-  let caption = ` *🛑 ENLACE:*\n${link}
-  *🥏 TAMAÑO:*\n${media.length} bytes\n
-  *🚀 EXPIRACION:*\n ${isTele ? '✅ NO EXPIRA' : '⚠️ DESCONOCIDO'}\n
-  *🔰 ACORTADO:*\n${await shortUrl(link)}`
-
-conn.reply(m.chat, caption, m, { contextInfo: {
-          externalAdReply :{
-    mediaUrl: md,
-    mediaType: 2,
-    title: wm,
-    body: botdate,
-    thumbnail: await(await fetch(link)).buffer(),
-    sourceUrl: link
-     }}
-  })
-}
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw `${mg} 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝙑𝙄𝘿𝙀𝙊\n𝙍𝙀𝙎𝙋𝙊𝙉𝘿 𝙏𝙊 𝘼𝙉 𝙄𝙈𝘼𝙂𝙀 𝙊𝙍 𝙑𝙄𝘿𝙀𝙊`
+let media = await q.download()
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+let link = await (isTele ? uploadImage : uploadFile)(media)
+let caption = `🛑 𝙀𝙉𝙇𝘼𝘾𝙀:\n${link}\n🥏 𝙏𝘼𝙈𝘼𝙉𝙊: ${media.length}\n🚀 𝙀𝙓𝙋𝙄𝙍𝘼𝘾𝙄𝙊𝙉: ${isTele ? '𝙉𝙊 𝙀𝙓𝙋𝙄𝙍𝘼' : '𝘿𝙀𝙎𝘾𝙊𝙉𝙊𝘾𝙄𝘿𝙊'}\n🔰 𝘼𝘾𝙊𝙍𝙏𝘼𝘿𝙊: ${await shortUrl(link)}`
+conn.reply(m.chat, caption, m, { contextInfo: {externalAdReply :{mediaUrl: md, mediaType: 2, title: wm, body: botdate, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link }}})}
 handler.help = ['tourl']
 handler.tags = ['herramientas']
 handler.command = /^(tourl|upload)$/i
 export default handler
 
 async function shortUrl(url) {
-	let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-	return await res.text()
+let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+return await res.text()
 }
