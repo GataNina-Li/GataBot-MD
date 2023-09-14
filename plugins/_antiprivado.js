723f68bfe1800603bc9fc01801d72613e5144f18
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-14 17:28:59 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,20 +1,28 @@
-//by https://github.com/elrebelde21 
-
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
-if (!m.message) return !0 
+if (!m.message) return !0
 if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('estado') || m.text.includes('verificar') || m.text.includes('creadora') || m.text.includes('bottemporal') || m.text.includes('grupos') || m.text.includes('instalarbot') || m.text.includes('términos') || m.text.includes('bots') || m.text.includes('deletebot') || m.text.includes('eliminarsesion') || m.text.includes('serbot') || m.text.includes('verify') || m.text.includes('register') || m.text.includes('registrar') || m.text.includes('reg') || m.text.includes('reg1') || m.text.includes('nombre') || m.text.includes('name') || m.text.includes('nombre2') || m.text.includes('name2') || m.text.includes('edad') || m.text.includes('age') || m.text.includes('edad2') || m.text.includes('age2') || m.text.includes('genero') || m.text.includes('género') || m.text.includes('gender') || m.text.includes('identidad') || m.text.includes('pasatiempo') || m.text.includes('hobby') || m.text.includes('identify') || m.text.includes('finalizar') || m.text.includes('pas2') || m.text.includes('pas3') || m.text.includes('pas4') || m.text.includes('pas5') || m.text.includes('registroC') || m.text.includes('deletesesion') || m.text.includes('registroR') || m.text.includes('jadibot')) return !0
+
 let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
 let bot = global.db.data.settings[this.user.jid] || {}
+
 if (bot.antiPrivate && !isOwner && !isROwner) {
-user.warn2 += 1;
-if (!(user.warn2 >= 4)) await m.reply(`Hola *@${m.sender.split`@`[0]}*, NO PUEDE USAR ESTE BOT EN CHAT PRIVADO\n\nUnirte al Grupo oficial del bot para poder usar el bot\n${nn}`, false, {mentions: [m.sender]})}
-if (user.warn2 >= 4) {
-user.warn2 = 500
-//await m.reply(`Te repite 5 veces no privado hora sera bloqueado bye`, false, {mentions: [m.sender]})
-//await this.updateBlockStatus(m.sender, 'block')
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat')
+if (user.counterPrivate === 0) {
+await m.reply(`*@${m.sender.split`@`[0]} ESTÁ PROHIBIDO ESCRIBIR AL PRIVADO.*\n\n⚠️ \`\`\`PRIMERA ADVERTENCIA\`\`\` ⚠️`)
+  
+} else if (user.counterPrivate === 1) {
+let grupos = [ nn, nnn, nnnt, nnntt, nnnttt ].getRandom()
+await m.reply(`*@${m.sender.split`@`[0]} YA SE MENCIONÓ QUE NO PUEDE ESCRIBIR AL PRIVADO.*\n\n*PUEDE UNIRSE A ESTE GRUPO OFICIAL*\n${grupos}\n\n‼️ *SI VUELVE A ESCRIBIR SERÁ BLOQUEADO(A)* ‼️\n⚠️ \`\`\`SEGUNDA ADVERTENCIA\`\`\` ⚠️`)
+  
+} else if (user.counterPrivate === 2) {
+await m.reply(`*@${m.sender.split`@`[0]} SERÁ BLOQUEADO(A). SE MENCIONÓ ANTES QUE NO PODÍA ESCRIBIR AL PRIVADO.*\n\n⚠️ \`\`\`TERCERA ADVERTENCIA\`\`\` ⚠️`)
+await this.updateBlockStatus(m.sender, 'block')
+user.counterPrivate = 0  
+}
+user.counterPrivate++
+}
 return !1
-}}
+}
+
