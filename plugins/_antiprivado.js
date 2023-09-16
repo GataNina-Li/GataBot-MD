8cc60ccdcb5769a5bedb4048701aa255119db235
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-16 16:08:09 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,9 +1,9 @@
 const comandos = /piedra|papel|tijera|estado|verificar|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, command }) {
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, usedPrefix, command }) {
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
 if (!m.message) return !0
-const regex = new RegExp(`^${prefix} ?(?:${comandos.source})$`, 'i')
+const regex = new RegExp(`^${usedPrefix} ?(?:${comandos.source})$`, 'i')
 if (regex.test(m.text.toLowerCase())) return !0
 
 let chat, user, bot, mensaje
