f3f018ad2a6226b19b95a99e4ecf1efaf45ed053
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-16 17:13:50 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -5,7 +5,7 @@ if (m.isGroup) return !1
 if (!m.message) return !0
 const letraText = m.text.toLowerCase().trim()
 const regex = new RegExp(`^${comandos.source}$`, 'i')
-if (letraText.startsWith(usedPrefix) && regex.test(m.text.toLowerCase().trim())) return !0
+if (letraText.startsWith(usedPrefix) === true && regex.test(m.text.toLowerCase().trim())) return !0
 
 let chat, user, bot, mensaje
 chat = global.db.data.chats[m.chat]
