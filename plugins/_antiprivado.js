dcf8c9a9c671bec2ff2140982142548a0537212e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-16 17:28:32 -0500

    `solución al baneo`

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -3,9 +3,8 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, u
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
 if (!m.message) return !0
-const letraText = m.text.toLowerCase().trim()
 const regex = new RegExp(`^${comandos.source}$`, 'i')
-if (letraText.startsWith(usedPrefix) === true && regex.test(m.text.toLowerCase().trim())) return !0
+if (regex.test(m.text.toLowerCase().trim())) return !0
 
 let chat, user, bot, mensaje
 chat = global.db.data.chats[m.chat]
