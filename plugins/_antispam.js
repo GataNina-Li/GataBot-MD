c844648965d499dd17e8ae9637f0145e861d4522
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-16 20:12:19 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -55,7 +55,7 @@ handler.before = async function (m, { conn }) {
     const sender = m.sender
     const currentTime = new Date().getTime()
     const timeWindow = 3000  
-    const messageLimit = 5  
+    const messageLimit = 3  
 
     if (!(sender in userSpamData)) {
         userSpamData[sender] = {
