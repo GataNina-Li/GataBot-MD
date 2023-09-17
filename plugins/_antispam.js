9aeb250b91906e7b582fa517721bbc84ec35179c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-16 19:59:10 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -52,34 +52,42 @@ const userSpamData = {}
 let handler = m => m
 handler.before = async function (m, { conn }) {
 
-const sender = m.sender
-const currentTime = new Date().getTime()
-const timeWindow = 5000 
-const messageLimit = 5 
+    const sender = m.sender
+    const currentTime = new Date().getTime()
+    const timeWindow = 5000  
+    const messageLimit = 5  
 
-if (!(sender in userSpamData)) {
-  
-userSpamData[sender] = {
-lastMessageTime: currentTime,
-messageCount: 1,
+    if (!(sender in userSpamData)) {
+        userSpamData[sender] = {
+            lastMessageTime: currentTime,
+            messageCount: 1,
+        }
+    } else {
+        const userData = userSpamData[sender]
+        const timeDifference = currentTime - userData.lastMessageTime
+
+        if (timeDifference <= timeWindow) {
+            userData.messageCount += 1
+
+            if (userData.messageCount >= messageLimit) {
+                
+                const mention = `@${sender.split("@")[0]}`
+                const warningMessage = `Baneado ${mention} por enviar spam.`
+                conn.reply(m.chat, warningMessage, m);
+
+                
+
+                
+                userData.messageCount = 1;
+            }
+        } else {
+            
+            if (timeDifference >= 2000) {
+                userData.messageCount = 1;
+            }
+        }
+userData.lastMessageTime = currentTime;
 }
-  
-} else {
-const userData = userSpamData[sender]
-const timeDifference = currentTime - userData.lastMessageTime
-
-if (timeDifference <= timeWindow) {
-userData.messageCount += 1
-
-if (userData.messageCount >= messageLimit) {
-const mention = `@${sender.split("@")[0]}`
-const warningMessage = `Baneado ${mention} por enviar spam.`
-conn.reply(m.chat, warningMessage, m)
-
-}} else {
-userData.messageCount = 1
 }
-userData.lastMessageTime = currentTime
-}}
 
 export default handler
