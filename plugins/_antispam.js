ba671e92492e933722bd4ac3be4764122bfebd98
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-16 21:12:01 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -48,7 +48,7 @@ handler.before = async function (m, { conn, isAdmin }) {
 
 export default handler*/
 
-const userSpamData = {}
+/*const userSpamData = {}
 let handler = m => m
 handler.before = async function (m, { conn }) {
 
@@ -61,6 +61,7 @@ handler.before = async function (m, { conn }) {
         userSpamData[sender] = {
             lastMessageTime: currentTime,
             messageCount: 1,
+            
         }
     } else {
         const userData = userSpamData[sender]
@@ -89,5 +90,77 @@ handler.before = async function (m, { conn }) {
 userData.lastMessageTime = currentTime;
 }
 }
+
+export default handler*/
+
+
+const userSpamData = {}
+let handler = m => m
+handler.before = async function (m, { conn }) {
+
+const sender = m.sender
+const currentTime = new Date().getTime()
+const timeWindow = 4000  
+const messageLimit = 4  
+
+if (!(sender in userSpamData)) {
+userSpamData[sender] = {
+lastMessageTime: currentTime,
+messageCount: 1,
+antiBan: 0, 
+}
+  
+} else {
+const userData = userSpamData[sender]
+const timeDifference = currentTime - userData.lastMessageTime
+
+if (userData.antiBan === 1) {
+conn.reply(m.chat, `@${sender.split("@")[0]} No puede hacer nada por 30 segundos`, m)
+} else if (userData.antiBan === 2) {
+conn.reply(m.chat, `@${sender.split("@")[0]} No puede hacer nada por 1 minuto`, m)
+} else if (userData.antiBan === 3) {
+conn.reply(m.chat, `@${sender.split("@")[0]} No puede hacer nada por 2 minutos`, m)
+}
+
+if (timeDifference <= timeWindow) {
+userData.messageCount += 1
+
+if (userData.messageCount >= messageLimit) {
+const mention = `@${sender.split("@")[0]}`
+const warningMessage = `Baneado ${mention} por enviar spam.`
+conn.reply(m.chat, warningMessage, m)
+  
+user.banned = true
+userData.antiBan += 1
+userData.messageCount = 1
+
+}} else {
+if (timeDifference >= 2000) {
+userData.messageCount = 1
+}}
+userData.lastMessageTime = currentTime;
+      
+if (userData.antiBan === 1) {
+setTimeout(() => {
+if (userData.antiBan === 1) {
+userData.antiBan = 1
+user.banned = false
+}}, 30000)
+  
+} else if (userData.antiBan === 2) {
+setTimeout(() => {
+if (userData.antiBan === 2) {
+userData.antiBan = 2
+user.banned = false
+}}, 60000)
+  
+} else if (userData.antiBan === 3) {
+setTimeout(() => {
+if (userData.antiBan === 3) {
+userData.antiBan = 0
+user.banned = false
+}}, 120000)
+  
+}}}
 
 export default handler
