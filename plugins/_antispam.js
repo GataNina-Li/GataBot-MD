87f0d8ebbb0305583fb1b44c49a6ba290057ffbd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-09-16 19:00:41 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,67 +1,49 @@
-//CRÉDITOS: https://github.com/Abiguelreyes75
-
-//let handler = m => m
-//handler.all = async function (m) {
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin} ) {
-if (m.isGroup && isAdmin) return null
-  
-let chat = global.db.data.chats[m.chat]
-let delet = m.key.participant
-let bang = m.key.id
-let bot = global.db.data.settings[this.user.jid] || {}
-let user = global.db.data.users[m.sender]
 
-if (bot.antiSpam) {
-this.spam = this.spam ? this.spam : {}
-if (!(m.sender in this.spam)) {
-let spaming = {
-jid: await m.sender, 
-spam: 0,
-lastspam: 0
+handler.before = async function (m, { conn, isAdmin }) {
+  if (m.isGroup && isAdmin) return null
+
+  let chat = global.db.data.chats[m.chat]
+  let delet = m.key.participant
+  let bang = m.key.id
+  let bot = global.db.data.settings[this.user.jid] || {}
+  let user = global.db.data.users[m.sender]
+
+  if (bot.antiSpam) {
+    this.spam = this.spam ? this.spam : {}
+    if (!(m.sender in this.spam)) {
+      let spaming = {
+        jid: await m.sender,
+        spam: 0,
+        lastspam: 0
+      }
+      this.spam[spaming.jid] = spaming
+    } else try {
+      this.spam[m.sender].spam += 1
+
+      if (new Date() - this.spam[m.sender].lastspam > 2000) {
+        this.spam[m.sender].spam = 0
+        this.spam[m.sender].lastspam = new Date()
+
+        let tiempo = 2000
+        let limiteMensajes = 5
+
+        if (this.spam[m.sender].spam > limiteMensajes) {
+          let texto = `*@${m.sender.split("@")[0]} No hagas spam, por favor.*`
+          
+          if (new Date() - user.antispam < tiempo) return
+          await conn.reply(m.chat, texto, m, { mentions: this.parseMention(texto) })
+          //user.banned = true
+          
+          await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } })
+          user.antispam = new Date()
+        }
+      }
+    } catch (e) {
+      console.log(e)
+      m.reply(lenguajeGB.smsMalError())
+    }
+  }
 }
-this.spam[spaming.jid] = spaming
-  
-} else try {
-this.spam[m.sender].spam += 1
-  
-if (new Date - this.spam[m.sender].lastspam > 4000) {
-if (this.spam[m.sender].spam > 6) {
-this.spam[m.sender].spam = 0
 
-this.spam[m.sender].lastspam = new Date * 1
-let tiempo = 60000 * 1
-let time = user.antispam + tiempo * 1
-let texto = `*@${m.sender.split("@")[0]} ${lenguajeGB['smsNoSpam']()}*` 
-
-if (new Date - user.antispam < tiempo * 1) return
-await conn.reply(m.chat, texto,  m, { mentions: this.parseMention(texto) })
-user.banned = true
-  
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-user.antispam = new Date * 1  
-  
-} else {
-this.spam[m.sender].spam = 0
-this.spam[m.sender].lastspam = new Date * 1
-}}
-  
-} catch (e) {
-console.log(e)
-m.reply(lenguajeGB.smsMalError())
-}}}
 export default handler
-
-function msToTime(duration) {
-var milliseconds = parseInt((duration % 1000) / 100),
-seconds = Math.floor((duration / 1000) % 60),
-minutes = Math.floor((duration / (1000 * 60)) % 60),
-hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-
-hours = (hours < 10) ? "0" + hours : hours
-minutes = (minutes < 10) ? "0" + minutes : minutes
-seconds = (seconds < 10) ? "0" + seconds : seconds
-
-return minutes + " m y " + seconds + " s " 
-}
- 
