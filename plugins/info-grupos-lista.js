40100080de691ae3c9a9ab5d4fd806da572bce22
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-09-21 07:52:21 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -1,24 +1,53 @@
-import PhoneNumber from 'awesome-phonenumber'
-let handler = async (m, { conn, __dirname, isRowner, isOwner, isBotAdmin, usedPrefix, groupMetadata, groups, _package, participants }) => { 
+const handler = async (m, { conn }) => {
+  let txt = '';
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
-}
-
-let txt
-const chats = Object.entries(conn.chats).filter(([jid, data]) => jid && data.isChats)
-groups = Object.values(await conn.groupFetchAllParticipating())
-txt = `${packname} ${lenguajeGB.smsLisA()}
-${lenguajeGB.smsLisB()} ${groups.length}\n`
-
+} 
+try {    
+const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats);
+const totalGroups = groups.length;
 for (let i = 0; i < groups.length; i++) {
-txt += ` 
-${lenguajeGB.smsLisC()} ${groups[i].subject}
-${lenguajeGB.smsLisD()} ${groups[i].id}
-${isOwner ? `${lenguajeGB.smsLisE()} ${groups[i].participants.length}` : ''}\n\n`
+const [jid, chat] = groups[i];
+const groupMetadata = ((conn.chats[jid] || {}).metadata || (await conn.groupMetadata(jid).catch((_) => null))) || {};
+const participants = groupMetadata.participants || [];
+const bot = participants.find((u) => conn.decodeJid(u.id) === conn.user.jid) || {};
+const isBotAdmin = bot?.admin || false;
+const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
+const participantStatus = isParticipant ? '✅ *SIGO AQUÍ | YES*' : '❌ *SIN ESTAR AQUÍ | NO*';
+const totalParticipants = participants.length;
+txt += `🐈 *${i + 1}*
+${lenguajeGB.smsLisC()} ${await conn.getName(jid)} ${participantStatus}
+${lenguajeGB.smsLisD()} ${jid}
+${lenguajeGB.smsLisF()} ${isBotAdmin ? '✅' : '❌'}
+${lenguajeGB.smsLisE()}  ${totalParticipants}
+${lenguajeGB.smsLisG()} ${isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(jid) || 'Error'}` : 'NO SOY ADMIN'}\n\n`;
 }
-m.reply(txt.trim())
+m.reply(`${packname} ${lenguajeGB.smsLisA()}
+${lenguajeGB.smsLisB()} ${totalGroups}\n\n${txt}`.trim());
+} catch {
+const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats);
+const totalGroups = groups.length;
+for (let i = 0; i < groups.length; i++) {
+const [jid, chat] = groups[i];
+const groupMetadata = ((conn.chats[jid] || {}).metadata || (await conn.groupMetadata(jid).catch((_) => null))) || {};
+const participants = groupMetadata.participants || [];
+const bot = participants.find((u) => conn.decodeJid(u.id) === conn.user.jid) || {};
+const isBotAdmin = bot?.admin || false;
+const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
+const participantStatus = isParticipant ? '✅ *SIGO AQUÍ | YES*' : '❌ *SIN ESTAR AQUÍ | NO*';
+const totalParticipants = participants.length;    
+    txt += `🐈 ${i + 1}
+${lenguajeGB.smsLisC()} ${await conn.getName(jid)} ${participantStatus}
+${lenguajeGB.smsLisD()} ${jid}
+${lenguajeGB.smsLisF()} ${isBotAdmin ? '✅' : '❌'}
+${lenguajeGB.smsLisE()} ${totalParticipants}
+${lenguajeGB.smsLisG()} ${isBotAdmin ? 'Error' : 'NO SOY ADMINS'}\n\n`
 }
+m.reply(`${packname} ${lenguajeGB.smsLisA()}
+${lenguajeGB.smsLisB()} ${totalGroups}\n\n${txt}`.trim());
+}}
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
 handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos|listadegrupos|grupolista|listagrupo)$/i
-handler.exp = 30
+handler.exp = 5
+handler.register = true
 export default handler
