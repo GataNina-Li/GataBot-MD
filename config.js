b9f7308ed3ad461729dfe4cac6e63ba8bb849cfa
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-09-25 18:09:37 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -113,7 +113,7 @@ global.author = "𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨";
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.vs = "1.4.9";
-global.vsJB = "2.0";
+global.vsJB = "2.5";
 
 global.gt = "𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿";
 global.yt = "https://youtube.com/@gatadios";
