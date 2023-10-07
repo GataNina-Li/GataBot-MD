5375dbf2480131e3575aef0ac3a8cda9bdc9f080
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-07 16:35:15 -0500

    Update fun-doxeo_falso.js

--- plugins/fun-doxeo_falso.js
+++ plugins/fun-doxeo_falso.js
@@ -99,136 +99,87 @@ seconds = (seconds < 10) ? "0" + seconds : seconds
 return minutes + " m y " + seconds + " s " 
 }  */
 
-import {performance} from 'perf_hooks';
-const handler = async (m, {conn, text}) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
-let user = global.db.data.users[m.sender];
-let time = user.prue + 90000; //1 min
-if (new Date - user.prue < 90000) return await conn.reply(m.chat, `🙌 HEY ALTO ESPERA UNOS MINUTOS PARA USAR OTRO COMANDO NO HAGA SPAM`, fkontak, m);  
-const start = performance.now();    
-const end = performance.now();
-const executionTime = (end - start);
-    
-const ipParts = [];
-for (let i = 0; i < 4; i++) {
-ipParts.push(Math.floor(Math.random() * 256))};
-const ipAddress = ipParts.join('.');
-const fakeData = {
-name_tag: '',
-ip: `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`,
-fakeCameraLink: `http://${ipAddress}.com/camera-feed`,    
-n: Math.floor(Math.random() * 100000),
-w: (Math.random() * (20 - 10) + 10).toFixed(4),
-ssNumber: Math.floor(Math.random() * 10000000000000000),
-ipv6: `fe80:${(Math.random() * 65535).toString(16)}:${(Math.random() * 65535).toString(16)}:${(Math.random() * 65535).toString(16)}:${(Math.random() * 65535).toString(16)}%${Math.floor(Math.random() * 100)}`,
-upnp: getRandomValue(['Enabled', 'Disabled']),
-dmz: `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`,
-mac: `${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}`,
-isp: getRandomValue(['Ucom universal', 'ISP Co', 'Internet Solutions Inc']),
-dns: `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`,
-altDns: `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`,
-dnsSuffix: getRandomValue(['Dlink', 'DNS', 'ISPsuffix']),
-wan: `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`,
-wanType: getRandomValue(['private nat', 'public nat', 'Dynamic IP']),
-gateway: `192.${Math.floor(Math.random() * 256)}.0.1`,
-subnetMask: `255.255.${Math.floor(Math.random() * 256)}.0`,
-udpOpenPorts: `${Math.floor(Math.random() * 10000)}.${Math.floor(Math.random() * 10000)}`,
-tcpOpenPorts: `${Math.floor(Math.random() * 10000)}`,
-routerVendor: getRandomValue(['ERICCSON', 'TPLINK', 'Cisco']),
-deviceVendor: getRandomValue(['WIN32-X', 'Device Co', 'SecureTech']),
-connectionType: getRandomValue(['TPLINK COMPANY', 'ISP Connect', 'Home Network']),
-icmphops: `192.${Math.floor(Math.random() * 256)}.0.1 192.${Math.floor(Math.random() * 256)}.1.1 100.${Math.floor(Math.random() * 256)}.43.4`,
-http: `192.168.${Math.floor(Math.random() * 256)}.1:433-->92.28.211.234:80`,
-http2: `192.168.${Math.floor(Math.random() * 256)}.625-->92.28.211.455:80`,
-http3: `192.168.${Math.floor(Math.random() * 256)}.817-->92.28.211.8:971`,
-udp: `192.168.${Math.floor(Math.random() * 256)}.452-->92.28.211.7265288`,
-tcp1: `192.168.${Math.floor(Math.random() * 256)}.682-->92.28.211.62227.7`,
-tcp2: `192.168.${Math.floor(Math.random() * 256)}.725-->92.28.211.67wu2`,
-tcp3: `192.168.${Math.floor(Math.random() * 256)}.629-->92.28.211.167:8615`,
-externalMac: `${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}:${Math.floor(Math.random() * 256).toString(16).toUpperCase()}`,
-modemJumps: Math.floor(Math.random() * 100);
-};
-if (m.mentionedJid[0]) {
-fakeData.name_tag = text //`@${m.mentionedJid[0].split('@')[0]}` //conn.getName(m.mentionedJid[0])
-} else {
-fakeData.name_tag = text  
-}
-    
-let start = `*😱 ¡¡Empezando Doxxeo!! 😱*`
+import { performance } from 'perf_hooks'
+
+var handler = async (m, { conn, text }) => {
+
+if (!text) throw '*⚠️ INGRESA EL @tag DE ALGUN USUARIO*'
+let who
+if (m.isGroup) who = m.mentionedJid[0]
+else who = m.chat
+if (!who) throw '*⚠️ INGRESA EL @tag DE ALGUN USUARIO*'
+let start = `*⚠️ EMPEZANDO DOXEO ⚠️*`
+let ala = `😨`
 let boost = `*${pickRandom(['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])}%*`
 let boost2 = `*${pickRandom(['21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}%*`
 let boost3 = `*${pickRandom(['41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60'])}%*`
 let boost4 = `*${pickRandom(['61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80'])}%*`
 let boost5 = `*${pickRandom(['81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100'])}%*`
-const { key } = await conn.sendMessage(m.chat, {text: start}, {quoted: m});
-await delay(1000 * 1);
-await conn.sendMessage(m.chat, {text: boost, edit: key});
-await delay(1000 * 1);
-await conn.sendMessage(m.chat, {text: boost3, edit: key});
-await delay(1000 * 1);
-await conn.sendMessage(m.chat, {text: boost5, edit: key});
-    
-const doxeo = `*_✅ Persona doxxeada con éxito_*\n\n*_Tiempo: ${executionTime} segundos!_*
 
-*RESULTADOS:*
+const { key } = await conn.sendMessage(m.chat, {text: `${start}`}, {quoted: m})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `${boost2}`, edit: key})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `${boost3}`, edit: key})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `${boost4}`, edit: key})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `${boost5}`, edit: key})
 
-*Nombre:* ${fakeData.name_tag}
-*Ip:* ${fakeData.ip}
-*N:* ${fakeData.n}
-*W:* ${fakeData.w}
-*SS NUMBER:* ${fakeData.ssNumber}
-*CAMARA DEL CELULAR:* ${fakeData.fakeCameraLink}
-*IPV6:* ${fakeData.ipv6}
-*UPNP:* ${fakeData.upnp}
-*DMZ:* ${fakeData.dmz}
-*MAC:* ${fakeData.mac}
-*ISP:* ${fakeData.isp}
-*DNS:* ${fakeData.dns}
-*ALT DNS:* ${fakeData.altDns}
-*DNS SUFFIX:* ${fakeData.dnsSuffix}
-*WAN:* ${fakeData.wan}
-*WAN TYPE:* ${fakeData.wanType}
-*GATEWAY:* ${fakeData.gateway}
-*SUBNET MASK:* ${fakeData.subnetMask}
-*UDP OPEN PORTS:* ${fakeData.udpOpenPorts}
-*TCP OPEN PORTS:* ${fakeData.tcpOpenPorts}
-*ROUTER VENDEDOR:* ${fakeData.routerVendor}
-*DEVICE VENDEDOR:* ${fakeData.deviceVendor}
-*CONNECTION TYPE:* ${fakeData.connectionType}
-*ICMPHOPS:* ${fakeData.icmphops}
-*HTTP:* ${fakeData.http}
-*Http:* ${fakeData.http2}
-*Http:* ${fakeData.http3}
-*Upd:* ${fakeData.udp}
-*Tcp:* ${fakeData.tcp1}
-*Tcp:* ${fakeData.tcp2}
-*Tcp:* ${fakeData.tcp3}
-*EXTERNAL MAC:* ${fakeData.externalMac}
-*MODEM JUMPS:* ${fakeData.modemJumps}`;
-await conn.sendMessage(m.chat, {text: doxeo, edit: key, mentions: conn.parseMention(doxeo)}, {quoted: m})
-user.prue = new Date * 1  
+let old = performance.now()
+let neww = performance.now()
+let speed = `${neww - old}`
+let doxeo = `*‼️ PERSONA DOXEADA CON EXITO ‼️*
+📅 ${fecha}
+⏰ ${tiempo}
+📢 Resultados:
+*Nombre:* ${text}
+*Ip:* 92.28.211.234
+*N:* 43 7462
+*W:* 12.4893
+*SS NUMBER:* 6979191519182016
+*IPV6:* fe80::5dcd::ef69::fb22::d9888%12 
+*UPNP:* Enabled
+*DMZ:* 10.112.42.15
+*MAC:* 5A:78:3E:7E:00
+*ISP:* Ucom unversal 
+*DNS:* 8.8.8.8
+*ALT DNS:* 1.1.1.8.1  
+*DNS SUFFIX:* Dlink
+*WAN:* 100.23.10.15
+*WAN TYPE:* private nat
+*GATEWAY:* 192.168.0.1
+*SUBNET MASK:* 255.255.0.255
+*UDP OPEN PORTS:* 8080.80
+*TCP OPEN PORTS:* 443
+*ROUTER VENDEDOR:* ERICCSON
+*DEVICE VENDEDOR:* WIN32-X
+*CONNECTION TYPE:* TPLINK COMPANY
+*ICMPHOPS:* 192.168.0.1 192.168.1.1 100.73.43.4
+host-132.12.32.167.ucom.com
+host-132.12.111.ucom.com
+36.134.67.189 216.239.78.11
+Sof02s32inf14.1e100.net
+*HTTP:* 192.168.3.1:433-->92.28.211.234:80
+*Http:* 192.168.625-->92.28.211.455:80
+*Http:* 192.168.817-->92.28.211.8:971
+*Upd:* 192.168452-->92.28.211:7265288
+*Tcp:* 192.168.682-->92.28.211:62227.7
+*Tcp:* 192.168.725-->92.28.211:67wu2
+*Tcp:* 192.168.629-->92.28.211.167:8615
+*EXTERNAL MAC:* 6U:77:89:ER:O4
+*MODEM JUMPS:* 64`
+m.reply(doxeo, null, { mentions: conn.parseMention(doxeo) })
+
 }
-handler.help = ['doxear <nombre> | <@tag>'];
-handler.tags = ['fun'];
-handler.command = /^Doxxeo|doxxeo|doxxear|Doxxear|doxeo|doxear|doxxeame|doxeame/i;
-export default handler;
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+handler.help = ['doxear']
+handler.tags = ['juegos']
+handler.command = /^Doxxeo|doxxeo|doxxear|Doxxear|doxeo|doxear|doxxeame|doxeame/i
+handler.group = true
+
+export default handler
 
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]}
-function getRandomValue(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
 
-function msToTime(duration) {
-var milliseconds = parseInt((duration % 1000) / 100),
-seconds = Math.floor((duration / 1000) % 60),
-minutes = Math.floor((duration / (1000 * 60)) % 60),
-hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-hours = (hours < 10) ? "0" + hours : hours
-minutes = (minutes < 10) ? "0" + minutes : minutes
-seconds = (seconds < 10) ? "0" + seconds : seconds
-
-return minutes + " m y " + seconds + " s " 
-}  
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
