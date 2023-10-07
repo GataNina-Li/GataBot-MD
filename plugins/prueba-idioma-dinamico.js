f1f5ee8a90ab24448c901f95abbfd28a7dbe0696
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-06 20:39:35 -0500

    Update prueba-idioma-dinamico.js

--- plugins/prueba-idioma-dinamico.js
+++ plugins/prueba-idioma-dinamico.js
@@ -1,6 +1,7 @@
 const handler = async (m, {text, command, args, usedPrefix}) => {
 let language = 'en'
-m.reply(`${lenguajeGB['smsAvisoAG']()}`, language)
+//m.reply(`${lenguajeGB['smsAvisoAG']()}`, language)
+m.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}`, language, null)
 } 
 handler.command = /^(prueba39)$/i
 export default handler
