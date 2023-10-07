13a98da0ea52259c06987bcab1315ee1787f5112
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-10-07 13:29:59 -0600

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- index.js
+++ index.js
@@ -29,42 +29,42 @@ var isRunning = false;
 * @param {String} file `path/to/file`
 */
 function start(file) {
-if (isRunning) return
+if (isRunning) return;
 isRunning = true;
-let args = [join(__dirname, file), ...process.argv.slice(2)]
+let args = [join(__dirname, file), ...process.argv.slice(2)];
   
 setupMaster({
 exec: args[0],
 args: args.slice(1),
-})
-let p = fork()
+});
+let p = fork();
 p.on('message', data => {
 //console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
 switch (data) {
 case 'reset':
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-break
+p.process.kill();
+isRunning = false;
+start.apply(this, arguments);
+break;
 case 'uptime':
-p.send(process.uptime())
-break
-}})
+p.send(process.uptime());
+break;
+}});
 p.on('exit', (_, code) => {
-isRunning = false
-console.error('⚠️ ERROR ⚠️', code)
-if (code === 0) return
+isRunning = false;
+console.error('⚠️ ERROR ⚠️', code);
+if (code === 0) return;
 watchFile(args[0], () => {
-unwatchFile(args[0])
-start(file)
-})
-})
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test'])
+unwatchFile(args[0]);
+start(file);
+});
+});
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+if (!opts['test']);
 if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim())
-})
+p.emit('message', line.trim());
+});
 //console.log(p)
 }
 
-start('main.js')
+start('main.js');
