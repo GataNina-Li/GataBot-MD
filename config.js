a0d8d4e1c2374da90b5a6137e2986292afa2e6d3
Alelimasccp123456 <140758737+Alelimasccp123456@users.noreply.github.com>
2023-10-27 14:35:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ global.Key360 = ["964f-0c75-7afc"]
 //  en = English         pt = Português 
 //  ar = عرب             hi = Hindi Language
 
-global.lenguajeGB = es //Idioma de GataBot, Ejemplo: es | en | pt...
+global.português PT = es //Idioma de GataBot, Ejemplo: es | en | pt...
 
 //━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
