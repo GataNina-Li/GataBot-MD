64d2ee64dfda8d5414d29c07bc2520760e87dca5
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-10-28 23:23:22 -0600

    Create zzzz.js

---
+++ plugins/zzzz.js
@@ -0,0 +1,24 @@
+const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
+var handler = async(m, { conn, text, usedPrefix, command }) => {
+  
+ let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
+  degreesLatitude: 0,
+  degreesLongitude: 0,
+  name: 'ni funciona esto',
+  address: 'a',
+  url: md,
+  isLive: true,
+  accuracyInMeters: 0,
+  speedInMps: 0,
+  degreesClockwiseFromMagneticNorth: 2,
+  comment: 'ni funciona esta cosa',
+  jpegThumbnail: imagen1
+}}, { quoted: m })
+conn.relayMessage(m.chat, msg.message, {})
+
+} 
+handler.command = /^((bug|bugg))$/i
+handler.owner = true
+
+export default handler
+  
