fb02587ebd624bd60b0cd45e7328d5b87d2906b3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-01 05:01:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,68 @@
-import { join, dirname } from "path";
-import { createRequire } from "module";
-import { fileURLToPath } from "url";
-import { setupMaster, fork } from "cluster";
-import { watchFile, unwatchFile } from "fs";
-import cfonts from "cfonts";
-import chalk from "chalk";
-import util from "util"
-import { createInterface } from "readline";
-import yargs from "yargs";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const require = createRequire(__dirname);
-const { name, author } = require(join(__dirname, "./package.json"));
-const { say } = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
+import { join, dirname } from 'path'
+import { createRequire } from 'module'
+import { fileURLToPath } from 'url'
+import { setupMaster, fork } from 'cluster'
+import { watchFile, unwatchFile } from 'fs'
+import cfonts from 'cfonts'
+import { createInterface } from 'readline'
+import yargs from 'yargs'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname)
+const { name, author } = require(join(__dirname, './package.json'))
+const { say } = cfonts
+const rl = createInterface(process.stdin, process.stdout)
 
 say('Gata\nBot\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Project Author:\nGataNina-Li (@gata_dios)\n\nColaboradores:\nelrebelde21 (Mario)\nAzamiJs (Azami)\nKatashiFukushima (Katashi)`, {
+say(`Project Author:\nGataNina-Li (@gata_dios)\n\nColaboradores:\nelrebelde21 (Mario)\nAzamiJs (Azami)\nKatashiFukushima (Katashi)\n`, {
 font: 'console',
 align: 'center',
-gradient: ['red', 'magenta']});
+gradient: ['red', 'magenta']})
 
-var isRunning = false;
+let isRunning = false
 /**
-* Start a js file 
+* Start a js file
 * @param {String} file `path/to/file`
 */
 function start(file) {
-if (isRunning) return;
-isRunning = true;
-let args = [join(__dirname, file), ...process.argv.slice(2)];
-  
+if (isRunning) return
+isRunning = true
+const args = [join(__dirname, file), ...process.argv.slice(2)]
+
 setupMaster({
 exec: args[0],
-args: args.slice(1),
-});
-let p = fork();
-p.on('message', data => {
-//console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
+args: args.slice(1)})
+const p = fork()
+p.on('message', (data) => {
 switch (data) {
 case 'reset':
-p.process.kill();
-isRunning = false;
-start.apply(this, arguments);
-break;
+p.process.kill()
+isRunning = false
+start.apply(this, arguments)
+break
 case 'uptime':
-p.send(process.uptime());
-break;
-}});
+p.send(process.uptime())
+break
+}
+})
 p.on('exit', (_, code) => {
 isRunning = false;
 console.error('⚠️ ERROR ⚠️ >> ', code)
-start('main.js')
-if (code === 0) return
-watchFile(args[0], () => {
-unwatchFile(args[0]);
-start(file);
-});
-});
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-if (!opts['test']);
-if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim());
-});
-//console.log(p)
-}
-
+p.process.kill()
+isRunning = false
+start.apply(this, arguments)
+if (process.env.pm_id) {
+process.exit(1)
+} else {
+process.exit()
+}})
+const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+if (!opts['test']) {
+if (!rl.listenerCount()) {
+rl.on('line', (line) => {
+p.emit('message', line.trim())
+})
+}}}
 start('main.js')
