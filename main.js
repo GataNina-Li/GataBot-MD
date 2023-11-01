4071090f8c56f8f6900b007f28aa5fd9b7039878
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-01 18:01:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 const question = (texto) => new Promise((resolver) => rl.question(texto, resolver))
 
 let opcion
-if (!fs.existsSync(`./${authFile}/creds.json`) && !methodCodeQR) {
+if (!fs.existsSync(`./${authFile}/creds.json`) && !methodCodeQR && !methodCode) {
 while (true) {
 opcion = await question('Seleccione una opción:\n1. Con código QR\n2. Con código de texto de 8 dígitos\n--> ')
 if (opcion === '1' || opcion === '2') {
