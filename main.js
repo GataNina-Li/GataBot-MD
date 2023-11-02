60e1a22f89d1a495b70dca2ef2c985f1ee258fac
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-02 03:05:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -124,8 +124,7 @@ opcion = await question('Seleccione una opción:\n1. Con código QR\n2. Con cód
 } else if (opcion === '1' || opcion === '2') {
 opcion = await question('Mal, Seleccione una opción:\n1. Con código QR\n2. Con código de texto de 8 dígitos\n--> ')
 //console.log('Por favor, seleccione solo 1 o 2.')
-}
-rl.close()
+rl.close() 
 }
 
 const connectionOptions = {
