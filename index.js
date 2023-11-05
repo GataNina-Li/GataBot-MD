88ceca63265b98078729ee3394260c5389389c6e
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-11-05 09:13:26 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const require = createRequire(__dirname)
 const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
-
+ 
 say('Gata\nBot\nMD', {
 font: 'chrome',
 align: 'center',
