4f6e89779aa1d2232affe1062cfc5343f564df55
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-05 02:47:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -135,6 +135,14 @@ opcion = await question(`╭${lineM}
 ┊ ${chalk.blueBright('┊')} ${chalk.italic.magenta('Escriba sólo el número de')}
 ┊ ${chalk.blueBright('┊')} ${chalk.italic.magenta('la opción para conectarse.')}
 ┊ ${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
+┊ ${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}    
+┊ ${chalk.blueBright('┊')} ${chalk.white.bgWhite.bold.green('CONSEJO:')}
+┊ ${chalk.blueBright('┊')} ${chalk.italic.cyan('Si usa Termux, Replit, Linux, o Windows')}
+┊ ${chalk.blueBright('┊')} ${chalk.italic.cyan('Use estos comandos para una ejecución directa:')}
+┊ ${chalk.blueBright('┊')} ${chalk.italic.yellow(`npm run qr ${chalk.italic.magenta('(Inicia con código QR)')}`)}
+┊ ${chalk.blueBright('┊')} ${chalk.italic.yellow(`npm run code ${chalk.italic.magenta('(Inicia con código de 8 dígitos)')}`)}
+┊ ${chalk.blueBright('┊')} ${chalk.italic.yellow(`npm start ${chalk.italic.magenta('(Inicio predeterminado con opciones)')}`)}
+┊ ${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
 ╰${lineM}\n${chalk.bold.magentaBright('---> ')}`)
 if (fs.existsSync(`./${authFile}/creds.json`)) {
 console.log(chalk.bold.redBright(`PRIMERO BORRE EL ARCHIVO ${chalk.bold.greenBright("creds.json")} QUE SE ENCUENTRA EN LA CARPETA ${chalk.bold.greenBright(authFile)} Y REINICIE.`))
