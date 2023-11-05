69071d941b555dd2aba804655f3b841f47e59b5a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-05 03:01:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ global.owner = [
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = "" //Ejemplo: +59309090909
+global.botNumberCode = "099" //Ejemplo: +59309090909
 global.confirmCode = ""
 
 global.mods = []
