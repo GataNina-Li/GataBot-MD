b0251288913db51effb939deb1096a8ff6a6c0ae
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-11-08 10:08:17 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -12,12 +12,12 @@ let response = `${eg}
 ┃💪 𝙋𝙀𝙎𝙊 |  𝙎𝙄𝙕𝙀: ${data5.size}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃ 🚀 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝘿𝙊 𝘼𝙋𝙆𝙎 🚀`
-conn.reply(m.chat, response, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+conn.sendMessage(m.chat, {caption: response, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
 body: 'Super Bot WhatsApp',         
-previewType: 0, thumbnail: data5.icon, 
-sourceUrl: md}}})
+previewType: 0, 
+thumbnailUrl: data5.icon, 
+sourceUrl: md}}}})
 //  await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
 return await conn.sendMessage(m.chat, {text: '*EL APK ES MUY PESADO.*'}, {quoted: m})}
