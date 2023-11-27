55fba70149246fa34c8316665b18ccaad082f5dc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-27 11:18:25 -0500

    Update prueba20.js

--- plugins/prueba20.js
+++ plugins/prueba20.js
@@ -2,10 +2,21 @@ import fs from 'fs'
 import yaoiImages from 'module-gatadios'
 
 let handler = async (m, { conn }) => {
-//const resultJson = JSON.parse(yaoiImages.getRandomImage())
-const resultJson = yaoiImages.getRandomImage()
+const resultJson = JSON.parse(yaoiImages.getRandomImage())
+//const resultJson = yaoiImages.getRandomImage()
 //const jsonText = JSON.stringify(resultJson, null, 2)
-m.reply(resultJson)
+//m.reply(resultJson)
+
+let txt
+txt = `
+Nombre: ${resultJson.name}
+
+Autor: ${resultJson.author}
+
+Descripción: ${resultJson.description}`
+  
+conn.sendMessage(m.chat, {image: {url: resultJson.link}, caption: txt.trim()}, {quoted: m})
+  
 }
 
 handler.command = /^(prueba36)$/i
