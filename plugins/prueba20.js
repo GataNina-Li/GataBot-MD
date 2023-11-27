e06e03b402664a967c5a07907e16b01b7d3cd13f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-27 10:59:45 -0500

    Update prueba20.js

--- plugins/prueba20.js
+++ plugins/prueba20.js
@@ -3,8 +3,8 @@ import yaoiImages from 'module-gatadios'
 
 let handler = async (m, { conn }) => {
 const resultJson = yaoiImages.getRandomImage()
-const jsonText = JSON.stringify(resultJson, null, 2)
-m.reply(jsonText)
+//const jsonText = JSON.stringify(resultJson, null, 2)
+m.reply(resultJson)
 }
 
 handler.command = /^(prueba36)$/i
