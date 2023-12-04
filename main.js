5fb25cb6dcf415377c545fef2bb9e0cf79f3e868
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2023-12-03 22:16:34 -0600

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js'
 import { createRequire } from 'module'
-import path, {join} from 'path'
+import path, { join } from 'path'
 import {fileURLToPath, pathToFileURL} from 'url'
 import { platform } from 'process'
 import * as ws from 'ws'
