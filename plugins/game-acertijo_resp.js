ed63c88b67c5c232dea6b79e90234d061d8ae1d2
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2023-12-08 10:25:44 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -6,7 +6,7 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
     this.tekateki = this.tekateki ? this.tekateki : {}
-    if (!(id in this.tekateki)) return m.reply('Ese acertijo ya ha terminado!')
+    if (!(id in this.tekateki)) return m.reply('Ese juego ya ha terminado!')
     if (m.quoted.id == this.tekateki[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
