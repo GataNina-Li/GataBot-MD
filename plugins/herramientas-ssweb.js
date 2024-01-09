c2f36c319dd93d48cc8cde3209de28f640da6791
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-09 01:15:08 -0500

    Update herramientas-ssweb.js

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -3,11 +3,10 @@ const handler = async (m, {conn, text, args, usedPrefix, isPrems}) => {
 if (!args[0]) return conn.reply(m.chat, '*Por favor ingresa una url de la página a la que se le tomará captura 🔎*', m)  
 let user = global.db.data.users[m.sender]
 let calidad, webIMG 
-//calidad = user.premiumTime > 0 ? '3840x2160' : '1280x720'
 try {  
 if (!user.premiumTime) {
 calidad = '1280x720' //HD
-webIMG = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=500x500`
+webIMG = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=${calidad}`
 await conn.sendMessage(m.chat, { image: { url: webIMG }, caption: `🎟️ *PREMIUM:* ${user.premiumTime > 0 ? '✅' : '❌'}\n🪄 *CALIDAD:* \`\`\`(${calidad}) HD\`\`\`\n\n👑 _Para una imagen en *4K*, adquiera un pase usando *${usedPrefix}pase premium*_` }, { quoted: m }) 
 } else {
 calidad = '3840x2160' //4K
