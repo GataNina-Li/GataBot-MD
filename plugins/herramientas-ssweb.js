5711812afffc46695c289c917407f654cba4ec5b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-09 00:46:04 -0500

    Update herramientas-ssweb.js

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -2,15 +2,15 @@ import fetch from 'node-fetch'
 const handler = async (m, {conn, text, args, usedPrefix}) => {   
 if (!args[0]) return conn.reply(m.chat, '*Por favor ingresa una url de la página a la que se le tomará captura 🔎*', m)  
 let user = global.db.data.users[m.sender]
-let calidad, webIMG 
+let calidad, calidad2, webIMG 
 try {  
-if (user.premiumTime > 0) {
+if (user.premiumTime < 0) {
 calidad = '1280x720' //HD
 webIMG = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=${calidad}`
 await conn.sendMessage(m.chat, { image: { url: webIMG }, caption: `🎟️ *PREMIUM:* ${user.premiumTime > 0 ? '✅' : '❌'}\n🪄 *CALIDAD:* \`\`\`(${calidad}) HD\`\`\`\n\n👑 _Para una imagen en *4K*, adquiera un pase usando ${usedPrefix}pase premium_` }, { quoted: m }) 
 } else {
-calidad = '3840x2160' //4K
-webIMG = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=${calidad}`
+calidad2 = '3840x2160' //4K
+webIMG = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=${calidad2}`
 await conn.sendMessage(m.chat, { image: { url: webIMG }, caption: `🎟️ *PREMIUM:* ${user.premiumTime > 0 ? '✅' : '❌'}\n👑 *CALIDAD:* \`\`\`(${calidad}) 4K\`\`\`` }, { quoted: m }) 
 }     
 //conn.sendMessage(m.chat, { image: { url: webIMG }, caption: `Imagen HD.\n\nPara una imagen de mayor calidad adquiera un pase usando ${usedPrefix}pase premium` }, { quoted: m }) 
