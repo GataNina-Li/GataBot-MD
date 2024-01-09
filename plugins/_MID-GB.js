33cd604e50063680d275ed641c832156609d4e4e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-09 08:07:18 -0500

    Update _MID-GB.js

--- plugins/_MID-GB.js
+++ plugins/_MID-GB.js
@@ -1,8 +1,9 @@
 import { es, en } from '../lib/multi-language/_default.js'
-let MID_GB
+global.mid = null
+
 export async function before( m, { conn }) {
 let idioma  = global.db.data.users[m.sender].midLanguage
-
+let MID_GB
   
 if (idioma == "es") {
 MID_GB = es
