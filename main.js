8a8ccd6e803a1e493a05c72756d18d3608d74dbd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-09 07:46:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js'
+import './plugins/_MID-GB.js'
 import { createRequire } from 'module'
 import path, { join } from 'path'
 import {fileURLToPath, pathToFileURL} from 'url'
