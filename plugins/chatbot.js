970046e10820426b19bfb0a0bbdf1d3cbb8fe238
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-17 10:39:04 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -5,6 +5,7 @@ handler.all = async function (m) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let chat = global.db.data.chats[m.chat]
 let name = conn.getName(m.sender)
+if (chat.isBanned) return
 
 if (/^e$/i.test(m.text) ) { //sin prefijo 
     let teks = `
