959195d62cfd9538267a85141f9ada6eb485c2e2
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-20 19:22:48 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/propietario(a)-setprefix.js
+++ plugins/propietario(a)-setprefix.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 if (!text) throw `${lenguajeGB['smsAvisoMG']()} 𝙉𝙊 𝙎𝙀 𝙀𝙈𝘾𝙊𝙉𝙏𝙏𝙍𝙊 𝙉𝙄𝙉𝙂𝙐𝙉 𝙋𝙍𝙀𝙁𝙄𝙅𝙊, 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙋𝙍𝙀𝙁𝙄𝙅𝙊 𝙌𝙐𝙀 𝙌𝙐𝙄𝙀𝙍𝘼 𝙀𝙎𝙏𝘼𝘽𝙇𝙀𝘾𝙀𝙍, 𝙀𝙅: ${usedPrefix + command} #`;
 global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
-await m.reply(`${lenguajeGB['smsAvisoEG']()} *𝙀𝙇 𝙋𝙍𝙀𝙁𝙄𝙅𝙊 𝘼𝘾𝙏𝙐𝘼𝙇 𝘿𝙀𝙇 ${wm} 𝙎𝙀 𝙀𝙎𝙏𝘼𝘽𝙇𝙀𝘾𝙄𝙊 𝘼 : ${text}*`);
+await m.reply(`${lenguajeGB['smsAvisoEG']()} *𝙀𝙇 𝙋𝙍𝙀𝙁𝙄𝙅𝙊 𝘼𝘾𝙏𝙐𝘼𝙇 𝘿𝙀𝙇 ${gt} 𝙎𝙀 𝙀𝙎𝙏𝘼𝘽𝙇𝙀𝘾𝙄𝙊 𝘼 :* [ ${text} ]`);
 };
 handler.help = ['setprefix'].map((v) => v + ' [prefix]');
 handler.tags = ['owner'];
