1f65c85b095d426dffda8395f5185df4a0c3a632
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-22 22:57:18 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -213,6 +213,15 @@ throw false
 chat.stickers = isEnable          
 break
     
+case 'game': case 'juegos': case 'fun': case 'ruleta':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.game = isEnable          
+break
+    
 case 'temporal':
 isAll = true
 if (!isOwner) {
--- plugins/rpg-crime.js
+++ plugins/rpg-crime.js
@@ -1,35 +1,33 @@
 //CÓDIGO CREADO POR elrebelde21 : https://github.com/elrebelde21
 const handler = async (m, {conn, isPrems}) => {
-const date = global.db.data.users[m.sender].crime + 10800000; //10800000 = 3 hs
-if (new Date - global.db.data.users[m.sender].crime < 10800000) return m.reply(`*《🚓︎》LA POLICIA ESTA VIGILANDO EN ESTE MOMENTO, VUELVE EN:* ${msToTime(date - new Date())}`)
+const date = global.db.data.users[m.sender].crime + 10800000; //3600000 = 3 hs
+if (new Date - global.db.data.users[m.sender].crime < 10800000) return m.reply(`『🚓︎』𝙇𝘼 𝙋𝙊𝙇𝙄𝘾𝙄𝘼 𝙀𝙎𝙏𝘼 𝙑𝙄𝙂𝙄𝙇𝘼𝙉𝘿𝙊 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙈𝙊𝙈𝙀𝙉𝙏𝙊, 𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 : ${msToTime(date - new Date())}`)
 const exp = Math.floor(Math.random() * 10000)
 const diamond = Math.floor(Math.random() * 300)
 const money = Math.floor(Math.random() * 10000)
 let or = ['text', 'text2', 'text3', 'text4']; 
 let media = or[Math.floor(Math.random() * 4)]
+global.db.data.users[m.sender].crime = new Date * 1;
 if (media === 'text') return m.reply(`《💰》*${pickRandom(global.robar)}* ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
 if (media === 'text2') return m.reply(`《🚓》*${pickRandom(global.robmal)}* ${exp} XP`).catch(global.db.data.users[m.sender].exp -= exp) 
 if (media === 'text3') return m.reply(`《💰》*${pickRandom(global.robar)}*\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 🐈 𝐆𝐀𝐓𝐀𝐂𝐎𝐈𝐍𝐒`).catch(global.db.data.users[m.sender].limit += diamond).catch(global.db.data.users[m.sender].money += money)
 if (media === 'text4') return m.reply(`《🚓》*${pickRandom(global.robmal)}*\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 🐈 𝐆𝐀𝐓𝐀𝐂𝐎𝐈𝐍𝐒`).catch(global.db.data.users[m.sender].limit -= diamond).catch(global.db.data.users[m.sender].money -= money) 
-global.db.data.users[m.sender].crime = new Date * 1;
 }
 handler.help = ['robar'];
 handler.tags = ['xp'];
 handler.command = /^(crime|Crime)$/i
-handler.fail = null;
 handler.register = true
 export default handler;
 
 function msToTime(duration) {
-  const milliseconds = parseInt((duration % 1000) / 100);
-  let seconds = Math.floor((duration / 1000) % 60);
-  let minutes = Math.floor((duration / (1000 * 60)) % 60);
-  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-  hours = (hours < 10) ? '0' + hours : hours;
-  minutes = (minutes < 10) ? '0' + minutes : minutes;
-  seconds = (seconds < 10) ? '0' + seconds : seconds;
-  return minutes + ' minutos ' + seconds + ' segundos ';
-}
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+return hours + " Hora(s) " + minutes + " Minuto(s)"}
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())];
