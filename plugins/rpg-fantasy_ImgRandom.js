dfeb710c2f5393a5c3cd1133cc83e7341767d4b4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-23 20:30:11 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -7,14 +7,14 @@ const response = await fetch(url)
 const data = await response.json()
 
 data.imagenesReclamadas.forEach((imagen, index) => {
-let fakeIMG = { contextInfo: { externalAdReply: { title: conn.getName(m.sender), body: imagen.descripcion, sourceUrl: redesMenu, thumbnailUrl: imagen.urlImagen }}}
+//let fakeIMG = { contextInfo: { externalAdReply: { title: conn.getName(m.sender), body: imagen.descripcion, sourceUrl: redesMenu, thumbnailUrl: imagen.urlImagen }}}
 let info = `Nombre: ${imagen.nombre}
 Origen: ${imagen.descripcion}
 Costo: $${imagen.costo}
 Estado: Libre
 Clase: ${imagen.clase}
 Id de imagen: ${imagen.codigoImagen}`
-conn.sendMessage(m.chat, { image: { url: imagen.urlImagen }, caption: info }, { quoted: fakeIMG })  
+conn.sendMessage(m.chat, { image: { url: imagen.urlImagen }, caption: info }, { quoted: m })  
 })
 } catch (error) {
 console.error('Error al obtener o procesar los datos: ', error)
