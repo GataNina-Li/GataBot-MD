54b7017ca9b6385b324ab893687c054b8e994656
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-23 20:16:13 -0500

    Create rpg-fantasy_ImgRandom.js

---
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch'
+let handler = async (m, { command, usedPrefix, conn }) => {
+
+async function reclamarImagen(url) {
+try {
+const response = await fetch(url)
+const data = await response.json()
+
+data.imagenesReclamadas.forEach((imagen, index) => {
+fakeIMG = { contextInfo: { externalAdReply: { title: conn.getName(m.sender), body: imagen.descripcion, sourceUrl: redesMenu, thumbnailUrl: imagen.urlImagen }}}
+let info = `Nombre: ${imagen.nombre}
+Origen: ${imagen.descripcion}
+Costo: $${imagen.costo}
+Estado: Libre
+Clase: ${imagen.clase}
+Id de imagen: ${imagen.codigoImagen}`
+conn.sendMessage(m.chat, { image: { url: imagen.urlImagen }, caption: info, mentions: [m.sender] }, { quoted: fakeIMG })  
+})
+} catch (error) {
+console.error('Error al obtener o procesar los datos: ', error)
+}}
+
+const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
+reclamarImagen(jsonURL)
+
+}
+handler.command = /^(fantasy|fy)$/i
+export default handler
