1fd13f968a0b7da0ac94fd885f6e44a4acd7b337
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-23 20:50:59 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -7,13 +7,13 @@ const data = await response.json();
 if (data.imagenesReclamadas && data.imagenesReclamadas.length > 0) {
 const imagenAleatoria = data.imagenesReclamadas[Math.floor(Math.random() * data.imagenesReclamadas.length)];
 //let fakeIMG = { contextInfo: { externalAdReply: { title: conn.getName(m.sender), body: imagen.descripcion, sourceUrl: redesMenu, thumbnailUrl: imagen.urlImagen }}}
-let info = `*Nombre:* ${imagen.nombre}
-*Origen:* ${imagen.descripcion}
-*Costo:* $${imagen.costo}
+let info = `*Nombre:* ${data.nombre}
+*Origen:* ${data.descripcion}
+*Costo:* $${data.costo}
 *Estado:* Libre
-*Clase:* ${imagen.clase}
-*ID:* \`\`\`${imagen.codigoImagen}\`\`\``
-conn.sendMessage(m.chat, { image: { url: imagenAleatoria.urlImagen }, caption: info }, { quoted: m })  
+*Clase:* ${data.clase}
+*ID:* \`\`\`${data.codigoImagen}\`\`\``
+conn.sendMessage(m.chat, { image: { url: data.urlImagen }, caption: info }, { quoted: m })  
 }} catch (error) {
 console.error('Error al obtener o procesar los datos: ', error)
 }
