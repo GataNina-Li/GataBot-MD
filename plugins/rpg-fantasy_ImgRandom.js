f7e81f8d6ec531281364aaf0d8fcd1239f9347fe
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-23 20:55:12 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -1,23 +1,30 @@
 import fetch from 'node-fetch'
+
 let handler = async (m, { command, usedPrefix, conn }) => {
 const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
+
 try {
 const response = await fetch(jsonURL)
-const data = await response.json();
+const data = await response.json()
+
 if (data.imagenesReclamadas && data.imagenesReclamadas.length > 0) {
-const imagenAleatoria = data.imagenesReclamadas[Math.floor(Math.random() * data.imagenesReclamadas.length)];
-//let fakeIMG = { contextInfo: { externalAdReply: { title: conn.getName(m.sender), body: imagen.descripcion, sourceUrl: redesMenu, thumbnailUrl: imagen.urlImagen }}}
-let info = `*Nombre:* ${data.nombre}
-*Origen:* ${data.descripcion}
-*Costo:* $${data.costo}
+const dato = data.imagenesReclamadas[Math.floor(Math.random() * data.imagenesReclamadas.length)]
+
+let info = `*Nombre:* ${dato.nombre}
+*Origen:* ${dato.descripcion}
+*Costo:* $${dato.costo}
 *Estado:* Libre
-*Clase:* ${data.clase}
-*ID:* \`\`\`${data.codigoImagen}\`\`\``
-conn.sendMessage(m.chat, { image: { url: data.urlImagen }, caption: info }, { quoted: m })  
+*Clase:* ${dato.clase}
+*ID:* \`\`\`${dato.codigoImagen}\`\`\``;
+await conn.sendMessage(m.chat, { image: { url: dato.urlImagen }, caption: info }, { quoted: m })
+} else {
+console.error('El JSON no contiene imágenes reclamadas.')
+conn.sendMessage(m.chat, 'Error al obtener o procesar los datos.', { quoted: m })
 }} catch (error) {
 console.error('Error al obtener o procesar los datos: ', error)
-}
+conn.sendMessage(m.chat, 'Error al procesar la solicitud.', { quoted: m })
+}}
 
-}
-handler.command = /^(fantasy|fy)$/i
-export default handler
+handler.command = /^(fantasy|fy)$/i;
+export default handler;
+
