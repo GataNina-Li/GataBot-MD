4c121de38b2d994377b3d6d73b36e0c6dac29bd7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-24 02:28:42 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -18,8 +18,24 @@ let info = `*Nombre:* ${dato.nombre}
 *Costo:* $${dato.costo}
 *Estado:* Libre
 *Clase:* ${dato.clase}
-*ID:* \`\`\`${dato.codigoImagen}\`\`\``;
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+*ID:* \`\`\`${dato.codigoImagen}\`\`\``
+let fkontak = {
+  "key": {
+    "participants": "0@s.whatsapp.net",
+    "remoteJid": "status@broadcast",
+    "fromMe": false,
+    "id": "Halo"
+  },
+  "message": {
+    "contactMessage": {
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+      "jpegThumbnail": "https://i.imgur.com/IXlUwTW.jpg"
+    }
+  },
+  "participant": "0@s.whatsapp.net"
+};
+
+//let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 conn.sendFile(m.chat, dato.urlImagen, 'error.jpg', info, fkontak, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${conn.getName(m.sender)}`, body: `${dato.descripcion}`, mediaType: 1, sourceUrl: accountsgb.getRandom(), thumbnailUrl: pp}}})
 } else {
 console.error('El JSON no contiene imágenes reclamadas.')
