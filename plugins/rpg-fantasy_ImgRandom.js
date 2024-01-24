21d90915345e7ac0f7cf71aff3bb337a2159b99c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-23 20:43:00 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -1,22 +1,21 @@
 import fetch from 'node-fetch'
 let handler = async (m, { command, usedPrefix, conn }) => {
 
-async function reclamarImagen(url) {
+async function reclamarImagen(url, m, conn) {
 try {
-const response = await fetch(url)
-const data = await response.json()
-
-data.imagenesReclamadas.forEach((imagen, index) => {
+const response = await fetch(url);
+const data = await response.json();
+if (data.imagenesReclamadas && data.imagenesReclamadas.length > 0) {
+const imagenAleatoria = data.imagenesReclamadas[Math.floor(Math.random() * data.imagenesReclamadas.length)];
 //let fakeIMG = { contextInfo: { externalAdReply: { title: conn.getName(m.sender), body: imagen.descripcion, sourceUrl: redesMenu, thumbnailUrl: imagen.urlImagen }}}
-let info = `Nombre: ${imagen.nombre}
-Origen: ${imagen.descripcion}
-Costo: $${imagen.costo}
-Estado: Libre
-Clase: ${imagen.clase}
-Id de imagen: ${imagen.codigoImagen}`
-conn.sendMessage(m.chat, { image: { url: imagen.urlImagen }, caption: info }, { quoted: m })  
-})
-} catch (error) {
+let info = `*Nombre:* ${imagen.nombre}
+*Origen:* ${imagen.descripcion}
+*Costo:* $${imagen.costo}
+*Estado:* Libre
+*Clase:* ${imagen.clase}
+*ID:* \`\`\`${imagen.codigoImagen}\`\`\``
+conn.sendMessage(m.chat, { image: { url: imagenAleatoria.urlImagen }, caption: info }, { quoted: m })  
+}} catch (error) {
 console.error('Error al obtener o procesar los datos: ', error)
 }}
 
