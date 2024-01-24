c4dc1955a560bbd72649c3e8a28f13147ea38c2a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-24 03:09:23 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -19,22 +19,6 @@ let info = `*Nombre:* ${dato.nombre}
 *Estado:* Libre
 *Clase:* ${dato.clase}
 *ID:* \`\`\`${dato.codigoImagen}\`\`\``
-let fkontak = {
-  "key": {
-    "participants": "0@s.whatsapp.net",
-    "remoteJid": "status@broadcast",
-    "fromMe": false,
-    "id": "Halo"
-  },
-  "message": {
-    "contactMessage": {
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
-      "jpegThumbnail": "https://i.imgur.com/IXlUwTW.jpg"
-    }
-  },
-  "participant": "0@s.whatsapp.net"
-};
-
 //let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let fkontak = {
   "key": {
