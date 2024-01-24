5028bff8051c881a5c60d488835df4acc0f5ac8b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-24 02:11:12 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -1,14 +1,17 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { command, usedPrefix, conn }) => {
-const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 
+  
+const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
 try {
 const response = await fetch(jsonURL)
 const data = await response.json()
 
 if (data.imagenesReclamadas && data.imagenesReclamadas.length > 0) {
 const dato = data.imagenesReclamadas[Math.floor(Math.random() * data.imagenesReclamadas.length)]
+let pp = await conn.profilePictureUrl(who, 'image').catch((_) => dato.urlImagen)
 //let fakeIMG = { contextInfo: { externalAdReply: { title: `${conn.getName(m.sender)}`, body: `${dato.descripcion}`, sourceUrl: redesMenu.getRandom(), thumbnailUrl: dato.urlImagen }}}
 let info = `*Nombre:* ${dato.nombre}
 *Origen:* ${dato.descripcion}
@@ -16,10 +19,8 @@ let info = `*Nombre:* ${dato.nombre}
 *Estado:* Libre
 *Clase:* ${dato.clase}
 *ID:* \`\`\`${dato.codigoImagen}\`\`\``;
-//await conn.sendMessage(m.chat, { image: { url: dato.urlImagen }, caption: info }, { quoted: fakeIMG })
-//await conn.sendMessage(m.chat, { image: { url: dato.urlImagen }, caption: info, mentions: [m.sender] }, { quoted: fakeIMG })
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-conn.sendFile(m.chat, dato.urlImagen, 'error.jpg', info, fkontak, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${conn.getName(m.sender)}`, body: `${dato.descripcion}`, mediaType: 1, sourceUrl: redesMenu.getRandom(), thumbnailUrl: dato.urlImagen}}})
+//let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+conn.sendFile(m.chat, dato.urlImagen, 'error.jpg', info, fkontak, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${conn.getName(m.sender)}`, body: `${dato.descripcion}`, mediaType: 1, sourceUrl: redesMenu.getRandom(), thumbnailUrl: pp}}})
 } else {
 console.error('El JSON no contiene imágenes reclamadas.')
 conn.sendMessage(m.chat, 'Error al obtener o procesar los datos.', { quoted: m })
