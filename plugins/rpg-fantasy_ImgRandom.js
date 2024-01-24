ffba52d0caaedac797ea4299c5fa061875f764b3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-23 20:49:26 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -1,9 +1,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, { command, usedPrefix, conn }) => {
-
-async function reclamarImagen(imagen, jsonURL) {
+const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
 try {
-const response = await fetch(jsonURL);
+const response = await fetch(jsonURL)
 const data = await response.json();
 if (data.imagenesReclamadas && data.imagenesReclamadas.length > 0) {
 const imagenAleatoria = data.imagenesReclamadas[Math.floor(Math.random() * data.imagenesReclamadas.length)];
@@ -17,10 +16,7 @@ let info = `*Nombre:* ${imagen.nombre}
 conn.sendMessage(m.chat, { image: { url: imagenAleatoria.urlImagen }, caption: info }, { quoted: m })  
 }} catch (error) {
 console.error('Error al obtener o procesar los datos: ', error)
-}}
-
-const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
-reclamarImagen(jsonURL)
+}
 
 }
 handler.command = /^(fantasy|fy)$/i
