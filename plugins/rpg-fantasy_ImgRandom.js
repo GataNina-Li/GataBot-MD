18fdecfcae8bc7255e424c57624e8e4fb061f66d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-26 16:44:51 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -39,7 +39,7 @@ conn.sendMessage(m.chat, 'Error al procesar la solicitud.', { quoted: m })
 
 handler.before = async (m) => {
 if (m.quoted && m.quoted.id === id_message && ['c', '🛒', '🐱'].includes(m.text.toLowerCase())) {
-let fake = { contextInfo: { externalAdReply: {title: `${conn.getName(m.sender)}`, body: `${dato.descripcion}`, sourceUrl: accountsgb.getRandom(), thumbnailUrl: 'https://images.vexels.com/media/users/3/200097/isolated/preview/942820836246f08c2d6be20a45a84139-icono-de-carrito-de-compras-carrito-de-compras.png' }}}
+let fake = { contextInfo: { externalAdReply: {title: `${conn.getName(m.sender)}`, body: `${dato.descripcion}`, sourceUrl: accountsgb.getRandom(), thumbnailUrl: 'https://img.freepik.com/vector-premium/icono-marca-verificacion_108855-4739.jpg' }}}
 //conn.sendMessage(m.chat, {text: `El usuario *${conn.getName(m.sender)}* ha comprado a *${dato.nombre}*` }, { quoted: ppp})
 conn.reply(m.chat, `El usuario *${conn.getName(m.sender)}* ha comprado a *${dato.nombre}*`, m, fake, )
 }}
