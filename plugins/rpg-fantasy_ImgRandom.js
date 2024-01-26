71f7e79d4e4b1302d80f8e74dc9f15ef395d6c62
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-26 15:36:29 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -1,41 +1,3 @@
-/*import fetch from 'node-fetch'
-
-let handler = async (m, { command, usedPrefix, conn }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-
-const isCommand1 = /^(fantasy|fy)$/i.test(command)
-
-if (isCommand1) {
-const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
-try {
-const response = await fetch(jsonURL)
-const data = await response.json()
-
-if (data.imagenesReclamadas && data.imagenesReclamadas.length > 0) {
-const dato = data.imagenesReclamadas[Math.floor(Math.random() * data.imagenesReclamadas.length)]
-let pp = await conn.profilePictureUrl(who, 'image').catch((_) => dato.urlImagen)
-let info = `*⛱️ FANTASÍA RPG ⛱️*
-*⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯*
-*✓ Nombre:* ${dato.nombre}
-*✓ Origen:* ${dato.descripcion}
-*✓ Costo:* $${dato.costo}
-*✓ Estado:* Libre
-*✓ Clase:* ${dato.clase}
-*✓ ID:* \`\`\`${dato.codigoImagen}\`\`\``
-conn.sendFile(m.chat, dato.urlImagen, 'error.jpg', info, fkontak, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${conn.getName(m.sender)}`, body: `${dato.descripcion}`, mediaType: 1, sourceUrl: accountsgb.getRandom(), thumbnailUrl: pp}}})
-} else {
-console.error('No se han encontrado imágenes.')
-conn.sendMessage(m.chat, 'Error al obtener o procesar los datos.', { quoted: m })
-}} catch (error) {
-console.error('Error al obtener o procesar los datos: ', error)
-conn.sendMessage(m.chat, 'Error al procesar la solicitud.', { quoted: m })
-}}
-
-}
-handler.command = /^(fantasy|fy)$/i
-export default handler*/
-
 import fetch from 'node-fetch';
 let id_message = null
 let dato = null
@@ -45,9 +7,6 @@ let handler = async (m, { command, usedPrefix, conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-const isCommand1 = /^(fantasy|fy)$/i.test(command);
-
-if (isCommand1) {
 const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
 try {
 const response = await fetch(jsonURL)
@@ -77,15 +36,16 @@ conn.sendMessage(m.chat, 'Error al obtener o procesar los datos.', { quoted: m }
 }} catch (error) {
 console.error('Error al obtener o procesar los datos: ', error)
 conn.sendMessage(m.chat, 'Error al procesar la solicitud.', { quoted: m })
-}}}
+}}
 
-handler.before = async (m) => {
+//handler.before = async (m) => {
+handler.before = async (m, { conn }) => {
 if (m.quoted && m.quoted.id === id_message && m.text.toLowerCase() === 'comprar') {
-conn.sendMessage(m.chat, `El siguiente usuario ${conn.getName(m.sender)} ha comprado a ${dato.nombre}`, { quoted: m });
+conn.sendMessage(m.chat, `El siguiente usuario ${conn.getName(m.sender)} ha comprado a ${dato.nombre}`, { quoted: m })
 //m.reply(`El siguiente usuario ${conn.getName(m.sender)} ha comprado a ${dato.nombre}`)
 }}
 
-handler.command = /^(fantasy|fy)$/i;
+handler.command = /^(fantasy|fy)$/i
 export default handler;
 
 
