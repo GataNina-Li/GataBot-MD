2372895cc94ba6ffe6d07ab7ad2da5ebf122b0e9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-26 12:50:58 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 
 let handler = async (m, { command, usedPrefix, conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
@@ -34,5 +34,76 @@ conn.sendMessage(m.chat, 'Error al procesar la solicitud.', { quoted: m })
 
 }
 handler.command = /^(fantasy|fy)$/i
+export default handler*/
+
+import fetch from 'node-fetch';
+
+let handler = async (m, { command, usedPrefix, conn }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
+const isCommand1 = /^(fantasy|fy)$/i.test(command)
+
+if (isCommand1) {
+const jsonURL = 'https://raw.githubusercontent.com/GataNina-Li/module/main/imagen_json/anime.json'
+try {
+const response = await fetch(jsonURL)
+const data = await response.json()
+
+if (data.imagenesReclamadas && data.imagenesReclamadas.length > 0) {
+const dato = data.imagenesReclamadas[Math.floor(Math.random() * data.imagenesReclamadas.length)]
+let pp = await conn.profilePictureUrl(who, 'image').catch((_) => dato.urlImagen)
+let info = `*⛱️ FANTASÍA RPG ⛱️*\n*⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯*\n✓ *Nombre:* ${dato.nombre}\n✓ *Origen:* ${dato.descripcion}\n✓ *Costo:* $${dato.costo}\n✓ *Estado:* Libre\n✓ *Clase:* ${dato.clase}\n✓ *ID:* \`\`\`${dato.codigoImagen}\`\`\``
+
+
+if (m.quoted && m.quoted.text.includes('imagen_info')) {
+conn.sendFile(m.chat, dato.urlImagen, 'error.jpg', info, fkontak, true, {
+contextInfo: {
+'forwardingScore': 200,
+'isForwarded': false,
+externalAdReply: {
+showAdAttribution: false,
+title: `${conn.getName(m.sender)}`,
+body: `${dato.descripcion}`,
+mediaType: 1,
+sourceUrl: accountsgb.getRandom(),
+thumbnailUrl: pp
+}}
+}, { caption: 'imagen_info' }).then(() => {
+if (m.text.toLowerCase().includes('comprar')) {
+let comprador = m.sender
+console.log('Se ha comprado la imagen. Comprador:', comprador)
+                            
+}})
+} else {
+                    
+                    conn.sendFile(m.chat, dato.urlImagen, 'error.jpg', info, fkontak, true, {
+                        contextInfo: {
+                            'forwardingScore': 200,
+                            'isForwarded': false,
+                            externalAdReply: {
+                                showAdAttribution: false,
+                                title: `${conn.getName(m.sender)}`,
+                                body: `${dato.descripcion}`,
+                                mediaType: 1,
+                                sourceUrl: accountsgb.getRandom(),
+                                thumbnailUrl: pp
+                            }
+                        }
+                    }, { caption: 'imagen_info' })
+                }
+            } else {
+                console.error('No se han encontrado imágenes.')
+                conn.sendMessage(m.chat, 'Error al obtener o procesar los datos.', { quoted: m })
+            }
+        } catch (error) {
+            console.error('Error al obtener o procesar los datos: ', error)
+            conn.sendMessage(m.chat, 'Error al procesar la solicitud.', { quoted: m })
+        }
+    }
+}
+
+handler.command = /^(fantasy|fy)$/i
 export default handler
+
 
