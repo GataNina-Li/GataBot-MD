eb928b86fa707fe306821f9a204cfa1b93916ee0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-28 11:27:24 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -68,9 +68,10 @@ id: dato.codigoImagen,
 like: false,
 estado: true
 }
-user.fantasy.push(user.fantasy_character)
+//user.fantasy.push(user.fantasy_character)
 user.fantasy = perd.length > 0 ? [...perd, user.fantasy_character] : [user.fantasy_character]
-user.fantasy.push(user.fantasy_character)        
+//user.fantasy.push(user.fantasy_character)  
+//user.fantasy_character = 0
         
 user.money -= dato.costo
 fake = { contextInfo: { externalAdReply: { title: `¡Disfruta de tú personaje!`, body: `${dato.descripcion}`, sourceUrl: accountsgb.getRandom(), thumbnailUrl: dato.urlImagen } } }
