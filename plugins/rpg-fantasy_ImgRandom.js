ab099b56be088d542dd7fb93de4b4799db628f2b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-28 11:17:09 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -62,13 +62,15 @@ if (user.money < dato.costo) {
 fake = { contextInfo: { externalAdReply: { title: `¡Insuficientes ${rpgshop.emoticon('money')}!`, body: `😼 Completa misiones del RPG`, sourceUrl: accountsgb.getRandom(), thumbnailUrl: gataMenu.getRandom() } } }
 conn.reply(m.chat, `Te falta *${cantidadFaltante} ${rpgshop.emoticon('money')}* para comprar a *${dato.nombre}*\n\n*Actualmente tienes ${user.money} ${rpgshop.emoticon('money')}*`, m, fake)
 } else {
+let perd = user.fantasy.length > 0 ? [...user.fantasy] : []
 user.fantasy_character = {
 id: dato.codigoImagen,
 like: false,
 estado: true
 }
 user.fantasy.push(user.fantasy_character)
-user.fantasy_character = 0        
+user.fantasy = perd.length > 0 ? [...perd, user.fantasy_character] : [user.fantasy_character]
+        
 user.money -= dato.costo
 fake = { contextInfo: { externalAdReply: { title: `¡Disfruta de tú personaje!`, body: `${dato.descripcion}`, sourceUrl: accountsgb.getRandom(), thumbnailUrl: dato.urlImagen } } }
 conn.reply(m.chat, `El usuario *${conn.getName(m.sender)}* ha comprado a *${dato.nombre}*`, m, fake)
