f1dd03dc1d84dddff30cb3edb1a394e1a90bf022
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-30 01:57:55 -0500

    Update rpg-fantasy_list.js

--- plugins/rpg-fantasy_list.js
+++ plugins/rpg-fantasy_list.js
@@ -30,13 +30,17 @@ charactersByType[type].push(`- ${character.name}`)
 
 const lowCostCharacters = data.infoImg
 .filter((character) => character.price >= 0 && character.price <= 700)
+.map((character) => ({ name: character.name, price: character.price }))
+.sort((a, b) => a.price - b.price)
 .map((character) => `- ${character.name} (${character.price})`)
 .join('\n')
 
-const highCostCharacters = data.infoImg
-.filter((character) => character.price > 700)
-.map((character) => `- ${character.name} (${character.price})`)
-.join('\n')
+ const highCostCharacters = data.infoImg
+ .filter((character) => character.price > 700)
+ .map((character) => ({ name: character.name, price: character.price }))
+ .sort((a, b) => a.price - b.price)
+ .map((character) => `- ${character.name} (${character.price})`)
+ .join('\n')
 
 let currentPage = text ? parseInt(text) : 1
 let totalPages = 1
