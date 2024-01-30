d22fe6fd2af6cb4a8e59b00e7f95fdff71abeb05
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-30 03:53:32 -0500

    Update rpg-fantasy_list.js

--- plugins/rpg-fantasy_list.js
+++ plugins/rpg-fantasy_list.js
@@ -63,7 +63,7 @@ totalPages = Math.ceil(maxSectionLength / numPersonaje)
 
 let reply = await conn.reply(m.chat, getFormattedReply(), m)
 handler.before = async (m) => {
-if (m.quoted && m.quoted.id === reply.id && ['sig'].includes(m.text.toLowerCase())) {
+if (m.quoted && m.quoted.id === reply.id && /^\d+$/.test(m.text.trim())) {
 currentPage = text ? parseInt(text) : 1
 if (isNaN(currentPage) || currentPage < 1 || currentPage > totalPages) {
 return conn.reply(m.chat, `Número de página inválido. Utiliza un número entre 1 y ${totalPages}.`, m)
