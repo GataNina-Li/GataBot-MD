aaba3f0223121aa3fdaaa854a8deb2acbf8e3683
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-30 08:39:05 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -122,9 +122,7 @@ if (m.quoted && m.quoted.id === id_message && ['👍', '❤️', '👎'].include
       const flow = usuarioExistente[idUsuarioExistente].flow || [];
       const votoExistente = flow.find((voto) => voto && voto.character_name === nombrePersonaje && voto[emoji.toLowerCase()]);
       
-      if (votoExistente && votoExistente[emoji.toLowerCase()]) {
-  const errorMessage = `No puedes dar *${emoji}* a *${nombrePersonaje}* porque ya lo hiciste antes.`;
-  conn.reply(m.chat, errorMessage, m)
+      
       } else {
         const emojiAntes = flow.find((voto) => voto && voto.character_name === nombrePersonaje && (voto.like || voto.dislike || voto.superlike))?.like ? '👍' : (voto && voto.dislike ? '👎' : '❤️');
         const updatedFlow = [
@@ -150,6 +148,10 @@ if (m.quoted && m.quoted.id === id_message && ['👍', '❤️', '👎'].include
         fs.writeFileSync(fantasyDBPath, JSON.stringify(fantasyDB, null, 2), 'utf8');
 
         if (emojiAntes) {
+        if (votoExistente && votoExistente[emoji.toLowerCase()]) {
+  const errorMessage = `No puedes dar *${emoji}* a *${nombrePersonaje}* porque ya lo hiciste antes.`;
+  conn.reply(m.chat, errorMessage, m)
+        }else{
           const cambioEmojiMessage = `Has decidido cambiar tu reacción anterior *${emojiAntes}* por *${emoji}* en *${nombrePersonaje}*.`;
           conn.reply(m.chat, cambioEmojiMessage, m);
         } else {
@@ -159,7 +161,7 @@ if (m.quoted && m.quoted.id === id_message && ['👍', '❤️', '👎'].include
       }
     }
   }
-}
+
 
         
 if (m.quoted && m.quoted.id === id_message && ['c', '🛒', '🐱'].includes(m.text.toLowerCase())) {
