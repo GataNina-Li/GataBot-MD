03f6e8fd05736cabd44e3e2438bf12f2d999834a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-30 11:06:14 -0500

    Update rpg-fantasy_ImgRandom.js

--- plugins/rpg-fantasy_ImgRandom.js
+++ plugins/rpg-fantasy_ImgRandom.js
@@ -128,7 +128,7 @@ if (m.quoted && m.quoted.id === id_message && ['👍', '❤️', '👎'].include
       const flow = usuarioExistente[idUsuarioExistente].flow || [];
       const votoExistente = flow.find((voto) => voto && voto.character_name === nombrePersonaje && voto[emoji.toLowerCase()]);
 
-      if (votoExistente && votoExistente[emoji.toLowerCase()] && votoExistente[emoji.toLowerCase()] === m.text) {
+      if (votoExistente && votoExistente[emoji.toLowerCase()]) {
         const errorMessage = `No puedes dar *${emoji}* a *${nombrePersonaje}* porque ya lo hiciste antes.`;
         conn.reply(m.chat, errorMessage, m);
       } else {
@@ -166,8 +166,6 @@ if (m.quoted && m.quoted.id === id_message && ['👍', '❤️', '👎'].include
     }
   }
 }
-
-
 
         
 if (m.quoted && m.quoted.id === id_message && ['c', '🛒', '🐱'].includes(m.text.toLowerCase())) {
