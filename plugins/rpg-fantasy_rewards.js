bfe5a00ca01cb938073e6f6b607f1b1d28665560
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-30 22:27:17 -0500

    Update rpg-fantasy_rewards.js

--- plugins/rpg-fantasy_rewards.js
+++ plugins/rpg-fantasy_rewards.js
@@ -13,31 +13,31 @@ fantasyDB = JSON.parse(data)
 // Si el usuario no existe en la base de datos borra su contador de registro
 usuarioExistente = fantasyDB.find((user) => Object.keys(user)[0] === userId)
 if (!usuarioExistente) {
-user.fantasy_character = 0
+user.fantasy_character = [0, 0, 0]
 }
 
 // Verifica si el usuario existe en la base de datos y si tiene la estructura fantasy
 usuarioExistente = fantasyDB.find((user) => Object.keys(user)[0] === userId && user[userId].fantasy)
-if (usuarioExistente && user.fantasy_character === 1) {
+if (usuarioExistente && user.fantasy_character[0] === 0) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} ahora puedes calificar personajes*`, m)
-user.fantasy_character++
+user.fantasy_character[0]++
 }
 
 // Verifica si el conjunto fantasy tiene id como cadena de texto y status como true
 usuarioExistente = fantasyDB.find((user) => Object.keys(user)[0] === userId)
 const fantasyArray = usuarioExistente[userId].fantasy
-if (fantasyArray.length >= 1 && typeof fantasyArray[0].id === 'string' && fantasyArray[0].status === true && user.fantasy_character === 2) {
+if (fantasyArray.length >= 1 && typeof fantasyArray[0].id === 'string' && fantasyArray[0].status === true && user.fantasy_character[1] === 0) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} recompensa por comprar ${fantasyArray.length} personaje 1*`, m)
-user.fantasy_character++
-} else if (fantasyArray.length >= 3 && typeof fantasyArray[2].id === 'string' && fantasyArray[2].status === true && user.fantasy_character === 3) {
+user.fantasy_character[1]++
+} else if (fantasyArray.length >= 3 && typeof fantasyArray[2].id === 'string' && fantasyArray[2].status === true && user.fantasy_character[1] === 1) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} recompensa por comprar ${fantasyArray.length} personaje 3*`, m)
-user.fantasy_character++
-} else if (fantasyArray.length >= 8 && typeof fantasyArray[7].id === 'string' && fantasyArray[7].status === true && user.fantasy_character === 4) {
+user.fantasy_character[1]++
+} else if (fantasyArray.length >= 8 && typeof fantasyArray[7].id === 'string' && fantasyArray[7].status === true && user.fantasy_character[1] === 2) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} recompensa por comprar ${fantasyArray.length} personaje 8*`, m)
-user.fantasy_character++
-} else if (fantasyArray.length >= 15 && typeof fantasyArray[14].id === 'string' && fantasyArray[14].status === true && user.fantasy_character === 5) {
+user.fantasy_character[1]++
+} else if (fantasyArray.length >= 15 && typeof fantasyArray[14].id === 'string' && fantasyArray[14].status === true && user.fantasy_character[1] === 3) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} recompensa por comprar ${fantasyArray.length} personaje 15*`, m)
-user.fantasy_character++
+user.fantasy_character[1]++
 }
 
 // Cuenta la cantidad de veces que se ha dado "like"
@@ -45,18 +45,18 @@ usuarioExistente = fantasyDB.find((user) => Object.keys(user)[0] === userId)
 if (usuarioExistente) {
 const flowArray = usuarioExistente[userId].flow || []
 const likesCount = flowArray.filter(voto => voto.like).length
-if (likesCount === 1 && user.fantasy_character === 6) {
+if (likesCount === 1 && user.fantasy_character[2] === 0) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} recompensa por dar ${likesCount} vez "👍"*`, m)
-user.fantasy_character++
-} else if (likesCount === 5 && user.fantasy_character === 7) {
+user.fantasy_character[2]++
+} else if (likesCount === 5 && user.fantasy_character[2] === 1) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} recompensa por dar ${likesCount} veces "👍"*`, m)
-user.fantasy_character++
-} else if (likesCount === 10 && user.fantasy_character === 8) {
+user.fantasy_character[2]++
+} else if (likesCount === 10 && user.fantasy_character[2] === 2) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} recompensa por dar ${likesCount} veces "👍"*`, m)
-user.fantasy_character++
-} else if (likesCount === 20 && user.fantasy_character === 9) {
+user.fantasy_character[2]++
+} else if (likesCount === 20 && user.fantasy_character[2] === 3) {
 conn.reply(m.chat, `\`\`\`Logro desbloqueado 🔓\`\`\`\n\n*${conn.getName(userId)} recompensa por dar ${likesCount} veces "👍"*`, m)
-user.fantasy_character++
+user.fantasy_character[2]++
 }}
   
 }
