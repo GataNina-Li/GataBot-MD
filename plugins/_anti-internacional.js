3236d42cd5668509ac7a06bfadd55ad9ff5b1ff4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-01-30 19:41:28 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,4 +1,4 @@
-/*import db from '../lib/database.js' //Esta función es para la versión LATAM
+import db from '../lib/database.js' //Esta función es para la versión LATAM
 let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
@@ -7,12 +7,12 @@ if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 if (isBotAdmin && chat.antifake) {
 let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
-	
+
 if (m.sender.startsWith('6' || '6')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-		
+
 if (m.sender.startsWith('90' || '90')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
@@ -22,7 +22,7 @@ if (m.sender.startsWith('92' || '92')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-	
+
 if (m.sender.startsWith('93' || '93')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
@@ -37,17 +37,17 @@ if (m.sender.startsWith('7' || '7')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-	
+
 if (m.sender.startsWith('49' || '49')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-	
+
 if (m.sender.startsWith('2' || '2')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-	
+
 if (m.sender.startsWith('91' || '91')) {
 global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
@@ -58,36 +58,4 @@ global.db.data.users[m.sender].block = true
 await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
 }}
-
-if (m.sender.startsWith('994' || '994')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-}}
-
-if (m.sender.startsWith('256' || '256')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-}}
-
-if (m.sender.startsWith('210' || '210')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-}}
-
-if (m.sender.startsWith('212' || '212')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-}}
-
-if (m.sender.startsWith('358' || '358')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-}}
 export default handler
-*/
-
