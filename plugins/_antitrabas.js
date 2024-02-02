4ec272b4750ebeebea80bf0b96ab5ef06be6c7d1
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-02-02 18:39:59 -0300

    Update _antitrabas.js

--- plugins/_antitrabas.js
+++ plugins/_antitrabas.js
@@ -2,7 +2,7 @@
 //By @NeKosmic || https://github.com/NeKosmic/
 //
 import * as fs from 'fs'
-export async function before(m, { conn, isAdmin, isBotAdmin, usedPrefix }) {
+export async function before(m, { conn, isAdmin, isBotAdmin, text, participants, usedPrefix }) {
 if (m.isBaileys && m.fromMe)
 return !0
 if (!m.isGroup) return !1
@@ -16,7 +16,7 @@ let name = await conn.getName(m.sender)
 let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": 'ɢᴀᴛᴀʙᴏᴛ-ᴍᴅ', 'jpegThumbnail': null}}}
 if (chat.antiTraba && m.text.length > 4000) { //Cantidad máxima de caracteres aceptados en un mensaje//
 if (isAdmin) return conn.sendMessage(m.chat, { text: `${lenguajeGB['smsAvisoAG']()} ${mid.smsAntiTraba(sender)}`, mentions: [m.sender] }, { quoted: fakemek })
-if (!isBotAdmin) return conn.sendMessage(m.chat, {text: `${mid.smsAntiTraba2}\n${listAdmin}\n\n${lenguajeGB['smsAllAdmin']()}`, mentions: [...groupAdmins.map(v => v.id)] }, {quoted: m})
+if (!isBotAdmin) return conn.sendMessage(m.chat, {text: mid.smsAntiTraba2 + `\n${listAdmin}\n\n${lenguajeGB['smsAllAdmin']()}`, mentions: [...groupAdmins.map(v => v.id)] }, {quoted: m})
 //await conn.sendButton(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, author, ['[ ᴅᴇsᴀᴄᴛɪᴠᴀ ᴀɴᴛɪᴛʀᴀʙᴀ ]', usedPrefix+'apagar antitraba'], fakemek )
 if (isBotAdmin) {
 conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
