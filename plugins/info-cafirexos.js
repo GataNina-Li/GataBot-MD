085ffcfb9df9e09429896cdf7855e31c15aa33e8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-04 16:27:23 -0500

    Update info-cafirexos.js

--- plugins/info-cafirexos.js
+++ plugins/info-cafirexos.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let cafirexos `
+let cafirexos = `
 _Optimice la implementación de *GataBot* mediante la integración en un servicio de alojamiento de alto rendimiento._
 
 🔵 \`\`\`Información del Host\`\`\`
