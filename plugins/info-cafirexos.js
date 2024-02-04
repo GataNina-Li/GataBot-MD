dc8d566045695790a81f9aff85301b58220cb805
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-04 16:26:20 -0500

    Create info-cafirexos.js

---
+++ plugins/info-cafirexos.js
@@ -0,0 +1,45 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let cafirexos `
+_Optimice la implementación de *GataBot* mediante la integración en un servicio de alojamiento de alto rendimiento._
+
+🔵 \`\`\`Información del Host\`\`\`
+💻 *Página*
+https://www.cafirexos.com
+
+✨ *Dashboard*
+https://dash.cafirexos.com
+
+⚙️ *Panel*
+https://panel.cafirexos.com
+
+📢 *Canal de WhatsApp*
+https://whatsapp.com/channel/0029VaFVSkRCMY0KFmCMDX2q
+
+💥 *Grupo de WhatsApp*
+https://chat.whatsapp.com/FBtyc8Q5w2iJXVl5zGJdFJ
+
+📧 *Correo*
+contacto@cafirexos.com
+
+🧑‍💻 *Contacto (Diego Flores)*
+https://wa.me/50497150165
+`
+await conn.sendFile(m.chat, 'https://grxcwmcwbxwj.objectstorage.sa-saopaulo-1.oci.customer-oci.com/n/grxcwmcwbxwj/b/cafirexos/o/logos%2Flogo.png', 'fantasy.jpg', cafirexos.trim(), fkontak, true, {
+contextInfo: {
+'forwardingScore': 200,
+'isForwarded': false,
+externalAdReply: {
+showAdAttribution: false,
+renderLargerThumbnail: true,
+title: `🔵 C A F I R E X O S 🔵`,
+body: `✅ Hosting de Calidad`,
+mediaType: 1,
+sourceUrl: accountsgb.getRandom(),
+thumbnailUrl: 'https://grxcwmcwbxwj.objectstorage.sa-saopaulo-1.oci.customer-oci.com/n/grxcwmcwbxwj/b/cafirexos/o/logos%2Flogo_2.png'
+}}
+}, { mentions: userId })
+  
+}
+handler.command = /^(cafirexos)$/i
+export default handler
