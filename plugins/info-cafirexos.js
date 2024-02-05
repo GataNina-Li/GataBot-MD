c963a51894ba77dae030f5fbab11c52878cfdcbd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-04 20:16:11 -0500

    Update info-cafirexos.js

--- plugins/info-cafirexos.js
+++ plugins/info-cafirexos.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+/*let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let cafirexos = `
 _Optimice la implementación de *GataBot* mediante la integración en un servicio de alojamiento de alto rendimiento._
 
@@ -43,7 +43,37 @@ sourceUrl: accountsgb.getRandom(),
 thumbnailUrl: 'https://grxcwmcwbxwj.objectstorage.sa-saopaulo-1.oci.customer-oci.com/n/grxcwmcwbxwj/b/cafirexos/o/logos%2Flogo_2.png'
 }}
 }, { mentions: m.sender })
+*/
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/8ca14ef9fa43e99d1d196.jpg')
+let nomeDelBot = "𝐁𝐢𝐱𝐛𝐲𝐁𝐨𝐭-𝐌𝐝 🔮"
+
+let prova = {
+"key": {
+"participants": "0@s.whatsapp.net",
+"fromMe": false,
+"id": "Halo"
+},
+"message": {
+"locationMessage": {
+"name": `${nomeDelBot}`,
+"jpegThumbnail": pp,
+"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+}},
+"participant": "0@s.whatsapp.net"
+}
+  
+let name = await conn.getName(m.sender)
+let text = `
+Texto principal`.trim() 
+conn.sendMessage(m.chat, { text: text, contextInfo:{
+"externalAdReply": {"title": `Únete : ${name} `, 
+"body": `Versión: ${vs}`, 
+"previewType": "PHOTO",
+"thumbnail": thumbnail,
+"sourceUrl": `https://whatsapp.com/channel/0029Va8SHGnId7nJi8Zdnz3x`, 
+"mediaType": 1}}}, {quoted: prova})
   
 }
-handler.command = /^(cafirexos)$/i
+handler.command = /^(cafirexos|prueba38)$/i
 export default handler
