575ae24ff54a3314cb3f1061245b6ab4a933a19c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-06 22:26:14 -0500

    Update rpg-fantasy_my.js

--- plugins/rpg-fantasy_my.js
+++ plugins/rpg-fantasy_my.js
@@ -4,6 +4,7 @@ import fs from 'fs'
 const fantasyDBPath = './fantasy.json'
 let fantasyDB = []
 const validClasses = ['Común', 'Poco Común', 'Raro', 'Épico', 'Legendario', 'Sagrado', 'Supremo', 'Transcendental']
+let cantidadUsuariosRanking = 5
 
 let handler = async (m, { command, usedPrefix, conn, text }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
