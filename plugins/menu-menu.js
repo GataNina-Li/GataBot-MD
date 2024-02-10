5617011b521e51feafd96613838b8557b0d556cb
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-10 03:15:05 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -84,7 +84,7 @@ const countryCode = parsedPhoneNumber.country
 const countryData = ct.getCountry(countryCode)
 const timezones = countryData.timezones
 const zonaHoraria = timezones.length > 0 ? timezones[0] : 'UTC'
-moment.locale(mid)
+moment.locale(mid.idioma_code)
 let lugarMoment = moment().tz(zonaHoraria)
 if (lugarMoment) {
 fechaMoment = lugarMoment.format('llll [(]a[)]')
