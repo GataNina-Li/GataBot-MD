8a6bacec9e4d19a14634332a161c67306513c2c3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-09 22:53:18 -0500

    Update rpg-fantasy_change.js

--- plugins/rpg-fantasy_change.js
+++ plugins/rpg-fantasy_change.js
@@ -35,7 +35,7 @@ return conn.reply(m.chat, `*No posee personajes.* Primero compre un personaje us
 
 const personajesDisponibles = obtenerPersonajesDisponibles(userId, fantasyUsuario, data.infoImg)
 const listaPersonajes = construirListaPersonajes(personajesDisponibles)
-await conn.sendFile(m.chat, gataImg.getRandom(), 'fantasy.jpg', `> Uso *${usedPrefix + command} nombre o código* del persoanje\n\n` + listaPersonajes, fkontak, true, {
+await conn.sendFile(m.chat, gataImg.getRandom(), 'fantasy.jpg', `> Use *${usedPrefix + command} nombre o código* del personaje\n\n` + listaPersonajes, fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
 'isForwarded': false,
