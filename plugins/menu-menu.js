29bc733532dd799709c56f93038aedd11f950105
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-10 02:40:14 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -84,14 +84,15 @@ const countryData = ct.getCountry(countryCode)
 const timezones = countryData.timezones
 const zonaHoraria = timezones.length > 0 ? timezones[0] : 'UTC'
 const nombreLugar = countryData.name
-moment.locale('es')
+moment.locale('en')
 const lugarMoment = moment().tz(zonaHoraria)
 const fechaMoment = lugarMoment.format('llll [(]a[)]')
-const formatDate = fechaMoment.charAt(0).toUpperCase() + fechaMoment.slice(1) + `\n> *_Lugar: ${nombreLugar}_*`
+const formatDate = fechaMoment.charAt(0).toUpperCase() + fechaMoment.slice(1) 
 	
 let menu = `${lenguajeGB['smsConfi2']()} *${user.genero === 0 ? '👤' : user.genero == 'Ocultado 🕶️' ? `🕶️` : user.genero == 'Mujer 🚺' ? `🚺` : user.genero == 'Hombre 🚹' ? `🚹` : '👤'} ${user.registered === true ? user.name : taguser}* ${(conn.user.jid == global.conn.user.jid ? '' : `\n*SOY SUB BOT DE: https://wa.me/${global.conn.user.jid.split`@`[0]}*`) || ''}
 
 > *_${formatDate}_*
+> *_Lugar:_* \`${nombreLugar}\`
 
 ⎔ *${lenguajeGB['smsTotalUsers']()}* ➺ _${Object.keys(global.db.data.users).length}_ 
 ⎔ *Registrados »* ${rtotalreg}/${totalreg}    
