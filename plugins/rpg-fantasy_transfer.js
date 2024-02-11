ec7ec05051192289d50e757f36c45bd1b6a519f4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-11 00:57:27 -0500

    Update rpg-fantasy_transfer.js

--- plugins/rpg-fantasy_transfer.js
+++ plugins/rpg-fantasy_transfer.js
@@ -46,20 +46,16 @@ return conn.reply(m.chat, `*Use un caracter en medio del Usuario y personaje*\n\
 if (m.quoted && !text) {
 return conn.reply(m.chat, `*Responda a un mensaje de @${m.quoted.sender.split('@')[0]} escribiendo el nombre o código del personaje*\n\n> *Para ver tus persoanjes, escriba:*\n\`${usedPrefix}fantasymy o ${usedPrefix}fymy\``, m, { mentions: [m.quoted.sender] })
 }
-if (!userText || !characterText) {
-return conn.reply(m.chat, '*Algo salió mal, asegúrese de etiquetar al usuario o escribir el número completo del usuario*', m)
-}
 return conn.reply(m.chat, `*Etiqueta o escriba el número del usuario y nombre o código del personaje*\n\n> *Ejemplo:*\n\`${usedPrefix + command} usuario | personaje\`\n\n> _También puede responder al mensaje del usuario escribiendo el nombre o código del personaje_\n\n> *Para ver tus persoanjes, escriba:*\n\`${usedPrefix}fantasymy o ${usedPrefix}fymy\``, m)
 }
 
 let senderIndex = fantasyDB.findIndex(obj => obj.hasOwnProperty(m.sender))
+let senderData = fantasyDB[senderIndex][m.sender]
+if (!senderData.fantasy || senderData.fantasy.length == 0) return conn.reply(m.chat, 'No hemos encontrado personajes en tu colección', m)       
 if (senderIndex == -1) return conn.reply(m.chat, `> *Primero compra un personaje usando:*\n\n\`${usedPrefix}fantasy o ${usedPrefix}fy\``, m)
 let recipientIndex = fantasyDB.findIndex(obj => obj.hasOwnProperty(user))
 if (recipientIndex == -1) return conn.reply(m.chat, `*El usuario @${user.split('@')[0]} no puede recibir transferencias de personajes*\n\n> *Motivo:* _Sólo puedes transferir tus personajes a usuarios que hayan comprado mínimo un personaje_\n\n*@${user.split('@')[0]} Compra un personaje para recibir/enviar transferencias*\n\`${usedPrefix}fantasy o ${usedPrefix}fy\``, m, { mentions: [user] })
-    
-let senderData = fantasyDB[senderIndex][m.sender]
-if (!senderData.fantasy || senderData.fantasy.length == 0) return conn.reply(m.chat, 'No hemos encontrado personajes en tu colección', m)   
-    
+      
 let characterIndex = senderData.fantasy.findIndex(obj => obj.name == character || obj.id == character)
 if (characterIndex == -1) return conn.reply(m.chat, `*No hemos encontrado "${character}"*\n\n> *Motivo:* _Puede deberse a que no tiene ese personaje o está mal escrito el nombre o código del personaje_\n\n> *Para ver tus persoanjes, escriba:*\n\`${usedPrefix}fantasymy o ${usedPrefix}fymy\``, m)
     
@@ -74,8 +70,6 @@ found = data.find(({ code }) => senderCharacter.id === code)
 } else {
 found = data.find(({ name }) => senderCharacter.name === name)
 }
-
-if (!found) return conn.reply(m.chat, `No hemos encontrado ${character} en la base de datos externa`, m)
 
 let characterData = senderData.fantasy.splice(characterIndex, 1)[0]
 fantasyDB[recipientIndex][user].fantasy.push(characterData)
