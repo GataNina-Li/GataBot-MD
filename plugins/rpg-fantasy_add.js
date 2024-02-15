859688506dc0a6fd5c51f493d93a62b4515c1238
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-15 03:41:16 -0500

    Update rpg-fantasy_add.js

--- plugins/rpg-fantasy_add.js
+++ plugins/rpg-fantasy_add.js
@@ -139,7 +139,7 @@ conn.reply(m.chat, '¡Ocurrió un error al procesar la solicitud!', m)
 function generarCodigo() {
 const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 const numeros = '0123456789'
-const caracteresEspeciales = '#@%_*!^><'
+const caracteresEspeciales = '#@%_*!^<'
 let codigo = ''
 for (let i = 0; i < 3; i++) {
 codigo += letras.charAt(Math.floor(Math.random() * letras.length))
