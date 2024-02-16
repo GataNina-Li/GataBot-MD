71a8113518ca70169618042f1e108349244c7b73
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-15 20:37:29 -0500

    Update prueba-encuesta_comando.js

--- plugins/prueba-encuesta_comando.js
+++ plugins/prueba-encuesta_comando.js
@@ -7,59 +7,16 @@ handler.command = ['pruebapoll']
 export default handler*/
 
 const handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {
-let sock = conn
-    
-function nullish(args) {
-    return !(args !== null && args !== undefined)
+let caption = '' 
+
+
+const pollMessage = {
+name: cap,
+values: a,
+multiselect: false,
+selectableCount: 1
 }
-
-async function getMessage(key){
-    if (store) {
-        const msg = await store.loadMessage(key.remoteJid, key.id)
-        return msg?.message
-    }
-    return { conversation: "hola" }
-}
-
-
-sock.ev.on('messages.update', async chatUpdate => {
-    for(const { key, update } of chatUpdate) {
-        if (update.pollUpdates && key.fromMe) {
-            const pollCreation = await getMessage(key)
-            if (pollCreation) {
-                const pollUpdate = await getAggregateVotesInPollMessage({
-                    message: pollCreation,
-                    pollUpdates: update.pollUpdates,
-                })
-                var toCmd = pollUpdate.filter(v => v.voters.length !== 0)[0]?.name
-                if (toCmd == undefined) return
-                var prefCmd = prefix + toCmd
-                sock.appenTextMessage(prefCmd, chatUpdate)
-            }
-        }
-    }
-})
-
-
-const sendPoll = async (jid, name = '', optiPoll, selectableCount = 1) => {
-    if (!Array.isArray(optiPoll[0]) && typeof optiPoll[0] === 'string') optiPoll = [optiPoll];
-    const values = optiPoll.map(btn => ({
-        optionName: !nullish(btn[0]) ? btn[0] : ''
-    }));
-    await conn.sendMessage(jid, { poll: { name, values, selectableCount }});
-};
-
-
-const jid = m.chat;
-const pregunta = `Selecciona una opción:`;
-const opciones = ['play3 billie eilish', 'estado', 'menu', 'fb'];
-
-try {
-    await sendPoll(jid, pregunta, opciones)
-    console.log("Encuesta enviada correctamente.");
-} catch (error) {
-    console.error("Error al enviar la encuesta:", error);
-}
+await conn.sendMessage(m.chat, { poll: pollMessage })
 
     
 }
