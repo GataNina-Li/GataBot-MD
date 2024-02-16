c4ea082aa833aceedc10835f5ad00cce7302e714
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-16 00:53:55 -0500

    Update prueba-encuesta_comando.js

--- plugins/prueba-encuesta_comando.js
+++ plugins/prueba-encuesta_comando.js
@@ -1,14 +1,6 @@
-/*let handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {	
-
-//conn.sendPoll(m.chat, texto, a, {mentions: m})
-await conn.sendPoll(m.chat, `Selecciona una opción:`, [usedPrefix + 'menu', usedPrefix + 'estado', usedPrefix + 'ping'], { mentions: m })
-}
-handler.command = ['pruebapoll'] 
-export default handler*/
-
 const handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {
 let caption = 'Selecciona una opción:' 
-let comandos = ['.menu', '.estado', '.ping']
+let comandos = [ usedPrefix+ 'menu', usedPrefix + 'estado', usedPrefix + 'ping']
 
 /*const pollMessage = {
 name: caption,
@@ -17,6 +9,7 @@ multiselect: false,
 selectableCount: 1
 }
 await conn.sendMessage(m.chat, { poll: pollMessage })*/
+    
 await conn.sendPoll(m.chat, caption, comandos)
     
 conn.on('message-new', async (message) => {
