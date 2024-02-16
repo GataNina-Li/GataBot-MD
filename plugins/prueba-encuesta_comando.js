9a6b8349302d671fdc9fb1f8fad74435abeb4b60
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-15 21:12:40 -0500

    Update prueba-encuesta_comando.js

--- plugins/prueba-encuesta_comando.js
+++ plugins/prueba-encuesta_comando.js
@@ -7,17 +7,53 @@ handler.command = ['pruebapoll']
 export default handler*/
 
 const handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {
-let caption = '' 
+let caption = 'Selecciona una opción:' 
+let comandos = ['menu', 'estado', 'ping']
 
-
-const pollMessage = {
-name: cap,
-values: a,
+/*const pollMessage = {
+name: caption,
+values: comandos,
 multiselect: false,
 selectableCount: 1
 }
-await conn.sendMessage(m.chat, { poll: pollMessage })
+await conn.sendMessage(m.chat, { poll: pollMessage })*/
 
+
+conn.ev.on('messages.update', async chatUpdate => {
+for(const { key, update } of chatUpdate) {
+if(update.pollUpdates && key.fromMe) {
+const pollCreation = await getMessage(key)
+if(pollCreation) {
+const pollUpdate = await getAggregateVotesInPollMessage({
+message: pollCreation,
+pollUpdates: update.pollUpdates,
+})
+var toCmd = pollUpdate.filter(v => v.voters.length !== 0)[0]?.name
+if (toCmd == undefined) return
+var prefCmd = prefix+toCmd
+sock.appenTextMessage(prefCmd, chatUpdate)
+}}}})
+    
+async function getMessage(key){
+if (store) {
+const msg = await store.loadMessage(key.remoteJid, key.id)
+return msg?.message
+}
+return { conversation: "hola" }}
+
+conn.ev.on('messages.update', async chatUpdate => {
+for(const { key, update } of chatUpdate) {
+if (update.pollUpdates && key.fromMe) {
+const pollCreation = await getMessage(key)
+if (pollCreation) {
+const pollUpdate = await getAggregateVotesInPollMessage({message: pollCreation, pollUpdates: update.pollUpdates, })
+var toCmd = pollUpdate.filter(v => v.voters.length !== 0)[0]?.name
+if (toCmd == undefined) return
+var prefCmd = usedPrefix+toCmd
+sock.appenTextMessage(prefCmd, chatUpdate)
+}}}})
+
+conn.sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return conn.sendMessage(jid, { poll: { name, values, selectableCount }}) }
     
 }
 
