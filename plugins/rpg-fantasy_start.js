bc39fb5b31195c63ffed9579c95db6a9e8c1baa5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-19 19:48:15 -0500

    Update rpg-fantasy_start.js

--- plugins/rpg-fantasy_start.js
+++ plugins/rpg-fantasy_start.js
@@ -42,7 +42,29 @@ if (nombreImagen) {
 estado = `*${nombreImagen}* fue comprado por *${conn.getName(idUsuarioExistente)}*`
 }}
 
-let info = `*⛱️ FANTASÍA RPG ⛱️*\n*⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯*\n✓ *Nombre:* ${dato.name}\n✓ *Origen:* ${dato.desp}\n✓ *Costo:* \`\`\`${dato.price}\`\`\` *${rpgshop.emoticon('money')}*\n✓ *Clase:* ${dato.class}\n✓ *Tipo:* ${dato.type}\n✓ *ID:* \`\`\`${codigoActual}\`\`\`\n*⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯*\n✓ *Estado:* ${estado}`
+const personaje = dato.name
+const calificacionesPersonaje = usuarioExistente[idUsuarioExistente]?.flow.filter(voto => voto.character_name === personaje) || []
+
+const likes = calificacionesPersonaje.filter(voto => voto.like).length || 0
+const superlikes = calificacionesPersonaje.filter(voto => voto.superlike).length || 0
+const dislikes = calificacionesPersonaje.filter(voto => voto.dislike).length || 0
+
+const incrementos_like = Math.floor(likes / 3)
+const incrementos_superlike = Math.floor(superlikes / 3)
+const incrementos_dislike = Math.floor(dislikes / 3)
+
+const aumento_por_like = incrementos_like * 0.02 // 2% por cada 3 likes
+const aumento_por_superlike = incrementos_superlike * 0.05 // 5% por cada 3 superlikes
+const decremento_por_dislike = incrementos_dislike * 0.01 // -1% por cada 3 dislikes
+
+global.nuevoPrecio = dato.price + (dato.price * aumento_por_like) + (dato.price * aumento_por_superlike) - (dato.price * decremento_por_dislike)
+nuevoPrecio = Math.round(nuevoPrecio) // Nuevo precio a un entero
+if (nuevoPrecio < 50) {
+nuevoPrecio = 50
+}
+
+let txtNewPrice = nuevoPrecio ? `\n✓ *Precio anterior:* ~\`${dato.price}\`~ *${rpgshop.emoticon('money')}*\n✓ *Nuevo Precio:* \`${nuevoPrecio}\` *${rpgshop.emoticon('money')}*` : `\n✓ *Precio:* \`\`\`${dato.price}\`\`\` *${rpgshop.emoticon('money')}*`
+let info = `*⛱️ FANTASÍA RPG ⛱️*\n*⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯*\n✓ *Nombre:* ${dato.name}\n✓ *Origen:* ${dato.desp}${txtNewPrice}\n✓ *Clase:* ${dato.class}\n✓ *Tipo:* ${dato.type}\n✓ *ID:* \`\`\`${codigoActual}\`\`\`\n*⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯*\n✓ *Estado:* ${estado}`
 info += `\n\n${estado === 'Libre' ? '_Responde a este mensaje con "c", "🛒", o "🐱" para comprarlo_' : ''}`
 id_message = (await conn.sendFile(m.chat, dato.url, 'error.jpg', info.trim(), fkontak, true, {
 contextInfo: {
