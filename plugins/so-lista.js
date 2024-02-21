dbe895fc5608230c1f328e453675a71e53bc0cce
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-02-20 22:14:30 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/so-lista.js
+++ plugins/so-lista.js
@@ -26,7 +26,8 @@ this.sendPresenceUpdate('recording', m.chat)
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })} 
 
 if (chat.audios && m.text.match(/(bienveni|🥳|🤗)/gi)) {
-let vn = './media/Bienvenido.mp3'
+//let vn = './media/Bienvenido.mp3'
+let vn = 'https://qu.ax/cUYg.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `🐈`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen1, "sourceUrl": md, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
 
