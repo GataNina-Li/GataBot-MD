c4745c399e3c5407801029ba374fecc07a290905
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-02-28 15:19:43 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -622,7 +622,7 @@ ${margen}
 await conn.sendFile(m.chat, gataImg.getRandom(), 'lp.jpg', menu, fkontak, false, { contextInfo: {mentionedJid, externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: redesMenu.getRandom()}}})
 //conn.sendFile(m.chat, gataVidMenu.getRandom(), 'gata.mp4', menu, fkontak)
 const textoc = "Bloque de cita.";
-const bloqueCita = chalk.bgGray.dim.bold(`  ${textoc}  `)
+const bloqueCita = chalk.bgGray.dim.bold(`┃ ${textoc}`)
 
 console.log(bloqueCita);	
 } catch (e) {
