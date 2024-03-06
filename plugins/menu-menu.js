e43575179bf09f3a095c56348414bedca1657842
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-06 15:53:44 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -728,9 +728,12 @@ const commandsAI = [
 ]
 
 const commandsConfig = [
-{ comando: (m) => m?.isGroup ? (chat.welcome ? 'off' : 'on') : false, descripcion: (m) => m?.isGroup ? (chat.welcome ? '✅ Activado || ✅ Activated' : '❌ Desactivado || ❌ Disabled') : false, contexto: 'Establecer bienvenida en grupos', showPrefix: true },
+{ comando: (m) => m?.isGroup ? (chat.welcome ? 'off' : 'on') + 'bienvenida , welcome' : false, descripcion: (m) => m?.isGroup ? (chat.welcome ? '✅ ' + 'Activado || Activated' : '❌ ' + 'Desactivado || Disabled') : false, contexto: 'Establecer bienvenida en grupos', showPrefix: true },
+{ comando: (m) => m?.isGroup ? (chat.detect  ? 'off' : 'on') + 'avisos , detect' : false, descripcion: (m) => m?.isGroup ? (chat.detect  ? '✅ ' + 'Activado || Activated' : '❌ ' + 'Desactivado || Disabled') : false, contexto: 'Avisos importantes en grupos', showPrefix: true },
+{ comando: (m) => m?.isGroup ? (chat.detect  ? 'off' : 'on') + 'autonivel , autolevelup' : false, descripcion: (m) => m?.isGroup ? (chat.detect  ? '✅ ' + 'Activado || Activated' : '❌ ' + 'Desactivado || Disabled') : false, contexto: 'Subir de nivel automáticamente', showPrefix: true },
+{ comando: 'restringir , restrict', descripcion: (m) => bot.restrict ? '✅ ' + 'Activado || Activated' : '❌ ' + 'Desactivado || Disabled', contexto: 'Permisos para el Bot', showPrefix: true },
 ]
-
+ 
 /*
 ✓ _${usedPrefix}on *o* off *bienvenida | welcome*_
 ✓ _${usedPrefix}on *o* off *avisos | detect*_
