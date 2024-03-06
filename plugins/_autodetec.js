9e8e51cbe391da866451a8ea12326d3b19d22c09
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-03-05 21:45:16 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_autodetec.js
+++ plugins/_autodetec.js
@@ -19,7 +19,7 @@ for (const file of files) {
 if (file.includes(uniqid)) {
 await fs.unlink(path.join(sessionPath, file));
 filesDeleted++;
-console.log(`⚠️ Eliminacion session (PreKey) que provocan el undefined el chat`)}}
+console.log(`⚠️ Eliminacion session (PreKey) que provocan el undefined el chat`)}}}
 if (chat.detect && m.messageStubType == 21) {
 await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoAG']() + mid.smsAutodetec1(usuario, m), mentions: [m.sender], mentions: [...groupAdmins.map(v => v.id)] }, { quoted: fkontak })   
 } else if (chat.detect && m.messageStubType == 22) {
