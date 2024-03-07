c9a50f7fc19b0406d6e5e95d0f73bd6af4f48c79
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-03-07 19:17:15 -0300

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["5492266466080", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["573026191480", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
 ["593968585383"], 
-["593968263524"], ["5492266613038"], ["5217294888993"], ["51948705559"]]
+["593968263524"], ["5492266613038"], ["5217294888993"], ["5492266466080"], ["51948705559"]]
 
 global.mods = []
 global.prems = []
