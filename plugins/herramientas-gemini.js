35d52172e2627f311fd5f0ace68380ac7229d380
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-03-15 19:49:10 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/herramientas-gemini.js
+++ plugins/herramientas-gemini.js
@@ -1,7 +1,8 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) throw `INGRESA UN TEXTO!`
+if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘽𝘼𝙍𝘿\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`
 try {
+conn.sendPresenceUpdate('composing', m.chat);
 var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
 var res = await apii.json()
 await m.reply(res.result)
