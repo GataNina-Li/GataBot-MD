accff71a7e20bcd1a44a234dcafaba75fa995094
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-16 19:17:26 -0500

    Update sticker-removebg.js

--- plugins/sticker-removebg.js
+++ plugins/sticker-removebg.js
@@ -8,10 +8,7 @@ let json
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/image/g.test(mime) && !/webp/g.test(mime)) {
-const { key } = await conn.sendMessage(m.chat, { text: wait }, {quoted: m })
-await conn.sendMessage(m.chat, { text: waitt, edit: key })
-await conn.sendMessage(m.chat, { text: waittt, edit: key })
-await conn.sendMessage(m.chat, { text: waitttt, edit: key })
+await mensajesEditados(conn, m)
 let buffer = await q.download()
 let media = await (uploadImage)(buffer)
 json = await (await fetch(`https://aemt.me/removebg?url=${media}`)).json()
@@ -29,3 +26,11 @@ export default handler
 const isUrl = (text) => {
 return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|png)/, 'gi'))
 }
+
+async function mensajesEditados(conn, m) {
+const mensajes = [ waitt, waittt, waitttt, waittttt ]
+for (let i = 0; i < mensajes.length; i++) {
+const { key } = await conn.sendMessage(m.chat, { text: wait, quoted: m })
+await new Promise(resolve => setTimeout(resolve, 1000))
+await conn.sendMessage(m.chat, { text: mensajes[i], edit: key })
+}}
