09cc0af518e127a473159806ba8b27994019ac0d
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-03-17 19:12:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -220,6 +220,14 @@ if (global.db.data) await global.db.write()
 if (opts['autocleartmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 'tmp', "GataJadiBot"], tmp.forEach(filename => cp.spawn('find', [filename, '-amin', '2', '-type', 'f', '-delete'])))}, 30 * 1000)}
 if (global.obtenerQrWeb === 1) (await import('./server.js')).default(global.conn, PORT)
 
+async function getMessage(key) {
+if (store) {
+const msg = store.loadMessage(key.remoteJid, key.id)
+return msg.message
+} return {
+conversation: 'SimpleBot',
+}}
+
 async function connectionUpdate(update) {  
 const {connection, lastDisconnect, isNewLogin} = update
 global.stopped = connection
