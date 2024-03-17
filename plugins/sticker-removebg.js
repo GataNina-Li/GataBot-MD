3e1265a9fa021c30802ed4508041e94965019d37
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-16 19:29:21 -0500

    Update sticker-removebg.js

--- plugins/sticker-removebg.js
+++ plugins/sticker-removebg.js
@@ -27,7 +27,7 @@ const isUrl = (text) => {
 return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|png)/, 'gi'))
 }
 
-async function enviarMensajesEditados(conn, m) {
+async function mensajesEditados(conn, m) {
 const mensajes = [ waitt, waittt, waitttt, waittttt ]
 const { key } = await conn.sendMessage(m.chat, { text: wait, quoted: m })
 await new Promise(resolve => setTimeout(resolve, 1000))
