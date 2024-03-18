0520811d8117a717a91141574bbf9c5e8cbdd412
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-18 18:30:49 -0500

    Update buscador-yt_yts.js

--- plugins/buscador-yt_yts.js
+++ plugins/buscador-yt_yts.js
@@ -1,7 +1,6 @@
 import yts from 'yt-search';
 import fs from 'fs';
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 if (!text) throw `${lenguajeGB['smsAvisoMG']()}${mid.smsMalused4}`
 try {
 let vids_ = { 
