94a52b6a88aec4e0890f00d51a4872d4e7e914e5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-18 18:09:13 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,8 +1,7 @@
 import db from '../lib/database.js' //Esta función es para la versión LATAM
 let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
-const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
-}
+
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 if (isBotAdmin && chat.antifake) {
