18ff9106ff72b30ccf9cd803caceec18a54610a4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-18 02:49:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -165,12 +165,14 @@ console.log(chalk.bold.redBright(mid.methodCode11(chalk)))
   
 console.info = () => {} 
 //console.log = () => {}
-const originalConsoleLog = console.log
+const originalConsoleLog = console.log;
 console.log = function() {
 const message = arguments[0]
-if (typeof message === 'string' && (!message.includes("Closing stale open") || !message.includes("Closing open session"))) {
+if (typeof message === 'string' && message.includes("Closing stale open")) {
+console.log = () => {}
+}
 originalConsoleLog.apply(console, arguments)
-}}
+}
 
 const connectionOptions = {
 logger: pino({ level: 'silent' }),
