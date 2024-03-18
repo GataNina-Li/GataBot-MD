0a99d93afb5d0640eb2ad1c5631949a7545deb13
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-18 18:24:21 -0500

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐈', body: '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', sourceUrl: `https://github.com/GataNina-Li/GataBot-MD`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐈', body: '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', sourceUrl: md, thumbnail: imagen2}}})}
     
 return !0 }
 export default handler
