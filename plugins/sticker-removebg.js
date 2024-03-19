7a3ced474f99a02582eb3eefee5963c21b415bd3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-18 23:44:19 -0500

    Update sticker-removebg.js

--- plugins/sticker-removebg.js
+++ plugins/sticker-removebg.js
@@ -17,7 +17,7 @@ json = await (await fetch(`https://aemt.me/removebg?url=${text.trim()}`)).json()
 } else return m.reply(`*Responde a una imagen o ingresa una url que sea \`(jpg, jpeg o png)\` para quitar el fondo*`)
 
 await mensajesEditados(conn, m, key)
-await conn.sendMessage(m.chat, { text: waitttttt, edit: key })
+//await conn.sendMessage(m.chat, { text: waitttttt, edit: key })
 await conn.sendMessage(m.chat, { image: { url: json.url.result }, caption: null }, { quoted: m })
 await conn.sendFile(m.chat, stiker ? stiker : await sticker(false, json.url.result, global.packname, global.author), 'sticker.webp', '', null, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: '• STICKER •', mediaType: 2, sourceUrl: redesMenu.getRandom(), thumbnail: gataImg.getRandom()}}})
 }
@@ -29,10 +29,3 @@ const urlRegex = /^(https?):\/\/[^\s/$.?#]+\.(jpe?g|png)$/i
 return urlRegex.test(text)
 }
 
-async function mensajesEditados(conn, m, key) {
-const mensajes = [ waitt, waittt, waitttt, waittttt ]
-key = await conn.sendMessage(m.chat, { text: wait, quoted: m })
-for (let i = 0; i < mensajes.length; i++) {
-await new Promise(resolve => setTimeout(resolve, 1000))
-await conn.sendMessage(m.chat, { text: mensajes[i], edit: key })
-}}
