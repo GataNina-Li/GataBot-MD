64626d48efbb78155d942d200f5194d9788c0fc4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-19 00:09:19 -0500

    Update sticker-removebg.js

--- plugins/sticker-removebg.js
+++ plugins/sticker-removebg.js
@@ -3,7 +3,7 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text, args }) => {
 let stiker = false
-let json, key
+let json
 
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
@@ -16,7 +16,7 @@ stiker = await sticker(false, json.url.result, global.packname, global.author)
 json = await (await fetch(`https://aemt.me/removebg?url=${text.trim()}`)).json()
 } else return m.reply(`*Responde a una imagen o ingresa una url que sea \`(jpg, jpeg o png)\` para quitar el fondo*`)
 
-await mensajesEditados(conn, m, key)
+await mensajesEditados(conn, m)
 //await conn.sendMessage(m.chat, { text: waitttttt, edit: key })
 await conn.sendMessage(m.chat, { image: { url: json.url.result }, caption: null }, { quoted: m })
 await conn.sendFile(m.chat, stiker ? stiker : await sticker(false, json.url.result, global.packname, global.author), 'sticker.webp', '', null, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: '• STICKER •', mediaType: 2, sourceUrl: redesMenu.getRandom(), thumbnail: gataImg.getRandom()}}})
