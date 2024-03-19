8446b35da631c307968d75d3b281aac2bb53307b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-19 00:18:35 -0500

    Update herramientas-morce.js

--- plugins/herramientas-morce.js
+++ plugins/herramientas-morce.js
@@ -1,18 +1,4 @@
 let handler = async(m, { conn, usedPrefix, command, text }) => {
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
-}
 
   let enc = {
     "a": "•-",
