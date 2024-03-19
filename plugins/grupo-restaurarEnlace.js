f2b94d77ba335f84b47aa733943d7f0284fd481a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-19 00:04:03 -0500

    Update grupo-restaurarEnlace.js

--- plugins/grupo-restaurarEnlace.js
+++ plugins/grupo-restaurarEnlace.js
@@ -1,5 +1,4 @@
 let handler = async (m, { conn }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
 
 let res = await conn.groupRevokeInvite(m.chat)
