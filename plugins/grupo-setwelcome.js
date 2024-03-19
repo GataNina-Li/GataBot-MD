e838c9e3f85878a9d6da096185149157ffe91f9e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-19 00:05:21 -0500

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,5 +1,4 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 if (text) {
 global.db.data.chats[m.chat].sWelcome = text
 conn.reply(m.chat, lenguajeGB.smsSetW(), fkontak, m)
