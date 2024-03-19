6a1fa90f3b55a2b70afe77aa15026bb94d9f72a9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-18 23:57:17 -0500

    Update grupo-advertencia_eliminar.js

--- plugins/grupo-advertencia_eliminar.js
+++ plugins/grupo-advertencia_eliminar.js
@@ -1,5 +1,4 @@
 let handler = async (m, { conn, text, command, usedPrefix }) => {//prems 
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let lenGB = lenguajeGB.lenguaje() == 'en' ? usedPrefix + 'on antitoxic' : usedPrefix + 'on antitoxicos';
 if (!db.data.chats[m.chat].antitoxic && m.isGroup) return conn.sendButton(m.chat, wm, lenguajeGB.smsAdveu1() + lenGB, null, [[lenguajeGB.smsEncender(), lenGB]], fkontak, m) 
 
