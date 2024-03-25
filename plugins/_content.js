e1d15fb47743ab91a1e40262ddd0ce69bba441f5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-25 18:30:50 -0500

    Update _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -55,8 +55,8 @@ global.accountsgb = [canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths
 
 var canalesInfo = [
 { link: canal1, id: "120363160031023229@newsletter", name: "INFINITY-WA 💫" },
-{ link: canal2, id: "120363203805910750@newsletter", name: "GB - UPDATE ✨" },
-{ link: canal3, id: "120363169294281316@newsletter", name: "Tips sobre GataBot 🤩" }
+{ link: canal2, id: "120363169294281316@newsletter", name: "GB - UPDATE ✨" },
+{ link: canal3, id: "120363203805910750@newsletter", name: "Tips sobre GataBot 🤩" } 
 ]
 var indiceAleatorio = Math.floor(Math.random() * canalesInfo.length)
 var channelRD = canalesInfo[indiceAleatorio]
