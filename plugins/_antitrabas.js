3bc7e6dd5b50329bd9599d741f20f92b47310e60
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-26 18:02:14 -0500

    Update _antitrabas.js

--- plugins/_antitrabas.js
+++ plugins/_antitrabas.js
@@ -2,7 +2,9 @@
 //By @NeKosmic || https://github.com/NeKosmic/
 //
 import * as fs from 'fs'
-export async function before(m, { conn, isAdmin, isBotAdmin, text, participants, usedPrefix }) {
+
+let handler = m => m
+handler.before = async function (m, { conn, isAdmin, isBotAdmin, text, participants, usedPrefix }) {
 if (m.isBaileys && m.fromMe)
 return !0
 if (!m.isGroup) return !1
@@ -29,3 +31,4 @@ conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 }
 return !0
 } 
+export default handler
