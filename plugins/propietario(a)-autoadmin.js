84d879dd2f648d7d981e4cbb2d4f53bcd75e957e
Lucas <123712208+Jackhokage@users.noreply.github.com>
2024-03-31 15:16:29 -0500

    Delete plugins/propietario(a)-autoadmin.js

--- plugins/propietario(a)-autoadmin.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw `*𝙔𝘼 𝙀𝙎 𝘼𝘿𝙈𝙄𝙉 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝙈𝙄 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊(𝘼)*`
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^admin.|atad|autoadmin$/i
-handler.rowner = true
-handler.botAdmin = true
-export default handler
