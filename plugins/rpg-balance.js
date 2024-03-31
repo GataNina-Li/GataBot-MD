d464400000f7f75ee7d1ced7d9796e3971d52191
Lucas <123712208+Jackhokage@users.noreply.github.com>
2024-03-30 23:35:26 -0500

    Update rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -13,7 +13,7 @@ await m.reply(`
 ${usedPrefix}buy *cantidad*
 ${usedPrefix}buyall *cantidad*
 
-*COMPRAR DIAMANTES CON GATACOINS*
+*COMPRAR DIAMANTES CON ZCOINS*
 ${usedPrefix}buy2 *cantidad*
 ${usedPrefix}buyall2 *cantidad*`)
 
