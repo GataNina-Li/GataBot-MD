0403d85e2d3f5f6d4ccd240850984448320f55bb
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-04-01 17:10:46 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -8,7 +8,57 @@ let name = conn.getName(m.sender)
 if (chat.isBanned) return
 let vn = 'https://qu.ax/Ocxm.mp3'
 let bot = `${pickRandom([`*¡𝑬𝒚! 𝑨𝒒𝒖í 𝒆𝒔𝒕𝒐𝒚. 𝒀𝒐 𝒑𝒖𝒆𝒅𝒐 𝒂𝒚𝒖𝒅𝒂𝒓 👉👈 𝑯𝒆𝒚! 𝑰'𝒎 𝒉𝒆𝒓𝒆. 𝑰 𝒄𝒂𝒏 𝒉𝒆𝒍𝒑 🙌*`, `Aqui estoy | Here I am 😼`, `*Hola Aqui estoy yo puedo ayudar? | Hello, here I am, can I help? 😸*`])}`.trim()
+let txt = `*\`💫 Te presentamos un nuevo hosting: "Infinity-Wa Host"\`*
 
+*¿Muy lento tu nokia y necesitas tener activo tu bot 24/7?*
+
+> *Te tenemos la mejor opción para mantener activo tu bot 24/7, a precios muy accesibles. Es muy barato y todos pueden comprar.*
+
+*🚩Precios :*
+* 1GB, 100 CPU = 1Dolar
+* 2GB, 120 CPU = 2Dolar
+* 3GB, 140 CPU = 3Dolar
+* 4GB, 175 CPU = 4Dolar
+* 5GB, 200 CPU = 5 Dólar. 
+
+🟢 \`\`\`Información del Host\`\`\`
+
+💻 *Página:*
+https://host.panel-infinitywa.store
+
+🧡 *Canal de WhatsApp:*
+https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A
+
+💚 *Grupo:*
+https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
+
+🛍️ *Método de pago:*
+*• PayPal :* paypal.me/OfcGB
+*• Mercado pago, alías:* OficialGB
+*• Naranja x, alías:* OficialGL
+*• Yape (Perú) :* +51948705559
+
+*\`💙 Contactanos para más información o alquidir los servicios:\`*
+• https://www.facebook.com/elrebelde21
+• wa.me/527294888993
+• wa.me/5492964650915` 
+
+if (/^infinity|infinityWa|host|hosting$/i.test(m.text)) {
+ await conn.sendMessage(m.chat, { text: txt,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false, 
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: `🤖 𝐈𝐍𝐅𝐈𝐍𝐈𝐓𝐘𝐖𝐀-𝐇𝐎𝐒𝐓 🤖`,
+body: `✅ Hosting de Calidad`,
+"previewType": "PHOTO",
+thumbnailUrl: 'https://qu.ax/EQTd.jpg', 
+sourceUrl: accountsgb}}},
+{ quoted: fkontak})
+} 
+ 
 if (/^bot$/i.test(m.text)) {
 await conn.reply(m.chat, bot, m, fakeChannel)
 await conn.sendPresenceUpdate('recording', m.chat)    
