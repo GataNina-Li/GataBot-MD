7814c9777b75a72c57e6c89d6bfe1dd337ea17cd
Lucas <123712208+Jackhokage@users.noreply.github.com>
2024-04-01 22:20:05 -0500

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -178,7 +178,7 @@ console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗
 console.log(e)}}
 
 handler.command = /^(menu|menú|memu|memú|help\?)$/i
-handler.register = true
+handler.register = false
 export default handler
 
 const more = String.fromCharCode(8206)
