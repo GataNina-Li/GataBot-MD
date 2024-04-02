722e05aa765099e1e17a900f6f86f6fb4998f08d
Lucas <123712208+Jackhokage@users.noreply.github.com>
2024-04-01 22:18:47 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -573,7 +573,7 @@ console.log(e)}
 
 //handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayuda|commands|commandos|menucompleto|allmenu|allm|m|\?)$/i
 handler.command = /^(menucompleto|allmenu|\?)$/i
-handler.register = true
+handler.register = false
 export default handler
 
 const more = String.fromCharCode(8206)
