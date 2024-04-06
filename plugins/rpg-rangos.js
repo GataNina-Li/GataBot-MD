84e4eba38acedd524d2b20c69361810d1d16d268
Lucas <123712208+Jackhokage@users.noreply.github.com>
2024-04-06 15:19:22 -0500

    Update rpg-rangos.js
    Signed-off-by: Lucas <123712208+Jackhokage@users.noreply.github.com>

--- plugins/rpg-rangos.js
+++ plugins/rpg-rangos.js
@@ -171,7 +171,7 @@ ${role}
 *NOVATO(A) IV* 🪤
 *NOVATO(A) V* 🪤
 ╰━━━━━━━━━━━━━━━━━━━⬣`.trim()
-await conn.sendFile(m.chat, pp, 'gata.mp4', menu)
+await conn.sendFile(m.chat, pp, './src/rol.mp4', menu)
 //conn.sendHydrated(m.chat, menu, `𝙍𝘼𝙉𝙂𝙊𝙎 | ${wm}`, pp, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [['𝙈𝙚𝙣𝙪́ 𝙘𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 💫', '.allmenu'],['𝙏𝙤𝙥𝙨 | 𝙍𝙖𝙣𝙠𝙞𝙣𝙜 🏆', `${usedPrefix}top`],['𝙈𝙚𝙣𝙪 𝙋𝙧𝙞𝙣𝙘𝙞𝙥𝙖𝙡 | 𝙈𝙖𝙞𝙣 𝙢𝙚𝙣𝙪 ⚡', '#menu']], m,)
 }
 handler.help = ['infomenu'].map(v => v + 'able <option>')
