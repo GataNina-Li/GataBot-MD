c6b4a5c641f22901d83912ef411fb7258f90c3d5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-10 01:34:47 -0500

    Update grupo-condicion_user.js

--- plugins/grupo-condicion_user.js
+++ plugins/grupo-condicion_user.js
@@ -1,3 +1,6 @@
+import { cpp } from "country-phone-prefix"
+import { codeToEmoji } from 'emoji-country-flags'
+
 let handler = async (m, { conn, usedPrefix, command, isOwner, isAdmin, text }) => {
 let phoneNumbers, conditions
 if (!(isAdmin || isOwner)) return m.reply(`*No tienes autorizado usar este comando, solo es para mí Creador(a) y Admins*`)
@@ -14,7 +17,7 @@ _Este comando es para definir qué hacer si alguien específico entra al grupo o
 [3] Prohibir que pueda usar el Bot.
 [4] Que no pueda usar comandos +18 (aún no disponible)
 
-⚠️ Presta atención
+⚠️ *Presta atención*
 - Si agregas un prefijo (país) y una o más condiciones, el efecto aplicará para todos los que coincidan con ese prefijo.
 - Estas condiciones no aplicarán si el usuario ingresado es mi Creador(a) o Admins.
 - No es posible aplicar condiciones al Bot.
@@ -66,17 +69,89 @@ return m.reply('Debes separar el prefijo y la condición con "|".')
 }
 
 let data = {
+grupo: {
 usuario: m.quoted && m.quoted.sender ? m.quoted.sender : user.split(', ').map(u => u.trim()),
 condicion: conditions || (user ? user.split(',').map(numero => parseInt(numero.trim())) : text.split(',').map(numero => parseInt(numero.trim()))),
 admin: m.sender
+},
+prefijos: []
 }
+
 let jsonData = JSON.stringify(data, null, 2)
 console.log(user)
 console.log(jsonData)
 //global.db.data.chats[m.chat].sCondition = jsonData
 
+  
+if (/^(newprefijo)$/i.test(command)) {
+txt = `*Escribe uno o más prefijos de países que desees que los usuarios con dicho prefijo sean eliminados del grupo al ingresar*
+
+> *Nota:* Separa con comas los prefijos y no es necesiro usar el símbolo *"+"* por cada prefijo agregado
+
+*Ejemplo:* 
+\`${usedPrefix + command}\` +593, 54, 7
+
+⚠️ *Presta atención*
+- Es posible que los prefijos que agregues puedan eliminar a usuarios que coincidan incluso los que ya llevan tiempo en el grupo
+`.trim()
+if (!text) return m.reply(txt)
+  
+let prefijos = text
+let prefijosArray = prefijos.split(',').map(prefijo => prefijo.trim())
+let encontrados = []
+let noEncontrados = []
+let paisesPorPrefijo = {}
+let codeTxt = ''
+let noCodeTxt = ''
+
+prefijosArray.forEach(prefijo => {
+prefijo = prefijo.startsWith('+') ? prefijo : `+${prefijo}`;
+let encontrado = false;
+for (let country in cpp) {
+if (cpp[country].prefix === prefijo) {
+let countryCode = cpp[country].iso2
+codeTxt += `El prefijo *${prefijo}* corresponde al país \`\`\`${country}\`\`\` \`(${countryCode})\``
+encontrados.push({ prefix: prefijo, country, countryCode })
+if (!paisesPorPrefijo[prefijo]) {
+paisesPorPrefijo[prefijo] = []
 }
-handler.command = /^(newcondicion)$/i
+paisesPorPrefijo[prefijo].push({ country, countryCode })
+encontrado = true
+}}
+
+if (!encontrado) {
+noCodeTxt += `No se encontró ningún país con el prefijo ${prefijo}`
+noEncontrados.push(prefijo)
+}})
+
+if (encontrados.length > 0) {
+codeTxt += `\n\n> *Se encontró el siguiente país para cada prefijo:*`
+let prefijosMostrados = new Set()
+encontrados.forEach((encontrado) => {
+const { prefix, country, countryCode } = encontrado
+const emoji = codeToEmoji(countryCode);
+if (!prefijosMostrados.has(prefix)) {
+codeTxt += `*El prefijo ${prefix} corresponde al país ${country} (${countryCode}):* ${emoji}`
+prefijosMostrados.add(prefix)
+}})
+} else {
+noCodeTxt += `\n\nNo se encontraron países con los prefijos especificados`
+}
+
+for (let prefijo in paisesPorPrefijo) {
+codeTxt +=`\n\n> *Países con el prefijo ${prefijo}:*`
+paisesPorPrefijo[prefijo].forEach((pais) => {
+const { country, countryCode } = pais
+const emoji = codeToEmoji(countryCode)
+noCodeTxt += `\`${country} (${countryCode}):\` ${emoji}`
+})
+}
+if (noEncontrados.length > 0) {
+noCodeTxt += `\n\nLos siguientes prefijos no coinciden con ningún país: ${noEncontrados.join(', ')}`
+}
+  
+}}
+handler.command = /^(newcondicion|newprefijo)$/i
 handler.group = true
 export default handler
 
