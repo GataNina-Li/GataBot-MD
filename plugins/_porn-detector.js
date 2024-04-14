c9a40479298c9189bcb9561773c1e8b92628b369
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-14 10:33:44 -0500

    Update _porn-detector.js

--- plugins/_porn-detector.js
+++ plugins/_porn-detector.js
@@ -32,7 +32,7 @@ link = await uploadImage(buffer)
 
 if (m.mtype == 'stickerMessage') {
 media = await q.download()
-let buffer = webp2png(media)
+buffer = webp2png(media)
 //buffer = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
 link = await uploadImage(buffer)
 }
