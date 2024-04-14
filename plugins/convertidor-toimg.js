ad359e8974dfdd338eef6d2dbb889adac37edd5b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-14 07:59:31 -0500

    Update convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,7 +1,7 @@
 import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 const notStickerMessage = `${lenguajeGB['smsAvisoMG']()}${mid.smsconvert3} *${usedPrefix + command}*`
-if (!m.quoted) return m.reply(notStickerMessage)
+//if (!m.quoted) return m.reply(notStickerMessage)
 const q = m.quoted || m
 let mime = q.mediaType || ''
 if (!/sticker/.test(mime)) return m.reply(notStickerMessage)
