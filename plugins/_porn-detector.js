093c7646ceb2937c6471fb0e89833bb52a46aee7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-16 02:41:47 -0500

    Update _porn-detector.js

--- plugins/_porn-detector.js
+++ plugins/_porn-detector.js
@@ -55,7 +55,7 @@ link = false
 })
 } else {
 link = false
-}}} else return 
+} else return 
 
 if (link) {
 const response = await fetch(`https://api.alyachan.dev/api/porn-detector?image=${link}&apikey=GataDios`)
