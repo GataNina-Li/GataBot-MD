092fc2fde6e74031424f84e197e1583ecc321e0b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-16 00:28:04 -0500

    Update _porn-detector.js

--- plugins/_porn-detector.js
+++ plugins/_porn-detector.js
@@ -3,6 +3,7 @@ import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
 import fetch from 'node-fetch'
 import axios from 'axios'
+import path from 'path'
 
 let handler = m => m
 handler.before = async function (m, { conn, __dirname, isBotAdmin }) {
