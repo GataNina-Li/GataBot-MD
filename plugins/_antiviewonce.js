3fbe9ec278c91e8ccaf5e83d75249c177d74530b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-17 22:48:04 -0500

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -2,33 +2,29 @@ let { downloadContentFromMessage } = (await import(global.baileys))
 
 let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
+
 const { antiver, isBanned } = global.db.data.chats[m.chat]
-let msg = m.msg.mimetype//m.message.viewOnceMessageV2.message
- 
-//if (!antiver || isBanned || !m.mtype || !(m.mtype == 'viewOnceMessageV2')) return
-try {
-const type = Object.keys(msg)[0]
-const media = await downloadContentFromMessage(msg[type], type)
+if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return
+if (!antiver || isBanned || !m.mtype || !(m.mtype == 'viewOnceMessageV2')) return
+if (m.mtype == 'viewOnceMessageV2') {
+let msg = m.message.viewOnceMessageV2.message
+let type = Object.keys(msg)[0]
+let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : type == 'videoMessage' ? 'video' : 'audio')
 let buffer = Buffer.from([])
 for await (const chunk of media) {
-buffer = Buffer.concat([buffer, chunk])
-}
+buffer = Buffer.concat([buffer, chunk])}
 
 const fileSize = formatFileSize(m.msg.fileLength)
 const description = `
-🕵️‍♀️ *ANTI VER UNA VEZ* 🕵️
-
+🕵️‍♀️ *ANTI VER UNA VEZ* 🕵️\n
 🚫 *No se permite ocultar* \`${type === 'imageMessage' ? 'Imagen 📷' : type === 'videoMessage' ? 'Vídeo 🎥' : type === 'audioMessage' ? 'Audio 🔊' : 'este mensaje'}\`
 - *Tamaño:* \`${fileSize}\`
 - *Usuario:* *@${m.sender.split('@')[0]}*
 - *Texto:* ${m.msg.caption || 'Ninguno'}`.trim()
 
- if (/image|video|audio/.test(type)) return await conn.sendFile(m.chat, buffer, msg[type], description || type, m, false, { mentions: [m.sender] })
- } catch (error) {
- return console.log(error)
- }
+if (/image|video|audio/.test(type)) return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : type == 'videoMessage' ? 'error.mp4' : 'error.mp3', description, m, false, { mentions: [m.sender] })
 
-}
+}}
 export default handler
 
 function formatFileSize(bytes) {
