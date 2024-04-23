416e5e6b974b25ccceb7766cf64c438ac9fef0d4
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-04-23 08:08:55 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -24,10 +24,16 @@ let txt = `*\`💫 Te presentamos un nuevo hosting: "Infinity-Wa Host"\`*
 🟢 \`\`\`Información del Host\`\`\`
 
 💻 *Página:*
-https://host.panel-infinitywa.store
+https://live.panel-infinitywa.store
+
+*🟢 Dashboard:*
+https://dashboard.infinitywa.xyz
+
+*⚙️ Bot de Support: exclusivo para host*
+https://wa.me/message/FETBF7YBO37CG1
 
 🧡 *Canal de WhatsApp:*
-https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A
+${canal1}
 
 💚 *Grupo:*
 https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
@@ -135,4 +141,3 @@ export default handler
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]
 }
-
