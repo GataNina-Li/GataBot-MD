74ddd2f8a08d7fbbed0ff4a2ae63d9ac71ccebbd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-28 02:31:11 -0500

    Update prueba-message.js

--- plugins/prueba-message.js
+++ plugins/prueba-message.js
@@ -68,39 +68,36 @@ var handler = async (m, { conn, usedPrefix }) => {
 }, {})*/
 
 let msg = generateWAMessageFromContent(m.chat, {
-    viewOnceMessage: {
-        message: {
-            "messageContextInfo": {
-                "deviceListMetadata": {},
-                "deviceListMetadataVersion": 2
-            },
-            interactiveMessage: proto.Message.InteractiveMessage.create({
-                body: proto.Message.InteractiveMessage.Body.create({
-                    text: "test"
-                }),
-                footer: proto.Message.InteractiveMessage.Footer.create({
-                    text: "test"
-                }),
-                header: proto.Message.InteractiveMessage.Header.create({
-                    title: "test",
-                    subtitle: "test",
-                    hasMediaAttachment: false
-                }),
-                nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-                    buttons: [
-                        {
-                            "name": "cta_copy",
-                            "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
-                        }
-                    ]
-                })
+    message: {
+        "messageContextInfo": {
+            "deviceListMetadata": {},
+            "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+            body: proto.Message.InteractiveMessage.Body.create({
+                text: "test"
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.create({
+                text: "test"
+            }),
+            header: proto.Message.InteractiveMessage.Header.create({
+                title: "test",
+                subtitle: "test",
+                hasMediaAttachment: false
+            }),
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+                buttons: [
+                    {
+                        "name": "cta_copy",
+                        "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
+                    }
+                ]
             })
-        }
+        })
     }
 }, {})
 
 await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id })
-
 
 }
 handler.command = /^(mboton)$/i
