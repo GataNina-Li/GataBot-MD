18ffea2491396b3988ad668e17a0dbc0d2bc51f7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-28 02:45:23 -0500

    Update prueba-message.js

--- plugins/prueba-message.js
+++ plugins/prueba-message.js
@@ -3,7 +3,7 @@ const { generateWAMessageFromContent, proto } = pkg
 
 var handler = async (m, { conn, usedPrefix }) => {
 
-/*let msg = generateWAMessageFromContent(m.chat, {
+let msg = generateWAMessageFromContent(m.chat, {
   viewOnceMessage: {
     message: {
         "messageContextInfo": {
@@ -65,36 +65,6 @@ var handler = async (m, { conn, usedPrefix }) => {
         })
     }
   }
-}, {})*/
-
-let msg = generateWAMessageFromContent(m.chat, {
-    message: {
-        "messageContextInfo": {
-            "deviceListMetadata": {},
-            "deviceListMetadataVersion": 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-            body: proto.Message.InteractiveMessage.Body.create({
-                text: "test"
-            }),
-            footer: proto.Message.InteractiveMessage.Footer.create({
-                text: "test"
-            }),
-            header: proto.Message.InteractiveMessage.Header.create({
-                title: "test",
-                subtitle: "test",
-                hasMediaAttachment: false
-            }),
-            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-                buttons: [
-                    {
-                        "name": "cta_copy",
-                        "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
-                    }
-                ]
-            })
-        })
-    }
 }, {})
 
 await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id })
