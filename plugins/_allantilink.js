231d4e1ce2c39d1173a189f7107389c737ad036d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-04 04:29:58 -0500

    `Adaptado a (mid) Multi idioma dinámico`

--- plugins/_allantilink.js
+++ plugins/_allantilink.js
@@ -5,6 +5,9 @@ const isLinkTel = /telegram.com|t.me/i
 const isLinkFb = /facebook.com|fb.me/i 
 const isLinkIg = /instagram.com/i 
 const isLinkTw = /twitter.com|x.com/i 
+const isLinkDc = /discord.com|discord.gg/i 
+const isLinkTh = /threads.net/i 
+const isLinkTch = /twitch.tv/i
   
 let handler = m => m
 handler.before = async function (m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
@@ -24,14 +27,17 @@ const isAntiLinkTel = isLinkTel.exec(m.text)
 const isAntiLinkFb = isLinkFb.exec(m.text)
 const isAntiLinkIg = isLinkIg.exec(m.text)
 const isAntiLinkTw = isLinkTw.exec(m.text)
+const isAntiLinkDc = isLinkDc.exec(m.text)
+const isAntiLinkTh = isLinkTh.exec(m.text)
+const isAntiLinkTch = isLinkTch.exec(m.text)
  
 if (chat.antiTiktok && isAntiLinkTik) {  
 if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)   
 if (isBotAdmin && bot.restrict) {
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mTiktok} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-// if (remove[0].status === '404') return
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
 } else if (!isBotAdmin) {
 return m.reply(mid.mAdvertencia + mid.mAdmin)
 } else if (!bot.restrict) {
@@ -43,8 +49,8 @@ if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)
 if (isBotAdmin && bot.restrict) {
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mYoutube} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-// if (remove[0].status === '404') return
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
 } else if (!isBotAdmin) {
 return m.reply(mid.mAdvertencia + mid.mAdmin)
 } else if (!bot.restrict) {
@@ -56,8 +62,8 @@ if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)
 if (isBotAdmin && bot.restrict) {
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mTelegram} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-// if (remove[0].status === '404') return
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
 } else if (!isBotAdmin) {
 return m.reply(mid.mAdvertencia + mid.mAdmin)
 } else if (!bot.restrict) {
@@ -69,8 +75,8 @@ if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)
 if (isBotAdmin && bot.restrict) {
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mFacebook} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-// if (remove[0].status === '404') return 
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return 
 } else if (!isBotAdmin) {
 return m.reply(mid.mAdvertencia + mid.mAdmin)
 } else if (!bot.restrict) {
@@ -82,8 +88,8 @@ if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)
 if (isBotAdmin && bot.restrict) {
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mInstagram} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-// if (remove[0].status === '404') return 
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return 
 } else if (!isBotAdmin) {
 return m.reply(mid.mAdvertencia + mid.mAdmin)
 } else if (!bot.restrict) {
@@ -95,13 +101,54 @@ if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)
 if (isBotAdmin && bot.restrict) {
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mX} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-// if (remove[0].status === '404') return
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
 } else if (!isBotAdmin) {
 return m.reply(mid.mAdvertencia + mid.mAdmin)
 } else if (!bot.restrict) {
 return m.reply(mid.mAdvertencia + mid.mOwner)
 }}
+
+if (chat.antiDiscord && isAntiLinkDc) {
+if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)   
+if (isBotAdmin && bot.restrict) {
+await conn.reply(m.chat, `${mid.mAdvertencia + mid.mDiscord} *@${toUser}*`, null, { mentions: [aa] })
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+/if (remove[0].status === '404') return
+} else if (!isBotAdmin) {
+return m.reply(mid.mAdvertencia + mid.mAdmin)
+} else if (!bot.restrict) {
+return m.reply(mid.mAdvertencia + mid.mOwner)
+}}
+
+if (chat.antiThreads && isAntiLinkTh) {
+if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)   
+if (isBotAdmin && bot.restrict) {
+await conn.reply(m.chat, `${mid.mAdvertencia + mid.mThreads} *@${toUser}*`, null, { mentions: [aa] })
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
+} else if (!isBotAdmin) {
+return m.reply(mid.mAdvertencia + mid.mAdmin)
+} else if (!bot.restrict) {
+return m.reply(mid.mAdvertencia + mid.mOwner)
+}}
+
+if (chat.antiTwitch && isAntiLinkTch) {
+if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)   
+if (isBotAdmin && bot.restrict) {
+await conn.reply(m.chat, `${mid.mAdvertencia + mid.mTwitch} *@${toUser}*`, null, { mentions: [aa] })
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
+} else if (!isBotAdmin) {
+return m.reply(mid.mAdvertencia + mid.mAdmin)
+} else if (!bot.restrict) {
+return m.reply(mid.mAdvertencia + mid.mOwner)
+}}
+
+  
 return !0
 }
 export default handler
