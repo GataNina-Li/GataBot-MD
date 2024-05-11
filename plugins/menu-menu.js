dcf73d70997e6c1b233f348881a37c9ef028aedb
EdderBot02 <107139979+EdderBot02@users.noreply.github.com>
2024-05-11 09:07:40 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -759,6 +759,7 @@ const commandsGames = [
 { comando: 'pelicula', descripcion: false, contexto: 'Descubre la película con emojis 🎬', showPrefix: true },
 { comando: 'adivinanza', descripcion: false, contexto: 'Adivina adivinador 🧞‍♀️', showPrefix: true },
 { comando: 'ruleta', descripcion: false, contexto: 'Suerte inesperada 💫', showPrefix: true },
+{ comando: 'ruletadelban', descripcion:false, contexto: 'Elimina un usuario al azar, solo para admins ☠️', showPrefix: true }
 ]
 const commandsAI = [
 { comando: 'simi', descripcion: '[texto] || [text]', contexto: 'Conversa con SimSimi', showPrefix: true },
