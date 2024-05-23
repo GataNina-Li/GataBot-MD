b4d318cdc6e53a8ef30dc559244cca3bcae35587
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-23 14:12:58 -0500

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -11,9 +11,9 @@ const user = `@${m.sender.split`@`[0]}`
 //const groupAdmins = participants.filter(p => p.admin)
 //const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
 let bot = global.db.data.settings[this.user.jid] || {}
-const isGroupLink = linkRegex.exec(m.text)
-if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)  
+const isGroupLink = linkRegex.exec(m.text) 
 if (chat.antiLink2 && isGroupLink) {
+if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete) 
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 const linkThisGroup2 = `https://www.youtube.com/`
