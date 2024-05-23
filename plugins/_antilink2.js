0d992f7ec94e61646edc14431a63ebecdd3499f9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-23 14:06:59 -0500

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -1,18 +1,19 @@
-let linkRegex = /https:/i
+let linkRegex = /https?:/i
 let handler = m => m
 handler.before = async function (m, { isAdmin, isBotAdmin, text, participants }) {
-if (m.isBaileys && m.fromMe)
-return !0
+//if (isAdmin || isOwner || isROwner || m.fromMe) return
+if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete)  
+if (m.isBaileys && m.fromMe) return
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 let delet = m.key.participant
 let bang = m.key.id
-const user = `@${m.sender.split`@`[0]}`;
-const groupAdmins = participants.filter(p => p.admin)
-const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
+const user = `@${m.sender.split`@`[0]}`
+//const groupAdmins = participants.filter(p => p.admin)
+//const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
-if (chat.antiLink2 && isGroupLink && !isAdmin) {
+if (chat.antiLink2 && isGroupLink) {
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 const linkThisGroup2 = `https://www.youtube.com/`
@@ -20,15 +21,14 @@ const linkThisGroup3 = `https://youtu.be/`
 if (m.text.includes(linkThisGroup)) return !0
 if (m.text.includes(linkThisGroup2)) return !0
 if (m.text.includes(linkThisGroup3)) return !0 
-}    
-await conn.sendMessage(m.chat, {text: `${lenguajeGB['smsEnlaceWatt']()} ${user}`, mentions: [m.sender]}, {quoted: m})
-//await conn.sendButton(m.chat, `${lenguajeGB['smsEnlaceWatt']()} ${await this.getName(m.sender)} ${isBotAdmin ? '' : `\n\n${lenguajeGB['smsAvisoFG']()}${lenguajeGB['smsAllAdmin']()}`}`, wm, [`${lenguajeGB['smsApagar']()}`, '/disable antilink'], m)    
-if (!isBotAdmin) return conn.sendMessage(m.chat, {text: `*⛔ ${lenguajeGB.smsAddB4()} ⛔*\n${listAdmin}\n\n${lenguajeGB['smsAllAdmin']()}`, mentions: [...groupAdmins.map(v => v.id)] }, {quoted: m})
-//m.reply(`${lenguajeGB['smsAvisoFG']()} ${lenguajeGB['smsAllAdmin']()}`)  
+}     
+if (!isBotAdmin) return m.reply(mid.mAdvertencia + mid.mAdmin)
 if (isBotAdmin) {
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!bot.restrict) return m.reply(`${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsSoloOwner']()}`)
-}
+await conn.sendMessage(m.chat, { text: `${mid.mAdvertencia + mid.mWhatsApp2} *${user}*`, mentions: [m.sender] }, { quoted: m })    
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+//if (remove[0].status === '404') return
+}}
 return !0
 }
 export default handler
