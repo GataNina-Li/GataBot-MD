8b91f774f679b78d5924cf6e4082c1650094f60d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-24 03:57:32 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,4 +1,4 @@
-//Esta función es para la versión LATAM
+// Esta función es para la versión LATAM
 let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
 
@@ -8,7 +8,7 @@ if (isBotAdmin && chat.antifake) {
 let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
 
 if (m.sender.startsWith('6') || m.sender.startsWith('9') ||  m.sender.startsWith('7') ||  m.sender.startsWith('4') || m.sender.startsWith('2')) {
-await conn.reply(m.chat, texto, m);
+await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
 }}
