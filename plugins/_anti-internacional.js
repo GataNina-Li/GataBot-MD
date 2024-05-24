98ffa7b9c548ccb63e7a7a64b0e1fda1b044712c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-24 04:07:15 -0500

    `Nuevo (beta): Eliminar usuarios con prefijos específicos o predeterminados`

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,4 +1,9 @@
 // Esta función es para la versión LATAM
+
+import fs from 'fs'
+const rutaArchivo = './prefijos.json'
+const existeArchivo = fs.existsSync(rutaArchivo)
+
 let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
 
@@ -7,9 +12,24 @@ let chat = global.db.data.chats[m.chat]
 if (isBotAdmin && chat.antifake) {
 let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
 
+if (existeArchivo) {
+try {
+const contenido = fs.readFileSync(rutaArchivo, 'utf-8')
+const prefijos = JSON.parse(contenido)
+if (Array.isArray(prefijos)) {
+const comienzaConPrefijo = prefijos.some(prefijo => m.sender.startsWith(prefijo))
+if (comienzaConPrefijo) {
+await conn.reply(m.chat, texto, m)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+}} 
+} catch (error) {
+console.log('Error al leer o procesar el archivo prefijos.json:', error)
+return
+}} else {
 if (m.sender.startsWith('6') || m.sender.startsWith('9') ||  m.sender.startsWith('7') ||  m.sender.startsWith('4') || m.sender.startsWith('2')) {
 await conn.reply(m.chat, texto, m)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+}
 
 }}
 export default handler
