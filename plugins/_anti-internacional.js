54d07c62cbab1bcfb9c1230c52ea1a31453ec59d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-26 01:35:13 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -16,12 +16,11 @@ if (existeArchivo) {
 try {
 const contenido = fs.readFileSync(rutaArchivo, 'utf-8')
 const prefijos = JSON.parse(contenido)
-//if (Array.isArray(prefijos)) {
 const comienzaConPrefijo = prefijos.some(prefijo => m.sender.startsWith(prefijo))
 if (comienzaConPrefijo) {
 await conn.sendMessage(m.chat, { text: texto, mentions: [m.sender] }, { quoted: m })
 //await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-}//} 
+}
 } catch (error) {
 console.log('Error al leer o procesar el archivo prefijos.json:', error)
 return
