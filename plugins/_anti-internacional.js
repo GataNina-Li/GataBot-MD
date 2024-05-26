c5bf061753935aaad8520283c88b9dac5f3ae710
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-26 03:56:10 -0500

    `Adaptado a (mid) Multi idioma dinámico`

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -6,11 +6,13 @@ const existeArchivo = fs.existsSync(rutaArchivo)
 
 let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
+let delet = m.key.participant
+let bang = m.key.id
 
 if (!m.isGroup) return 
 let chat = global.db.data.chats[m.chat]
-if (isBotAdmin && chat.antifake) {
-let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
+if (isBotAdmin && chat.antifake && !isAdmin) {
+let texto = mid.mAdvertencia + mid.mFake
 
 if (existeArchivo) {
 try {
@@ -19,19 +21,20 @@ const prefijos = JSON.parse(contenido)
 const comienzaConPrefijo = prefijos.some(prefijo => m.sender.startsWith(prefijo))
 if (comienzaConPrefijo) {
 await conn.sendMessage(m.chat, { text: texto, mentions: [m.sender] }, { quoted: m })
-//let responseb = await this.groupParticipantsUpdate(id, [user], 'remove')
-//if (responseb[0].status === "404") return   
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+let responseb = await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
 }
 } catch (error) {
-console.log('Error al leer o procesar el archivo prefijos.json:', error)
+console.log('Error "prefijos.json": ', error)
 return
 }} else {
 if (m.sender.startsWith('6') || m.sender.startsWith('9') ||  m.sender.startsWith('7') ||  m.sender.startsWith('4') || m.sender.startsWith('2')) {
 await conn.sendMessage(m.chat, { text: texto, mentions: [m.sender] }, { quoted: m })
-//let responseb = await this.groupParticipantsUpdate(id, [user], 'remove')
-//if (responseb[0].status === "404") return   
-}
-}
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+let responseb = await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+}}
 
 }}
 export default handler
