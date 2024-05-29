7c1950605ad78affc5ae54ea6df0de7c60682a40
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-29 00:08:52 -0500

    `Adaptado a (mid) Multi idioma dinámico`

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -5,11 +5,14 @@ const rutaArchivo = './prefijos.json'
 const existeArchivo = fs.existsSync(rutaArchivo)
 
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
+handler.before = async function (m, { conn, isAdmin, isOwner, isROwner, isBotAdmin } ) {
+if (!m.isGroup) return 
+if (m.fromMe) return
+if (isAdmin || isOwner || isROwner || m.fromMe || !isBotAdmin) return
+  
 let delet = m.key.participant
 let bang = m.key.id
 
-if (!m.isGroup) return 
 let chat = global.db.data.chats[m.chat]
 if (isBotAdmin && chat.antifake && !isAdmin) {
 let texto = mid.mAdvertencia + mid.mFake(m)
@@ -22,8 +25,8 @@ const comienzaConPrefijo = prefijos.some(prefijo => m.sender.startsWith(prefijo)
 if (comienzaConPrefijo) {
 await conn.sendMessage(m.chat, { text: texto, mentions: [m.sender] }, { quoted: m })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let responseb = await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-//if (responseb[0].status === "404") return   
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
 }
 } catch (error) {
 console.log('Error "prefijos.json": ', error)
@@ -32,8 +35,8 @@ return
 if (m.sender.startsWith('6') || m.sender.startsWith('9') ||  m.sender.startsWith('7') ||  m.sender.startsWith('4') || m.sender.startsWith('2')) {
 await conn.sendMessage(m.chat, { text: texto, mentions: [m.sender] }, { quoted: m })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let responseb = await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-//if (responseb[0].status === "404") return   
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
 }}
 
 }}
