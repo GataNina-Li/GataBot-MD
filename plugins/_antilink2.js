bbcdebc279dce1f79433002773774feb83c8ab47
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-29 00:03:47 -0500

    `Adaptado a (mid) Multi idioma dinámico`

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -3,9 +3,10 @@
 let linkRegex = /\b((https?:\/\/|www\.)?[\w-]+\.[\w-]+(?:\.[\w-]+)*(\/[\w\.\-\/]*)?)\b/i
 let handler = m => m
 handler.before = async function (m, { isAdmin, isBotAdmin, isOwner, isROwner, participants }) {
+if (!m.isGroup) return 
+if (m.fromMe) return
 if (isAdmin || isOwner || isROwner || m.fromMe || !isBotAdmin) return
-if (m.isBaileys && m.fromMe) return
-if (!m.isGroup) return !1
+
 let chat = global.db.data.chats[m.chat]
 let delet = m.key.participant
 let bang = m.key.id
@@ -15,7 +16,7 @@ const user = `@${m.sender.split`@`[0]}`
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text) 
 if (chat.antiLink2 && isGroupLink !== null) {
-if (chat.delete) return m.reply(mid.mAdvertencia + mid.mAntiDelete) 
+if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m }) 
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
