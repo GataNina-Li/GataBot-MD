7ef7984bcd6a27c5bdcd2edfb0c4a55a0064f05e
Gladysgi <gzgonzalezpinto@gmail.com>
2024-06-02 15:31:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ import { es as esDefault, en as enDefault } from "./lib/multi-language/_default.
 import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-// [ES] > Agrega el número que será Bot y los que serán propietarios.
+// [ES] > ["0981011942"]Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
 ["573147616444", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
