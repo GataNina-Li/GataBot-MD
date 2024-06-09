11040896ec0b093f8360d68d8d51ea9f9b5750d3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 12:59:43 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 
-/*const messages = [
+const messages = [
 [
 'Descripción 1', 
 'Footer 1',
@@ -9,15 +9,12 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 'Texto para copiar 1',
 [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
 [[
-            'Lista',
-            'Sección ',
-            'Titulo',
-            'Tema',
-            'Descripcion',
-            'Id',
-            
-                
-            
+'Lista',
+'Sección ',
+'Titulo',
+'Tema',
+'Descripcion',
+'Id',        
 ]]],
 [
 'Descripción 2',
@@ -33,25 +30,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 'Tema',  
 'Descripcion', 
 'Id',
-]]]]*/
-
-const messages = [
-    [
-        'Descripción 1', // Descripción general del mensaje
-        'Footer 1',      // Pie de página del mensaje
-        'https://telegra.ph/file/b31cd03f716d362b33716.jpg', // URL de la imagen
-        [['Botón 1', 'id1'], ['Botón 2', 'id2']], // Botones de acción
-        'Texto para copiar 1', // Texto que se puede copiar
-        [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']], // Enlaces
-        [[['Lista',
-            'Sección ',
-            'Titulo',
-            'Tema',
-            'Descripcion',
-            'Id',
-    ]]]]],
-];
-
+]]]]
   
 await conn.sendCarousel(m.chat, 'Texto', 'Linea', 'TEXTO', messages, m)
 }
