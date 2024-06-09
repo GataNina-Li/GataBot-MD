fdff482b20fbb696f3cdcda380a06ccae52e636a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 08:11:54 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -20,7 +20,7 @@ const messages = [
     ['Header opción 1'], ['Opción 1', 'Descripción opción 1', 'id_opcion_1'],
     ['Header opción 2'], ['Opción 2', 'Descripción opción 2', 'id_opcion_2']
   ]
-],
+]],
   [
     'Descripción 2',
     'Footer 2',
