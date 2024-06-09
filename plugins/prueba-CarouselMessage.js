fabe8cf2f02b0333e7e4c1d6b7bc6579806d1ad8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 07:43:10 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -16,10 +16,10 @@ const messages = [
     [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
     [['Lista 1', [
       
-        ['Header opción 1', 'Opción 1', 'Descripción opción 1', 'id_opcion_1'], // Elemento de la lista
+        ['Header opción 1', 'Opción 1', 'Descripción opción 1', 'id_opcion_1', // Elemento de la lista
         ['Header opción 2', 'Opción 2', 'Descripción opción 2', 'id_opcion_2']  // Otro elemento de la lista
       
-    ]]]
+    ]]]]
   ],
   [
     'Descripción 2',
