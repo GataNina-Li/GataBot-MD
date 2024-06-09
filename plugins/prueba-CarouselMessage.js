60ae88be9f45da827177d0cab9882aa49fbe63a3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 16:28:08 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -3,6 +3,7 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 
 // MENSAJE CARUSEL CON TODOS LOS BOTONES DISPONIBLES
+// Si las ids no te funciona con usedPrefix, tendrás que definirlas, ejemplo /menu
 const sections = [{
 title: `Título de la sección`,
 rows: [
@@ -11,42 +12,41 @@ rows: [
 { header: 'Encabezado3', title: "Título3", description: 'Descripción3', id: "Id3" }, 
 { header: 'Encabezado4', title: "Título4", description: 'Descripción4', id: "Id4" }, 
 ]},]  
-const messages = [[ // CARUSEL 1
-'Descripción de Carusel 1', 
-'Footer de Carusel 1',
+const messages = [[ // CARRUSEL 1
+'Descripción de Carrusel 1', 
+'Footer de Carrusel 1',
 'https://telegra.ph/file/24b24c495b5384b218b2f.jpg',
 [['Botón1', 'Id1'], ['Botón2', 'Id2'] /* etc... */],
-[['Copiar texto 1'], ['Copiar texto 2']],
-[['Enlace1', 'https://'], ['Enlace2', 'https://'] /* etc... */],
+['Texto para copiar 1', 'Texto para copiar 2'],
+[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2'] /* etc... */],
 [['Botón Lista 1', sections], ['Botón Lista 2', sections] /* etc... */]
-], [ // CAROUSEL 2
-'Descripción de Carusel 2',
-'Footer de Carusel 2',
+], [ // CARRUSEL 2
+'Descripción de Carrusel 2',
+'Footer de Carrusel 2',
 'https://telegra.ph/file/e9239fa926d3a2ef48df2.jpg',
 [['Botón1', 'Id1'], ['Botón2', 'Id2']],
 'Copiar texto 2',
 [['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
 [['Botón Lista 1', sections], ['Botón Lista 2', sections]]
-], [ // CAROUSEL 3
-'Descripción de Carusel 3',
-'Footer de Carusel 3',
+], [ // CARRUSEL 3
+'Descripción de Carrusel 3',
+'Footer de Carrusel 3',
 'https://telegra.ph/file/ec725de5925f6fb4d5647.jpg',
 [['Botón1', 'Id1'], ['Botón2', 'Id2']],
 'Copiar texto 3',
 [['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
 [['Botón Lista 1', sections], ['Botón Lista 2', sections]]
-], [ // CAROUSEL 4
-'Descripción de Carusel 4',
-'Footer de Carusel 4',
+], [ // CARRUSEL 4
+'Descripción de Carrusel 4',
+'Footer de Carrusel 4',
 'https://telegra.ph/file/7acad0975febb71446da5.jpg',
 [['Botón1', 'Id1'], ['Botón2', 'Id2']],
 'Copiar texto 4',
 [['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
 [['Botón Lista 1', sections], ['Botón Lista 2', sections]]
 ]] /* etc... */
-await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carusel', messages, m)            
+await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carrusel', messages, m)            
 
 }
-
 handler.command = /^(carousel)$/i
 export default handler
