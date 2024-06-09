c47fa2988f4d200d1ddd917fe28d358aae4ad019
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 08:57:50 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -7,31 +7,34 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 ]*/
 
 const messages = [
-  [
-    'Descripción 1',
-    'Footer 1',
-    'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
-    [['Botón A', 'idA'], ['Botón B', 'idB']],
-    'Texto para copiar 1',
-    [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
     [
-      [['Lista 1'], [['ddddddd'], ['ffffffff']], ['ddddddddd'], ['eeeeeeeeee']], ['Descripción opción 1', 'id_opcion_1', 'id_opcion_1'],
-      ['Header opción 2'], ['Descripción opción 2', 'id_opcion_2', 'id_opcion_1']
-    ]
-  ],
-  [
-    'Descripción 2',
-    'Footer 2',
-    'https://telegra.ph/file/98c672926fbc35a4b9948.jpg',
-    [['Botón C', 'idC'], ['Botón D', 'idD']],
-    'Texto para copiar 2',
-    [['Enlace 3', 'https://example.com/link3'], ['Enlace 4', 'https://example.com/link4']],
+        'Descripción 1',
+        'Footer 1',
+        'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
+        [['Botón A', 'idA'], ['Botón B', 'idB']],
+        'Texto para copiar 1',
+        [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
+        [
+            ['Lista 1'], 
+            [
+                ['Header opción 1', 'Opción 1', 'Descripción opción 1', 'id_opcion_1'] 
+            ]
+        ]
+    ],
     [
-      'Lista 2',
-      'Header opción 3', 'Descripción opción 3', 'id_opcion_3',
-      'Header opción 4', 'Descripción opción 4', 'id_opcion_4'
+        'Descripción 2',
+        'Footer 2',
+        'https://telegra.ph/file/98c672926fbc35a4b9948.jpg',
+        [['Botón C', 'idC'], ['Botón D', 'idD']],
+        'Texto para copiar 2',
+        [['Enlace 3', 'https://example.com/link3'], ['Enlace 4', 'https://example.com/link4']],
+        [
+            ['Lista 2'], 
+            [
+                ['Header opción 3', 'Opción 3', 'Descripción opción 3', 'id_opcion_3']
+            ]
+        ]
     ]
-  ]
 ];
 
   
