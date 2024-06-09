ea08889537e3216c441611ad2e22c989eb72aca0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 04:08:20 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -11,135 +11,50 @@ const messages = [
     'Descripción 1',
     'Footer 1',
     'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
-    [['Botón A', 'idA'], ['Botón B', 'idB']],
-    'Texto para copiar 1',
-    [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
     [
-      {
-        title: 'Lista 1',
-        rows: [
-          {
-            header: 'Header opción 1',
-            title: 'Opción 1',
-            description: 'Descripción opción 1',
-            id: 'id_opcion_1'
-          },
-          {
-            header: 'Header opción 2',
-            title: 'Opción 2',
-            description: 'Descripción opción 2',
-            id: 'id_opcion_2'
-          }
+      ['Botón A', 'idA'], // Botones para la tarjeta 1
+      ['Botón B', 'idB']
+    ],
+    'Texto para copiar 1', // Texto para copiar para la tarjeta 1
+    [
+      ['Enlace 1', 'https://example.com/link1'], // URLs para la tarjeta 1
+      ['Enlace 2', 'https://example.com/link2']
+    ],
+    [
+      [
+        'Lista 1', // Título de la lista para la tarjeta 1
+        [
+          ['Header opción 1', 'Opción 1', 'Descripción opción 1', 'id_opcion_1'], // Opciones para la lista 1
+          ['Header opción 2', 'Opción 2', 'Descripción opción 2', 'id_opcion_2']
         ]
-      },
-      {
-        title: 'Lista 2',
-        rows: [
-          {
-            header: 'Header opción 3',
-            title: 'Opción 3',
-            description: 'Descripción opción 3',
-            id: 'id_opcion_3'
-          },
-          {
-            header: 'Header opción 4',
-            title: 'Opción 4',
-            description: 'Descripción opción 4',
-            id: 'id_opcion_4'
-          }
-        ]
-      }
+      ]
     ]
   ],
   [
     'Descripción 2',
     'Footer 2',
     'https://telegra.ph/file/98c672926fbc35a4b9948.jpg',
-    [['Botón C', 'idC'], ['Botón D', 'idD']],
-    'Texto para copiar 2',
-    [['Enlace 3', 'https://example.com/link3'], ['Enlace 4', 'https://example.com/link4']],
     [
-      {
-        title: 'Lista 3',
-        rows: [
-          {
-            header: 'Header opción 5',
-            title: 'Opción 5',
-            description: 'Descripción opción 5',
-            id: 'id_opcion_5'
-          },
-          {
-            header: 'Header opción 6',
-            title: 'Opción 6',
-            description: 'Descripción opción 6',
-            id: 'id_opcion_6'
-          }
+      ['Botón C', 'idC'], // Botones para la tarjeta 2
+      ['Botón D', 'idD']
+    ],
+    'Texto para copiar 2', // Texto para copiar para la tarjeta 2
+    [
+      ['Enlace 3', 'https://example.com/link3'], // URLs para la tarjeta 2
+      ['Enlace 4', 'https://example.com/link4']
+    ],
+    [
+      [
+        'Lista 2', // Título de la lista para la tarjeta 2
+        [
+          ['Header opción 3', 'Opción 3', 'Descripción opción 3', 'id_opcion_3'], // Opciones para la lista 2
+          ['Header opción 4', 'Opción 4', 'Descripción opción 4', 'id_opcion_4']
         ]
-      },
-      {
-        title: 'Lista 4',
-        rows: [
-          {
-            header: 'Header opción 7',
-            title: 'Opción 7',
-            description: 'Descripción opción 7',
-            id: 'id_opcion_7'
-          },
-          {
-            header: 'Header opción 8',
-            title: 'Opción 8',
-            description: 'Descripción opción 8',
-            id: 'id_opcion_8'
-          }
-        ]
-      }
+      ]
     ]
   ],
-  [
-    'Descripción 3',
-    'Footer 3',
-    'https://telegra.ph/file/f689a972cfc1c5efff189.jpg',
-    [['Botón E', 'idE'], ['Botón F', 'idF']],
-    'Texto para copiar 3',
-    [['Enlace 5', 'https://example.com/link5'], ['Enlace 6', 'https://example.com/link6']],
-    [
-      {
-        title: 'Lista 5',
-        rows: [
-          {
-            header: 'Header opción 9',
-            title: 'Opción 9',
-            description: 'Descripción opción 9',
-            id: 'id_opcion_9'
-          },
-          {
-            header: 'Header opción 10',
-            title: 'Opción 10',
-            description: 'Descripción opción 10',
-            id: 'id_opcion_10'
-          }
-        ]
-      },
-      {
-        title: 'Lista 6',
-        rows: [
-          {
-            header: 'Header opción 11',
-            title: 'Opción 11',
-            description: 'Descripción opción 11',
-            id: 'id_opcion_11'
-          },
-          {
-            header: 'Header opción 12',
-            title: 'Opción 12',
-            description: 'Descripción opción 12',
-            id: 'id_opcion_12'
-          }
-        ]
-      }
-    ]
-  ]
 ]
+
 
 
 await conn.sendCarousel(m.chat, 'Prueba', 'SuperBot', messages, m)
