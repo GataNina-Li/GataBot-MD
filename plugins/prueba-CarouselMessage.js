51e04d24a82f640101cb8fba1e6f148fd52b3899
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 04:24:06 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -11,22 +11,34 @@ const messages = [
     'Descripción 1',
     'Footer 1',
     'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
+    [['Botón A', 'idA'], ['Botón B', 'idB']],
+    'Texto para copiar 1',
+    [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
     [
-      ['Botón A', 'idA'], // Botones para la tarjeta 1
-      ['Botón B', 'idB']
-    ],
-    'Texto para copiar 1', // Texto para copiar para la tarjeta 1
-    [
-      ['Enlace 1', 'https://example.com/link1'], // URLs para la tarjeta 1
-      ['Enlace 2', 'https://example.com/link2']
-    ],
-    [
-      [
-        'Lista 1', // Título de la lista para la tarjeta 1
-        [
-          ['Header opción 1', 'Opción 1', 'Descripción opción 1', 'id_opcion_1'], // Opciones para la lista 1
-          ['Header opción 2', 'Opción 2', 'Descripción opción 2', 'id_opcion_2']
-        ]
+      'Lista 1', // Título de la lista
+      [ // Sección de la lista
+        { // Elemento de la lista
+          title: 'Header opción 1', // Título del elemento
+          rows: [ // Filas del elemento
+            {
+              header: 'Header opción 1', // Encabezado de la fila
+              title: 'Opción 1', // Título de la fila
+              description: 'Descripción opción 1', // Descripción de la fila
+              id: 'id_opcion_1' // ID de la fila
+            }
+          ]
+        },
+        { // Otro elemento de la lista
+          title: 'Header opción 2', // Título del elemento
+          rows: [ // Filas del elemento
+            {
+              header: 'Header opción 2', // Encabezado de la fila
+              title: 'Opción 2', // Título de la fila
+              description: 'Descripción opción 2', // Descripción de la fila
+              id: 'id_opcion_2' // ID de la fila
+            }
+          ]
+        }
       ]
     ]
   ],
@@ -34,27 +46,38 @@ const messages = [
     'Descripción 2',
     'Footer 2',
     'https://telegra.ph/file/98c672926fbc35a4b9948.jpg',
+    [['Botón C', 'idC'], ['Botón D', 'idD']],
+    'Texto para copiar 2',
+    [['Enlace 3', 'https://example.com/link3'], ['Enlace 4', 'https://example.com/link4']],
     [
-      ['Botón C', 'idC'], // Botones para la tarjeta 2
-      ['Botón D', 'idD']
-    ],
-    'Texto para copiar 2', // Texto para copiar para la tarjeta 2
-    [
-      ['Enlace 3', 'https://example.com/link3'], // URLs para la tarjeta 2
-      ['Enlace 4', 'https://example.com/link4']
-    ],
-    [
-      [
-        'Lista 2', // Título de la lista para la tarjeta 2
-        [
-          ['Header opción 3', 'Opción 3', 'Descripción opción 3', 'id_opcion_3'], // Opciones para la lista 2
-          ['Header opción 4', 'Opción 4', 'Descripción opción 4', 'id_opcion_4']
-        ]
+      'Lista 2', // Título de la lista
+      [ // Sección de la lista
+        { // Elemento de la lista
+          title: 'Header opción 3', // Título del elemento
+          rows: [ // Filas del elemento
+            {
+              header: 'Header opción 3', // Encabezado de la fila
+              title: 'Opción 3', // Título de la fila
+              description: 'Descripción opción 3', // Descripción de la fila
+              id: 'id_opcion_3' // ID de la fila
+            }
+          ]
+        },
+        { // Otro elemento de la lista
+          title: 'Header opción 4', // Título del elemento
+          rows: [ // Filas del elemento
+            {
+              header: 'Header opción 4', // Encabezado de la fila
+              title: 'Opción 4', // Título de la fila
+              description: 'Descripción opción 4', // Descripción de la fila
+              id: 'id_opcion_4' // ID de la fila
+            }
+          ]
+        }
       ]
     ]
-  ],
-]
-
+  ]
+];
 
 
 await conn.sendCarousel(m.chat, 'Prueba', 'SuperBot', messages, m)
