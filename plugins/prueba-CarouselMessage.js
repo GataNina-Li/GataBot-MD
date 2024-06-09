410fe30cdef15bd0c6efa486fe314eb6184055e0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 03:33:55 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 ]*/
 
 const messages = [
-['Descripción 1', 'Footer 1', 'https://telegra.ph/file/b31cd03f716d362b33716.jpg', [['Botón A', 'idA'], ['Botón B', 'idB']], 'Texto para copiar 1', [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']], [['Lista 1', ['Opción 1', 'Opción 2']], ['Lista 2', ['Opción 3', 'Opción 4']]]],
+['Descripción 1', 'Footer 1', 'https://telegra.ph/file/b31cd03f716d362b33716.jpg', [['Botón A', 'idA'], ['Botón B', 'idB']], 'Texto para copiar 1', [['Lista 1', ['Opción 1', 'Opción 2']], ['Lista 2', ['Opción 3', 'Opción 4']]]],
 ['Descripción 2', 'Footer 2', 'https://telegra.ph/file/98c672926fbc35a4b9948.jpg', [['Botón C', 'idC'], ['Botón D', 'idD']], 'Texto para copiar 2', [['Enlace 3', 'https://example.com/link3'], ['Enlace 4', 'https://example.com/link4']], [['Lista 3', ['Opción 5', 'Opción 6']], ['Lista 4', ['Opción 7', 'Opción 8']]]],
 ['Descripción 3', 'Footer 3', 'https://telegra.ph/file/f689a972cfc1c5efff189.jpg', [['Botón E', 'idE'], ['Botón F', 'idF']], 'Texto para copiar 3', [['Enlace 5', 'https://example.com/link5'], ['Enlace 6', 'https://example.com/link6']], [['Lista 5', ['Opción 9', 'Opción 10']], ['Lista 6', ['Opción 11', 'Opción 12']]]],
 ['Descripción 4', 'Footer 4', 'https://telegra.ph/file/f689a972cfc1c5efff189.jpg', [['Botón G', 'idG'], ['Botón H', 'idH']], 'Texto para copiar 4', [['Enlace 7', 'https://example.com/link7'], ['Enlace 8', 'https://example.com/link8']], [['Lista 7', ['Opción 13', 'Opción 14']], ['Lista 8', ['Opción 15', 'Opción 16']]]],
