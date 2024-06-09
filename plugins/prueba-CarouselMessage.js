b0e2ded2d73cd8304304473f3632b9aa3e44bae9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 14:49:51 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -1,13 +1,14 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 
+
+
 const messages = [
 [
-'Descripción 1', 
-'Footer 1',
+null, null,
 'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
-[['Botón 1', 'id1'], ['Botón 2', 'id2']],
-'Texto para copiar 1',
-[['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
+[['Buscar con Google', 'id1'], ['Botón 2', 'id2']],
+null,
+[['Enlace', 'https://example.com/link1'], [null, null]],
 [
             [[['Lista'], [ 'Sección'], ['Título]', ['[Tema'], ['Descripción', 'Id']]]],
             [['Sección 1', 'Título 1', 'Tema 1', 'Descripción 1', 'Id 1']],
