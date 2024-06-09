d5e08e31d7199f0574e31d5f525dd957d696e299
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 06:28:25 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -9,38 +9,35 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 const messages = [
   [
     'Descripción 1',
+    'Footer 1',
+    'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
+    [['Botón A', 'idA'], ['Botón B', 'idB']],
+    'Texto para copiar 1',
+    [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
     [
-      [
-        'Header opción 1', // Título del elemento
-        'id_opcion_1', // ID de la fila
-        'Descripción opción 1' // Descripción de la fila
-      ],
-      [
-        'Header opción 2', // Título del elemento
-        'id_opcion_2', // ID de la fila
-        'Descripción opción 2' // Descripción de la fila
+      'Lista 1', // Título de la lista
+      [ // Sección de la lista
+        ['Header opción 1', 'Opción 1', 'Descripción opción 1', 'id_opcion_1'], // Elemento de la lista
+        ['Header opción 2', 'Opción 2', 'Descripción opción 2', 'id_opcion_2']  // Otro elemento de la lista
       ]
     ]
   ],
   [
     'Descripción 2',
+    'Footer 2',
+    'https://telegra.ph/file/98c672926fbc35a4b9948.jpg',
+    [['Botón C', 'idC'], ['Botón D', 'idD']],
+    'Texto para copiar 2',
+    [['Enlace 3', 'https://example.com/link3'], ['Enlace 4', 'https://example.com/link4']],
     [
-      [
-        'Header opción 3', // Título del elemento
-        'id_opcion_3', // ID de la fila
-        'Descripción opción 3' // Descripción de la fila
-      ],
-      [
-        'Header opción 4', // Título del elemento
-        'id_opcion_4', // ID de la fila
-        'Descripción opción 4' // Descripción de la fila
+      'Lista 2', // Título de la lista
+      [ // Sección de la lista
+        ['Header opción 3', 'Opción 3', 'Descripción opción 3', 'id_opcion_3'], // Elemento de la lista
+        ['Header opción 4', 'Opción 4', 'Descripción opción 4', 'id_opcion_4']  // Otro elemento de la lista
       ]
     ]
   ]
 ];
-
-
-
 
   
 await conn.sendCarousel(m.chat, 'Texto', 'Linea', 'TEXTO', messages, m)
