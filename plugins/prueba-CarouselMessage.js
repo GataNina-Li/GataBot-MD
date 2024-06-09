bcf30ef63ae13b0db747d350e19d94dc42596bdc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 12:32:30 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -1,6 +1,17 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 
-const messages = [
+let listSections = [
+    ['Sección 1', [
+        ['Título 1', 'ID1', 'Descripción 1', 'id_opcion_1'],
+        ['Título 2', 'ID2', 'Descripción 2', 'id_opcion_2']
+    ]],
+    ['Sección 2', [
+        ['Título 3', 'ID3', 'Descripción 3', 'id_opcion_3'],
+        ['Título 4', 'ID4', 'Descripción 4', 'id_opcion_4']
+    ]]
+];
+
+/*const messages = [
 [
 'Descripción 1', 
 'Footer 1',
@@ -34,10 +45,23 @@ const messages = [
 'Tema',  
 'Descripcion', 
 'Id',
-]]]]
+]]]]*/
+
+const messages = [
+    [
+        'Descripción 1',
+        'Footer 1',
+        'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
+        [['Botón 1', 'id1'], ['Botón 2', 'id2']],
+        'Texto para copiar 1',
+        [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
+        sections
+    ],
+    
+]
 
   
-await conn.sendCarousel(m.chat, 'Texto', 'Linea', 'TEXTO', messages, m)
+await conn.sendCarousel(m.chat, 'Texto', 'Linea', 'TEXTO', messages, sections, m)
 }
 
 handler.command = /^(carousel)$/i
