f4f7b3f0cdba9e9fc229cf41ab25f26098959307
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 03:45:05 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -8,7 +8,57 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 
 const messages = [
 ['Descripción 1', 'Footer 1', 'https://telegra.ph/file/b31cd03f716d362b33716.jpg', [['Botón A', 'idA'], ['Botón B', 'idB']], 'Texto para copiar 1', [['Lista 1', ['Opción 1', 'Opción 2']], ['Lista 2', ['Opción 3', 'Opción 4']]]],
-['Descripción 2', 'Footer 2', 'https://telegra.ph/file/98c672926fbc35a4b9948.jpg', [['Botón C', 'idC'], ['Botón D', 'idD']], 'Texto para copiar 2', [['Enlace 3', 'https://example.com/link3'], ['Enlace 4', 'https://example.com/link4']], [['Lista 3', ['Opción 5', 'Opción 6']], ['Lista 4', ['Opción 7', 'Opción 8']]]],
+[
+  'Descripción 2',
+  'Footer 2',
+  'https://telegra.ph/file/98c672926fbc35a4b9948.jpg',
+  [
+    ['Botón C', 'idC'],
+    ['Botón D', 'idD']
+  ],
+  'Texto para copiar 2',
+  [
+    ['Enlace 3', 'https://example.com/link3'],
+    ['Enlace 4', 'https://example.com/link4']
+  ],
+  [
+    {
+      title: 'Lista 3',
+      rows: [
+        {
+          header: 'Header opción 5',
+          title: 'Opción 5',
+          description: 'Descripción opción 5',
+          id: 'id_opcion_5'
+        },
+        {
+          header: 'Header opción 6',
+          title: 'Opción 6',
+          description: 'Descripción opción 6',
+          id: 'id_opcion_6'
+        }
+      ]
+    },
+    {
+      title: 'Lista 4',
+      rows: [
+        {
+          header: 'Header opción 7',
+          title: 'Opción 7',
+          description: 'Descripción opción 7',
+          id: 'id_opcion_7'
+        },
+        {
+          header: 'Header opción 8',
+          title: 'Opción 8',
+          description: 'Descripción opción 8',
+          id: 'id_opcion_8'
+        }
+      ]
+    }
+  ]
+],
+//['Descripción 2', 'Footer 2', 'https://telegra.ph/file/98c672926fbc35a4b9948.jpg', [['Botón C', 'idC'], ['Botón D', 'idD']], 'Texto para copiar 2', [['Enlace 3', 'https://example.com/link3'], ['Enlace 4', 'https://example.com/link4']], [['Lista 3', ['Opción 5', 'Opción 6']], ['Lista 4', ['Opción 7', 'Opción 8']]]],
 ['Descripción 3', 'Footer 3', 'https://telegra.ph/file/f689a972cfc1c5efff189.jpg', [['Botón E', 'idE'], ['Botón F', 'idF']], 'Texto para copiar 3', [['Enlace 5', 'https://example.com/link5'], ['Enlace 6', 'https://example.com/link6']], [['Lista 5', ['Opción 9', 'Opción 10']], ['Lista 6', ['Opción 11', 'Opción 12']]]],
 ['Descripción 4', 'Footer 4', 'https://telegra.ph/file/f689a972cfc1c5efff189.jpg', [['Botón G', 'idG'], ['Botón H', 'idH']], 'Texto para copiar 4', [['Enlace 7', 'https://example.com/link7'], ['Enlace 8', 'https://example.com/link8']], [['Lista 7', ['Opción 13', 'Opción 14']], ['Lista 8', ['Opción 15', 'Opción 16']]]],
 ['Descripción 5', 'Footer 5', 'https://telegra.ph/file/f689a972cfc1c5efff189.jpg', [['Botón I', 'idI'], ['Botón J', 'idJ']], 'Texto para copiar 5', [['Enlace 9', 'https://example.com/link9'], ['Enlace 10', 'https://example.com/link10']], [['Lista 9', ['Opción 17', 'Opción 18']], ['Lista 10', ['Opción 19', 'Opción 20']]]]
