93df5fd9ac24c5b5c605b5cf4385a59b554f8ffa
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-09 16:24:41 -0500

    Actualizar descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -1,149 +1,69 @@
 import axios from 'axios'
-
 import fetch from 'node-fetch'
-
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-
 import search from 'yt-search'
-
-
-
 async function spotifyxv(query) {
-
-  let token = await tokens();
-
-  let response = await axios({
-
-    method: 'get',
-
-    url: 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(query) + '&type=track',
-
-    headers: {
-
-      Authorization: 'Bearer ' + token,
-
-    },
-
-  })
-
-  const tracks = response.data.tracks.items
-
-  const results = tracks.map((track) => ({
-
-    name: track.name,
-
-    artista: track.artists.map((artist) => artist.name),
-
-    album: track.album.name,
-
-    duracion: timestamp(track.duration_ms),
-
-    url: track.external_urls.spotify,
-
-    imagen: track.album.images.length ? track.album.images[0].url : '',
-
-  }))
-
-  return results
-
+let token = await tokens();
+let response = await axios({
+method: 'get',
+url: 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(query) + '&type=track',
+headers: {
+Authorization: 'Bearer ' + token,
+},
+})
+const tracks = response.data.tracks.items
+const results = tracks.map((track) => ({
+name: track.name,
+artista: track.artists.map((artist) => artist.name),
+album: track.album.name,
+duracion: timestamp(track.duration_ms),
+url: track.external_urls.spotify,
+imagen: track.album.images.length ? track.album.images[0].url : '',
+}))
+return results
 }
-
-
-
 async function tokens() {
-
-  const response = await axios({
-
-    method: 'post',
-
-    url: 'https://accounts.spotify.com/api/token',
-
-    headers: {
-
-      'Content-Type': 'application/x-www-form-urlencoded',
-
-      Authorization: 'Basic ' + Buffer.from('acc6302297e040aeb6e4ac1fbdfd62c3:0e8439a1280a43aba9a5bc0a16f3f009').toString('base64'),
-
-    },
-
-    data: 'grant_type=client_credentials',
-
-  })
-
-  return response.data.access_token
-
+const response = await axios({
+method: 'post',
+url:
+'https://accounts.spotify.com/api/token',
+headers: {
+'Content-Type': 'application/x-www-form-urlencoded',
+Authorization: 'Basic ' + Buffer.from('acc6302297e040aeb6e4ac1fbdfd62c3:0e8439a1280a43aba9a5bc0a16f3f009').toString('base64'),
+},
+data: 'grant_type=client_credentials',
+})
+return response.data.access_token
 }
-
-
-
 function timestamp(time) {
-
-  const minutes = Math.floor(time / 60000);
-
-  const seconds = Math.floor((time % 60000) / 1000);
-
-  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-
+const minutes = Math.floor(time / 60000);
+const seconds = Math.floor((time % 60000) / 1000);
+return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
 }
-
-
-
 async function getBuffer(url, options) {
-
-  try {
-
-    options = options || {};
-
-    const res = await axios({
-
-      method: 'get',
-
-      url,
-
-      headers: {
-
-        DNT: 1,
-
-        'Upgrade-Insecure-Request': 1,
-
-      },
-
-      ...options,
-
-      responseType: 'arraybuffer',
-
-    });
-
-    return res.data;
-
-  } catch (err) {
-
-    return err;
-
-  }
-
-}
-
-
-
-async function getTinyURL(text) {
-
 try {
-
-    let response = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`);
-
-    return response.data;
-
-  } catch (error) {
-
-    return text;
-
-  }
-
-}
-
-
-
+options = options || {};
+const res = await axios({
+method: 'get',
+url,
+headers: {
+DNT: 1,
+'Upgrade-Insecure-Request': 1,
+},
+...options,
+responseType: 'arraybuffer',
+});
+return res.data;
+} catch (err) {
+return err;
+}}
+async function getTinyURL(text) {
+try {
+let response = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`);
+return response.data;
+} catch (error) {
+return text;
+}}
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `*Uso Correcto*\n\nEjemplo:\n${usedPrefix + command} save yours tear`
 try {
