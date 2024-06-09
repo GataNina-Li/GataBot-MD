49256877f7866687150acdfb0ce802491f101866
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 17:03:01 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 const sections = [{
 title: `Título de la sección`,
 rows: [
-{ header: 'Encabezado1', title: "Título1", description: 'Descripción1', id: "Id1" }, 
+{ header: 'Encabezado1', title: "Título1", description: 'Descripción1', id: usedPrefix + "menu" }, 
 { header: 'Encabezado2', title: "Título2", description: 'Descripción2', id: "Id2" }, 
 { header: 'Encabezado3', title: "Título3", description: 'Descripción3', id: "Id3" }, 
 { header: 'Encabezado4', title: "Título4", description: 'Descripción4', id: "Id4" }, 
@@ -16,7 +16,7 @@ const messages = [[ // CARRUSEL 1
 'Descripción de Carrusel 1', 
 'Footer de Carrusel 1',
 'https://telegra.ph/file/24b24c495b5384b218b2f.jpg',
-[['Botón1', usedPrefix + 'menu'], ['Botón2', 'Id2'] /* etc... */],
+[['Botón1', 'Id1'], ['Botón2', 'Id2'] /* etc... */],
 [['Texto para copiar 1'], ['Texto para copiar 2'] /* etc... */],
 [['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2'] /* etc... */],
 [['Botón Lista 1', sections], ['Botón Lista 2', sections] /* etc... */]
