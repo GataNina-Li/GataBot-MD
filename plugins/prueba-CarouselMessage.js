99283802e6e72b3d798429c743706e70cb404f03
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 12:43:40 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -44,10 +44,10 @@ const messages = [
         'Texto para copiar 1', // Texto que se puede copiar
         [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']], // Enlaces
         [ // Lista de secciones y filas
-            ['Sección 1', [ // Título de la sección y filas asociadas
-                'Header opción 1', 'Opción 1', 'Descripción opción 1', 'id_opcion_1', // Fila 1
-                'Header opción 2', 'Opción 2', 'Descripción opción 2', 'id_opcion_2'  // Fila 2
-            ]],
+            ['Sección 1',  // Título de la sección y filas asociadas
+                ['Header opción 1', 'Opción 1', 'Descripción opción 1', 'id_opcion_1'], // Fila 1
+                ['Header opción 2', 'Opción 2', 'Descripción opción 2', 'id_opcion_2']  // Fila 2
+            ],
             ['Sección 2', [ // Título de la segunda sección y filas asociadas
                 ['Header opción 3', 'Opción 3', 'Descripción opción 3', 'id_opcion_3'], // Fila 1
                 ['Header opción 4', 'Opción 4', 'Descripción opción 4', 'id_opcion_4']  // Fila 2
