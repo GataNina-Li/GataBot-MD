3a91d2cf5623a74e9a5b055860677d7009dd496b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 12:54:43 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -43,13 +43,13 @@ const messages = [
         [['Botón 1', 'id1'], ['Botón 2', 'id2']], // Botones de acción
         'Texto para copiar 1', // Texto que se puede copiar
         [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']], // Enlaces
-        [
+        [[
             'Lista', [
                 ['Sección 1', 'Título 1', 'Tema 1', 'Descripción 1', 'Id 1'],
                 ['Sección 2', 'Título 2', 'Tema 2', 'Descripción 2', 'Id 2']
             ]
         ]
-    ],
+    ]],
 ];
 
   
