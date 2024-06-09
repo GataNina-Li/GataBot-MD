397706c61af44185f85dc49912f2662aabe95f64
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 10:25:05 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -14,15 +14,18 @@ const messages = [
 [['Botón 1', 'id1'], ['Botón 2', 'id2']],
 'Texto para copiar 1',
 [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
-[[
-'Lista', 
-'Sección ',  
-'id',  
-'Titulo',  
-'Subtitulo', 
-'Descripcion',
-]]],
 [
+            [
+                'Lista 1', 
+                [
+                    ['Sección 1', 'Titulo 1', 'Subtitulo 1', 'Descripcion 1', 'id1'],
+                    ['Sección 1', 'Titulo 2', 'Subtitulo 2', 'Descripcion 2', 'id2'],
+                    ['Sección 1', 'Titulo 3', 'Subtitulo 3', 'Descripcion 3', 'id3']
+                ]
+            ]
+        ]
+    ],
+    [
 'Descripción 2',
 'Footer 2',
 'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
