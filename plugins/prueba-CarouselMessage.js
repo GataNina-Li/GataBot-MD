27960cca73ba9e53c685b4ead863e860f252e932
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-09 08:11:04 -0500

    Update prueba-CarouselMessage.js

--- plugins/prueba-CarouselMessage.js
+++ plugins/prueba-CarouselMessage.js
@@ -15,16 +15,12 @@ const messages = [
     'Texto para copiar 1',
   [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
 [
-      ['Lista 1'],
-      [
-        ['Header opción 1'], ['Opción 1', 'Descripción opción 1', 'id_opcion_1'], 
-        ['Header opción 2'], 
-        ['Opción 2'], 
-        ['Descripción opción 2'], 
-        ['id_opcion_2']  // Otro elemento de la lista
-      ]
-    ]
-  ],
+  ['Lista 1'], // Título de la lista
+  [ 
+    ['Header opción 1'], ['Opción 1', 'Descripción opción 1', 'id_opcion_1'],
+    ['Header opción 2'], ['Opción 2', 'Descripción opción 2', 'id_opcion_2']
+  ]
+],
   [
     'Descripción 2',
     'Footer 2',
