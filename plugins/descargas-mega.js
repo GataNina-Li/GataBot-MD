9e07b6c9bbd880ef2650ec12d20e55636c4b44c5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-13 09:11:33 -0500

    Update descargas-mega.js

--- plugins/descargas-mega.js
+++ plugins/descargas-mega.js
@@ -42,3 +42,4 @@ function formatBytes(bytes) {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
