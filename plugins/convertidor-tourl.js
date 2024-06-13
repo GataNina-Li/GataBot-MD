69e6a2ed51111c1bf044b5c1f6555018df7cfd17
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-13 08:08:17 -0500

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -7,7 +7,7 @@ import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
 let handler = async (m) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => gataImg.getRandom())
+let pp = await conn.profilePictureUrl(who).catch(_ => gataImg)
 let name = await conn.getName(who)
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
