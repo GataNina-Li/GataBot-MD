f14fa363e997ac6b9aca2444e00e07ad3cfecdce
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-06-13 18:23:36 -0300

    Add files via upload

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -7,6 +7,8 @@ const openaiii = new OpenAIApi(configuration);
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 if (usedPrefix == 'a' || usedPrefix == 'A') return;
 if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`    
+
+if (command == 'ia' || command == 'chatgpt') {
 try {
 conn.sendPresenceUpdate('composing', m.chat);
 let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
@@ -19,6 +21,13 @@ let res = await gpt.json()
 await m.reply(res.data)
 } catch {
 }}}
+
+if (command == 'openai' || command == 'ia2' || command == 'chatgpt2') {
+conn.sendPresenceUpdate('composing', m.chat);
+let gpt = await fetch(`https://delirius-official.vercel.app/ia/gptweb?text=${text}`) 
+let res = await gpt.json()
+await m.reply(res.gpt)
+}}
 handler.command = /^(openai|chatgpt|ia|ai|openai2|chatgpt2|ia2)$/i;
 export default handler;
 
