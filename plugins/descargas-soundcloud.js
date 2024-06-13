5b80818a4937407cec21c8287476f79ca10bcd7a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-13 08:25:37 -0500

    Update descargas-soundcloud.js

--- plugins/descargas-soundcloud.js
+++ plugins/descargas-soundcloud.js
@@ -46,6 +46,8 @@ if (!text) throw `Ingresa el nombre de la canción a buscar`;
 
 handler.limit = 2
 handler.register = true
-export default handler;
+
 handler.command = /^(soundcloud|soundcloudr)$/i
+export default handler
+
 const delay = time => new Promise(res => setTimeout(res, time))
