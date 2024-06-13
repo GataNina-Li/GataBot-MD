18f7816de9377d3ecb46a29de42ecd2dbad78fa9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-13 08:55:23 -0500

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -2,7 +2,7 @@
 # Créditos a https://github.com/AzamiJs
 •• @AzamiJs ••
 */
-import uploadFile from '../lib/uploadFile.js'
+/*import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
 let handler = async (m) => {
@@ -25,4 +25,4 @@ export default handler
 async function shortUrl(url) {
 let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
 return await res.text()
-}
+}*/
