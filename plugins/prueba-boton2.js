beb1997e6b163e13deadaa5ef688e449f307bbf4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-18 12:56:18 -0500

    Create prueba-boton2.js

---
+++ plugins/prueba-boton2.js
@@ -0,0 +1,12 @@
+import { googleImage } from '@bochilteam/scraper'
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (!text) return conn.reply(m.chat, '_ingresa el texto de lo que quieres buscar\n\n`ejemplo`\n' + `> ❒ *${usedPrefix + command}* bad bunny`, m )
+const res = await googleImage(text)
+const image = await res.getRandom()
+const link = image
+await conn.sendButton2(m.chat,'Resultados de:', [[`${text}`]], null, [['resp.1', `${usedPrefix}🌠 ${text}`], ['resp.2', `${usedPrefix}🌠 ${text}`], ['resp.3', `${usedPrefix}🌠 ${text}`]], m)
+}
+handler.help = ['ɪᴍᴀɢᴇɴ']
+handler.tags = ['search']
+handler.command = /^(prueba96)$/i
+export default handler
