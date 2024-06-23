4979b5dcfa9f05dba550a1da36f8c2c90946ff05
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-23 11:19:36 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/so-lista.js
+++ plugins/so-lista.js
@@ -13,6 +13,12 @@ let vn = 'https://qu.ax/xynz.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}  
 
+if (/^Chupame|Pingo|Qliao$/i.test(m.text) && chat.audios) {  
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
+let vn = 'https://qu.ax/SCpi.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}  
+
 if (/^ara ara$/i.test(m.text) && chat.audios) {  
 let vn = 'https://qu.ax/PPgt.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
