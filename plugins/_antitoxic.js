c65c615b2d9849385d4a9039156fb330b29d4a0a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-30 00:27:08 -0500

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -6,25 +6,25 @@ if (m.isBaileys && m.fromMe)
 return !0
 if (!m.isGroup)
 return !1
-  let user = global.db.data.users[m.sender]
-  let chat = global.db.data.chats[m.chat]
-  let bot = global.db.data.settings[this.user.jid] || {}
-  let img = 'https://i.imgur.com/5Q1MqGD.jpg'
- const isToxic = toxicRegex.exec(m.text)
+let delet = m.key.participant
+let bang = m.key.id
+let user = global.db.data.users[m.sender]
+let chat = global.db.data.chats[m.chat]
+let bot = global.db.data.settings[this.user.jid] || {}
+let img = 'https://telegra.ph/file/94f45d76340fc61982bb7.jpg'
+const isToxic = toxicRegex.exec(m.text)
     
-if (isToxic && chat.antitoxic && !isOwner && !isAdmin) {
+if (isToxic && chat.antitoxic && !isOwner && !isAdmin && isBotAdmin) {
 user.warn += 1
-if (!(user.warn >= 4)) await m.reply(`${user.warn == 1 ? `*@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, ${lenguajeGB['smsToxic1']()} (${isToxic}) ${lenguajeGB['smsToxic2']()} ${lenguajeGB['smsToxic3']()} *${user.warn}/4*\n\n${wm}`, false, { mentions: [m.sender] })}
-/*await conn.sendButton(m.chat,`${user.warn == 1 ? `*@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`} *${lenguajeGB['smsToxic1']()} (${isToxic}) ${lenguajeGB['smsToxic2']()}*`, `${lenguajeGB['smsToxic3']()} *${user.warn}/4*\n\n${wm}`, img, [
-[lenguajeGB.smsToxic4(), 'ok'],
-[lenguajeGB.smsToxic5(), '.off antitoxic'],
-[lenguajeGB.smsConMenu(), '/menu']], false, { mentions: [m.sender] })}*/
+if (!(user.warn >= 4)) {
+await conn.sendMessage(m.chat, { text: mid.antitoxic1(isToxic, m, user), mentions: [m.sender] }, { quoted: m })  
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+}
 
 if (user.warn >= 4) {
 user.warn = 0
 await m.reply(`*${lenguajeGB['smsToxic6']()}*\n*@${m.sender.split`@`[0]} ${lenguajeGB['smsToxic7']()}*`, false, { mentions: [m.sender] })
-user.banned = true
-await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+//await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 //await this.updateBlockStatus(m.sender, 'block')
 }
 return !1
