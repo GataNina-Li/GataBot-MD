63b9f9e74ff3b50cf1b45cd689825262714beabb
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-06-30 12:29:00 -0500

    𝑱𝒐𝒂𝒏𝑩𝒐𝒕-𝑻𝑲

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,22 +1,23 @@
-const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
+const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i;
 
-let handler = m => m
+let handler = m => m;
 handler.before = async function (m, { conn, isOwner, isROwner }) {
-if (m.fromMe) return !0
-if (m.isGroup) return !1
-if (!m.message) return !0
-const regexWithPrefix = new RegExp(`^${prefix.source}\\s?${comandos.source}`, 'i')
-if (regexWithPrefix.test(m.text.toLowerCase().trim())) return !0
+    if (m.fromMe) return true;
+    if (m.isGroup) return false;
+    if (!m.message) return true;
 
-let chat, user, bot, mensaje
-chat = global.db.data.chats[m.chat]
-user = global.db.data.users[m.sender]
-bot = global.db.data.settings[this.user.jid] || {}
+    const regexWithPrefix = new RegExp(`^${prefix.source}\\s?${comandos.source}`, 'i');
+    if (regexWithPrefix.test(m.text.toLowerCase().trim())) return true;
 
-if (bot.antiPrivate && !isOwner && !isROwner) {
-return await conn.reply(m.chat, mid.mAdvertencia + mid.smsprivado(m, cuentas), m, { mentions: [m.sender] })  
-await this.updateBlockStatus(m.sender, 'block')
-}
-return !1
-} 
-export default handler
+    let chat, user, bot, mensaje;
+    chat = global.db.data.chats[m.chat];
+    user = global.db.data.users[m.sender];
+    bot = global.db.data.settings[this.user.jid] || {};
+
+    if (bot.antiPrivate && !isOwner && !isROwner) {
+        return false;
+    }
+    
+    return false;
+};
+export default handler;
