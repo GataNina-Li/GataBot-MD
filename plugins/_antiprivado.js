c0fe7cea54cfc5acc75722fad7926bcd003f4c84
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-07-06 12:19:36 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,6 +1,25 @@
+/*----------------------[ AUTOREAD ]-----------------------*/
+let handler = m => m
+handler.all = async function (m) {
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+
+let setting = global.db.data.settings[this.user.jid]
+const settingsREAD = global.db.data.settings[this.user.jid] || {}
+
+if (settingsREAD.autoread && m.text && prefixRegex.test(m.text)) {
+await this.sendPresenceUpdate('composing', m.chat)
+await this.readMessages([m.key]) 
+        
+let usedPrefix = m.text.match(prefixRegex)[0]
+let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
+}} 
+
+export default handler  
+
+/*----------------------[ ANTIPRIVADO ]-----------------------*/
 const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
 
-let handler = m => m
+//let handler = m => m
 handler.before = async function (m, { conn, isOwner, isROwner }) {
 if (m.fromMe) return !0
 if (m.isGroup) return !1
@@ -18,5 +37,5 @@ return await conn.reply(m.chat, mid.mAdvertencia + mid.smsprivado(m, cuentas), m
 await this.updateBlockStatus(m.sender, 'block')
 }
 return !1
-} 
-export default handler
+}
+//export default handler
