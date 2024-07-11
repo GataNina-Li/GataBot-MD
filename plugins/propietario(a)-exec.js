1b1ccd05cba3a68d4a1f69714e2ee013d76ef7fb
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-07-11 12:50:16 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/propietario(a)-exec.js
+++ plugins/propietario(a)-exec.js
@@ -1,4 +1,4 @@
-/*import syntaxerror from 'syntax-error'
+import syntaxerror from 'syntax-error'
 import { format } from 'util'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
@@ -43,4 +43,3 @@ if (typeof args[0] == 'number') return super(Math.min(args[0], 10000))
 else return super(...args)}}
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]}
-*/
