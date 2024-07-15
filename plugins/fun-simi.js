0d5e420951fee0f4721c17be7425020e10cc0f6b
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-07-15 12:43:45 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -4,9 +4,9 @@ const handler = async (m, {text, command, args, usedPrefix}) => {
 if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Hola Gata Bot*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝙏𝘼𝙇𝙆 𝙏𝙊 𝙈𝙀\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Hello Gata Bot*`
 try {
 await conn.sendPresenceUpdate('composing', m.chat)
-const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
-const resSimi = await api.json();
-m.reply(resSimi.success);
+let api = await fetch(`https://deliriusapi-official.vercel.app/tools/simi?text=${text}`)
+let resSimi = await api.json()
+await m.reply(resSimi.data.message)
 } catch {
 try {
 if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
