8eb2d9c0d246bffec37a4f1c52b5c4e2ee360307
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-07-15 12:39:45 -0300

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -7,12 +7,16 @@ if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
 if (!m.text) return
 let textodem = m.text  
+if (m.text.includes('serbot') || m.text.includes('bots')|| m.text.includes('jadibot')|| m.text.includes('menu')|| m.text.includes('play')|| m.text.includes('play2') || m.text.includes('playdoc') || m.text.includes('tiktok') || m.text.includes('facebook') || m.text.includes('menu2') ||  m.text.includes('infobot') || m.text.includes('estado') ||  m.text.includes('ping') ||  m.text.includes('instalarbot') ||  m.text.includes('sc') ||  m.text.includes('sticker') ||  m.text.includes('s') || m.text.includes('wm') ||  m.text.includes('qc')) return  
 try {
 await conn.sendPresenceUpdate('composing', m.chat)
-let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
-let data = await ressimi.json();
+//let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
+//let data = await ressimi.json();
+let simsimi = await fetch(`https://deliriusapi-official.vercel.app/tools/simi?text=${encodeURIComponent(textodem)}`)
+let res = await simsimi.json() 
 if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`) /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
-await m.reply(data.success)
+await m.reply(res.data.message) 
+//await m.reply(data.success)
 } catch {
 /* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
 if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello')
