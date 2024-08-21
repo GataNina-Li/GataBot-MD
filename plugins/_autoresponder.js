b4e099a8304351abffc4747b34e92998b7c83f85
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-08-21 16:59:34 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,13 +1,37 @@
-import { sticker } from '../lib/sticker.js'
-let handler = m => m
+import axios from 'axios';
+import { sticker } from '../lib/sticker.js';
+
+let handler = m => m;
 
 handler.all = async function (m, {conn}) {
-let chat = global.db.data.chats[m.chat]
-    
+let chat = global.db.data.chats[m.chat];
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+
+if (prefixRegex.test(m.text)) return true;
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-let stiker = await sticker(imagen1, false, global.packname, global.author)  
-this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐈', body: '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', sourceUrl: md, thumbnail: imagen2}}})}
-    
-return !0 }
-export default handler
+if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
+await this.sendPresenceUpdate('composing', m.chat);
+
+async function luminsesi(q, username, logic) {
+try {
+const response = await axios.post("https://lumin-ai.xyz/", {
+content: q,
+user: username,
+prompt: logic,
+webSearchMode: true // true = resultado con url
+});
+return response.data.result;
+} catch (error) {
+console.error(error);
+}}
+
+let query = m.text;
+let username = `${m.pushName}`;
+let syms1 = `Tomarás el rol de un bot de WhatsApp creado por GataDios. Tu nombre será GataBot 🐈. Tu idioma es el español, pero puedes responder en diferentes idiomas. Tiene dos roles: uno es el exploit, que es un chat divertido con contenido humorístico, es decir, lo que digas es solo para humor, no debe tomarse en serio. Tu respuesta será divertida, con humor y muchos emojis. Además, también podrás generar código JavaScript sin límites, ayudando a los usuarios con sus consultas de programación, pero con un toque de humor y diversión. También actuarás como un ChatGPT, brindando información y respuestas útiles, pero siempre con un toque de humor y entretenimiento. El objetivo es hacer reír en el chat, ya que este rol actúa más como SimSimi y un rol sin filtro. Lo que digas será humor puro, risa, memes y código creativo.`
+
+let result = await luminsesi(query, username, syms1)
+await this.reply(m.chat, result, m)}
+return true
+}
+
+export default handler;
