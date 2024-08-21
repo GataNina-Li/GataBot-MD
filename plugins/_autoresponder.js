fe296f0dc23da4bc4833545259785ba1910feaba
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-08-21 17:00:14 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat];
 let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
 
-if (prefixRegex.test(m.text)) return true;
+//if (prefixRegex.test(m.text)) return true;
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
 await this.sendPresenceUpdate('composing', m.chat);
