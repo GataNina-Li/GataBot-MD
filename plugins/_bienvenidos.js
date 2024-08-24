88925b12c1c379654573f9a476b22bc4ff19a014
edar123 <78393307+edar123@users.noreply.github.com>
2024-08-24 14:04:53 -0600

    _bienvenidos.js
    MentionsUSER

---
+++ plugins/_bienvenidos.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, participants, groupMetadata }) => {
+
+    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.png'
+   
+    let text =`_👋🏻Hola, sean todos bienvenidos a:_\n *${groupMetadata.subject}*
+
+${groupMetadata.desc?.toString() || 'desconocido'}
+`.trim()
+const mentionedJid = groupMetadata.participants.map(v => v.id);
+    conn.sendFile(m.chat, pp, 'pp.jpg', text, m, true, { mentions: mentionedJid})
+}
+
+handler.command = ['welcome','bienvenidos','bienbenidos'] 
+handler.group = true
+handler.admin = true
+
+export default handler
