12b755ebe2b53793d6a67a111c6794bc8ddf8f15
edar123 <78393307+edar123@users.noreply.github.com>
2024-08-26 19:48:39 -0600

    _bienvenidos.js

--- plugins/_bienvenidos.js
+++ plugins/_bienvenidos.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, participants, groupMetadata }) => {
 ${groupMetadata.desc?.toString() || 'desconocido'}
 `.trim()
 const mentionedJid = groupMetadata.participants.map(v => v.id);
-    conn.sendFile(m.chat, pp, 'pp.jpg', text, m, true, { mentions: mentionedJid})
+    conn.sendFile(m.chat, pp, 'pp.jpg', text, null, true, { mentions: mentionedJid})
 }
 
 handler.command = ['welcome','bienvenidos','bienbenidos'] 
