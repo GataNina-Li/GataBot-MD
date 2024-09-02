1efd8e4b3b89468c0b3373ba246063f66b95c10b
Charles Zam's <141449338+AzamiJs@users.noreply.github.com>
2024-09-01 21:35:28 -0600

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -82,7 +82,7 @@ global.keysxteammm = ["29d4b59a4aa687ca", "5LTV57azwaid7dXfz5fzJu", "cb15ed422c7
 global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
 global.keysneoxrrr = ["5VC9rvNx", "cfALv5"]
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = "GataDiosV2"
+global.lolkeysapi = "GataDiosV3"
 global.itsrose = ["4b146102c4d500809da9d1ff"]
 global.baileys = "@whiskeysockets/baileys"
 
