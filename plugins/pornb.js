0ab6c93d637ba4b6a0ea2faab1080bdd54bf9b17
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2024-09-03 15:54:23 -0600

    Create pornb.js

---
+++ plugins/pornb.js
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+let url = pack[Math.floor(Math.random() * pack.length)]
+conn.sendButton(m.chat, `_🥵 Pack 🥵_`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+}
+handler.help = ['pack']
+handler.tags = ['internet']
+handler.command = /^(pack092)$/i
+export default handler
+
+global.pack = [
+  "https://i.imgur.com/XbW7FO2.jpg",
+  "https://i.imgur.com/ciuzM98.jpg",
+  "https://i.imgur.com/uHsrrrx.jpg",
+  "https://i.imgur.com/PVi8YDi.jpg",
+  "https://i.imgur.com/FLC3ZXE.jpg",
+  "https://i.imgur.com/54m52tX.jpg",
+  "https://i.imgur.com/OsxYPgQ.jpg",
+  "https://i.imgur.com/vpw6Xnr.jpg",
+  "https://i.imgur.com/aAyPrZx.jpg",
+  "https://i.imgur.com/Gh3ORCd.jpg",
+  "https://i.imgur.com/rjGhygM.jpg",
+  "https://i.imgur.com/zdHVFEI.jpg",
+  "https://i.imgur.com/kAplnSG.jpg",
+  "https://i.imgur.com/15UiO8o.jpg",
+  "https://i.imgur.com/qjjyr6G.jpg",
+  "https://i.imgur.com/bQZRhBU.jpg",
+  "https://i.imgur.com/vpw6Xnr.jpg",
+  "https://i.imgur.com/aAyPrZx.jpg",
+  "https://i.imgur.com/Gh3ORCd.jpg",
+  "https://i.imgur.com/rjGhygM.jpg",
+  "https://i.imgur.com/0MhmmF4.jpg",
+  "https://i.imgur.com/2MX4wvq.jpg",
+  "https://i.imgur.com/HYL5ggu.jpg",
+  "https://i.imgur.com/7ZjOD2a.jpg",
+  "https://i.imgur.com/zbEUy3m.jpg",
+  "https://i.imgur.com/tZ6vlg6.jpg",
+  "https://i.imgur.com/jdPns8O.jpg",
+  "https://i.imgur.com/VyjBQHT.jpg",
+  "https://i.imgur.com/ozAGqBD.jpg",
+  "https://i.imgur.com/DsSj9S1.jpg",
+  "https://i.imgur.com/KYHpjNc.jpg",
+]
