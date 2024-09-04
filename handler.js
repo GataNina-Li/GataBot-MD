7ca044a540e0bcb0873a610420090af3554b70bc
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-09-04 20:54:25 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1214,11 +1214,11 @@ return
 }
 
 //Antispam 2		
-/*if (user.antispam2 && isROwner) return
+if (user.antispam2 && isROwner) return
 let time = global.db.data.users[m.sender].spam + 3000
 if (new Date - global.db.data.users[m.sender].spam < 3000) return console.log(`[ SPAM ]`) 
 global.db.data.users[m.sender].spam = new Date * 1
-}*/
+}
 		
 let hl = _prefix 
 let adminMode = global.db.data.chats[m.chat].modoadmin
