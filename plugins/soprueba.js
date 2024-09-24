431148112ae13dbb1be70f71b11e6163b2f86041
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2024-09-23 20:44:51 -0600

    Update soprueba.js

--- plugins/soprueba.js
+++ plugins/soprueba.js
@@ -6,7 +6,7 @@ global.db.data.users[m.sender].money += 100
 global.db.data.users[m.sender].exp += 100
 
 
- if (command == 'muma') {
+ if (command == 'lala') {
    let vn = 'https://qu.ax/SCpi.mp3'
    conn.sendPresenceUpdate('recording', m.chat)
    conn.sendFile(m.chat, vn, 'a.mp3', null, m, true, {
@@ -28,6 +28,6 @@ ptt: true
     } catch {
     conn.reply(m.chat, `${fg}𝘼𝙇𝙂𝙊 𝙎𝘼𝙇𝙄𝙊 𝙈𝘼𝙇 𝙑𝙐𝙀𝙇𝘼𝙑𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝙍\n\n𝙎𝙊𝙈𝙀𝙏𝙃𝙄𝙉𝙂 𝙒𝙀𝙉𝙏 𝙒𝙍𝙊𝙉𝙂 𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`, fkontak, m)    
     }}
-handler.customPrefix = /muma|lala/
+    handler.customPrefix = /muma|lala/
     handler.command = /^lala|muma/i
     export default handler
