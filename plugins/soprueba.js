50f42e72705da0510060127251e4616011664768
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2024-09-23 20:46:19 -0600

    Update soprueba.js

--- plugins/soprueba.js
+++ plugins/soprueba.js
@@ -5,7 +5,7 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
 
-
+try {
  if (command == 'lala') {
    let vn = 'https://qu.ax/SCpi.mp3'
    conn.sendPresenceUpdate('recording', m.chat)
