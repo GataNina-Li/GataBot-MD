69be43d9ecbc7c8927791c28e7a8cc185de31c4e
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2024-09-23 20:40:11 -0600

    Update soprueba.js

--- plugins/soprueba.js
+++ plugins/soprueba.js
@@ -14,13 +14,20 @@ global.db.data.users[m.sender].exp += 100
 ptt: true 
 })
 }
-   
+    if (command == 'pinguita') {
+   let vn = 'https://qu.ax/xynz.mp3'
+   conn.sendPresenceUpdate('recording', m.chat)
+   conn.sendFile(m.chat, vn, 'a.mp3', null, m, true, {
+  type: 'audioMessage', 
+ptt: true 
+})
+
     
   
 
     } catch {
     conn.reply(m.chat, `${fg}𝘼𝙇𝙂𝙊 𝙎𝘼𝙇𝙄𝙊 𝙈𝘼𝙇 𝙑𝙐𝙀𝙇𝘼𝙑𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝙍\n\n𝙎𝙊𝙈𝙀𝙏𝙃𝙄𝙉𝙂 𝙒𝙀𝙉𝙏 𝙒𝙍𝙊𝙉𝙂 𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`, fkontak, m)    
     }}
-handler.customPrefix = /muma/
-    handler.command = /^logohacker|muma/i
+handler.customPrefix = /muma|pinguita/
+    handler.command = /^pinguita|muma/i
     export default handler
