09c89ac6edfb44d1f23ab4c68e283a34a0643055
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2024-09-23 22:07:48 -0600

    Update descargas-yta.js

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -33,7 +33,7 @@ const size = await yt.audio[q].fileSizeH
 await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
 } catch {
 try {
-let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${youtubeLink}`)    
+let lolhuman = await fetch(`https://deliriusapi-official.vercel.app/download/ytmp3?&url=${youtubeLink}`)    
 let lolh = await lolhuman.json()
 let n = lolh.result.title || 'error'
 await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  
