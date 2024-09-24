02ed6728d1ff7dca5a54d04a22888adaefb9f24d
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2024-09-23 21:11:42 -0600

    Update soprueba.js

--- plugins/soprueba.js
+++ plugins/soprueba.js
@@ -1,12 +1,11 @@
 import util from 'util'
 import path from 'path'
-//let handler = async (m, { conn }) => {
+let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
-
-if (customPrefix == 'muma'){
-let vn = 'https://qu.ax/SCpi.mp3'
+  
+let vn = 'https://qu.ax/xynz.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
 conn.sendFile(m.chat, vn, 'a.mp3', null, m, true, { 
 type: 'audioMessage', 
@@ -16,3 +15,4 @@ ptt: true
 handler.customPrefix = /muma|a|A/
 handler.command = /^(muma|ª|A?$)/
 export default handler
+
