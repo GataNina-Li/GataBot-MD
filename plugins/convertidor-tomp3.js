f362902131467050ddec869bfef5e39d6434139f
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2024-10-10 22:36:51 -0600

    Update convertidor-tomp3.js

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -12,7 +12,7 @@ if (!audio.data && !/audio/.test(mime)) throw `${lenguajeGB['smsAvisoFG']()}${mi
 if (!audio.data && !/video/.test(mime)) throw `${lenguajeGB['smsAvisoFG']()}${mid.smsconvert6}`
 conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
-handler.help = ['tomp3 (reply)']
+handler.help = ['tomp33 (reply)']
 handler.tags = ['audio']
 handler.command = ['toomp3', 'toaudio', 'mmp3']
 export default handler
