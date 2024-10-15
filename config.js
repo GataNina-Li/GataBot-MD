04cf6feacffd0007bd8ca6c5885e3989c421eb0b
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-10-15 17:07:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -74,6 +74,7 @@ global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = "GataDiosV3"
 global.itsrose = ["4b146102c4d500809da9d1ff"]
 global.baileys = "@whiskeysockets/baileys"
+global.apis = 'https://deliriussapi-oficial.vercel.app'
 
 global.APIs = { 
 xteam: 'https://api.xteam.xyz',
