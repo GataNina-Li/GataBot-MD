1b58263a19a11db82e350d97dd72718ea4811b4b
EdderBot02 <107139979+EdderBot02@users.noreply.github.com>
2024-10-15 07:28:06 -0600

    Update descargas-tw.js

--- plugins/descargas-tw.js
+++ plugins/descargas-tw.js
@@ -13,7 +13,7 @@ await conn.sendMessage(m.chat, {text: waitt, edit: key});
 await conn.sendMessage(m.chat, {text: waittt, edit: key});
 await delay(1000 * 1);
 await conn.sendMessage(m.chat, {text: waitttt, edit: key});
-const apiUrl = `https://deliriusapi-official.vercel.app/download/twitterv2?url=${encodeURIComponent(text)}`
+const apiUrl = `https://deliriussapi-oficial.vercel.app/download/twitterv2?url=${encodeURIComponent(text)}`
 
 const response = await fetch(apiUrl)
 const jsonData = await response.json()
