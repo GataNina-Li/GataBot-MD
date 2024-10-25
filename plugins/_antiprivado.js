4f0866d40409fdd09b4875f2db4ff9ea165731de
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-10-25 10:18:19 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -26,7 +26,7 @@ user = global.db.data.users[m.sender]
 bot = global.db.data.settings[this.user.jid] || {}
 
 if (bot.antiPrivate && !isOwner && !isROwner) {
-return await conn.reply(m.chat, mid.mAdvertencia + mid.smsprivado(m, cuentas), m, { mentions: [m.sender] })  
+await conn.reply(m.chat, mid.mAdvertencia + mid.smsprivado(m, cuentas), m, { mentions: [m.sender] })  
 await this.updateBlockStatus(m.chat, 'block')
 //await this.updateBlockStatus(m.sender, 'block')
 }
