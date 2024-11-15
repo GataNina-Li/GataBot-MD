a85829457ec44b62441f85e66f5de9634e78d932
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-11-15 19:23:44 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,4 +1,5 @@
-let linkRegex = /whatsapp.com|wa.me|whatsapp.com\/channel/i
+let linkRegex1 = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+let linkRegex2 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
 
 let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
@@ -10,13 +11,14 @@ let chat = global.db.data.chats[m.chat]
 let delet = m.key.participant
 let bang = m.key.id
 const user = `@${m.sender.split`@`[0]}`
-//const groupAdmins = participants.filter(p => p.admin)
-//const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
+const groupAdmins = participants.filter(p => p.admin)
+const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
 let bot = global.db.data.settings[this.user.jid] || {}
-const isGroupLink = linkRegex.exec(m.text)
+const isGroupLink = linkRegex1.exec(m.text) || linkRegex2.exec(m.text);
 if (chat.antiLink && isGroupLink) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m }) 
+//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m }) 
+if (!isBotAdmin) return conn.sendMessage(m.chat, { text: mid.mAdminTrue, mentions: [...groupAdmins.map(v => v.id)] }, { quoted: m });
 if (isBotAdmin) {
 if (m.text.includes(linkThisGroup)) return
 await conn.sendMessage(m.chat, { text: `${mid.mAdvertencia + mid.mWhatsApp} *${user}*`, mentions: [m.sender] }, { quoted: m })    
