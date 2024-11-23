1a92dd36ba1afdd3113ad9eba999ac6a3d88a23c
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-11-23 07:18:11 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/propietario(a)-exec.js
+++ plugins/propietario(a)-exec.js
@@ -7,9 +7,9 @@ import { createRequire } from 'module'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
 
-let handler = async (m, _2, isOwner ) => {
+let handler = async (m, _2) => {
   let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
-if (!isOwner) return;
+//if (!isOwner) return;
   let _return
   let _syntax = ''
   let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
