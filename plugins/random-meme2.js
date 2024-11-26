170a7616b9283aef31c9a94c1a5bf71053adc15a
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-11-26 20:24:19 -0300

    Delete plugins/random-meme2.js

--- plugins/random-meme2.js
+++
@@ -1,9 +0,0 @@
-import hispamemes from 'hispamemes'
-let handler = async (m, { conn, usedPrefix, command }) => {
-const meme = hispamemes.meme()
-//conn.sendFile(m.chat, meme, 'error.jpg', `😂😂🤣`, m)
-await conn.sendButton(m.chat, `*_🤣 ${command} 🤣_*`.trim(), wm, meme, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], null, null, m)
-}
-handler.command = ['meme2', 'memes2'] 
-handler.level = 3
-export default handler
