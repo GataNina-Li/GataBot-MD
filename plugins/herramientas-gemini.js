e0824c2db2e67eb62eb25258567eedb2cd242518
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-26 21:59:52 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/herramientas-gemini.js
+++ plugins/herramientas-gemini.js
@@ -3,7 +3,7 @@ var handler = async (m, { text,  usedPrefix, command }) => {
 if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘽𝘼𝙍𝘿\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`
 try {
 conn.sendPresenceUpdate('composing', m.chat);
-var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
+var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${text}`)
 var res = await apii.json()
 await m.reply(res.result)
 } catch (e) {
