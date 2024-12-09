f0887c9527a201bc00de454bec6e9a746ece3ae7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-12-09 11:52:25 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_autodetec.js
+++ plugins/_autodetec.js
@@ -13,18 +13,14 @@ let users = participants.map(u => conn.decodeJid(u.id))
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
 if (chat.detect && m.messageStubType == 2) {
-const chatId = m.isGroup ? m.chat : m.sender;
-const uniqid = chatId.split('@')[0];
-const sessionPath = './GataBotSession/';
-const files = await fs.readdir(sessionPath);
-let filesDeleted = 0;
-for (const file of files) {
+const uniqid = (m.isGroup ? m.chat : m.sender).split('@')[0]
+const sessionPath = './GataBotSession/'
+for (const file of await fs.readdir(sessionPath)) {
 if (file.includes(uniqid)) {
-await fs.unlink(path.join(sessionPath, file));
-filesDeleted++;
-console.log(`⚠️ Eliminacion session (PreKey) que provocan el undefined el chat`)
+await fs.unlink(path.join(sessionPath, file))
+console.log(`*[ ⚠️ Archivo eliminado ]* > '${file}' (Session PreKey) que provoca el "undefined" en el chat`)
 }}}
-  
+
 if (chat.detect && m.messageStubType == 21) {
 await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoAG']() + mid.smsAutodetec1(usuario, m), mentions: [m.sender], mentions: [...groupAdmins.map(v => v.id)] }, { quoted: fkontak })   
 } else if (chat.detect && m.messageStubType == 22) {
@@ -73,6 +69,7 @@ return;
 } if (chat.detect && m.messageStubType == 123) {
 await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoIIG']() + mid.smsAutodetec10(usuario, m), mentions: [m.sender] }, { quoted: fkontak })
 } else {
+if (m.messageStubType == 2) return
 console.log({messageStubType: m.messageStubType,
 messageStubParameters: m.messageStubParameters,
 type: WAMessageStubType[m.messageStubType], 
