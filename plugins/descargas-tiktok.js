bd76a87c78a00f7c0142cf5086bb8080a4baffe0
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2024-12-11 19:58:09 -0400

    Jjj

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,63 +1,41 @@
-import fg from 'api-dylux' 
-import axios from 'axios'
-import cheerio from 'cheerio'
-import { tiktok } from "@xct007/frieren-scraper";
 let generateWAMessageFromContent = (await import(global.baileys)).default
 import { tiktokdl } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, args, usedPrefix, command}) => {
 if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}${mid.smsTikTok2}\n*${usedPrefix + command} https://vm.tiktok.com/ZM6n8r8Dk/*`, fkontak,  m)
 if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}${mid.smsTikTok3}`, fkontak,  m)  
 await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}${mid.smsTikTok4}`, fkontak,  m) 
-try {
-const dataF = await tiktok.v1(args[0])
-conn.sendFile(m.chat, dataF.play, 'tiktok.mp4', `⛱️ ${mid.user}\n*${nickname}*\n${description ? '\n⛱️ ${mid.smsYT14}\n*${description}*' : ''}\n${wm}`.trim(), m) 
-} catch (e1) {
-try {
-const tTiktok = await tiktokdlF(args[0])
-conn.sendFile(m.chat, tTiktok.video, 'tiktok.mp4', `⛱️ ${mid.user}\n*${nickname}*\n${description ? '\n⛱️ ${mid.smsYT14}\n*${description}*' : ''}\n${wm}`.trim(), m) 
-} catch (e2) {
-try {
-let p = await fg.tiktok(args[0]) 
-conn.sendFile(m.chat, p.nowm, 'tiktok.mp4', `⛱️ ${mid.user}\n*${nickname}*\n${description ? '\n⛱️ ${mid.smsYT14}\n*${description}*' : ''}\n${wm}`.trim(), m)
-} catch (e3) {
-try { 
-const { author: { nickname }, video, description } = await tiktokdl(args[0])
-const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-conn.sendFile(m.chat, url, 'tiktok.mp4', `⛱️ ${mid.user}\n*${nickname}*\n${description ? `\n⛱️ ${mid.smsYT14}\n*${description}*` : ''}\n${wm}`.trim(), m)
-handler.limit = 2
-} catch (e4) {
-try{
-const response=await fetch(`${apis}/download/tiktok?url=${args[0]}`)
-const dataR = await response.json()
-const { author,title, meta} = dataR.data
-conn.sendFile(m.chat, meta.media[0].org, 'tiktok.mp4', `⛱️ ${mid.user}\n*${author.nickname}*\n${wm}`.trim(), m) 
-}
-catch (e5){
-try{
-const response=await fetch(`https://deliriusapi-official.vercel.app/download/tiktok?&query=${text}`)
-const dataR = await response.json()
-conn.sendFile(m.chat,dataR.result.link, 'tiktok.mp4', `⛱️ ${mid.user}\n*${dataR.result.author.username}*\n${wm}`.trim(), m)
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+  try {
+      const response = await axios.get(`https://api.dorratz.com/v2/tiktok-dl?url=${text}`);
+      if (response.data.status && response.data.data) {
+        const videoData = response.data.data.media;
+       const videoUrl = videoData.org; 
+        const videoDetails = `*Título*: ${response.data.data.title}\n` +
+                             `*Autor*: ${response.data.data.author.nickname}\n` +
+                             `*Duración*: ${response.data.data.duration}s
+` +
+                             `*Likes*: ${response.data.data.like}\n` +
+                             `*Comentarios*: ${response.data.data.comment}`;
+        await conn.sendMessage(
+          m.chat,
+          {
+            video: { url: videoUrl },
+            caption: videoDetails
+          },
+          { quoted: m }
+        );
+      } else {
+        await conn.sendMessage(m.chat, "No se pudo obtener el video. Verifica la URL.", { quoted: m });
+      }
+    } catch (error) {
+      console.error(error);
+      await conn.sendMessage(m.chat, "Se produjo un error al intentar descargar el video.", { quoted: msg });
+  }}
 console.log(e)
 handler.limit = false
-}}}}}}}
 handler.help = ['tiktok']
 handler.tags = ['dl']
 handler.command = /^(tt|tiktok)(dl|nowm)?$/i
 //handler.limit = 2
 export default handler
-
-async function tiktokdlF(url) {
-if (!/tiktok/.test(url)) return 'Enlace incorrecto';
-const gettoken = await axios.get("https://tikdown.org/id");
-const $ = cheerio.load(gettoken.data);
-const token = $("#download-form > input[type=hidden]:nth-child(2)").attr( "value" );
-const param = { url: url, _token: token };
-const { data } = await axios.request("https://tikdown.org/getAjax?", { method: "post", data: new URLSearchParams(Object.entries(param)), headers: { "content-type": "application/x-www-form-urlencoded; charset=UTF-8", "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36" }, });
-var getdata = cheerio.load(data.html);
-if (data.status) {
-return { status: true, thumbnail: getdata("img").attr("src"), video: getdata("div.download-links > div:nth-child(1) > a").attr("href"), audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"), }} else
-return { status: false }}
 
