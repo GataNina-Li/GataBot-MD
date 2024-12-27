6cbdc0da8f67df99be2e62b4fc34a8f0b0b7a498
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-12-27 20:36:39 -0300

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -7,8 +7,8 @@ let setting = global.db.data.settings[this.user.jid]
 const settingsREAD = global.db.data.settings[this.user.jid] || {}
 
 if (m.text && prefixRegex.test(m.text)) {
-//await conn.sendPresenceUpdate('composing', m.chat)
-//await conn.readMessages([m.key])
+await conn.sendPresenceUpdate('composing', m.chat)
+await conn.readMessages([m.key])
         
 let usedPrefix = m.text.match(prefixRegex)[0]
 let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
