bdc50677a7b8c848f43c2813f19467c86b1e2f75
Rey Endymion <33460024+ReyEndymion@users.noreply.github.com>
2024-12-28 10:39:24 -0600

    Reapply "Update handler.js"
    This reverts commit 54531afe05d1dae00761064ad9b24039f27b44c8.

--- handler.js
+++ handler.js
@@ -1583,10 +1583,5 @@ const file = global.__filename(import.meta.url, true);
 watchFile(file, async () => {
 unwatchFile(file)
 console.log(chalk.redBright('Update \'handler.js\''))
-//if (global.reloadHandler) console.log(await global.reloadHandler())
-	
-if (global.conns && global.conns.length > 0 ) {
-const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
-for (const userr of users) {
-userr.subreloadHandler(false)
-}}})
+if (global.reloadHandler) console.log(await global.reloadHandler())
+})
