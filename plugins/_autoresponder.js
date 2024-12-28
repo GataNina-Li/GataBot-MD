25958ae9dbd8a2c5d0a9dbf10bb3e657f38d6f57
Rey Endymion <33460024+ReyEndymion@users.noreply.github.com>
2024-12-28 11:27:03 -0600

    Update _autoresponder.js
    la propiedad conn en un plugin all no esta disponible, sin embargo se puede llamar a this que es el objeto anidado a esa funcion desde el handler.js linea 1139

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
-handler.all = async function (m, {conn}) {
+handler.all = async function (m) {
 let user = global.db.data.users[m.sender]
 let chat = global.db.data.chats[m.chat]
 m.isBot = m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20;
@@ -94,7 +94,7 @@ let syms1 = chat.sAutorespond ? chat.sAutorespond : txtDefault
 if (!chat.autorespond) return 
 if (m.fromMe) return
 if (!user.registered) return
-conn.sendPresenceUpdate('composing', m.chat)
+this.sendPresenceUpdate('composing', m.chat)
 
 let result
 if (result && result.trim().length > 0) {
