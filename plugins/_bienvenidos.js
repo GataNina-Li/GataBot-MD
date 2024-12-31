b8f31e2a9840c9bd6abd070cf779271df3d5efe8
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-30 19:05:13 -0500

    Gata Dios

--- plugins/_bienvenidos.js
+++ plugins/_bienvenidos.js
@@ -2,19 +2,20 @@ let handler = async (m, { conn, participants, groupMetadata }) => {
 
     let ppch = await conn.profilePictureUrl(m.sender, 'image').catch(_ => gataMenu)
    
-let text =`*╭────── ✨ ¡Bienvenido(a) al Grupo! ✨ ──────╮*\n`
+let text =`*╭─── ✨ ¡Bienvenido(a) al Grupo! ✨ ───╮*\n`
 text += `*│*\n`
 text += `*├─ 👋 ¡Hola @${m.sender.split('@')[0]}⁨⁩!*\n`
+text += `*├─ *${groupMetadata.subject}\n`
 text += `*├─ 🎉 Nos alegra tenerte aquí. ¡Esperamos que disfrutes tu estadía!*\n`
 text += `*├─ 📜 No olvides revisar la descripción del grupo para más detalles.*\n`
 text += `*│*\n`
-text += `*╰────── 🌟 ¡Diviértete y participa! 🌟 ──────╯*\n\n`
-text += `${String.fromCharCode(8206).repeat(850)}`
+text += `*╰─── 🐈 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐈 ───╯*\n\n`
+text += `${groupMetadata.desc?.toString() || '¡SIN DESCRIPCIÓN!\n> *Gata Bot - MD* 🌻🐈'}`
 
     conn.sendFile(m.chat, ppch, 'pp.jpg', text, null, true, { mentions: m.sender})
 }
 
-handler.command = ['welcome','bienvenidos','bienbenidos'] 
+handler.command = ['welcome','bienvenido'] 
 handler.group = true
 handler.admin = true
 
