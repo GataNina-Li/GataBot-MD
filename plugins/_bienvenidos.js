5c48913d68a06f60c1dbe75c1e1aab1f77680526
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2025-01-01 17:45:43 -0500

    Gata Dios

--- plugins/_bienvenidos.js
+++ plugins/_bienvenidos.js
@@ -15,7 +15,7 @@ let buttons = [
 
 let fake = { contextInfo: { isForwarded: true, externalAdReply: { showAdAttribution: true, title: name, body: gt, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: ppch, sourceUrl: 'https://github.com/GataNina-Li', mediaType: 1, renderLargerThumbnail: false }}};
 
-let buttonMessage = { image: { url: ppch }, caption: welcomeMessage, footer: gt, buttons: buttons, viewOnce: true, headerType: 4, mentions: [m.sender],  ...fake };
+let buttonMessage = { image: { url: ppch }, caption: txt, footer: gt, buttons: buttons, viewOnce: true, headerType: 4, mentions: [m.sender],  ...fake };
 
 await conn.sendMessage(m.chat, buttonMessage, { quoted: null });
 }
