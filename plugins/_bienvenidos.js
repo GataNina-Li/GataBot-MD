0fec7fcffa0b5c0de54e1f3ba075a9e5107d4411
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2025-01-01 17:39:04 -0500

    Gata Dios

--- plugins/_bienvenidos.js
+++ plugins/_bienvenidos.js
@@ -9,16 +9,18 @@ let senderId = m.sender.split('@')[0];
 let txt = `*╭┈⊰* ${groupMetadata.subject} *⊰┈ ✦*\n*┊ 👋 ¡Hola @${senderId}!*\n*┊ 📜 No olvides revisar la descripción del grupo para más detalles.*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰ ✦*\n\n${groupMetadata.desc?.toString() || '¡SIN DESCRIPCIÓN!\n> _*Gata Bot - MD*_ 🌻🐈'}`;
 
 let buttons = [
-{ buttonId: "/menu", buttonText: { displayText: 'Hola. 🤍' }, type: 1 },
-{ buttonId: "/serbot code", buttonText: { displayText: 'Welcome. ✨' }, type: 1 }
+{ buttonId: "/menu", buttonText: { displayText: 'Menu. 🐈' }, type: 1 },
+{ buttonId: "/serbot code", buttonText: { displayText: 'SerBot. 🐱' }, type: 1 }
 ];
 
-let buttonMessage = { image: { url: ppch }, caption: welcomeMessage, footer: gt, buttons: buttons, viewOnce: true, headerType: 4, mentions: [m.sender],  ...fakeChannel };
+let fake = { contextInfo: { isForwarded: true, externalAdReply: { showAdAttribution: true, title: name, body: gt, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: ppch, sourceUrl: 'https://github.com/GataNina-Li', mediaType: 1, renderLargerThumbnail: false }}};
+
+let buttonMessage = { image: { url: ppch }, caption: welcomeMessage, footer: gt, buttons: buttons, viewOnce: true, headerType: 4, mentions: [m.sender],  ...fake };
 
 await conn.sendMessage(m.chat, buttonMessage, { quoted: null });
 }
 
-handler.command = ['testwelcome'];
+handler.command = ['welcome', 'bienvenido'];
 handler.group = true;
 //handler.admin = true;
 
