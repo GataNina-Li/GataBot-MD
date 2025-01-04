ec0a998b17f5d085a470ae04586391b7c499ca6a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-04 05:35:08 -0500

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -3,6 +3,7 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
 handler.all = async function (m) {
+if (m.isBaileys || (m.sender === m?.user?.jid)) return
 let user = global.db.data.users[m.sender]
 let chat = global.db.data.chats[m.chat]
 m.isBot = m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20;
