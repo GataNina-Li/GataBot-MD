86e6e0a73fec0f3055087930a167823aa316da0b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-05 18:19:26 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,9 +1,11 @@
 const userSpamData = {}
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, isPrems}) {
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, isPrems, isChannel}) {
+console.log(isChannel)
+if (isChannel) return
 const chat = global.db.data.chats[m.chat]
 const bot = global.db.data.settings[conn.user.jid] || {}
-if (m.chat === "120363336642332098@newsletter") return; 
+//if (m.chat === "120363336642332098@newsletter") return; 
 if (m.fromMe) return
 if (!bot.antiSpam) return
 if (m.isGroup) {
