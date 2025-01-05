5c68ca6cb87d56e4a025b09d43071f7a8e5f3e0f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-04 20:55:35 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- handler.js
+++ handler.js
@@ -1123,7 +1123,6 @@ const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == this.use
 const isRAdmin = user?.admin == 'superadmin' || false
 const isAdmin = isRAdmin || user?.admin == 'admin' || false //user admins? 
 const isBotAdmin = bot?.admin || false //Detecta sin el bot es admin
-//const isWABusiness = global.conn.authState?.creds?.platform === 'smba' || global.conn.authState?.creds?.platform === 'smbi'
 m.isWABusiness = global.conn.authState?.creds?.platform === 'smba' || global.conn.authState?.creds?.platform === 'smbi'
 
 const ___dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), './plugins')
