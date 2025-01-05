391053c7afc2dc87a18d99133e53ebd9ae66a2c7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-05 18:47:39 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- handler.js
+++ handler.js
@@ -1124,7 +1124,7 @@ const isRAdmin = user?.admin == 'superadmin' || false
 const isAdmin = isRAdmin || user?.admin == 'admin' || false //user admins? 
 const isBotAdmin = bot?.admin || false //Detecta sin el bot es admin
 m.isWABusiness = global.conn.authState?.creds?.platform === 'smba' || global.conn.authState?.creds?.platform === 'smbi'
-m.isChannel = m.chat.endsWith('@newsletter') || m.sender.endsWith('@newsletter')
+m.isChannel = m.chat.includes('@newsletter') || m.sender.includes('@newsletter')
 	
 const ___dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), './plugins')
 for (let name in global.plugins) {
