bb368ef5a9146c81662cd408f05922b7f5e0be97
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-05 18:20:49 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,8 +1,8 @@
 const userSpamData = {}
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, isPrems, isChannel}) {
-console.log(isChannel)
-if (isChannel) return
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, isPrems}) {
+console.log(m.isChannel)
+if (m.isChannel) return
 const chat = global.db.data.chats[m.chat]
 const bot = global.db.data.settings[conn.user.jid] || {}
 //if (m.chat === "120363336642332098@newsletter") return; 
