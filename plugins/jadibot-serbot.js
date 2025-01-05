e5b342e47dda067c568399928492d4fcdbf23ab7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-05 10:21:59 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -136,8 +136,8 @@ setTimeout(() => { conn.sendMessage(m.sender, { delete: txtQR.key })}, 30000)
 return
 } 
 if (mcode) {
-await sleep(2000)
-let secret = await conn.requestPairingCode((m.sender.split`@`[0]))
+//await sleep(2000)
+let secret = await sock.requestPairingCode(m.sender.split`@`[0])
 //const code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
 if (m.isWABusiness) {
 txtCode = await conn.sendMessage(m.chat, { image: { url: 'https://qu.ax/wyUjT.jpg' || gataMenu.getRandom() }, caption: rtx2.trim() + '\n' + drmer.toString("utf-8") }, { quoted: m })
