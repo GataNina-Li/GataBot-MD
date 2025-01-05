3f7c2539117c94e122afcff630e4171c3438a3aa
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-05 18:41:37 -0500

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+/*import axios from 'axios'
 import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
@@ -111,3 +111,4 @@ await this.reply(m.chat, result, m)
 return true
 }
 export default handler
+*/
