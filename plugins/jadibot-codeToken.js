97d7ca88255c2b22f9d13da01d80033e7b98262e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-06 10:46:04 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

---
+++ plugins/jadibot-codeToken.js
@@ -0,0 +1,26 @@
+/*
+El codigo de este archivo esta totalmente hecho por:
+- ReyEndymion (https://github.com/ReyEndymion)
+*/
+
+import fs from "fs"
+let handler = async (m, { conn, usedPrefix }, args, command) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let uniqid = `${who.split`@`[0]}`
+try {
+if(global.conns.push(conn))
+await conn.sendMessage(m.chat, {text : usedPrefix + 'serbot' + " " + Buffer.from(fs.readFileSync("./GataJadiBot/" + uniqid + "/creds.json"), "utf-8").toString("base64")}, { quoted: m })
+} catch(e) {
+await conn.reply(m.chat, `*No eres Sub-Bot de este Bot.*
+
+> 👉 Para convertirte en Sub-Bot, utiliza el comando:
+\`${usedPrefix + 'serbot'}\`
+
+> ⚠️ ¿Problemas para iniciar sesión?
+*Si no puedes acceder a tu sesión, elimina la sesión creada desde los dispositivos vinculados y utiliza el comando:*
+\`${usedPrefix + 'eliminarjb'}\`
+De esta manera, podrás solicitar una nueva sesión.`, m)
+}}
+handler.command = /^(codetoken)$/i
+handler.private = true
+export default handler
