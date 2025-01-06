43666a29ce89fa9fa5e614aa6a2315cfe3cdbc05
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-06 12:22:49 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/jadibot-finalizar.js
+++ plugins/jadibot-finalizar.js
@@ -1,27 +1,23 @@
-let handler  = async (m, { conn, command }) => {
-if (m.sender === conn.user.jid && m.fromMe) return
-
-if (/stop/i.test(command)) {
+let handler = async (m, { conn, command }) => {
+if (global.conns.some(c => c.user.jid === conn.user.jid)) {
 let resp
+if (/stop/i.test(command)) {
 let i = global.conns.indexOf(conn)
-    
-if (global.conn.user.jid != conn.user.jid && m.sender != global.conn.user.jid) { 
+if (global.conn.user.jid != conn.user.jid && m.sender != global.conn.user.jid) {
 if (i < 0) return
-        
 resp = 'Me apagaré'
 global.conns.splice(i, 1)
 conn.isInit = false
-        
 conn.ev.removeAllListeners()
 conn.ws.close()
-        
-return conn.sendMessage(m.chat, resp, m)
-} else {
-resp = '*Este comando solo puede ser ejecutado por Sub-Bots*'
-return conn.sendMessage(m.chat, resp, m)
+return conn.sendMessage(m.chat, resp, { quoted: m })
 }}
-}
+} else {
+let resp = 'Este comando solo puede ser ejecutado por un Sub-Bot registrado.'
+return conn.sendMessage(m.chat, resp, { quoted: m })
+}}
+
 handler.command = /^(berhenti|stop|detener)$/i
-//handler.owner = true  
-//handler.fail = null
+handler.private = true  
 export default handler
+
