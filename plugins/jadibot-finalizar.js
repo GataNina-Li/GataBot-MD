9a3cf5c82f86196cc04a4cae8abc9864d661fca5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-06 13:26:19 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/jadibot-finalizar.js
+++ plugins/jadibot-finalizar.js
@@ -7,18 +7,18 @@ let i = global.conns.indexOf(conn)
 if (global.conn.user.jid != conn.user.jid && m.sender != global.conn.user.jid) {
 if (i < 0) return
 resp = 'Me apagaré'
-global.conns.splice(i, 1)
-conn.isInit = false
 conn.ev.removeAllListeners()
 conn.ws.close()
+conn.isInit = false
+global.conns.splice(i, 1)
 return conn.sendMessage(m.chat, { text: resp }, { quoted: m })
 }}
 } else {
 resp = 'Este comando solo puede ser ejecutado por un Sub-Bot registrado.'
 return conn.sendMessage(m.chat, { text: resp }, { quoted: m })
 }} catch (e) {
-console.log('Error al intentar apagar el Sub-Bot:', e)
 resp = 'Hubo un error al intentar apagar el Sub-Bot.'
+console.log('Error al intentar apagar el Sub-Bot: ', e)
 return conn.sendMessage(m.chat, { text: resp }, { quoted: m })
 }}
 
