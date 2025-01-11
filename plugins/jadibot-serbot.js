a1b78a9b662204d0620e7c4fb461d300d95e8e6b
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-11 19:49:55 -0300

    Add files via upload

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -132,14 +132,18 @@ return
 if (qr && mcode) {
 let secret = await sock.requestPairingCode((m.sender.split`@`[0]))
 secret = secret.match(/.{1,4}/g)?.join("-")
-const dispositivo = await getDevice(m.key.id)
-if (!m.isWABusiness && /web|desktop|unknown/i.test(dispositivo)) {
+const dispositivo = await getDevice(m.key.id);
+if (!m.isWABusiness) {
+if (/web|desktop|unknown/i.test(dispositivo)) {
 txtCode = await conn.sendMessage(m.chat, { image: { url: 'https://qu.ax/wyUjT.jpg' || gataMenu.getRandom() }, caption: rtx2.trim() + '\n' + drmer.toString("utf-8") }, { quoted: m })
-codeBot = await m.reply(secret)
+codeBot = await m.reply(secret);
 } else {
 txtCode = await conn.sendButton(m.chat, rtx2.trim() + '\n' + drmer.toString("utf-8"), wm + `\n*Código:* ${secret}`, 'https://qu.ax/wyUjT.jpg' || gataMenu.getRandom(), null,  [[`Copiar código`, secret]], null, null, m)
+}} else {
+txtCode = await conn.sendMessage(m.chat, { image: { url: 'https://qu.ax/wyUjT.jpg' || gataMenu.getRandom() }, caption: rtx2.trim() + '\n' + drmer.toString("utf-8") }, { quoted: m })
+codeBot = await m.reply(secret);
 }
-console.log(secret)
+console.log(secret);
 }
 if ((txtCode && txtCode.key) || (txtCode && txtCode.id)) {
 const messageId = txtCode.key || txtCode.id
