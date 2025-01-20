fce402865a6160fdfa7c52398d2cccbdc090d4a6
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-20 19:04:14 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,28 +1,23 @@
-/* 
-# Créditos a https://github.com/AzamiJs
-•• @AzamiJs ••
-*/
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import fetch from 'node-fetch'
-let handler = async (m) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => gataImg)
-let name = await conn.getName(who)
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
+import uploadFile from '../lib/uploadFile.js';
+import upload from '../lib/uploadFile2.js';
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m) => {
+const q = m.quoted ? m.quoted : m;
+const mime = (q.msg || q).mimetype || '';
 if (!mime) throw `${mg} ${mid.smsconvert10}`
-let media = await q.download()
-let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-let link = await (isTele ? uploadImage : uploadFile)(media)
-let caption = `🛑 ${mid.smsYT4}:\n${link}\n🥏 ${mid.smsconvert11}: ${media.length}\n🚀 ${mid.smsconvert12}: ${isTele ? '𝙉𝙊 𝙀𝙓𝙋𝙄𝙍𝘼' : '𝘿𝙀𝙎𝘾𝙊𝙉𝙊𝘾𝙄𝘿𝙊'}\n🔰 ${mid.smsconvert13}: ${await shortUrl(link)}`
-conn.reply(m.chat, caption, m, { contextInfo: {externalAdReply :{mediaUrl: md, mediaType: 2, title: wm, body: botdate, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link }}})}
+const media = await q.download();
+try {
+const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+const link = await (isTele ? uploadImage : uploadFile)(media);
+m.reply(link);
+} catch (e) {
+try {    
+const link = await upload(media);
+m.reply(link);
+} catch (e) {
+console.log(e) 
+}}}
 handler.help = ['tourl']
 handler.tags = ['herramientas']
 handler.command = /^(tourl|upload)$/i
 export default handler
-
-async function shortUrl(url) {
-let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-return await res.text()
-}
