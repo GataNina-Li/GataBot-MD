1a68d03a69d1476157881656fb0f6a55c2de18e5
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-22 11:31:11 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -343,7 +343,7 @@ for (const channelId of Object.values(global.ch)) {
 await conn.newsletterFollow(channelId).catch(() => {})
 }}
 
-async function checkSubBots() {
+/*async function checkSubBots() {
   for (let sock of global.conns) {
     if (!sock.ws || sock.ws.readyState !== ws.OPEN) {
       console.log(`Sub bot ${sock.user.jid} está desconectado, intentando reiniciar...`);
@@ -358,3 +358,4 @@ async function checkSubBots() {
 }
 
 setInterval(checkSubBots, 300000) //5 min
+*/
