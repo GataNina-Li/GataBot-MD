294b247ff96cc2e4cdfd3ef8a3a4eadf5bab46c3
EdderBot02 <107139979+EdderBot02@users.noreply.github.com>
2025-02-03 18:24:17 -0600

    Update sticker-attp y ttp.js

--- plugins/sticker-attp y ttp.js
+++ plugins/sticker-attp y ttp.js
@@ -1,52 +1,29 @@
 import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
+import axios from 'axios';
+
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-if (!text) throw `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙋𝘼𝙍𝘼 𝙌𝙐𝙀 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊 𝙎𝙀 𝘾𝙊𝙉𝙑𝙄𝙀𝙍𝙏𝘼 𝙀𝙉 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command}* Nuevo Sticker\n\n𝙒𝙍𝙄𝙏𝙀 𝙎𝙊 𝙏𝙃𝙀 𝙏𝙀𝙓𝙏 𝘽𝙀𝘾𝙊𝙈𝙀𝙎 𝘼 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command}* New Sticker*`
-let teks = encodeURI(text)
-
-if (command == 'attp') {
-let stiker = await sticker(null,`https://btch.us.kg/attp?text=${teks}`,global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })}
-
-if (command == 'ttp') {
-let stiker = await sticker(null,`https://btch.us.kg/ttp?text=${teks}`,global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })}
+    if (!text) throw `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙋𝘼𝙍𝘼 𝙌𝙐𝙀 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊 𝙎𝙀 𝘾𝙊𝙉𝙑𝙄𝙀𝙍𝙏𝘼 𝙀𝙉 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command}* Nuevo Sticker\n\n𝙒𝙍𝙄𝙏𝙀 𝙎𝙊 𝙏𝙃𝙀 𝙏𝙀𝙓𝙏 𝘽𝙀𝘾𝙊𝙈𝙀𝙎 𝘼 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command}* New Sticker*`
+    let teks = encodeURI(text)
+    if (command == 'attp') {
+        const data = {
+            text: `${text}`
+        };
+        const response = await axios.post('https://salism3api.pythonanywhere.com/text2gif', data);
+        const x=response.data.image;
+        let stiker = await sticker(null,x,global.packname, global.author)
+        conn.sendFile(m.chat, stiker, null, { asSticker: true })
+    }
+    if (command == 'ttp') {
+        const data = {
+            text: `${text}`,"outlineColor":"255,0,0,255", "textColor":"0,0,0,255"
+        };
+        const response = await axios.post('https://salism3api.pythonanywhere.com/text2img', data);
+        const x=response.data.image;
+        let stiker = await sticker(null,x,global.packname, global.author)
+        conn.sendFile(m.chat, stiker, null, { asSticker: true })
+    }
 }
 handler.command = handler.help = ['ttp', 'attp']
 handler.tags = ['sticker']
 export default handler
-
-
-
-//Código con api lolhuman
-/*
-let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-if (!text) throw `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙋𝘼𝙍𝘼 𝙌𝙐𝙀 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊 𝙎𝙀 𝘾𝙊𝙉𝙑𝙄𝙀𝙍𝙏𝘼 𝙀𝙉 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command}* Nuevo Sticker\n\n𝙒𝙍𝙄𝙏𝙀 𝙎𝙊 𝙏𝙃𝙀 𝙏𝙀𝙓𝙏 𝘽𝙀𝘾𝙊𝙈𝙀𝙎 𝘼 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command}* New Sticker*`
-let teks = encodeURI(text)
-
-if (command == 'attp') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'attp2') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp2?apikey=9b817532fadff8fc7cb86862&text=${teks}`, 'sticker.webp', '', m)}
-
-if (command == 'ttp5') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp6?apikey=9b817532fadff8fc7cb86862&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ttp4') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp5?apikey=9b817532fadff8fc7cb86862&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ttp3') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp3?apikey=9b817532fadff8fc7cb86862&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ttp2') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp2?apikey=9b817532fadff8fc7cb86862&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ttp') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp?apikey=9b817532fadff8fc7cb86862&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-}
-handler.command = handler.help = ['ttp', 'ttp2', 'ttp3', 'ttp4', 'ttp5', 'attp', 'attp2']
-handler.tags = ['sticker']
-export default handler
-*/
