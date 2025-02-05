f1969cb00328ffbc728a02990351a28e0158199f
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-04 23:08:13 -0500

    Mejorar el manejador de acortamiento de URL

--- plugins/herramientas-acortar.js
+++ plugins/herramientas-acortar.js
@@ -1,16 +1,18 @@
 import axios from "axios"
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `${mg}${mid.smsAcorta}`
-try {
-let json = await (await axios.get(`https://tinyurl.com/api-create.php?url=` + encodeURIComponent(url))).data
-if (!json.status) throw json
-let hasil = `${smsAcorta(text)}\n*${json}*`.trim()   
-m.reply(hasil)
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)}}
-handler.help = ['tinyurl','acortar'].map(v => v + ' <link>')
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `${mg}${mid.smsAcorta}`
+    try {
+        let json = await axios.get(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(text)}`)
+        if (!json.data) throw json
+        let hasil = `${smsAcorta(text)}\n*${json.data}*`.trim()
+        m.reply(hasil)
+    } catch (e) {
+        await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
+        console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+        console.log(e)
+    }
+}
+handler.help = ['tinyurl', 'acortar'].map(v => v + ' <link>')
 handler.tags = ['tools']
 handler.command = /^(tinyurl|short|acortar|corto)$/i
 handler.fail = null
