a478c8e30cf8faa2c2fd23f2a0b0c1a73a79d7f8
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-06 11:59:48 -0500

    Actualizar el plugin de stickers Brat para usar la nueva API de CaliphDev y mejorar el manejo de errores

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -2,9 +2,10 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    let stiker = await sticker(null, `https://brat.caliphdev.com/api/brat?text=${encodeURIComponent(teks)}`, global.packname, global.author)
+    let stiker = await sticker(null, global.API('caliphdev', '/api/brat', { text: encodeURIComponent(teks) }), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-    throw teks.toString()
+    throw stiker.toString()
+
 }
 handler.help = ['brat <teks>']
 handler.tags = ['sticker']
