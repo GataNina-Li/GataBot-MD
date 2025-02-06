27dbb5345779c5f569a794f572420fb8ca44a0f5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-05 21:41:22 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -4,7 +4,7 @@ let linkRegex2 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
 let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, participants }) {
 if (!m.isGroup) return 
-//if (isAdmin || isOwner || m.fromMe || isROwner || !isBotAdmin) return
+if (isAdmin || isOwner || m.fromMe || isROwner || !isBotAdmin) return
 
 let chat = global.db.data.chats[m.chat]
 let delet = m.key.participant
@@ -22,8 +22,8 @@ if (isBotAdmin) {
 if (m.text.includes(linkThisGroup)) return
 await conn.sendMessage(m.chat, { text: `${mid.mAdvertencia + mid.mWhatsApp} *${user}*`, mentions: [m.sender] }, { quoted: m })    
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-//if (remove[0].status === '404') return
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
 }}
 return !0
 }
