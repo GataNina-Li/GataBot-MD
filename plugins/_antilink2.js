7fee4985f1c17bec128ea29dc1eb3d10b212ec5f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-06 04:34:47 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -2,7 +2,6 @@
 
 const linkRegex = (text) => {
 const urlRegex = /(?:[a-zA-Z]+:\/\/[^\s]+)|(?:\b(www\.|ftp\.)[^\s]+\.[a-z]{2,}\/?[^\s]*)|(?:\b[a-z0-9.-]+\.[a-z]{2,}(?:\/[^\s]*)?)/gi
-
 const urls = [...new Set((text.match(urlRegex) || [])
 .map(m => m.replace(/[.,;!?]+$/, '')) 
 .map(m => /^(?:www\.|ftp\.)/.test(m) ? 'http://' + m : m) 
@@ -21,7 +20,7 @@ return urls.length > 0
 let handler = m => m
 handler.before = async function (m, { isAdmin, isBotAdmin, isOwner, isROwner, participants }) {
 if (!m.isGroup) return 
-//if (isAdmin || isOwner || m.fromMe || isROwner || !isBotAdmin) return
+if (isAdmin || isOwner || m.fromMe || isROwner || !isBotAdmin) return
 
 let chat = global.db.data.chats[m.chat]
 let delet = m.key.participant
@@ -40,8 +39,8 @@ if (m.text.includes(linkThisGroup)) return !0
 if (isBotAdmin) {
 await conn.sendMessage(m.chat, { text: `${mid.mAdvertencia + mid.mWhatsApp2} *${user}*`, mentions: [m.sender] }, { quoted: m })    
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-//if (remove[0].status === '404') return
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
 }}
 return !0
 }
