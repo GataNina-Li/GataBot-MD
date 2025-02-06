bd495f75e47d2951805c1a99fdb6aa01a2d0b255
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-05 20:31:29 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/herramientas-deepseek.js
+++ plugins/herramientas-deepseek.js
@@ -3,18 +3,17 @@
 //❀ Canal Principal ≽^•˕• ྀི≼
 ///https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04
 
-
 import axios from 'axios'
 
 const handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '❀ Ingresa un texto para hablar con DeepSeek AI.', m)
+if (!text) return conn.reply(m.chat, '*Ingresa un texto para hablar con DeepSeek AI.*', m)
   
 try {
 let { data } = await axios.get(`https://archive-ui.tanakadomp.biz.id/ai/deepseek?text=${encodeURIComponent(text)}`)
 await m.reply(data?.result || '❌ No se obtuvo una respuesta válida de DeepSeek AI.')
 } catch {
-await m.reply('❌ Error al procesar la solicitud.')
+await m.reply('*❌ Error al procesar la solicitud.*')
 }}
 
-handler.command = ['deepseek', 'ia3']
+handler.command = /^(deepseek|ia3)$/i
 export default handler
