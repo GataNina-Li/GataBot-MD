b74449e9af503734c194dbb3dfc9f7f7b7a8dbdb
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-06 11:35:40 -0500

    Agregar un nuevo plugin de stickers que genera stickers a partir de texto usando la API de Brat

---
+++ plugins/sticker-brat.js
@@ -0,0 +1,12 @@
+import { sticker } from '../lib/sticker.js'
+
+let handler = async (m, { conn, text }) => {
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+    let stiker = await sticker(null, `https://brat.caliphdev.com/api/brat?text=${encodeURIComponent(teks)}`, global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    throw stiker.toString()
+}
+handler.help = ['brat <teks>']
+handler.tags = ['sticker']
+handler.command = /^brat6$/i
+export default handler
