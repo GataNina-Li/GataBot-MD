feb48aeb6b5e792693d214a9f5a0ded399b3bdcb
OfcKing <argumedodie@gmail.com>
2025-02-05 21:36:16 -0500

    Gata Dios

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -25,26 +25,31 @@ await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype:
 }}}
 export default handler*/
 
-import { downloadContentFromMessage } from "@whiskeysockets/baileys"
+import { downloadContentFromMessage } from "@whiskeysockets/baileys";
 
 export async function before(m, { isAdmin, isBotAdmin }) {
-let chat = db.data.chats[m.chat]
-if (!chat.antiver || chat.isBanned) return
-if (m.mtype == 'viewOnceMessageV2' || m.mtype.hasOwnProperty("viewOnce")) {
-let msg = m.message.viewOnceMessageV2.message
-let type = Object.keys(msg)[0]
-let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
-let buffer = Buffer.from([])
+let chat = db.data.chats[m.chat];
+if (!chat.antiver || chat.isBanned) return;
+
+if (m.mtype == 'viewOnceMessage' || m.mtype == 'viewOnceMessageV2') {
+let msg = m.message[m.mtype].message;
+let type = Object.keys(msg)[0];
+let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video');
+let buffer = Buffer.from([]);
 for await (const chunk of media) {
-buffer = Buffer.concat([buffer, chunk])}
+buffer = Buffer.concat([buffer, chunk]);
+}
+
 if (/video/.test(type)) {
-return this.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption}` + lenguajeGB.smsAntiView1(), m)
+await this.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption || ''} ${lenguajeGB.smsAntiView1()}`, m);
 } else if (/image/.test(type)) {
-return this.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption}` + lenguajeGB.smsAntiView2(), m)
+await this.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption || ''} ${lenguajeGB.smsAntiView2()}`, m);
+} else if (/audio/.test(type)) {
+await this.sendFile(m.chat, buffer, 'error.mp3', `${msg[type].caption || ''} ${lenguajeGB.smsAntiView3()}`, m);
 }}}
 
 function formatFileSize(bytes) {
-const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY']
-const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
-return Math.round(100 * (bytes / Math.pow(1024, i))) / 100 + ' ' + sizes[i]
+const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY'];
+const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+return Math.round(100 * (bytes / Math.pow(1024, i))) / 100 + ' ' + sizes[i];
 }
