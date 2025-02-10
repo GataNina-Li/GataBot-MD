d50d4e347a1d40846da3f4dfeadb752e33eac5dc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-09 19:53:23 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_allantilink.js
+++ plugins/_allantilink.js
@@ -1,19 +1,19 @@
 import fetch from 'node-fetch'  
 
-const isLinkTik = /tiktok.com/i 
-const isLinkYt = /youtube.com|youtu.be/i 
-const isLinkTel = /telegram.com|t.me/i 
-const isLinkFb = /facebook.com|fb.me/i 
-const isLinkIg = /instagram.com/i 
-const isLinkTw = /twitter.com|x.com/i 
-const isLinkDc = /discord.com|discord.gg/i 
-const isLinkTh = /threads.net/i 
-const isLinkTch = /twitch.tv/i
+const isLinkTik = /\b(?:https?:\/\/)?(?:www\.)?tiktok\.com(\/\S*)?/i
+const isLinkYt = /\b(?:https?:\/\/)?(?:www\.)?(youtube\.com|youtu\.be)(\/\S*)?/i
+const isLinkTel = /\b(?:https?:\/\/)?(?:www\.)?(telegram\.org|t\.me)(\/\S*)?/i
+const isLinkFb = /\b(?:https?:\/\/)?(?:www\.)?(facebook\.com|fb\.me|fb\.watch)(\/\S*)?/i
+const isLinkIg = /\b(?:https?:\/\/)?(?:www\.)?instagram\.com(\/\S*)?/i
+const isLinkTw = /\b(?:https?:\/\/)?(?:www\.)?(twitter\.com|x\.com)(\/\S*)?/i
+const isLinkDc = /\b(?:https?:\/\/)?(?:www\.)?(discord\.com|discord\.gg)(\/\S*)?/i
+const isLinkTh = /\b(?:https?:\/\/)?(?:www\.)?threads\.net(\/\S*)?/i
+const isLinkTch = /\b(?:https?:\/\/)?(?:www\.)?twitch\.tv(\/\S*)?/i
   
 let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
 if (!m.isGroup) return 
-//if (isAdmin || isOwner || m.fromMe || isROwner || !isBotAdmin) return
+if (isAdmin || isOwner || m.fromMe || isROwner || !isBotAdmin) return
 
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[this.user.jid] || {}
@@ -32,36 +32,33 @@ const isAntiLinkDc = isLinkDc.exec(m.text)
 const isAntiLinkTh = isLinkTh.exec(m.text)
 const isAntiLinkTch = isLinkTch.exec(m.text)
  
-if (chat.antiTiktok && isAntiLinkTik) {  
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
+if (chat.antiTiktok && isAntiLinkTik) {   
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mTiktok} *@${toUser}*`, null, { mentions: [aa] })
 await new Promise(resolve => setTimeout(resolve, 100)); 
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-//if (remove[0].status === '404') return;
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (remove[0].status === '404') return
 await new Promise(resolve => setTimeout(resolve, 100)); 
 chat.delete = wasDeleteActive;
 }}
     
 if (chat.antiYoutube && isAntiLinkYt) {
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mYoutube} *@${toUser}*`, null, { mentions: [aa] })
 await new Promise(resolve => setTimeout(resolve, 100)); 
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-//if (remove[0].status === '404') return;
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (remove[0].status === '404') return;
 await new Promise(resolve => setTimeout(resolve, 100)); 
 chat.delete = wasDeleteActive;
 }}
     
-if (chat.antiTelegram && isAntiLinkTel) {
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })   
+if (chat.antiTelegram && isAntiLinkTel) { 
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
@@ -70,90 +67,84 @@ await new Promise(resolve => setTimeout(resolve, 100));
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
 //let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
 //if (remove[0].status === '404') return;
-await new Promise(resolve => setTimeout(resolve, 100)); 
-chat.delete = wasDeleteActive;
+await new Promise(resolve => setTimeout(resolve, 100))
+chat.delete = wasDeleteActive
 }}
     
 if (chat.antiFacebook && isAntiLinkFb) {
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mFacebook} *@${toUser}*`, null, { mentions: [aa] })
 await new Promise(resolve => setTimeout(resolve, 100)); 
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-//if (remove[0].status === '404') return;
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (remove[0].status === '404') return;
 await new Promise(resolve => setTimeout(resolve, 100)); 
 chat.delete = wasDeleteActive;
 }}
     
-if (chat.antiInstagram && isAntiLinkIg) {
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
+if (chat.antiInstagram && isAntiLinkIg) { 
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mInstagram} *@${toUser}*`, null, { mentions: [aa] })
 await new Promise(resolve => setTimeout(resolve, 100)); 
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-//if (remove[0].status === '404') return;
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (remove[0].status === '404') return;
 await new Promise(resolve => setTimeout(resolve, 100)); 
 chat.delete = wasDeleteActive;
 }}
     
 if (chat.antiTwitter && isAntiLinkTw) {
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mX} *@${toUser}*`, null, { mentions: [aa] });
 await new Promise(resolve => setTimeout(resolve, 100)); 
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-//if (remove[0].status === '404') return;
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (remove[0].status === '404') return;
 await new Promise(resolve => setTimeout(resolve, 100)); 
 chat.delete = wasDeleteActive;
 }}
 
 if (chat.antiDiscord && isAntiLinkDc) {
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mDiscord} *@${toUser}*`, null, { mentions: [aa] })
 await new Promise(resolve => setTimeout(resolve, 100)); 
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-//if (remove[0].status === '404') return;
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (remove[0].status === '404') return;
 await new Promise(resolve => setTimeout(resolve, 100)); 
 chat.delete = wasDeleteActive;
 }}
 
 if (chat.antiThreads && isAntiLinkTh) {
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mThreads} *@${toUser}*`, null, { mentions: [aa] })
 await new Promise(resolve => setTimeout(resolve, 100)); 
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-//if (remove[0].status === '404') return;
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (remove[0].status === '404') return;
 await new Promise(resolve => setTimeout(resolve, 100)); 
 chat.delete = wasDeleteActive;
 }}
 
 if (chat.antiTwitch && isAntiLinkTch) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
 if (isBotAdmin) {
 let wasDeleteActive = chat.delete; 
 chat.delete = false; 
 await conn.reply(m.chat, `${mid.mAdvertencia + mid.mTwitch} *@${toUser}*`, null, { mentions: [aa] })
 await new Promise(resolve => setTimeout(resolve, 100)); 
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-//let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-//if (remove[0].status === '404') return;
+let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (remove[0].status === '404') return;
 await new Promise(resolve => setTimeout(resolve, 100)); 
 chat.delete = wasDeleteActive;
 }}
