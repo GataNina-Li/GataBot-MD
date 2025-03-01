0c32c7eda5e93cc4cf73550c182dbb60e7f8a5d6
OfcKing <argumedodie@gmail.com>
2025-02-28 21:34:23 -0500

    Gata Dios

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,113 +1,48 @@
-import fg from 'api-dylux';
-import fetch from 'node-fetch';
-//import {savefrom, facebookdl, facebookdlv2} from '@bochilteam/scraper';
-//import fbDownloader from 'fb-downloader-scrapper';
-//import {facebook} from '@xct007/frieren-scraper';
-import axios from 'axios';
-const handler = async (m, {conn, args, command, usedPrefix}) => {
-let user = global.db.data.users[m.sender]
-if (!args[0]) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://www.facebook.com/watch?v=636541475139*\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙇𝙄𝙉𝙆 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙏𝙃𝙀 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} https://fb.watch/dcXq_0CaHi/*`, fkontak, m)
-if (!args[0].match(/www.facebook.com|fb.watch/g)) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://www.facebook.com/watch?v=636541475139*\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙇𝙄𝙉𝙆 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙏𝙃𝙀 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} https://fb.watch/dcXq_0CaHi/*`, fkontak, m)
-let contenido = `✅ 𝙑𝙄𝘿𝙀𝙊 𝘿𝙀 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\n${wm}`
-await m.react('⏱️')
-try {
-const api = await fetch(`https://api.agatz.xyz/api/facebook?url=${args}`);
-const data = await api.json();
-const videoUrl = data.data.hd || data.data.sd; 
-const imageUrl = data.data.thumbnail; 
-if (videoUrl && videoUrl.endsWith('.mp4')) {
-await conn.sendFile(m.chat, videoUrl, 'video.mp4', `${gt}`, m, null, fake);
-m.react('✅');
-} else if (imageUrl && (imageUrl.endsWith('.jpg') || imageUrl.endsWith('.png'))) {
-await conn.sendFile(m.chat, imageUrl, 'thumbnail.jpg', `${gt}`, m, null, fake);
-m.react('✅');
-}} catch {   
-try {
-const api = await fetch(`https://api.alyachan.dev/api/fb?url=${args}&apikey=Gata-Dios`);
-const data = await api.json();
-const downloadUrl = data.data[0].url;
-if (downloadUrl.endsWith('.jpg') || downloadUrl.endsWith('.png')) {
-await conn.sendFile(m.chat, downloadUrl, 'ig.jpg', `${gt}`, m, null, fake);
-m.react('✅');
-} else if (downloadUrl.endsWith('.mp4')) {
-await conn.sendFile(m.chat, downloadUrl, 'ig.mp4', `${gt}`, m, null, fake);
-m.react('✅');
-}
-} catch {   
-try {
-const apiUrl = `https://api.dorratz.com/fbvideo?url=${encodeURIComponent(args[0])}`;
-const response = await fetch(apiUrl);
-const data = await response.json();
-if (data.result) {
-const hdUrl = data.result.hd;
-const sdUrl = data.result.sd;
-const audioUrl = data.result.audio;        
-const downloadUrl = hdUrl || sdUrl; 
-await conn.sendFile(m.chat, downloadUrl, `error.mp4`, contenido, m)
-}} catch (err1) {
-try {
-const apiUrl = `${apis}/download/facebook?url=${encodeURIComponent(args[0])}`;
-const apiResponse = await fetch(apiUrl);
-const delius = await apiResponse.json();
-if (!delius || !delius.urls || delius.urls.length === 0) return m.react("❌")
-const downloadUrl = delius.urls[0].hd || delius.urls[0].sd;
-if (!downloadUrl) return m.react("❌");
-await conn.sendFile(m.chat, downloadUrl, `error.mp4`, contenido, m)
-} catch (err2) {
-try {
-const d2ata = await facebook.v1(args[0]);
-let r2es = '';
-if (d2ata.urls && d2ata.urls.length > 0) {
-r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}` }
-conn.sendFile(m.chat, r2es, `error.mp4`, contenido, m)
-m.react(`✅`) 
-} catch (err3) {
-try {
-const req = await igeh(args[0]);
-conn.sendMessage(m.chat, {video: {url: req.url_list}}, m);
-} catch (err5) {
-try {
-const ress = await fg.fbdl(args[0]);
-const urll = await ress.data[0].url;
-await conn.sendFile(m.chat, urll, `error.mp4`, contenido, m)
-m.react(`✅`) 
-} catch (err6) {
-try {
-const res = await fbDownloader(args[0]);
-for (const result of res.download) {
-const ur = result.url;
-await conn.sendFile(m.chat, ur, `error.mp4`, contenido, m) 
-m.react(`✅`)               
-}} catch (err7) {
-try {
-const {result} = await facebookdl(args[0]).catch(async (_) => await facebookdlv2(args[0])).catch(async (_) => await savefrom(args[0]));
-for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `error.mp4`, contenido, m) 
-m.react(`✅`)                 
-} catch (e) {   
-console.log(e) 
-}}}}}}}}}}
-handler.command = /^(facebook|fb|facebookdl|fbdl|facebook1|fb1|facebookdl1|fbdl1|facebook2|fb2|facebookdl2|fbdl2)$/i
-handler.limit = 3
-export default handler
+import { igdl } from 'ruhend-scraper'
 
-async function igeh(url_media) {
-return new Promise(async (resolve, reject)=>{
-const BASE_URL = 'https://instasupersave.com/';
-try {
-const resp = await axios(BASE_URL);
-const cookie = resp.headers['set-cookie']; // obtener cookie de la solicitud
-const session = cookie[0].split(';')[0].replace('XSRF-TOKEN=', '').replace('%3D', '');
-const config = {method: 'post', url: `${BASE_URL}api/convert`, headers: {'origin': 'https://instasupersave.com', 'referer': 'https://instasupersave.com/pt/', 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52', 'x-xsrf-token': session, 'Content-Type': 'application/json', 'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`}, data: {url: url_media}};
-axios(config).then(function(response) {
-const ig = [];
-if (Array.isArray(response.data)) {
-response.data.forEach((post) => {
-ig.push(post.sd === undefined ? post.thumb : post.sd.url);
-})} else {
-ig.push(response.data.url[0].url)}
-resolve({results_number: ig.length, url_list: ig});
-}).catch(function(error) {
-reject(error.message)});
-} catch (e) {
-reject(e.message);
-}})}
+const handler = async (m, { text, conn, args }) => {
+
+if (!args[0]) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://www.facebook.com/watch?v=636541475139*\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙇𝙄𝙉𝙆 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙏𝙃𝙀 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} https://fb.watch/dcXq_0CaHi/*`, fkontak, m)
+
+if (!args[0].match(/www.facebook.com|fb.watch/g)) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://www.facebook.com/watch?v=636541475139*\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙇𝙄𝙉𝙆 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙏𝙃𝙀 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} https://fb.watch/dcXq_0CaHi/*`, fkontak, m)
+
+  let res;
+  try {
+  m.reply('⏱️')
+
+    res = await igdl(args[0]);
+  } catch (e) {
+    return conn.reply(m.chat, 'Error al obtener datos. Verifica el enlace.', m)
+  }
+
+  let result = res.data;
+  if (!result || result.length === 0) {
+    return conn.reply(m.chat, 'No se encontraron resultados.', m)
+  }
+
+  let data;
+  try {
+    data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
+  } catch (e) {
+    return conn.reply(m.chat, 'Error al procesar los datos.', m)
+  }
+
+  if (!data) {
+    return conn.reply(m.chat, 'No se encontró una resolución adecuada.', m)
+  }
+
+  let video = data.url;
+  try {
+    let contenido = `✅ 𝙑𝙄𝘿𝙀𝙊 𝘿𝙀 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\n${wm}`
+    await conn.sendFile(m.chat, video, 'video.mp4', `${contenido}`, m, null, fake);
+    await m.react('✅')
+  } catch (e) {
+    return conn.reply(m.chat, '❌ Ocurrió un error inesperado: ' + e, m)
+  }
+}
+
+handler.help = ['facebook', 'fb']
+handler.tags = ['downloader']
+handler.command = ['facebook', 'fb']
+
+export default handler
