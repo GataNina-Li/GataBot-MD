cb4976e4810477f86534876f5eabe1a00169adb9
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-03-22 12:34:57 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/herramientas-readmore.js
+++ plugins/herramientas-readmore.js
@@ -2,11 +2,30 @@ let handler = async (m, { conn, text }) => {
 let [l, r] = text.split`|`
 if (!l) l = ''
 if (!r) r = ''
+
+function insertReadMoreEverySixWords(str) {
+let words = str.split(' ');
+let result = [];
+for (let i = 0; i < words.length; i += 6) {
+result.push(words.slice(i, i + 6).join(' '));
+}
+return result.join(` ${readMore} `);
+}
+
+if (l.split(' ').length > 6) {
+l = insertReadMoreEverySixWords(l);
+}
+if (r.split(' ').length > 6) {
+r = insertReadMoreEverySixWords(r);
+}
+
 conn.reply(m.chat, l + readMore + r, m)
 }
-handler.help = ['readmore', 'spoiler'].map(v => v + ' <teks>|<teks>')
+handler.help = ['readmore <text1>|<text2>']
 handler.tags = ['tools']
-handler.command = /^(spoiler|hidetext|readmore|selengkapnya)$/i 
+handler.command = ['leermas', 'readmore']
+handler.register = true 
+handler.limit = 1
 export default handler
 
 const more = String.fromCharCode(8206)
