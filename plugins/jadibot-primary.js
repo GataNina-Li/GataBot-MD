a6b9d7599d3d85ff49c0e1472882492862e01bb2
Alex-X <argumedodie@gmail.com>
2025-03-30 13:35:22 -0500

    Gata Dios

--- plugins/jadibot-primary.js
+++ plugins/jadibot-primary.js
@@ -1,10 +1,10 @@
 import ws from 'ws';
 
 let handler = async (m, { conn, usedPrefix, args }) => {
-if (!args[0] && !m.quoted) 
-return m.reply(`⚠️ Menciona el número de un bot o responde al mensaje de un bot.\n> Ejemplo: *${usedPrefix}setprimary @0*`);
+if (!args[0] && !m.quoted) return m.reply(`⚠️ Menciona el número de un bot o responde al mensaje de un bot.\n> Ejemplo: *${usedPrefix}setprimary @0*`);
 
 const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
+
 let botJid;
 let selectedBot;
 
@@ -14,21 +14,23 @@ botJid = m.mentionedJid[0];
 botJid = m.quoted.sender;
 } else {
 botJid = args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net';
-} if (botJid === conn.user.jid || global.conn.user.jid) {
+} if (botJid === conn.user.jid || botJid === global.conn.user.jid) {
 selectedBot = conn;
 } else {
 selectedBot = users.find(conn => conn.user.jid === botJid);
 }
 
-if (!selectedBot) 
+if (!selectedBot) {
 return m.reply("⚠️ El bot no es un bot de la misma sessión, verifica los bots conectados, usando */bots*.");
+}
 
 let chat = global.db.data.chats[m.chat];
+
 if (chat.primaryBot === botJid) {
 return conn.reply(m.chat, `⚠️ @${botJid.split`@`[0]} ya es el bot primario.`, m, { mentions: [botJid] });
 }
 
-chat.primaryBot = botJid; 
+chat.primaryBot = botJid;
 conn.sendMessage(m.chat, { text: `✅ El bot @${botJid.split('@')[0]} ha sido establecido como primario en este grupo. Los demás bots no responderán aquí.`, mentions: [botJid]}, { quoted: m });
 };
 
