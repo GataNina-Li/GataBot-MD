a5412b0b6811dcde92870ef91e3cdd4e7f226998
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-04-20 13:38:40 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1127,8 +1127,14 @@ await delay(time)
 }, time)
 }
 
-if(m.id.startsWith('NJX-') || m.id.startsWith('Lyru-') || m.id.startsWith('EvoGlobalBot-') || m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') || m.id.startsWith('8SCO') && m.id.length === 20 || m.id.startsWith('FizzxyTheGreat-')) return
-
+if (m.id.startsWith('NJX-') ||
+m.id.startsWith('Lyru-') ||
+m.id.startsWith('EvoGlobalBot-') ||
+(m.id.startsWith('BAE5') && m.id.length === 16) ||
+m.id.startsWith('B24E') ||
+(m.id.startsWith('8SCO') && m.id.length === 20) ||
+m.id.startsWith('FizzxyTheGreat-')) return
+	
 if (opts['nyimak']) return
 if (!isROwner && opts['self']) return 
 if (opts['pconly'] && m.chat.endsWith('g.us')) return
