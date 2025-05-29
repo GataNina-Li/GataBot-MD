db45d19ae6c81b879c75768caaa3b60043d92ea2
Alex-X <argumedodie@gmail.com>
2025-05-28 20:25:28 -0500

    Delete plugins/_Test.js

--- plugins/_Test.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    const userLid = m.sender;
-    await conn.sendMessage(m.chat, { text: `${userLid}` }, { quoted: m });
-};
-
-handler.command = ['test'];
-
-export default handler;
