cd892842eaed59b4f1bd69d3a7401b0f30baf2fa
Alex-X <argumedodie@gmail.com>
2025-05-28 19:22:54 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1150,7 +1150,8 @@ let _user = global.db.data && global.db.data.users && global.db.data.users[m.sen
 
 const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
 const participants = (m.isGroup ? groupMetadata.participants : []) || []
-const detectwhat2 = m.sender.includes('@lid') ? '@lid' : '@s.whatsapp.net';
+let numBot = conn.user.lid.replace(/:.*/, '')
+const detectwhat2 = m.sender.includes('@lid') ? `${numBot}@lid` : conn.user.jid;
 const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {}
 const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == detectwhat2) : {}) || {}
 const isRAdmin = user?.admin == 'superadmin' || false
