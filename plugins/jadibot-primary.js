36c95446578b97a431667e562d65133e88da46a6
Alex-X <argumedodie@gmail.com>
2025-05-28 20:29:10 -0500

    Actualizar jadibot-primary.js

--- plugins/jadibot-primary.js
+++ plugins/jadibot-primary.js
@@ -3,6 +3,10 @@ import ws from 'ws';
 let handler = async (m, { conn, usedPrefix, args }) => {
 if (!args[0] && !m.quoted) return m.reply(`⚠️ Menciona el número de un bot o responde al mensaje de un bot.\n> Ejemplo: *${usedPrefix}setprimary @0*`);
 
+let numBot = conn.user.lid.replace(/:.*/, '')
+let numBot2 = global.conn.user.lid.replace(/:.*/, '')
+const detectwhat = m.sender.includes('@lid') ? `${numBot2}@lid` : global.conn.user.jid;
+const detectwhat2 = m.sender.includes('@lid') ? `${numBot}@lid` : conn.user.jid;
 const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
 
 let botJid;
@@ -14,23 +18,23 @@ botJid = m.mentionedJid[0];
 botJid = m.quoted.sender;
 } else {
 botJid = args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net';
-} if (botJid === conn.user.jid || botJid === global.conn.user.jid) {
+} if (botJid === detectwhat2 || botJid === detectwhat) {
 selectedBot = conn;
 } else {
-selectedBot = users.find(conn => conn.user.jid === botJid);
+selectedBot = users.find(conn => detectwhat2 === botJid);
 }
 
 if (!selectedBot) {
-return conn.reply(m.chat, `⚠️ @${botJid.split`@`[0]} no es un bot de la misma sessión, verifica los bots conectados, usando */bots*.`, m, { mentions: [botJid] });
+return conn.reply(m.chat, `⚠️ Ese bot no es un bot de la misma sessión, verifica los bots conectados, usando */bots*.`, m);
 }
 
 let chat = global.db.data.chats[m.chat];
 if (chat.primaryBot === botJid) {
-return conn.reply(m.chat, `⚠️ @${botJid.split`@`[0]} ya es el bot primario.`, m, { mentions: [botJid] });
+return conn.reply(m.chat, `⚠️ Listo, ese bot ya es el bot primario.`, m);
 }
 
 chat.primaryBot = botJid;
-conn.sendMessage(m.chat, { text: `✅ El bot @${botJid.split('@')[0]} ha sido establecido como primario en este grupo. Los demás bots no responderán aquí.`, mentions: [botJid]}, { quoted: m });
+conn.sendMessage(m.chat, { text: `✅ El bot ha sido establecido como primario en este grupo. Los demás bots no responderán aquí.` }, { quoted: m });
 };
 
 handler.help = ['setprimary <@tag>'];
