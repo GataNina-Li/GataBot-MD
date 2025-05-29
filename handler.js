30993fd1a35c672725b72804b211ed5f486745d2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-05-28 23:03:59 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1150,6 +1150,7 @@ let _user = global.db.data && global.db.data.users && global.db.data.users[m.sen
 
 const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
 const participants = (m.isGroup ? groupMetadata.participants : []) || []
+console.log(conn.user.lid)
 let numBot = conn.user.lid.replace(/:.*/, '')
 const detectwhat2 = m.sender.includes('@lid') ? `${numBot}@lid` : conn.user.jid;
 const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {}
