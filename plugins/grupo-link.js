979fbb0d28f0ca3091e0bb46a24c8217ead79c67
Alex-X <argumedodie@gmail.com>
2025-05-28 20:13:19 -0500

    Actualizar grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,22 +1,13 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-let username = conn.getName(who)
-let group = m.chat
-
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
-let fsizedoc = '1'.repeat(10)
-let adReply = { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: { forwardingScore: fsizedoc, externalAdReply: { showAdAttribution: true, title: wm, body: '👋 ' + username, mediaUrl: ig, description: 'Hola', previewType: 'PHOTO', thumbnail: await(await fetch(gataMenu)).buffer(), sourceUrl: redesMenu }}}
-
-try{
-///await conn.sendFile(m.chat, pp, 'error.jpg', '*https://chat.whatsapp.com/' + await conn.groupInviteCode(group) + '*', m)
-await conn.sendButton(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), wm, pp, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], null, null, fkontak, adReply)
+let handler = async (m, { conn }) => {
+try {
+let res = await conn.groupInviteCode(m.chat)
+let link = 'https://chat.whatsapp.com/' + res
+await conn.reply(m.chat, `${link}`, m)
 } catch (e) {
 await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)
-}} 
+}}
 handler.help = ['linkgroup']
 handler.tags = ['group']
 handler.command = /^enlace|link(gro?up)?$/i
