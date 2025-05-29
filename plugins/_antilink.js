ea37ba0b9dd5e351e053a0a4b429b9c51fa7be3c
Alex-X <argumedodie@gmail.com>
2025-05-28 19:44:52 -0500

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -9,15 +9,14 @@ if (isAdmin || isOwner || m.fromMe || isROwner || !isBotAdmin) return
 let chat = global.db.data.chats[m.chat]
 let delet = m.key.participant
 let bang = m.key.id
-const user = `@${m.sender.split`@`[0]}`
-const groupAdmins = participants.filter(p => p.admin)
-const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
-let bot = global.db.data.settings[this.user.jid] || {}
+const detectwhat = m.sender.includes('@lid') ? `${m.sender.split(':')[0]`}@lid' : m.sender;
+const user = await conn.getName(detectwhat);
 const isGroupLink = linkRegex1.exec(m.text) || linkRegex2.exec(m.text);
 if (chat.antiLink && isGroupLink) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-//if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m }) 
-if (!isBotAdmin) return conn.sendMessage(m.chat, { text: mid.mAdminTrue, mentions: [...groupAdmins.map(v => v.id)] }, { quoted: m });
+
+if (!isBotAdmin) return conn.sendMessage(m.chat, { text: mid.mAdminTrue }, { quoted: m });
+
 if (isBotAdmin) {
 if (m.text.includes(linkThisGroup)) return
 await conn.sendMessage(m.chat, { text: `${mid.mAdvertencia + mid.mWhatsApp} *${user}*`, mentions: [m.sender] }, { quoted: m })    
