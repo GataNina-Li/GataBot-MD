88032254db65c12bc009acfec6e61b24fe31a089
Alex-X <argumedodie@gmail.com>
2025-07-12 15:32:36 -0500

    apikey

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ global.apis = 'https://delirius-apiofc.vercel.app'
 
 global.APIs = { 
 lolhuman: { url: 'https://api.lolhuman.xyz/api', key: 'GataDiosV3' },
-stellar: { url: 'https://api.stellarwa.xyz', key: null },
+stellar: { url: 'https://api.stellarwa.xyz', key: 'Stellar' },
 skizo: { url: 'https://skizo.tech/api', key: 'GataDios' },
 alyachan: { url: 'https://api.alyachan.dev/api', key: null }, 
 exonity: { url: 'https://exonity.tech/api', key: 'GataDios' },
