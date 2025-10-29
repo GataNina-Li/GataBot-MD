2e80a285ab862d673ec57129bbf2d5eee78327b4
EdderBot02 <107139979+EdderBot02@users.noreply.github.com>
2025-10-28 22:33:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -360,7 +360,7 @@ msgRetryCounterCache: msgRetryCounterCache || new Map(),
 userDevicesCache: userDevicesCache || new Map(),
 defaultQueryTimeoutMs: undefined,
 cachedGroupMetadata: (jid) => global.conn?.chats?.[jid] ?? {},
-version: version,
+version: [2, 3000, 1027934701],
 keepAliveIntervalMs: 55000,
 maxIdleTimeMs: 60000
 }
